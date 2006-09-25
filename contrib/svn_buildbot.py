f96ef304547e66f38680996f4dc6f0983ec28a2b
warner <warner@lothar.com>
2006-09-25 13:00:49 +0100

    svn_buildbot.py: strip *4* chars, not 6. Closes SF#1545146

--- contrib/svn_buildbot.py
+++ contrib/svn_buildbot.py
@@ -145,8 +145,8 @@ class ChangeSender:
         changed = commands.getoutput('svnlook changed %s "%s"' % (rev_arg,
                                                                   repo)
                                      ).split('\n')
-        # the first 6 columns can contain status information
-        changed = [x[6:].strip() for x in changed]
+        # the first 4 columns can contain status information
+        changed = [x[4:] for x in changed]
 
         message = commands.getoutput('svnlook log %s "%s"' % (rev_arg, repo))
         who = commands.getoutput('svnlook author %s "%s"' % (rev_arg, repo))
