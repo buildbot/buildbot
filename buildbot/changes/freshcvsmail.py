bd5c638947710420fb413cdd69d85be846ef1755
warner <warner@lothar.com>
2006-12-12 09:43:28 +0100

    mark buildbot.changes.freshcvsmail as deprecated

--- buildbot/changes/freshcvsmail.py
+++ buildbot/changes/freshcvsmail.py
@@ -2,4 +2,9 @@
 
 # leftover import for compatibility
 
+import warnings
+
+warnings.warn("buildbot.changes.freshcvsmail is deprecated as of 0.7.6 . Please import buildbot.changes.mail.FCMaildirSource instead. This compatibility import will be removed in 0.7.7",
+              DeprecationWarning)
+
 from buildbot.changes.mail import FCMaildirSource
