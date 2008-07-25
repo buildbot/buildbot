03006280b58f976df163a56a559850e37acba5d5
warner <warner@lothar.com>
2008-07-25 01:44:40 +0100

    setup.py: reduce setuptools-visible Twisted dependency to >=2.0.0

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ try:
 except ImportError:
     pass
 else:
-    setup_args['install_requires'] = ['twisted >= 2.4.0']
+    setup_args['install_requires'] = ['twisted >= 2.0.0']
     entry_points={
         'console_scripts': [
             'buildbot = buildbot.scripts.runner:run'
