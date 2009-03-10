5b1ddddd9cf519fdd9b5a590c45b3f5ae460e68c
Steve 'Ashcrow' Milner <smilner+buildbot@redhat.com>
2009-03-09 23:43:57 -0400

    added a test and sdist_test commands to setup.py

--- setup.py
+++ setup.py
@@ -14,11 +14,113 @@ import sys
 import os
 import re
 
-from distutils.core import setup
+from distutils.core import setup, Command
 from buildbot import version
 
 # Path: twisted!cvstoys!buildbot
 from distutils.command.install_data import install_data
+
+
+class _SetupBuildCommand(Command):
+    """
+    Master setup build command to subclass from.
+    """
+
+    user_options = []
+
+    def initialize_options(self):
+        """
+        Setup the current dir.
+        """
+        self._dir = os.getcwd()
+
+    def finalize_options(self):
+        """
+        Required.
+        """
+        pass
+
+
+class TestCommand(_SetupBuildCommand):
+    """
+    Executes tests from setup.
+    """
+
+    description = "Run unittests inline"
+
+    def run(self):
+        """
+        Public run method.
+        """
+        self._run(os.path.normpath(os.path.abspath(
+           os.path.join('buildbot', 'test'))))
+
+    def _run(self, test_loc):
+        """
+        Executes the test step.
+
+        @param test_loc: location of test module
+        @type test_loc: str
+        """
+        from twisted.scripts.trial import run
+
+        # Mimick the trial script by adding the path as the last arg
+        sys.argv.append(test_loc)
+
+        # No superuser should execute tests
+        if hasattr(os, "getuid") and os.getuid() == 0:
+            raise SystemExit('Do not test as a superuser! Exiting ...')
+
+        # Add the current dir to path and pull it all together
+        sys.path.insert(0, os.path.curdir)
+        sys.path[:] = map(os.path.abspath, sys.path)
+        # GO!
+        run()
+
+
+class SdistTestCommand(TestCommand):
+    """
+    Runs unittests from the sdist output.
+    """
+
+    description = "Run unittests from inside an sdist distribution"
+
+    def run(self):
+        """
+        Interesting magic to get a source dist and running trial on it.
+
+        NOTE: there is magic going on here! If you know a better way feel
+              free to update it.
+        """
+        # Clean out dist/
+        if os.path.exists('dist'):
+            for root, dirs, files in os.walk('dist', topdown=False):
+                for name in files:
+                    os.remove(os.path.join(root, name))
+                for name in dirs:
+                    os.rmdir(os.path.join(root, name))
+        # Import setup making it as if we ran setup.py with the sdist arg
+        sys.argv.append('sdist')
+        import setup
+        try:
+            # attempt to extract the sdist data
+            from gzip import GzipFile
+            from tarfile import TarFile
+            # We open up the gzip as well as using the first item as the sdist
+            gz = GzipFile(os.path.join('dist', os.listdir('dist')[0]))
+            tf = TarFile(fileobj=gz)
+            # Make the output dir and generate the extract path
+            os.mkdir(os.path.join('dist', 'sdist_test'))
+            ex_path = os.path.join('dist', 'sdist_test',
+                tf.getmembers()[0].name, 'buildbot', 'test')
+            # Extract the data and run tests
+            print "Extracting to %s" % ex_path
+            tf.extractall(os.path.join('dist', 'sdist_test'))
+            print "Executing tests ..."
+            self._run(os.path.normpath(os.path.abspath(ex_path)))
+        except IndexError, ie:
+            # We get called twice and the IndexError is OK
+            pass
 
 
 class install_data_twisted(install_data):
@@ -106,7 +208,9 @@ setup_args = {
                 ("buildbot/test/subdir", ["buildbot/test/subdir/emit.py"]),
                 ],
     'scripts': scripts,
-    'cmdclass': {'install_data': install_data_twisted},
+    'cmdclass': {'install_data': install_data_twisted,
+                 'test': TestCommand,
+                 'sdist_test': SdistTestCommand},
     }
 
 try:
