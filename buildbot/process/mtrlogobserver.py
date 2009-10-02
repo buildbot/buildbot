9ec7f18e0b673f63962f744a2739338ed67265d4
Kristian Nielsen <knmeister@gmail.com>
2009-10-02 19:55:15 +0200

    Update mysql-test-run parsing regexps for latest MySQL 5.1.38 changes. (the variants of a test may now contain spaces).

--- buildbot/process/mtrlogobserver.py
+++ buildbot/process/mtrlogobserver.py
@@ -68,8 +68,8 @@ class MtrLogObserver(LogLineObserver):
     the Waterfall page. It also passes the information to methods that can be
     overridden in a subclass to do further processing on the information."""
 
-    _line_re = re.compile(r"^([-._0-9a-zA-z]+)( '[a-z]+')?\s+(w[0-9]+\s+)?\[ (fail|pass) \]\s*(.*)$")
-    _line_re2 = re.compile(r"^[-._0-9a-zA-z]+( '[a-z]+')?\s+(w[0-9]+\s+)?\[ [-a-z]+ \]")
+    _line_re = re.compile(r"^([-._0-9a-zA-z]+)( '[-_ a-zA-Z]+')?\s+(w[0-9]+\s+)?\[ (fail|pass) \]\s*(.*)$")
+    _line_re2 = re.compile(r"^[-._0-9a-zA-z]+( '[-_ a-zA-Z]+')?\s+(w[0-9]+\s+)?\[ [-a-z]+ \]")
     _line_re3 = re.compile(r"^\*\*\*Warnings generated in error logs during shutdown after running tests: (.*)")
     _line_re4 = re.compile(r"^The servers were restarted [0-9]+ times$")
     _line_re5 = re.compile(r"^Only\s+[0-9]+\s+of\s+[0-9]+\s+completed.$")
