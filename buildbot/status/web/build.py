buildbot/buildbot

commit 32ebee2a2adf0a1824c21982f50ca18648073d64

Bugid: 365, add custom properties fields to force and resubmit forms.
    * buildbot/status/web/base.py: Part of this fix was already
      commited, but was funky to use in more than one place.  Refactored
      the change into a function.
    * buildbot/status/web/build.py: use the refactored function to add
      property fields to the resubmit form.

buildbot/status/web/build.py
@@ -6,7 +6,9 @@ from twisted.internet import defer, reactor
 import urllib, time
 from twisted.python import log
 from buildbot.status.web.base import HtmlResource, make_row, make_stop_form, \
-     css_classes, path_to_builder, path_to_slave, make_name_user_passwd_form
+     make_extra_property_row, css_classes, path_to_builder, path_to_slave, \
+     make_name_user_passwd_form
+
 
 from buildbot.status.web.tests import TestsResource
 from buildbot.status.web.step import StepsResource
@@ -30,7 +32,6 @@ class StatusResourceBuild(HtmlResource):
     def body(self, req):
         b = self.build_status
         status = self.getStatus(req)
-        projectURL = status.getProjectURL()
         projectName = status.getProjectName()
         data = ('<div class="title"><a href="%s">%s</a></div>\n'
                 % (self.path_to_root(req), projectName))
@@ -206,6 +207,9 @@ class StatusResourceBuild(HtmlResource):
             data += ('<form method="post" action="%s" class="command rebuild">\n'
                      % rebuildURL)
             data += make_name_user_passwd_form(self.isUsingUserPasswd(req))
+	    data += make_extra_property_row(1)
+	    data += make_extra_property_row(2)
+	    data += make_extra_property_row(3)
             data += make_row("Reason for re-running build:",
                              "<input type='text' name='comments' />")
             data += '<input type="submit" value="Rebuild" />\n'

buildbot/status/web/base.py
@@ -84,6 +84,22 @@ def make_stop_form(stopURL, useUserPasswd, on_all=False, label="Build"):
     data += '<input type="submit" value="Stop %s" /></form>\n' % label
     return data
 
+def make_extra_property_row(N):
+    """helper function to create the html for adding extra build
+    properties to a forced (or resubmitted) build. "N" is an integer
+    inserted into the form names so that more than one property can be
+    used in the form.
+    """
+    prop_html = '''
+    <div class="row">Property %(N)i
+      <span class="label">Name:</span>
+      <span class="field"><input type="text" name="property%(N)iname" /></span>
+      <span class="label">Value:</span>
+      <span class="field"><input type="text" name="property%(N)ivalue" /></span>
+    </div>
+    ''' % {"N": N}
+    return prop_html
+
 def make_force_build_form(forceURL, useUserPasswd, on_all=False):
     if on_all:
         data = """<form method="post" action="%s" class="command forcebuild">
@@ -101,15 +117,9 @@ def make_force_build_form(forceURL, useUserPasswd, on_all=False):
                  "<input type='text' name='branch' />")
       + make_row("Revision to build:",
                  "<input type='text' name='revision' />")
-      + make_row("Property 1, ",
-                 "Name: <input type='text' name='prop1name' /> " + \
-                 "Value: <input type='text' name='prop1value' />")
-      + make_row("Property 2, ",
-                 "Name: <input type='text' name='prop2name' /> " + \
-                 "Value: <input type='text' name='prop2value' />")
-      + make_row("Property 3, ",
-                 "Name: <input type='text' name='prop3name' /> " + \
-                 "Value: <input type='text' name='prop3value' />")
+      + make_extra_property_row(1)
+      + make_extra_property_row(2)
+      + make_extra_property_row(3)
       + '<input type="submit" value="Force Build" /></form>\n')
 
 def td(text="", parms={}, **props):
@@ -300,10 +310,10 @@ class HtmlResource(resource.Resource):
     def path_to_root(self, request):
         return path_to_root(request)
 
-    def footer(self, s, req):
+    def footer(self, status, req):
         # TODO: this stuff should be generated by a template of some sort
-        projectURL = s.getProjectURL()
-        projectName = s.getProjectName()
+        projectURL = status.getProjectURL()
+        projectName = status.getProjectName()
         data = '<hr /><div class="footer">\n'
 
         welcomeurl = self.path_to_root(req) + "index.html"

commit 1fc591ea18fe72d1cc8594182308d2aeb51634e0

Fix a regression introduced in 32ebee2a2adf0a1824c21982f50ca18648073d64 where projectURL is used but not defined.

buildbot/status/web/build.py
@@ -33,6 +33,7 @@ class StatusResourceBuild(HtmlResource):
         b = self.build_status
         status = self.getStatus(req)
         projectName = status.getProjectName()
+        projectURL = status.getProjectURL()
         data = ('<div class="title"><a href="%s">%s</a></div>\n'
                 % (self.path_to_root(req), projectName))
         builder_name = b.getBuilder().getName()