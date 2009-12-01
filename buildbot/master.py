buildbot/buildbot

commit 0c60cf7040b6595766a6509be4faa47ce77fbe5b

Add support for slavebuilddir in builder's dictionnary.
builddir defaults to builder's name (escaped)

This permits sharing a slave checkout across multiple builders

Includes unit test update and doc.

buildbot/master.py
@@ -6,6 +6,7 @@ try:
     import signal
 except ImportError:
     pass
+import string
 from cPickle import load
 import warnings
 
@@ -642,6 +643,8 @@ class BuildMaster(service.MultiService):
         slavenames = [s.slavename for s in slaves]
         buildernames = []
         dirnames = []
+        badchars_map = string.maketrans("\t !#$%&'()*+,./:;<=>?@[\\]^{|}~",
+                                        "______________________________")
         for b in builders:
             if type(b) is tuple:
                 raise ValueError("builder %s must be defined with a dict, "
@@ -657,6 +660,10 @@ class BuildMaster(service.MultiService):
                 raise ValueError("duplicate builder name %s"
                                  % b['name'])
             buildernames.append(b['name'])
+
+            # Fix the dictionnary with default values.
+            b.setdefault('builddir', b['name'].translate(badchars_map))
+            b.setdefault('slavebuilddir', b['builddir'])
             if b['builddir'] in dirnames:
                 raise ValueError("builder %s reuses builddir %s"
                                  % (b['name'], b['builddir']))
@@ -892,7 +899,7 @@ class BuildMaster(service.MultiService):
         # everything in newList is either unchanged, changed, or new
         for name, data in newList.items():
             old = self.botmaster.builders.get(name)
-            basedir = data['builddir'] # used on both master and slave
+            basedir = data['builddir']
             #name, slave, builddir, factory = data
             if not old: # new
                 # category added after 0.6.2

buildbot/buildslave.py
@@ -321,7 +321,7 @@ class AbstractBuildSlave(NewCredPerspective, service.MultiService):
 
     def sendBuilderList(self):
         our_builders = self.botmaster.getBuildersForSlave(self.slavename)
-        blist = [(b.name, b.builddir) for b in our_builders]
+        blist = [(b.name, b.slavebuilddir) for b in our_builders]
         d = self.slave.callRemote("setBuilderList", blist)
         return d
 

buildbot/process/builder.py
@@ -370,7 +370,7 @@ class Builder(pb.Referenceable):
         @type  setup: dict
         @param setup: builder setup data, as stored in
                       BuildmasterConfig['builders'].  Contains name,
-                      slavename(s), builddir, factory, locks.
+                      slavename(s), builddir, slavebuilddir, factory, locks.
         @type  builder_status: L{buildbot.status.builder.BuilderStatus}
         """
         self.name = setup['name']
@@ -380,6 +380,7 @@ class Builder(pb.Referenceable):
         if setup.has_key('slavenames'):
             self.slavenames.extend(setup['slavenames'])
         self.builddir = setup['builddir']
+        self.slavebuilddir = setup['slavebuilddir']
         self.buildFactory = setup['factory']
         self.nextSlave = setup.get('nextSlave')
         if self.nextSlave is not None and not callable(self.nextSlave):
@@ -432,6 +433,9 @@ class Builder(pb.Referenceable):
         if setup['builddir'] != self.builddir:
             diffs.append('builddir changed from %s to %s' \
                          % (self.builddir, setup['builddir']))
+        if setup['slavebuilddir'] != self.slavebuilddir:
+            diffs.append('slavebuilddir changed from %s to %s' \
+                         % (self.slavebuilddir, setup['slavebuilddir']))
         if setup['factory'] != self.buildFactory: # compare objects
             diffs.append('factory changed')
         oldlocks = [(lock.__class__, lock.name)
@@ -919,5 +923,3 @@ class BuildRequestControl:
 
     def cancel(self):
         self.original_builder.cancelBuildRequest(self.original_request)
-
-

buildbot/test/test_run.py
@@ -30,7 +30,8 @@ c['slaves'] = [BuildSlave('bot1', 'sekrit')]
 c['schedulers'] = []
 c['builders'] = []
 c['builders'].append({'name':'quick', 'slavename':'bot1',
-                      'builddir': 'quickdir', 'factory': f1})
+                      'builddir': 'quickdir', 'factory': f1,
+                      'slavebuilddir': 'slavequickdir'})
 c['slavePortnum'] = 0
 """
 
@@ -64,7 +65,7 @@ from buildbot.scheduler import Scheduler
 c['schedulers'] = [Scheduler('dummy', None, 0.1, ['dummy', 'dummy2'])]
 
 c['builders'].append({'name': 'dummy', 'slavename': 'bot1',
-                      'builddir': 'dummy', 'factory': f2})
+                      'factory': f2})
 c['builders'].append({'name': 'dummy2', 'slavename': 'bot1',
                       'builddir': 'dummy2', 'factory': f2})
 """
@@ -72,8 +73,8 @@ c['builders'].append({'name': 'dummy2', 'slavename': 'bot1',
 config_2 = config_base + """
 c['builders'] = [{'name': 'dummy', 'slavename': 'bot1',
                   'builddir': 'dummy1', 'factory': f2},
-                 {'name': 'testdummy', 'slavename': 'bot1',
-                  'builddir': 'dummy2', 'factory': f2, 'category': 'test'}]
+                 {'name': 'test dummy', 'slavename': 'bot1',
+                  'factory': f2, 'category': 'test'}]
 """
 
 config_3 = config_2 + """
@@ -86,7 +87,7 @@ c['builders'].append({'name': 'bdummy', 'slavename': 'bot1',
 
 config_4 = config_base + """
 c['builders'] = [{'name': 'dummy', 'slavename': 'bot1',
-                  'builddir': 'dummy', 'factory': f2}]
+                  'slavebuilddir': 'sdummy', 'factory': f2}]
 """
 
 config_4_newbasedir = config_4 + """
@@ -244,9 +245,9 @@ class BuilderNames(unittest.TestCase):
         m.readConfig = True
 
         self.failUnlessEqual(s.getBuilderNames(),
-                             ["dummy", "testdummy", "adummy", "bdummy"])
+                             ["dummy", "test dummy", "adummy", "bdummy"])
         self.failUnlessEqual(s.getBuilderNames(categories=['test']),
-                             ["testdummy", "bdummy"])
+                             ["test dummy", "bdummy"])
 
 class Disconnect(RunMixin, unittest.TestCase):
 
@@ -263,7 +264,7 @@ class Disconnect(RunMixin, unittest.TestCase):
         m.readConfig = True
         m.startService()
 
-        self.failUnlessEqual(s.getBuilderNames(), ["dummy", "testdummy"])
+        self.failUnlessEqual(s.getBuilderNames(), ["dummy", "test dummy"])
         self.s1 = s1 = s.getBuilder("dummy")
         self.failUnlessEqual(s1.getName(), "dummy")
         self.failUnlessEqual(s1.getState(), ("offline", []))
@@ -531,7 +532,7 @@ class Disconnect2(RunMixin, unittest.TestCase):
         m.readConfig = True
         m.startService()
 
-        self.failUnlessEqual(s.getBuilderNames(), ["dummy", "testdummy"])
+        self.failUnlessEqual(s.getBuilderNames(), ["dummy", "test dummy"])
         self.s1 = s1 = s.getBuilder("dummy")
         self.failUnlessEqual(s1.getName(), "dummy")
         self.failUnlessEqual(s1.getState(), ("offline", []))
@@ -605,9 +606,10 @@ class Basedir(RunMixin, unittest.TestCase):
         self.bot = bot = self.slaves['bot1'].bot
         self.builder = builder = bot.builders.get("dummy")
         self.failUnless(builder)
-        self.failUnlessEqual(builder.builddir, "dummy")
+        # slavebuilddir value.
+        self.failUnlessEqual(builder.builddir, "sdummy")
         self.failUnlessEqual(builder.basedir,
-                             os.path.join("slavebase-bot1", "dummy"))
+                             os.path.join("slavebase-bot1", "sdummy"))
 
         d = self.master.loadConfig(config_4_newbasedir)
         d.addCallback(self._testChangeBuilddir_2)

docs/buildbot.texinfo
@@ -1846,7 +1846,7 @@ f.addStep(Compile())
 c['builders'] = [
   @{'name': 'test', 'slavenames': ['slave1', 'slave2', 'slave3', 'slave4',
                                    'slave5', 'slave6'],
-    'builddir': 'test', 'factory': f',
+    'builddir': 'test', 'factory': f', 'slavebuilddir': 'test',
     'env': @{'PATH': '/opt/local/bin:/opt/app/bin:/usr/local/bin:/usr/bin'@}@}
 
 @end example
@@ -3325,12 +3325,6 @@ occurs on some of the buildslaves and not the others. Different
 platforms, operating systems, versions of major programs or libraries,
 all these things mean you should use separate Builders.
 
-@item builddir
-This specifies the name of a subdirectory (under the base directory)
-in which everything related to this builder will be placed. On the
-buildmaster, this holds build status information. On the buildslave,
-this is where checkouts, compiles, and tests are run.
-
 @item factory
 This is a @code{buildbot.process.factory.BuildFactory} instance which
 controls how the build is performed. Full details appear in their own
@@ -3343,6 +3337,18 @@ generally provided as arguments to the factory's constructor.
 Other optional keys may be set on each Builder:
 
 @table @code
+
+@item builddir
+Specifies the name of a subdirectory (under the base directory) in which
+everything related to this builder will be placed on the buildmaster.
+This holds build status information. If not set, defaults to @code{name}
+with some characters escaped.
+
+@item slavebuilddir
+Specifies the name of a subdirectory (under the base directory) in which
+everything related to this builder will be placed on the buildslave.
+This is where checkouts, compiles, and tests are run. If not set,
+defaults to @code{builddir}.
 
 @item category
 If provided, this is a string that identifies a category for the

commit 99d65c9ad09e68327a758efe8eaf099051f8cc37

Restore support for unicode builder name.
The regression was introduced in 0c60cf7040b6595766a6509be4faa47ce77fbe5b.

Add a regression unit test.

buildbot/master.py
@@ -647,8 +647,16 @@ class BuildMaster(service.MultiService):
         slavenames = [s.slavename for s in slaves]
         buildernames = []
         dirnames = []
+
+        # Remove potentially harmful characters from builder name if it is to be
+        # used as the build dir.
         badchars_map = string.maketrans("\t !#$%&'()*+,./:;<=>?@[\\]^{|}~",
                                         "______________________________")
+        def safeTranslate(str):
+            if isinstance(str, unicode):
+                str = str.encode('utf8')
+            return str.translate(badchars_map)
+
         for b in builders:
             if type(b) is tuple:
                 raise ValueError("builder %s must be defined with a dict, "
@@ -666,7 +674,7 @@ class BuildMaster(service.MultiService):
             buildernames.append(b['name'])
 
             # Fix the dictionnary with default values.
-            b.setdefault('builddir', b['name'].translate(badchars_map))
+            b.setdefault('builddir', safeTranslate(b['name']))
             b.setdefault('slavebuilddir', b['builddir'])
             if b['builddir'] in dirnames:
                 raise ValueError("builder %s reuses builddir %s"

buildbot/test/test_vc.py
@@ -1,4 +1,5 @@
 # -*- test-case-name: buildbot.test.test_vc -*-
+# -*- coding: utf-8 -*-
 
 import sys, os, time, re
 from email.Utils import mktime_tz, parsedate_tz
@@ -64,7 +65,7 @@ f1 = factory.BuildFactory([
 c = {}
 c['slaves'] = [BuildSlave('bot1', 'sekrit')]
 c['schedulers'] = []
-c['builders'] = [{'name': 'vc', 'slavename': 'bot1',
+c['builders'] = [{'name': 'vç', 'slavename': 'bot1',
                   'builddir': 'vc-dir', 'factory': f1}]
 c['slavePortnum'] = 0
 # do not compress logs in tests
@@ -429,7 +430,7 @@ class VCBase(SignalMixin):
                                self.slavebase, keepalive=0, usePTY=False)
         self.slave = slave
         slave.startService()
-        d = self.master.botmaster.waitUntilBuilderAttached("vc")
+        d = self.master.botmaster.waitUntilBuilderAttached("vç")
         return d
 
     def loadConfig(self, config):
@@ -438,7 +439,7 @@ class VCBase(SignalMixin):
         # to stop and restart the slave.
         d = defer.succeed(None)
         if self.slave:
-            d = self.master.botmaster.waitUntilBuilderDetached("vc")
+            d = self.master.botmaster.waitUntilBuilderDetached("vç")
             self.slave.stopService()
         d.addCallback(lambda res: self.master.loadConfig(config))
         d.addCallback(lambda res: self.connectSlave())
@@ -459,7 +460,7 @@ class VCBase(SignalMixin):
         #print "doBuild(ss: b=%s rev=%s)" % (ss.branch, ss.revision)
         req = base.BuildRequest("test_vc forced build", ss, 'test_builder')
         d = req.waitUntilFinished()
-        c.getBuilder("vc").requestBuild(req)
+        c.getBuilder("vç").requestBuild(req)
         d.addCallback(self._doBuild_1, shouldSucceed)
         return d
     def _doBuild_1(self, bs, shouldSucceed):
@@ -1076,7 +1077,7 @@ class VCBase(SignalMixin):
         self.tearDownSignalHandler()
         d = defer.succeed(None)
         if self.slave:
-            d2 = self.master.botmaster.waitUntilBuilderDetached("vc")
+            d2 = self.master.botmaster.waitUntilBuilderDetached("vç")
             d.addCallback(lambda res: self.slave.stopService())
             d.addCallback(lambda res: d2)
         if self.master: