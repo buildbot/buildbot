bf2dce71770139e49d4c4e408ac1c54818af3aaa
Jonathan S. Romero <jonnyro@disti-vm-jromero-ubuntu-workstation.(none)>
2010-03-22 14:37:18 -0400

    Pass always_purge arg along to buildslave in SVN step.

--- buildbot/steps/source.py
+++ buildbot/steps/source.py
@@ -566,6 +566,8 @@ class SVN(Source):
         self.args['revision'] = revision
         self.args['patch'] = patch
 
+	self.args['always_purge'] = self.always_purge
+
         #Set up depth if specified
         if self.depth is not None:
             if self.slaveVersionIsOlderThan("svn","2.9"):
