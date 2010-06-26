8c94ca7b1dcef07b3c98ff901173359bca6744c0
Dan Savilonis <djs@n-cube.org>
2010-06-25 22:28:16 -0400

    Install buildslave.bat wrapper for win32

--- slave/setup.py
+++ slave/setup.py
@@ -18,8 +18,8 @@ from buildslave import version
 
 scripts = ["bin/buildslave"]
 # TODO: windows stuff??
-#if sys.platform == "win32":
-#    scripts.append("contrib/windows/buildslave.bat")
+if sys.platform == "win32":
+    scripts.append("../contrib/windows/buildslave.bat")
 #    scripts.append("contrib/windows/buildslave_service.py")
 
 setup_args = {
