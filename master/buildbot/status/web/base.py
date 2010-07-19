118e4f5b997c13c6b1e2855edd8bf1b91ec4e6e5
Dustin J. Mitchell <dustin@zmanda.com>
2010-07-18 19:06:45 -0500

    Fix authfail URI, removing spurious / (fixes #920)

--- master/buildbot/status/web/base.py
+++ master/buildbot/status/web/base.py
@@ -102,7 +102,7 @@ def path_to_root(request):
     return root
 
 def path_to_authfail(request):
-    return path_to_root(request) + "/authfail"
+    return path_to_root(request) + "authfail"
 
 def path_to_builder(request, builderstatus):
     return (path_to_root(request) +
