ba1e68c3b980bf96d42b8417480da0fe60beacd9
Andrew Melo <andrew.melo@gmail.com>
2010-07-23 17:03:28 -0500

    need to test before I commit

--- master/buildbot/status/web/baseweb.py
+++ master/buildbot/status/web/baseweb.py
@@ -319,7 +319,7 @@ class WebStatus(service.MultiService):
         # do we want to allow change_hook
         self.change_hook = enable_change_hook
         self.change_hook_dialects = change_hook_dialects
-        if change_hook:
+        if enable_change_hook:
             self.putChild("change_hook", ChangeHookResource(dialects = self.change_hook_dialects))
 
     def setupUsualPages(self, numbuilds, num_events, num_events_max):
