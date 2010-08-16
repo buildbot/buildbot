eedde195779b9249c6c882d1a6b0b0b8730e8a08
Dustin J. Mitchell <dustin@zmanda.com>
2010-08-16 15:29:08 -0500

    move getAuthz and getBuildmaster to ContextMixin
    ContextMixin depends on getAuthz, after all.  Fixes #963

--- master/buildbot/status/web/base.py
+++ master/buildbot/status/web/base.py
@@ -184,6 +184,13 @@ class ContextMixin(object):
     def getTitle(self, request):
         return self.title
 
+    def getAuthz(self, request):
+        return request.site.buildbot_service.authz
+
+    def getBuildmaster(self, request):
+        return request.site.buildbot_service.master
+
+
 class HtmlResource(resource.Resource, ContextMixin):
     # this is a cheap sort of template thingy
     contentType = "text/html; charset=utf-8"
@@ -239,12 +246,6 @@ class HtmlResource(resource.Resource, ContextMixin):
             request.setHeader("content-length", len(data))
             return ''
         return data
-
-    def getAuthz(self, request):
-        return request.site.buildbot_service.authz
-
-    def getBuildmaster(self, request):
-        return request.site.buildbot_service.master
 
 
 class StaticHTML(HtmlResource):
