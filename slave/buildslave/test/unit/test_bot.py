3bd35929a56b1a47aa6c825dea96f5299f57d2bc
Dustin J. Mitchell <dustin@zmanda.com>
2010-08-15 19:49:54 -0500

    more tests for setBuilderList

--- slave/buildslave/test/unit/test_bot.py
+++ slave/buildslave/test/unit/test_bot.py
@@ -71,3 +71,69 @@ class TestBot(unittest.TestCase):
         d.addCallback(check)
         return d
 
+    def test_setBuilderList_single(self):
+        d = self.bot.callRemote("setBuilderList", [ ('mybld', 'myblddir') ])
+        def check(builders):
+            self.assertEqual(builders.keys(), ['mybld'])
+            self.assertTrue(os.path.exists(os.path.join(self.basedir, 'myblddir')))
+            # note that we test the SlaveBuilder instance below
+        d.addCallback(check)
+        return d
+
+    def test_setBuilderList_updates(self):
+        d = defer.succeed(None)
+
+        slavebuilders = {}
+
+        def add_my(_):
+            d = self.bot.callRemote("setBuilderList", [
+                        ('mybld', 'myblddir') ])
+            def check(builders):
+                self.assertEqual(builders.keys(), ['mybld'])
+                self.assertTrue(os.path.exists(os.path.join(self.basedir, 'myblddir')))
+                slavebuilders['my'] = builders['mybld']
+            d.addCallback(check)
+            return d
+        d.addCallback(add_my)
+
+        def add_your(_):
+            d = self.bot.callRemote("setBuilderList", [
+                        ('mybld', 'myblddir'), ('yourbld', 'yourblddir') ])
+            def check(builders):
+                self.assertEqual(sorted(builders.keys()), sorted(['mybld', 'yourbld']))
+                self.assertTrue(os.path.exists(os.path.join(self.basedir, 'myblddir')))
+                self.assertTrue(os.path.exists(os.path.join(self.basedir, 'yourblddir')))
+                # 'my' should still be the same slavebuilder object
+                self.assertEqual(id(slavebuilders['my']), id(builders['mybld']))
+                slavebuilders['your'] = builders['yourbld']
+            d.addCallback(check)
+            return d
+        d.addCallback(add_your)
+
+        def remove_my(_):
+            d = self.bot.callRemote("setBuilderList", [
+                        ('yourbld', 'yourblddir') ])
+            def check(builders):
+                self.assertEqual(sorted(builders.keys()), sorted(['yourbld']))
+                # note that build dirs are not deleted..
+                self.assertTrue(os.path.exists(os.path.join(self.basedir, 'myblddir')))
+                self.assertTrue(os.path.exists(os.path.join(self.basedir, 'yourblddir')))
+                # 'your' should still be the same slavebuilder object
+                self.assertEqual(id(slavebuilders['your']), id(builders['yourbld']))
+            d.addCallback(check)
+            return d
+        d.addCallback(remove_my)
+
+        def add_and_remove(_):
+            d = self.bot.callRemote("setBuilderList", [
+                        ('theirbld', 'theirblddir') ])
+            def check(builders):
+                self.assertEqual(sorted(builders.keys()), sorted(['theirbld']))
+                self.assertTrue(os.path.exists(os.path.join(self.basedir, 'myblddir')))
+                self.assertTrue(os.path.exists(os.path.join(self.basedir, 'yourblddir')))
+                self.assertTrue(os.path.exists(os.path.join(self.basedir, 'theirblddir')))
+            d.addCallback(check)
+            return d
+        d.addCallback(add_and_remove)
+
+        return d
