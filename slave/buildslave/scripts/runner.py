f6b0d6355b548c8a699ce2d37982c0fc7b13dfe0
Amber Yust <ayust@yelp.com>
2010-10-25 12:11:00 -0700

    Change log rotation to be bounded.

--- slave/buildslave/scripts/runner.py
+++ slave/buildslave/scripts/runner.py
@@ -317,10 +317,10 @@ class SlaveOptions(MakerBase):
          "controls permissions of generated files. Use --umask=022 to be world-readable"],
         ["maxdelay", None, 300,
          "Maximum time between connection attempts"],
-        ["log-size", "s", "1000000",
+        ["log-size", "s", "10000000",
          "size at which to rotate twisted log files"],
-        ["log-count", "l", "None",
-         "limit the number of kept old twisted log files"],
+        ["log-count", "l", "10",
+         "limit the number of kept old twisted log files (None for unlimited)"],
         ]
     
     longdesc = """
