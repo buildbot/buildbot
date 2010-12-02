buildbot/buildbot

commit 226e56d21b42f677cc54c70db751d1b4dd2b6167

remove stripping of leading digits from category in console
Fixes #1059

master/buildbot/status/web/console.py
@@ -349,11 +349,6 @@ class ConsoleStatusResource(HtmlResource):
         
         for category in categories:            
             c = {}
-            # TODO(nsylvain): Another hack to display the category in a pretty
-            # way.  If the master owner wants to display the categories in a
-            # given order, he/she can prepend a number to it. This number won't
-            # be shown.
-            c["name"] = category.lstrip('0123456789')
 
             # To be able to align the table correctly, we need to know
             # what percentage of space this category will be taking. This is

commit 0f1abadd808127fb5905fd578d2f43d84fb060c7

fix console view breakage in 226e56d2

master/buildbot/status/web/console.py
@@ -350,6 +350,8 @@ class ConsoleStatusResource(HtmlResource):
         for category in categories:            
             c = {}
 
+            c["name"] = category
+
             # To be able to align the table correctly, we need to know
             # what percentage of space this category will be taking. This is
             # (#Builders in Category) / (#Builders Total) * 100.