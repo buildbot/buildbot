6ef7160f4ac06fa7756b0b50b2afb3c5f3935ebd
Dustin J. Mitchell <dustin@mozilla.com>
2011-01-29 16:24:31 -0600

    Deprecate GitPoller workdir default; fixes #1781

--- master/buildbot/changes/gitpoller.py
+++ master/buildbot/changes/gitpoller.py
@@ -57,7 +57,7 @@ class GitPoller(base.PollingChangeSource):
         
         if self.workdir == None:
             self.workdir = tempfile.gettempdir() + '/gitpoller_work'
-            log.msg("WARNING: gitpoller using temporary workdir " +
+            log.msg("WARNING: gitpoller using deprecated temporary workdir " +
                     "'%s'; consider setting workdir=" % self.workdir)
 
     def startService(self):
