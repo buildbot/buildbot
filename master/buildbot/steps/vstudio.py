0b74a93754cdd22ddb05033461cd51fca8d249f2
Tom Prince <tom.prince@ualberta.net>
2011-03-26 03:42:38 -0400

    VC6: Add option for building a specific project in a workspace file.
    Signed-off-by: Tom Prince <tom.prince@ualberta.net>

--- master/buildbot/steps/vstudio.py
+++ master/buildbot/steps/vstudio.py
@@ -200,10 +200,13 @@ class VC6(VisualStudio):
 
     default_installdir = 'C:\\Program Files\\Microsoft Visual Studio'
 
-    def __init__(self, **kwargs):  
-
+    def __init__(self,
+                project = "ALL",
+                 **kwargs):
+        self.project = project
         # always upcall !
         VisualStudio.__init__(self, **kwargs)
+        self.addFactoryArguments(project = project)
 
     def setupEnvironment(self, cmd):
         VisualStudio.setupEnvironment(self, cmd)
@@ -229,7 +232,7 @@ class VC6(VisualStudio):
         command = ["msdev"]
         command.append(self.projectfile)
         command.append("/MAKE")
-        command.append("ALL - " + self.config)
+        command.append(self.project + " - " + self.config)
         if self.mode == "rebuild":
             command.append("/REBUILD")
         else:
