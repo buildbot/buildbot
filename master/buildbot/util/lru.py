30f61e3c28b00e3da405d51c748ad0023ea53e25
Dustin J. Mitchell <dustin@v.igoro.us>
2011-05-22 16:43:24 -0500

    AsyncLRUCache changes
    * add set_max_size method
    * make miss_fn an instance attr
    * allow **miss_fn_kwargs for get() invocations, to pass "hints"
    * do not cache negative results
    * add 'put' method to update a value

--- master/buildbot/util/lru.py
+++ master/buildbot/util/lru.py
@@ -29,7 +29,6 @@ class AsyncLRUCache(object):
 
     Asynchronous locking is used to ensure that in the common case of multiple
     concurrent requests for the same key, only one fetch is performed.
-    (TODO)
 
     All values are also stored in a weak valued dictionary, even after they
     have expired from the cache.  This allows values that are used elsewhere in
@@ -37,6 +36,9 @@ class AsyncLRUCache(object):
     component.  Weak references cannot be used for some types, so these types
     are not compatible with this class.  Note that dictionaries can be weakly
     referenced if they are an instance of a subclass of C{dict}.
+
+    If the result of the C{miss_fn} is C{None}, then the value is not cached;
+    this is intended to avoid caching negative results.
 
     This is based on Raymond Hettinger's implementation in
     U{http://code.activestate.com/recipes/498245-lru-and-lfu-cache-decorators/}
@@ -47,14 +49,24 @@ class AsyncLRUCache(object):
     @ivar misses: cache misses leading to re-fetches, so far
     """
 
-    __slots__ = ('max_size max_queue '
+    __slots__ = ('max_size max_queue miss_fn '
                  'queue cache weakrefs refcount concurrent '
                  'hits refhits misses'.split())
     sentinel = object()
+    QUEUE_SIZE_FACTOR = 10
 
-    def __init__(self, max_size=50):
+    def __init__(self, miss_fn, max_size=50):
+        """
+        Constructor.
+
+        @param miss_fn: function to call, with key as parameter, for cache
+        misses.  This function I{must} return a deferred.
+
+        @param max_size: maximum number of objects in the cache
+        """
+        self.miss_fn = miss_fn
         self.max_size = max_size
-        self.max_queue = max_size * 10
+        self.max_queue = max_size * self.QUEUE_SIZE_FACTOR
         self.queue = deque()
         self.cache = {}
         self.weakrefs = WeakValueDictionary()
@@ -62,13 +74,20 @@ class AsyncLRUCache(object):
         self.hits = self.misses = self.refhits = 0
         self.refcount = defaultdict(default_factory = lambda : 0)
 
-    def get(self, key, miss_fn):
+    def get(self, key, **miss_fn_kwargs):
         """
-        Fetch a value from the cache by key, invoking C{miss_fn(key)} if the
-        key is not in the cache.  The C{miss_fn} should return a Deferred.
+        Fetch a value from the cache by key, invoking C{self.miss_fn(key)} if
+        the key is not in the cache.
+
+        Any additional keyword arguments are passed to the C{miss_fn} as
+        keyword arguments; these can supply additional information relating to
+        the key.  It is up to the caller to ensure that this information is
+        functionally identical for each key value: if the key is already in the
+        cache, the C{miss_fn} will not be invoked, even if the keyword
+        arguments differ.
 
         @param key: cache key
-        @param miss_fn: function to call for cache misses
+        @param **miss_fn_kwargs: keyword arguments to  the miss_fn
         @returns: value via Deferred
         """
         cache = self.cache
@@ -125,21 +144,14 @@ class AsyncLRUCache(object):
         assert key not in concurrent
         concurrent[key] = [ d ]
 
-        miss_d = miss_fn(key)
+        miss_d = self.miss_fn(key, **miss_fn_kwargs)
 
         def handle_result(result):
-            cache[key] = result
-            weakrefs[key] = result
+            if result is not None:
+                cache[key] = result
+                weakrefs[key] = result
 
-            # purge least recently used entry, using refcount
-            # to count repeatedly-used entries
-            if len(cache) > self.max_size:
-                refc = 1
-                while refc:
-                    k = queue.popleft()
-                    refc = refcount[k] = refcount[k] - 1
-                del cache[k]
-                del refcount[k]
+            self._purge()
 
             # reference the key once, possibly standing in for multiple
             # concurrent accesses
@@ -160,3 +172,47 @@ class AsyncLRUCache(object):
         miss_d.addErrback(log.err)
 
         return d
+
+    def _purge(self):
+        if len(self.cache) <= self.max_size:
+            return
+
+        cache = self.cache
+        refcount = self.refcount
+        queue = self.queue
+        max_size = self.max_size
+
+        # purge least recently used entries, using refcount
+        # to count repeatedly-used entries
+        while len(cache) > max_size:
+            refc = 1
+            while refc:
+                k = queue.popleft()
+                refc = refcount[k] = refcount[k] - 1
+            del cache[k]
+            del refcount[k]
+
+    def put(self, key, value):
+        """
+        Update the cache with the given key and value, if the key is already in
+        the cache.  This is intended to be used when updated values are
+        available for an existing cached object, and does not record a
+        reference to the key.
+
+        @param key: key to update
+        @param value: new value
+        @returns: nothing
+        """
+        if key in self.cache:
+            self.cache[key] = value
+            self.weakrefs[key] = value
+        elif key in self.weakrefs:
+            self.weakrefs[key] = value
+
+    def set_max_size(self, max_size):
+        if self.max_size == max_size:
+            return
+
+        self.max_size = max_size
+        self.max_queue = max_size * self.QUEUE_SIZE_FACTOR
+        self._purge()
