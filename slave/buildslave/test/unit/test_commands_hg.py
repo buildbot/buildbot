a945d91bcf41da6c7bf1aea52fa22f03ec5dba92
Pradeepkumar Gayam <in3xes@gmail.com>
2011-06-17 00:16:37 +0530

    More tests!!

--- slave/buildslave/test/unit/test_commands_hg.py
+++ slave/buildslave/test/unit/test_commands_hg.py
@@ -30,9 +30,6 @@ class TestMercurial(SourceCommandTestMixin, unittest.TestCase):
     def patch_sourcedirIsUpdateable(self, result):
         self.cmd.sourcedirIsUpdateable = lambda : result
 
-    def patch_sourcedataMatches(self, result):
-        self.cmd.sourcedataMatches = lambda : result
-
     def test_simple(self):
         self.patch_getCommand('hg', 'path/to/hg')
         self.clean_environ()
@@ -177,6 +174,142 @@ class TestMercurial(SourceCommandTestMixin, unittest.TestCase):
                 + 0,
             Expect(['path/to/hg', 'update', '--clean', '--repository',
                     'workdir', '--rev', 'stable'],
+                self.basedir,
+                sendRC=False, timeout=120, usePTY=False)
+                + 0,
+            Expect(['path/to/hg', 'identify', '--id', '--debug'],
+                self.basedir_workdir,
+                sendRC=False, timeout=120, usePTY=False, environ=exp_environ,
+                keepStdout=True)
+                + { 'stdout' : 'b7ddc0b638fa11cdac7c0345c40c6f76d8a7166d' }
+                + 0,
+
+        ]
+        self.patch_runprocess(*expects)
+        d = self.run_command()
+        d.addCallback(self.check_sourcedata, "http://bitbucket.org/nicolas17/pyboinc\n")
+        return d
+
+    def test_update_handle_emptyupdate(self):
+        self.patch_getCommand('hg', 'path/to/hg')
+        self.clean_environ()
+        self.make_command(hg.Mercurial, dict(
+            workdir='workdir',
+            mode='update',
+            revision=None,
+            repourl='http://bitbucket.org/nicolas17/pyboinc',
+        ),
+            initial_sourcedata = "http://bitbucket.org/nicolas17/pyboinc\n",
+        )
+        self.patch_sourcedirIsUpdateable(True)
+
+        exp_environ = dict(PWD='.', LC_MESSAGES='C')
+        expects = [
+            Expect(['path/to/hg', 'pull', '--verbose', 'http://bitbucket.org/nicolas17/pyboinc'],
+                self.basedir_workdir,
+                sendRC=False, timeout=120, keepStdout=True, usePTY=False)
+                + 1, 
+            Expect([ 'clobber', 'workdir' ],
+                self.basedir)
+                + 0,
+            Expect(['path/to/hg', 'clone', '--verbose', '--noupdate',
+                    'http://bitbucket.org/nicolas17/pyboinc', 'workdir'],
+                self.basedir,
+                sendRC=False, timeout=120, usePTY=False)
+                + 0,
+            Expect(['path/to/hg', 'identify', '--num', '--branch'],
+                self.basedir_workdir,
+                sendRC=False, timeout=120, usePTY=False, keepStdout=True,
+                keepStderr=True)
+                + { 'stdout' : '-1 default\n' }
+                + 0,
+            Expect(['path/to/hg', 'paths', 'default'],
+                self.basedir_workdir,
+                sendRC=False, timeout=120, usePTY=False, keepStdout=True,
+                keepStderr=True)
+                + { 'stdout' : 'http://bitbucket.org/nicolas17/pyboinc\n' }
+                + 0,
+            Expect(['path/to/hg', 'update', '--clean', '--repository',
+                    'workdir', '--rev', 'default'],
+                self.basedir,
+                sendRC=False, timeout=120, usePTY=False)
+                + 0,
+            Expect(['path/to/hg', 'identify', '--id', '--debug'],
+                self.basedir_workdir,
+                sendRC=False, timeout=120, usePTY=False, environ=exp_environ,
+                keepStdout=True)
+                + { 'stdout' : 'b7ddc0b638fa11cdac7c0345c40c6f76d8a7166d' }
+                + 0,
+        ]
+        self.patch_runprocess(*expects)
+        d = self.run_command()
+        d.addCallback(self.check_sourcedata, "http://bitbucket.org/nicolas17/pyboinc\n")
+        return d
+
+    def test_update_existing_change_branch_purge_fail(self):
+        self.patch_getCommand('hg', 'path/to/hg')
+        self.clean_environ()
+        self.make_command(hg.Mercurial, dict(
+            workdir='workdir',
+            mode='update',
+            revision="abcdef01",
+            branch='stable',
+            repourl='http://bitbucket.org/nicolas17/pyboinc',
+            clobberOnBranchChange=True,
+        ),
+            initial_sourcedata = "http://bitbucket.org/nicolas17/pyboinc\n",
+        )
+        self.patch_sourcedirIsUpdateable(True)
+
+        exp_environ = dict(PWD='.', LC_MESSAGES='C')
+        expects = [
+            Expect(['path/to/hg', 'pull', '--verbose', 'http://bitbucket.org/nicolas17/pyboinc'],
+                self.basedir_workdir,
+                sendRC=False, timeout=120, keepStdout=True, usePTY=False)
+                + 0,
+            Expect(['path/to/hg', 'identify', '--num', '--branch'],
+                self.basedir_workdir,
+                sendRC=False, timeout=120, usePTY=False, keepStdout=True,
+                keepStderr=True)
+                + { 'stdout' : '1 default\n' }
+                + 0,
+            Expect(['path/to/hg', 'paths', 'default'],
+                self.basedir_workdir,
+                sendRC=False, timeout=120, usePTY=False, keepStdout=True,
+                keepStderr=True)
+                + { 'stdout' : 'http://bitbucket.org/nicolas17/pyboinc\n' }
+                + 0,
+            Expect(['path/to/hg', 'purge', '--all'],
+                self.basedir_workdir,
+                keepStdout=True, keepStderr=True, usePTY=False)
+                + 1,
+            Expect([ 'clobber', 'workdir' ],
+                self.basedir)
+                + 0,
+            Expect(['path/to/hg', 'clone', '--verbose', '--noupdate',
+                    'http://bitbucket.org/nicolas17/pyboinc', 'workdir'],
+                self.basedir,
+                sendRC=False, timeout=120, usePTY=False)
+                + 0,
+            Expect(['path/to/hg', 'identify', '--num', '--branch'],
+                self.basedir_workdir,
+                sendRC=False, timeout=120, usePTY=False, keepStdout=True,
+                keepStderr=True)
+                + { 'stdout' : '-1 default\n' }
+                + 0,
+            Expect(['path/to/hg', 'paths', 'default'],
+                self.basedir_workdir,
+                sendRC=False, timeout=120, usePTY=False, keepStdout=True,
+                keepStderr=True)
+                + { 'stdout' : 'http://bitbucket.org/nicolas17/pyboinc\n' }
+                + 0,
+            Expect(['path/to/hg', 'update', '--clean', '--repository',
+                    'workdir', '--rev', 'abcdef01'],
+                self.basedir,
+                sendRC=False, timeout=120, usePTY=False)
+                + 0,
+            Expect(['path/to/hg', 'update', '--clean', '--repository',
+                    'workdir', '--rev', 'abcdef01'],
                 self.basedir,
                 sendRC=False, timeout=120, usePTY=False)
                 + 0,
