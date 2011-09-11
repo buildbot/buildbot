143f4170c7557fa1a920d90d311cb0be2687cfba
Dustin J. Mitchell <dustin@mozilla.com>
2011-09-11 12:05:36 -0500

    tests and docs for MaxQ

--- master/buildbot/steps/maxq.py
+++ master/buildbot/steps/maxq.py
@@ -14,7 +14,6 @@
 # Copyright Buildbot Team Members
 
 from buildbot.steps.shell import ShellCommand
-from buildbot.status.event import Event
 from buildbot.status.results import SUCCESS, FAILURE
 
 class MaxQ(ShellCommand):
@@ -28,33 +27,20 @@ class MaxQ(ShellCommand):
         ShellCommand.__init__(self, **kwargs)
         self.addFactoryArguments(testdir=testdir)
 
-    def startStatus(self):
-        evt = Event("yellow", ['running', 'maxq', 'tests'],
-                    files={'log': self.log})
-        self.setCurrentActivity(evt)
+    def commandComplete(self, cmd):
+        output = cmd.logs['stdio'].getText()
+        self.failures = output.count('\nTEST FAILURE:')
 
-
-    def finished(self, rc):
-        self.failures = 0
-        if rc:
+    def evaluateCommand(self, cmd):
+        # treat a nonzero exit status as a failure, if no other failures are
+        # detected
+        if not self.failures and cmd.rc != 0:
             self.failures = 1
-        output = self.log.getAll()
-        self.failures += output.count('\nTEST FAILURE:')
-
-        result = (SUCCESS, ['maxq'])
-
         if self.failures:
-            result = (FAILURE, [str(self.failures), 'maxq', 'failures'])
+            return FAILURE
+        return SUCCESS
 
-        return self.stepComplete(result)
-
-    def finishStatus(self, result):
+    def getText(self, cmd, results):
         if self.failures:
-            text = ["maxq", "failed"]
-        else:
-            text = ['maxq', 'tests']
-        self.updateCurrentActivity(text=text)
-        self.finishStatusSummary()
-        self.finishCurrentActivity()
-
-
+            return [ str(self.failures), 'maxq', 'failures' ]
+        return ['maxq', 'tests']
