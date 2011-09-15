f5bb76d4cfe4cb859e475407a7f65494aeca30ac
Pierre Tardy <pierre.tardy@intel.com>
2011-09-15 16:04:29 +0200

    web-status:authz: rework for cookie based session management
    Instead of requiring the user to type password in each and
    every form inside buildbot, we implement a simple session
    manager.
    
    Existing IAuth interface is augmented with getUserInfos()
    which is used to display info about the user
    /login and /logout ActionResources are added to manage login and logout
    
    template helpers like advertiseAction() now needs to pass request, so
    that authz can get the session cookie from headers
    
    Signed-off-by: Pierre Tardy <pierre.tardy@intel.com>

--- master/buildbot/status/web/authz.py
+++ master/buildbot/status/web/authz.py
@@ -15,7 +15,9 @@
 
 from twisted.internet import defer
 from buildbot.status.web.auth import IAuth
+from buildbot.status.web.session import SessionManager
 
+COOKIE_KEY="BuildBotSession"
 class Authz(object):
     """Decide who can do what."""
 
@@ -41,33 +43,48 @@ class Authz(object):
         self.auth = auth
         if auth:
             assert IAuth.providedBy(auth)
-
         self.config = dict( (a, default_action) for a in self.knownActions )
         for act in self.knownActions:
             if act in kwargs:
                 self.config[act] = kwargs[act]
                 del kwargs[act]
 
+        self.sessions = SessionManager()
         if kwargs:
             raise ValueError("unknown authorization action(s) " + ", ".join(kwargs.keys()))
 
-    def advertiseAction(self, action):
+    def advertiseAction(self, action, request):
         """Should the web interface even show the form for ACTION?"""
         if action not in self.knownActions:
             raise KeyError("unknown action")
         cfg = self.config.get(action, False)
         if cfg:
-            return True
-        return False
-
-    def needAuthForm(self, action):
-        """Does this action require an authentication form?"""
-        if action not in self.knownActions:
-            raise KeyError("unknown action")
-        cfg = self.config.get(action, False)
-        if cfg == 'auth' or callable(cfg):
-            return True
-        return False
+            if cfg == 'auth' or callable(cfg):
+                return self.authenticated(request)
+        return cfg
+    def session(self, request):
+        if COOKIE_KEY in request.received_cookies:
+            cookie = request.received_cookies[COOKIE_KEY]
+            return self.sessions.get(cookie)
+        return None
+            
+    def authenticated(self, request):
+        return self.session(request) != None
+    def currentUserHTML(self, request):
+        s = self.session(request)
+        if s:
+            return s.userInfosHTML()
+        return "not authenticated?!"
+    def currentUser(self, request):
+        s = self.session(request)
+        if s:
+            return s.user
+    def currentUserFull(self, request):
+        s = self.session(request)
+        if s:
+            return "%(fullName)s <%(email)s>"%(s.infos)
+        else:
+            return req.args.get("username", ["<unknown>"])[0]
 
     def actionAllowed(self, action, request, *args):
         """Is this ACTION allowed, given this http REQUEST?"""
@@ -78,20 +95,51 @@ class Authz(object):
             if cfg == 'auth' or callable(cfg):
                 if not self.auth:
                     return defer.succeed(False)
-                user = request.args.get("username", ["<unknown>"])[0]
-                passwd = request.args.get("passwd", ["<no-password>"])[0]
-                if user == "<unknown>" or passwd == "<no-password>":
+                def check_authenticate(res):
+                    if callable(cfg) and not cfg(self.currentUser(request), *args):
+                        return False
+                    return True
+                # retain old behaviour, if people have scripts
+                # without cookie support
+                passwd = request.args.get("passwd", [None])[0]
+                if self.authenticated(request):
+                    return defer.succeed(check_authenticate(None))
+                elif passwd:
+                    def check_login(cookie):
+                        ret = False
+                        if type(cookie) is str:
+                            ret = check_authenticate(None)
+                            self.sessions.remove(cookie)
+                        return ret
+                    d = self.login(request)
+                    d.addBoth(check_login)
+                    return d
+                else:
                     return defer.succeed(False)
-
-                d = defer.maybeDeferred(self.auth.authenticate, user, passwd)
-                def check_authenticate(res, cfg, user, *args):
-                    if res:
-                        if callable(cfg) and not cfg(user, *args):
-                            return False
-                        return True
-                    return False
-                d.addCallback(check_authenticate, cfg, user, *args)
-                return d
+        return defer.succeed(cfg)
+    def login(self, request):
+        """Login one user, and return session cookie"""
+        if self.authenticated(request):
+            return defer.succeed(False)
+            
+        user = request.args.get("username", ["<unknown>"])[0]
+        passwd = request.args.get("passwd", ["<no-password>"])[0]
+        if user == "<unknown>" or passwd == "<no-password>":
+            return defer.succeed(False)
+        if not self.auth:
+            return defer.succeed(False)
+        d = defer.maybeDeferred(self.auth.authenticate, user, passwd)
+        def check_authenticate(res):
+            if res:
+                cookie, s = self.sessions.new(user, self.auth.getUserInfo(user))
+                request.addCookie(COOKIE_KEY, cookie, s.getExpiration(),path="/")
+                request.received_cookies = {COOKIE_KEY:cookie}
+                return cookie
             else:
-                return defer.succeed(True) # anyone can do this..
-        return defer.succeed(False)
+                return False
+        d.addBoth(check_authenticate)
+        return d
+    def logout(self, request):
+        if COOKIE_KEY in request.received_cookies:
+            cookie = request.received_cookies[COOKIE_KEY]
+            self.sessions.remove(cookie)
