6205898b273bd470f3c1eb2316f2c0c26bebe608
Pierre Tardy <tardyp@gmail.com>
2011-11-07 13:25:20 +0100

    slave:repo: error recovery cleanup fixes
        We try our best to recover from previous build
        error. This leaded to several cleanups steps
        It is more simple to merge all those steps into
        one shell script
        The resulting code is more readable, and there is
        less polution (envvar dump and co) in the stdio log.
    
        part of this commit was wrongly squashed into:
        3b3ed8d3bf450b943b687281c5acc3c441761ddb
        and merged. this commit is the rest of the error
        recovery cleanup
    
    Signed-off-by: Pierre Tardy <tardyp@gmail.com>

--- slave/buildslave/commands/repo.py
+++ slave/buildslave/commands/repo.py
@@ -129,17 +129,12 @@ class Repo(SourceBaseCommand):
         # on fresh init, this file may confuse repo.
         if os.path.exists(os.path.join(self._fullSrcdir(), ".repo/project.list")):
             os.unlink(os.path.join(self._fullSrcdir(), ".repo/project.list"))
-        # remove previous overriden manifest
-        if os.path.exists(os.path.join(self._fullSrcdir(), ".repo/manifests")):
-            os.system("cd %s/.repo; ln -sf manifests/%s manifest.xml"%(self._fullSrcdir(),self.manifest_file))
         return self._repoCmd(['init', '-u', self.manifest_url, '-b', self.manifest_branch, '-m', self.manifest_file], self._didInit)
 
     def _didInit(self, res):
         return self.doVCUpdate()
 
     def doVCUpdate(self):
-        # remove previous overriden manifest even on partial sync
-        os.system("cd %s/.repo; ln -sf manifests/%s manifest.xml"%(self._fullSrcdir(),self.manifest_file))
         if self.repo_downloads:
             self.sendStatus({'header': "will download:\n" + "repo download "+ "\nrepo download ".join(self.repo_downloads) + "\n"})
         return self._doPreSyncCleanUp(None)
@@ -207,7 +202,7 @@ class Repo(SourceBaseCommand):
 
     def _doDownload(self, dummy):
         if hasattr(self.command, 'stderr') and self.command.stderr:
-            if "Automatic cherry-pick failed" in self.command.stderr:
+            if "Automatic cherry-pick failed" in self.command.stderr or "Automatic revert failed" in self.command.stderr:
                 command = ['forall','-c' ,'git' ,'diff', 'HEAD']
                 self.cherry_pick_failed = True
                 return self._repoCmd(command, self._DownloadAbandon, abandonOnFailure = False, keepStderr=True) # call again
