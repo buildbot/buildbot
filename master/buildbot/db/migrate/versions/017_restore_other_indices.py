ae887a9dacbf22711f848352f308860ed07b4017
Dustin J. Mitchell <dustin@mozilla.com>
2011-11-29 04:40:35 -0600

    more index fixes
    This restores changes_author changes_branch changes_category
    changes_revision changes_when_timestamp name_and_class that were
    accidentally deleted on SQLite, and also fixes the uniqueness constraint
    on users.identifier.

--- master/buildbot/db/migrate/versions/017_restore_other_indices.py
+++ master/buildbot/db/migrate/versions/017_restore_other_indices.py
@@ -19,9 +19,9 @@ def upgrade(migrate_engine):
     metadata = sa.MetaData()
     metadata.bind = migrate_engine
 
-    # the column drops in 011_add_buildrequest_claims.py unfortunately
-    # also drop a great deal of other stuff on sqlite.  In particular, all
-    # indexes and foreign keys.
+    # the column changes in 010_fix_column_lengths.py unfortunately also drop a
+    # great deal of other stuff on sqlite.  In particular, all indexes and
+    # foreign keys on the 'changes' and 'schedulers' tables.
     #
     # The foreign keys do not matter anyway - SQLite tracks them but ignores
     # them.  The indices, however, are important, so they are re-added here,
@@ -30,7 +30,22 @@ def upgrade(migrate_engine):
     if migrate_engine.dialect.name != 'sqlite':
         return
 
-    buildrequests = sa.Table('buildrequests', metadata, autoload=True)
-    sa.Index('buildrequests_buildsetid', buildrequests.c.buildsetid).create()
-    sa.Index('buildrequests_buildername', buildrequests.c.buildername).create()
-    sa.Index('buildrequests_complete', buildrequests.c.complete).create()
+    schedulers = sa.Table('schedulers', metadata, autoload=True)
+
+    sa.Index('name_and_class',
+            schedulers.c.name, schedulers.c.class_name).create()
+
+    changes = sa.Table('changes', metadata, autoload=True)
+
+    sa.Index('changes_branch', changes.c.branch).create()
+    sa.Index('changes_revision', changes.c.revision).create()
+    sa.Index('changes_author', changes.c.author).create()
+    sa.Index('changes_category', changes.c.category).create()
+    sa.Index('changes_when_timestamp', changes.c.when_timestamp).create()
+
+    # Due to a coding bug in version 012, the users_identifier index is not
+    # unique
+
+    users = sa.Table('users', metadata, autoload=True)
+    migrate_engine.execute("DROP INDEX users_identifier")
+    sa.Index('users_identifier', users.c.identifier, unique=True).create()
