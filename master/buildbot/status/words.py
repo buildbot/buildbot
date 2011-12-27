d8d23315b1f58d64b1f91b714f5fc49706ac573b
Dustin J. Mitchell <dustin@mozilla.com>
2011-12-27 09:58:35 -0600

    fix TypeError when using allowForce with IRC

--- master/buildbot/status/words.py
+++ master/buildbot/status/words.py
@@ -1058,7 +1058,7 @@ class IRC(base.StatusReceiverMultiService):
         base.StatusReceiverMultiService.setServiceParent(self, parent)
         self.f.status = parent
         if self.allowForce:
-            self.f.control = interfaces.IControl(parent)
+            self.f.control = interfaces.IControl(self.master)
 
     def stopService(self):
         # make sure the factory will stop reconnecting
