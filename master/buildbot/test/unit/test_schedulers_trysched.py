337218841b06bf50908b7c6c412b4784db2d2867
Doug Latornell <djl@douglatornell.ca>
2012-03-13 11:26:58 -0700

    PEP 8 cleanup; mostly whitespace and long lines.

--- master/buildbot/test/unit/test_schedulers_trysched.py
+++ master/buildbot/test/unit/test_schedulers_trysched.py
@@ -15,34 +15,38 @@
 
 from __future__ import with_statement
 
-import os
-import sys
 import mock
+import os
 import shutil
 import StringIO
+import sys
 
 import twisted
-from twisted.trial import unittest
 from twisted.internet import defer
+from twisted.trial import unittest
 
 from buildbot.schedulers import trysched
 from buildbot.test.util import dirs
 from buildbot.test.util import scheduler
 from buildbot.util import json
 
+
 class TryBase(unittest.TestCase):
 
     def test_filterBuilderList_ok(self):
-        sched = trysched.TryBase(name='tsched', builderNames=['a', 'b', 'c'], properties={})
-        self.assertEqual(sched.filterBuilderList(['b', 'c']), [ 'b', 'c' ])
+        sched = trysched.TryBase(
+            name='tsched', builderNames=['a', 'b', 'c'], properties={})
+        self.assertEqual(sched.filterBuilderList(['b', 'c']), ['b', 'c'])
 
     def test_filterBuilderList_bad(self):
-        sched = trysched.TryBase(name='tsched', builderNames=['a', 'b'], properties={})
-        self.assertEqual(sched.filterBuilderList(['b', 'c']), [ ])
+        sched = trysched.TryBase(
+            name='tsched', builderNames=['a', 'b'], properties={})
+        self.assertEqual(sched.filterBuilderList(['b', 'c']), [])
 
     def test_filterBuilderList_empty(self):
-        sched = trysched.TryBase(name='tsched', builderNames=['a', 'b'], properties={})
-        self.assertEqual(sched.filterBuilderList([]), [ 'a', 'b' ])
+        sched = trysched.TryBase(
+            name='tsched', builderNames=['a', 'b'], properties={})
+        self.assertEqual(sched.filterBuilderList([]), ['a', 'b'])
 
 
 class JobdirService(dirs.DirsMixin, unittest.TestCase):
@@ -101,7 +105,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
 
         # build scheduler
         kwargs = dict(name="tsched", builderNames=['a'], jobdir=self.jobdir)
-        sched = self.attachScheduler(trysched.Try_Jobdir(**kwargs), self.OBJECTID)
+        sched = self.attachScheduler(
+            trysched.Try_Jobdir(**kwargs), self.OBJECTID)
 
         # start it
         sched.startService()
@@ -129,26 +134,30 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
     # parseJob
 
     def test_parseJob_empty(self):
-        sched = trysched.Try_Jobdir(name='tsched', builderNames=['a'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['a'], jobdir='foo')
         self.assertRaises(trysched.BadJobfile,
-            lambda : sched.parseJob(StringIO.StringIO('')))
+            lambda: sched.parseJob(StringIO.StringIO('')))
 
     def test_parseJob_invalid(self):
-        sched = trysched.Try_Jobdir(name='tsched', builderNames=['a'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['a'], jobdir='foo')
         self.assertRaises(trysched.BadJobfile,
-            lambda : sched.parseJob(StringIO.StringIO('this is not a netstring')))
+            lambda: sched.parseJob(
+                StringIO.StringIO('this is not a netstring')))
 
     def test_parseJob_invalid_version(self):
-        sched = trysched.Try_Jobdir(name='tsched', builderNames=['a'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['a'], jobdir='foo')
         self.assertRaises(trysched.BadJobfile,
-            lambda : sched.parseJob(StringIO.StringIO('1:9,')))
+            lambda: sched.parseJob(StringIO.StringIO('1:9,')))
 
     def makeNetstring(self, *strings):
-        return ''.join([ '%d:%s,' % (len(s), s) for s in strings ])
+        return ''.join(['%d:%s,' % (len(s), s) for s in strings])
 
     def test_parseJob_v1(self):
-        sched = trysched.Try_Jobdir(name='tsched',
-                builderNames=['buildera','builderb'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['buildera', 'builderb'], jobdir='foo')
         jobstr = self.makeNetstring(
             '1', 'extid', 'trunk', '1234', '1', 'this is my diff, -- ++, etc.',
             'buildera', 'builderc'
@@ -169,8 +178,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         })
 
     def test_parseJob_v1_empty_branch_rev(self):
-        sched = trysched.Try_Jobdir(name='tsched',
-                builderNames=['buildera','builderb'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['buildera', 'builderb'], jobdir='foo')
         jobstr = self.makeNetstring(
                 # blank branch, rev are turned to None
             '1', 'extid', '', '', '1', 'this is my diff, -- ++, etc.',
@@ -199,8 +208,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         self.assertEqual(parsedjob['properties'], {})
 
     def test_parseJob_v2(self):
-        sched = trysched.Try_Jobdir(name='tsched',
-                builderNames=['buildera','builderb'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['buildera', 'builderb'], jobdir='foo')
         jobstr = self.makeNetstring(
             '2', 'extid', 'trunk', '1234', '1', 'this is my diff, -- ++, etc.',
             'repo', 'proj',
@@ -222,8 +231,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         })
 
     def test_parseJob_v2_empty_branch_rev(self):
-        sched = trysched.Try_Jobdir(name='tsched',
-                builderNames=['buildera','builderb'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['buildera', 'builderb'], jobdir='foo')
         jobstr = self.makeNetstring(
                 # blank branch, rev are turned to None
             '2', 'extid', '', '', '1', 'this is my diff, -- ++, etc.',
@@ -235,8 +244,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         self.assertEqual(parsedjob['baserev'], None)
 
     def test_parseJob_v2_no_builders(self):
-        sched = trysched.Try_Jobdir(name='tsched',
-                builderNames=['buildera','builderb'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['buildera', 'builderb'], jobdir='foo')
         jobstr = self.makeNetstring(
             '2', 'extid', 'trunk', '1234', '1', 'this is my diff, -- ++, etc.',
             'repo', 'proj',
@@ -255,8 +264,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         self.assertEqual(parsedjob['properties'], {})
 
     def test_parseJob_v3(self):
-        sched = trysched.Try_Jobdir(name='tsched',
-                builderNames=['buildera','builderb'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['buildera', 'builderb'], jobdir='foo')
         jobstr = self.makeNetstring(
             '3', 'extid', 'trunk', '1234', '1', 'this is my diff, -- ++, etc.',
             'repo', 'proj', 'who',
@@ -278,8 +287,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         })
 
     def test_parseJob_v3_empty_branch_rev(self):
-        sched = trysched.Try_Jobdir(name='tsched',
-                builderNames=['buildera','builderb'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['buildera', 'builderb'], jobdir='foo')
         jobstr = self.makeNetstring(
                 # blank branch, rev are turned to None
             '3', 'extid', '', '', '1', 'this is my diff, -- ++, etc.',
@@ -291,8 +300,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         self.assertEqual(parsedjob['baserev'], None)
 
     def test_parseJob_v3_no_builders(self):
-        sched = trysched.Try_Jobdir(name='tsched',
-                builderNames=['buildera','builderb'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['buildera', 'builderb'], jobdir='foo')
         jobstr = self.makeNetstring(
             '3', 'extid', 'trunk', '1234', '1', 'this is my diff, -- ++, etc.',
             'repo', 'proj', 'who'
@@ -311,11 +320,11 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         self.assertEqual(parsedjob['properties'], {})
 
     def test_parseJob_v4(self):
-        sched = trysched.Try_Jobdir(name='tsched',
-                builderNames=['buildera','builderb'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['buildera', 'builderb'], jobdir='foo')
         jobstr = self.makeNetstring(
             '4', 'extid', 'trunk', '1234', '1', 'this is my diff, -- ++, etc.',
-            'repo', 'proj', 'who', 'comment', 
+            'repo', 'proj', 'who', 'comment',
             'buildera', 'builderc'
         )
         parsedjob = sched.parseJob(StringIO.StringIO(jobstr))
@@ -334,8 +343,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         })
 
     def test_parseJob_v4_empty_branch_rev(self):
-        sched = trysched.Try_Jobdir(name='tsched',
-                builderNames=['buildera','builderb'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['buildera', 'builderb'], jobdir='foo')
         jobstr = self.makeNetstring(
                 # blank branch, rev are turned to None
             '4', 'extid', '', '', '1', 'this is my diff, -- ++, etc.',
@@ -347,8 +356,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         self.assertEqual(parsedjob['baserev'], None)
 
     def test_parseJob_v4_no_builders(self):
-        sched = trysched.Try_Jobdir(name='tsched',
-                builderNames=['buildera','builderb'], jobdir='foo')
+        sched = trysched.Try_Jobdir(
+            name='tsched', builderNames=['buildera', 'builderb'], jobdir='foo')
         jobstr = self.makeNetstring(
             '4', 'extid', 'trunk', '1234', '1', 'this is my diff, -- ++, etc.',
             'repo', 'proj', 'who', 'comment'
@@ -440,11 +449,11 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
 
     def call_handleJobFile(self, parseJob):
         sched = self.attachScheduler(
-            trysched.Try_Jobdir(name='tsched', builderNames=['buildera','builderb'],
-                                jobdir='foo'),
-            self.OBJECTID)
-
+            trysched.Try_Jobdir(
+                name='tsched', builderNames=['buildera', 'builderb'],
+                jobdir='foo'), self.OBJECTID)
         fakefile = mock.Mock()
+
         def parseJob_(f):
             assert f is fakefile
             return parseJob(f)
@@ -461,7 +470,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         return pj
 
     def test_handleJobFile(self):
-        d = self.call_handleJobFile(lambda f : self.makeSampleParsedJob())
+        d = self.call_handleJobFile(lambda f: self.makeSampleParsedJob())
+
         def check(_):
             self.db.buildsets.assertBuildset('?',
                     dict(reason="'try' job by user who",
@@ -484,18 +494,22 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
         def parseJob(f):
             raise trysched.BadJobfile
         d = self.call_handleJobFile(parseJob)
+
         def check(bsid):
             self.db.buildsets.assertBuildsets(0)
-            self.assertEqual(1, len(self.flushLoggedErrors(trysched.BadJobfile)))
+            self.assertEqual(
+                1, len(self.flushLoggedErrors(trysched.BadJobfile)))
         d.addCallback(check)
         return d
-    if twisted.version.major <= 9 and sys.version_info[:2] >= (2,7):
+    if twisted.version.major <= 9 and sys.version_info[:2] >= (2, 7):
         test_handleJobFile_exception.skip = (
-            "flushLoggedErrors does not work correctly on 9.0.0 and earlier with Python-2.7")
+            "flushLoggedErrors does not work correctly on 9.0.0 "
+            "and earlier with Python-2.7")
 
     def test_handleJobFile_bad_builders(self):
         d = self.call_handleJobFile(
-                lambda f : self.makeSampleParsedJob(builderNames=['xxx']))
+                lambda f: self.makeSampleParsedJob(builderNames=['xxx']))
+
         def check(_):
             self.db.buildsets.assertBuildsets(0)
         d.addCallback(check)
@@ -503,7 +517,8 @@ class Try_Jobdir(scheduler.SchedulerMixin, unittest.TestCase):
 
     def test_handleJobFile_subset_builders(self):
         d = self.call_handleJobFile(
-                lambda f : self.makeSampleParsedJob(builderNames=['buildera']))
+            lambda f: self.makeSampleParsedJob(builderNames=['buildera']))
+
         def check(_):
             self.db.buildsets.assertBuildset('?',
                     dict(reason="'try' job by user who",
@@ -561,14 +576,10 @@ class Try_Userpass_Perspective(scheduler.SchedulerMixin, unittest.TestCase):
     def makeScheduler(self, **kwargs):
         sched = self.attachScheduler(trysched.Try_Userpass(**kwargs),
                 self.OBJECTID)
-
         # Try will return a remote version of master.status, so give it
         # something to return
         sched.master.status = mock.Mock()
-
         return sched
-
-    # tests
 
     def call_perspective_try(self, *args, **kwargs):
         sched = self.makeScheduler(name='tsched', builderNames=['a', 'b'],
@@ -577,8 +588,10 @@ class Try_Userpass_Perspective(scheduler.SchedulerMixin, unittest.TestCase):
         return persp.perspective_try(*args, **kwargs)
 
     def test_perspective_try(self):
-        d = self.call_perspective_try('default', 'abcdef', (1, '-- ++'), 'repo',
-                'proj', ['a'], properties={'pr':'op'})
+        d = self.call_perspective_try(
+            'default', 'abcdef', (1, '-- ++'), 'repo', 'proj', ['a'],
+            properties={'pr': 'op'})
+
         def check(_):
             self.db.buildsets.assertBuildset('?',
                     dict(reason="'try' job",
@@ -588,12 +601,12 @@ class Try_Userpass_Perspective(scheduler.SchedulerMixin, unittest.TestCase):
                             ('pr', ('op', 'try build')),
                             ('scheduler', ('tsched', 'Scheduler')),
                         ],
-                        sourcestampsetid = 100,
+                        sourcestampsetid=100,
                         ),
                     {'':
                      dict(branch='default', repository='repo', codebase='',
                         project='proj', revision='abcdef',
-                        sourcestampsetid = 100,
+                        sourcestampsetid=100,
                         patch_body='-- ++', patch_level=1, patch_subdir='',
                         patch_author="", patch_comment="")
                     })
@@ -601,8 +614,10 @@ class Try_Userpass_Perspective(scheduler.SchedulerMixin, unittest.TestCase):
         return d
 
     def test_perspective_try_who(self):
-        d = self.call_perspective_try('default', 'abcdef', (1, '-- ++'), 'repo',
-                'proj', ['a'], who='who', comment='comment', properties={'pr':'op'})
+        d = self.call_perspective_try(
+            'default', 'abcdef', (1, '-- ++'), 'repo', 'proj', ['a'],
+            who='who', comment='comment', properties={'pr': 'op'})
+
         def check(_):
             self.db.buildsets.assertBuildset('?',
                     dict(reason="'try' job by user who (comment)",
@@ -612,12 +627,12 @@ class Try_Userpass_Perspective(scheduler.SchedulerMixin, unittest.TestCase):
                             ('pr', ('op', 'try build')),
                             ('scheduler', ('tsched', 'Scheduler')),
                         ],
-                        sourcestampsetid = 100,
+                        sourcestampsetid=100,
                         ),
                     {'':
                      dict(branch='default', repository='repo',  codebase='',
                         project='proj', revision='abcdef',
-                        sourcestampsetid = 100,
+                        sourcestampsetid=100,
                         patch_body='-- ++', patch_level=1, patch_subdir='',
                         patch_author='who', patch_comment="comment")
                     })
@@ -625,8 +640,10 @@ class Try_Userpass_Perspective(scheduler.SchedulerMixin, unittest.TestCase):
         return d
 
     def test_perspective_try_bad_builders(self):
-        d = self.call_perspective_try('default', 'abcdef', (1, '-- ++'), 'repo',
-                'proj', ['xxx'], properties={'pr':'op'})
+        d = self.call_perspective_try(
+            'default', 'abcdef', (1, '-- ++'), 'repo', 'proj', ['xxx'],
+            properties={'pr': 'op'})
+
         def check(_):
             self.db.buildsets.assertBuildsets(0)
         d.addCallback(check)
@@ -636,12 +653,14 @@ class Try_Userpass_Perspective(scheduler.SchedulerMixin, unittest.TestCase):
         sched = self.makeScheduler(name='tsched', builderNames=['a', 'b'],
                 port='xxx', userpass=[('a', 'b')])
         persp = trysched.Try_Userpass_Perspective(sched, 'a')
-        d = defer.maybeDeferred(lambda :
-                persp.perspective_getAvailableBuilderNames())
+        d = defer.maybeDeferred(
+            lambda: persp.perspective_getAvailableBuilderNames())
+
         def check(buildernames):
             self.assertEqual(buildernames, ['a', 'b'])
         d.addCallback(check)
         return d
+
 
 class Try_Userpass(scheduler.SchedulerMixin, unittest.TestCase):
 
@@ -658,30 +677,25 @@ class Try_Userpass(scheduler.SchedulerMixin, unittest.TestCase):
                 self.OBJECTID)
         return sched
 
-    # tests
-
     def test_service(self):
         sched = self.makeScheduler(name='tsched', builderNames=['a'],
                 port='tcp:9999', userpass=[('fred', 'derf')])
-
         # patch out the pbmanager's 'register' command both to be sure
         # the registration is correct and to get a copy of the factory
         registration = mock.Mock()
-        registration.unregister = lambda : defer.succeed(None)
+        registration.unregister = lambda: defer.succeed(None)
         sched.master.pbmanager = mock.Mock()
+
         def register(portstr, user, passwd, factory):
             self.assertEqual([portstr, user, passwd],
                              ['tcp:9999', 'fred', 'derf'])
             self.got_factory = factory
             return registration
         sched.master.pbmanager.register = register
-
         # start it
         sched.startService()
-
         # make a fake connection by invoking the factory, and check that we
         # get the correct perspective
         persp = self.got_factory(mock.Mock(), 'fred')
         self.failUnless(isinstance(persp, trysched.Try_Userpass_Perspective))
-
         return sched.stopService()
