7cc35b936db9a8d31d77b9e72e629e085b137d1e
John Carr <john.carr@unrouted.co.uk>
2012-06-06 00:36:38 +0100

    Add test coverage for project-branch-file splitter

--- master/buildbot/test/unit/test_changes_svnpoller.py
+++ master/buildbot/test/unit/test_changes_svnpoller.py
@@ -578,3 +578,15 @@ class TestSplitFile(unittest.TestCase):
                 svnpoller.split_file_branches('tags/testthis/subdir/file.c'),
                 None)
 
+    def test_split_file_project_branches(self):
+        self.assertEqual(
+                svnpoller.split_file_project_branches('buildbot/trunk/subdir/file.c'),
+                dict(project='buildbot', path='subdir/file.c'))
+        self.assertEqual(
+                svnpoller.split_file_project_branches('buildbot/branches/1.5.x/subdir/file.c'),
+                dict(project='buildbot', branch='branches/1.5.x', path='subdir/file.c'))
+        # tags are ignored:
+        self.assertEqual(
+                svnpoller.split_file_project_branches('buildbot/tags/testthis/subdir/file.c'),
+                None)
+
