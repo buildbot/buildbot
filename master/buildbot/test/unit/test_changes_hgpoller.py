401032ea7d400a887964fd247d10c44995506abe
Georges Racinet <gracinet@anybox.fr>
2012-06-11 10:21:40 +0200

    hgpoller: using FakeDB in tests & poll tests now is initial

--- master/buildbot/test/unit/test_changes_hgpoller.py
+++ master/buildbot/test/unit/test_changes_hgpoller.py
@@ -20,6 +20,7 @@ from twisted.internet import defer
 from exceptions import Exception
 from buildbot.changes import hgpoller
 from buildbot.test.util import changesource, gpo
+from buildbot.test.fake.fakedb import FakeDBConnector
 from buildbot.util import epoch2datetime
 
 # Test that environment variables get propagated to subprocesses (See #2116)
@@ -107,8 +108,8 @@ class HgOutputParsing(gpo.GetProcessOutputMixin, unittest.TestCase):
     # _get_changes is tested in TestHgPoller, below
 
 class TestHgPoller(gpo.GetProcessOutputMixin,
-                    changesource.ChangeSourceMixin,
-                    unittest.TestCase):
+                   changesource.ChangeSourceMixin,
+                   unittest.TestCase):
 
     def setUp(self):
         self.setUpGetProcessOutput()
@@ -118,12 +119,27 @@ class TestHgPoller(gpo.GetProcessOutputMixin,
             self.poller = hgpoller.HgPoller(self.remote_repo,
                                             workdir='/some/dir')
             self.poller.master = self.master
+        def create_db(_):
+            db = self.master.db = FakeDBConnector(self)
+            return db.setup()
         d.addCallback(create_poller)
+        d.addCallback(create_db)
         return d
 
     def tearDown(self):
         self.tearDownGetProcessOutput()
         return self.tearDownChangeSource()
+
+    def gpoFullcommandPattern(self, commandName, *expected_args):
+        """Match if the command is commandName and arg list start as expected.
+
+        This allows to test a bit more if expected GPO are issued, be it
+        by obscure failures due to the result not being given.
+        """
+        def matchesSubcommand(bin, given_args, **kwargs):
+            return bin == commandName and tuple(
+                given_args[:len(expected_args)]) == expected_args
+        return matchesSubcommand
 
     def test_describe(self):
         self.assertSubstring("HgPoller", self.poller.describe())
@@ -131,31 +147,27 @@ class TestHgPoller(gpo.GetProcessOutputMixin,
     def test_hgbin_default(self):
         self.assertEqual(self.poller.hgbin, "hg")
 
-    def test_poll(self):
-        # Test that environment variables get propagated to subprocesses (See #2116)
+    def test_poll_initial(self):
+        # Test that environment variables get propagated to subprocesses
+        # (See #2116)
         os.putenv('TEST_THAT_ENVIRONMENT_GETS_PASSED_TO_SUBPROCESSES', 'TRUE')
-        self.addGetProcessOutputExpectEnv({'TEST_THAT_ENVIRONMENT_GETS_PASSED_TO_SUBPROCESSES': 'TRUE'})
-        self.addGetProcessOutputAndValueExpectEnv({'TEST_THAT_ENVIRONMENT_GETS_PASSED_TO_SUBPROCESSES': 'TRUE'})
+        self.addGetProcessOutputExpectEnv(
+            {'TEST_THAT_ENVIRONMENT_GETS_PASSED_TO_SUBPROCESSES': 'TRUE'})
+        self.addGetProcessOutputAndValueExpectEnv(
+            {'TEST_THAT_ENVIRONMENT_GETS_PASSED_TO_SUBPROCESSES': 'TRUE'})
 
-        # patch out getProcessOutput and getProcessOutputAndValue for the
-        # benefit of the _get_changes method
+        # patch out getProcessOutput and getProcessOutputAndValue for
+        # expected hg calls
         self.addGetProcessOutputResult(
-                self.gpoSubcommandPattern('hg', 'pull'),# '-b', 'default',
-                                          #self.remote_repo),
-                "no interesting output")
-
+                self.gpoSubcommandPattern('hg', 'pull'), "any output")
         self.addGetProcessOutputResult(
-                self.gpoSubcommandPattern('hg', 'heads'), '1')
-
+                self.gpoFullcommandPattern('hg', 'heads', 'default'), '1')
         self.addGetProcessOutputResult(
-                self.gpoSubcommandPattern('hg', 'log'),
-                '\n'.join([
+                self.gpoFullcommandPattern('hg', 'log', '-b', 'default',
+                                           '-r', '0:1'),
+                os.linesep.join([
                     '0:64a5dc2a4bd4f558b5dd193d47c83c7d7abc9a1a',
                     '1:4423cdbcbb89c14e50dd5f4152415afd686c5241']))
-
-        def cur():
-            return defer.succeed(('test_oid', None))
-        self.patch(self.poller, 'getCurrentRev', cur)
 
         # and patch out the _get_commit_foo methods which were already tested
         # above
@@ -193,5 +205,12 @@ class TestHgPoller(gpo.GetProcessOutputMixin,
             self.assertEqual(self.changes_added[1]['files'], [ '/etc/1' ])
             self.assertEqual(self.changes_added[1]['src'], 'hg')
         d.addCallback(check_changes)
+
+        def check_state(_):
+            st = self.poller.getCurrentRev()
+            def check(oid_rev):
+                self.assertEqual(oid_rev[1], 1)
+            st.addCallback(check)
+        d.addCallback(check_state)
 
         return d
