ddf2f6127e5137a55392a5859c9c01b477fb16da
Naveen Kumar K <naveen.iitm90@gmail.com>
2012-06-24 15:52:44 -0500

    Update Nightly tests to work in non-whole-hour timezones
    Fixes #2261.  This is a modified version of Navneen's patch from bug
    2261.

--- master/buildbot/test/unit/test_schedulers_timed_Nightly.py
+++ master/buildbot/test/unit/test_schedulers_timed_Nightly.py
@@ -27,12 +27,19 @@ class Nightly(scheduler.SchedulerMixin, unittest.TestCase):
 
     OBJECTID = 132
 
+    # not all timezones are even multiples of 1h from GMT.  This variable
+    # holds the number of seconds ahead of the hour for the current timezone.
+    # This is then added to the clock before each test is run (to get to 0
+    # minutes past the hour) and subtracted before the time offset is reported.
+    localtime_offset = time.timezone % 3600
+
     def makeScheduler(self, firstBuildDuration=0, **kwargs):
         sched = self.attachScheduler(timed.Nightly(**kwargs),
                 self.OBJECTID)
 
         # add a Clock to help checking timing issues
         self.clock = sched._reactor = task.Clock()
+        self.clock.advance(self.localtime_offset) # get to 0 min past the hour
 
         # keep track of builds in self.events
         self.events = []
@@ -40,7 +47,9 @@ class Nightly(scheduler.SchedulerMixin, unittest.TestCase):
                 branch=None, repository='', project=''):
             self.assertIn('scheduler named', reason)
             isFirst = (self.events == [])
-            self.events.append('B(%s)@%d' % (branch,self.clock.seconds()))
+            self.events.append('B(%s)@%d' % (branch,
+                # show the offset as seconds past the GMT hour
+                self.clock.seconds() - self.localtime_offset))
             if isFirst and firstBuildDuration:
                 d = defer.Deferred()
                 self.clock.callLater(firstBuildDuration, d.callback, None)
@@ -51,7 +60,8 @@ class Nightly(scheduler.SchedulerMixin, unittest.TestCase):
 
         def addBuildsetForChanges(reason='', external_idstring='', changeids=[]):
             self.events.append('B%s@%d' % (`changeids`.replace(' ',''),
-                                           self.clock.seconds()))
+                            # show the offset as seconds past the GMT hour
+                            self.clock.seconds() - self.localtime_offset))
             return defer.succeed(None)
         sched.addBuildsetForChanges = addBuildsetForChanges
 
@@ -256,8 +266,7 @@ class Nightly(scheduler.SchedulerMixin, unittest.TestCase):
                         minute=[10, 20, 21, 40, 50, 51])
 
         # add a change classification
-        self.db.schedulers.fakeClassifications(self.OBJECTID,
-                                                            { 19 : True })
+        self.db.schedulers.fakeClassifications(self.OBJECTID, { 19 : True })
 
         sched.startService()
 
@@ -266,10 +275,11 @@ class Nightly(scheduler.SchedulerMixin, unittest.TestCase):
         self.db.schedulers.assertClassifications(self.OBJECTID, {})
 
         self.clock.advance(0) # let it get set up
-        while self.clock.seconds() < 30*60: # run for 30 minutes
+        while self.clock.seconds() < self.localtime_offset + 30*60:
             self.clock.advance(60)
         self.assertEqual(self.events, [ 'B(None)@600', 'B(None)@1200', 'B(None)@1260' ])
-        self.db.state.assertStateByClass('test', 'Nightly', last_build=1260)
+        self.db.state.assertStateByClass('test', 'Nightly',
+            last_build=1260 + self.localtime_offset)
 
         d = sched.stopService()
         return d
@@ -282,10 +292,11 @@ class Nightly(scheduler.SchedulerMixin, unittest.TestCase):
         sched.startService()
 
         self.clock.advance(0)
-        while self.clock.seconds() < 10*60: # run for 10 minutes
+        while self.clock.seconds() < self.localtime_offset + 10*60:
             self.clock.advance(60)
         self.assertEqual(self.events, [ 'B(master)@300' ])
-        self.db.state.assertStateByClass('test', 'Nightly', last_build=300)
+        self.db.state.assertStateByClass('test', 'Nightly',
+                last_build=300 + self.localtime_offset)
 
         d = sched.stopService()
         return d
@@ -306,9 +317,11 @@ class Nightly(scheduler.SchedulerMixin, unittest.TestCase):
         # excitement to take place
         changes_at = list(changes_at)
         self.clock.advance(0) # let it trigger the first build
-        while self.clock.seconds() < 30*60:
+        while self.clock.seconds() < self.localtime_offset + 30*60:
             # inject any new changes..
-            while changes_at and self.clock.seconds() >= changes_at[0][0]:
+            while (changes_at and
+                    self.clock.seconds() >=
+                                    self.localtime_offset + changes_at[0][0]):
                 when, newchange, important = changes_at.pop(0)
                 self.sched.gotChange(newchange, important).addErrback(log.err)
             # and advance the clock by a minute
@@ -317,7 +330,8 @@ class Nightly(scheduler.SchedulerMixin, unittest.TestCase):
     def test_iterations_onlyIfChanged_no_changes(self):
         self.do_test_iterations_onlyIfChanged()
         self.assertEqual(self.events, [])
-        self.db.state.assertStateByClass('test', 'Nightly', last_build=1500)
+        self.db.state.assertStateByClass('test', 'Nightly',
+                                         last_build=1500 + self.localtime_offset)
         return self.sched.stopService()
 
     def test_iterations_onlyIfChanged_unimp_changes(self):
@@ -325,7 +339,8 @@ class Nightly(scheduler.SchedulerMixin, unittest.TestCase):
                 (60, mock.Mock(), False),
                 (600, mock.Mock(), False))
         self.assertEqual(self.events, [])
-        self.db.state.assertStateByClass('test', 'Nightly', last_build=1500)
+        self.db.state.assertStateByClass('test', 'Nightly',
+                                         last_build=1500 + self.localtime_offset)
         return self.sched.stopService()
 
     def test_iterations_onlyIfChanged_off_branch_changes(self):
@@ -333,7 +348,8 @@ class Nightly(scheduler.SchedulerMixin, unittest.TestCase):
                 (60, self.makeFakeChange(branch='testing'), True),
                 (1700, self.makeFakeChange(branch='staging'), True))
         self.assertEqual(self.events, [])
-        self.db.state.assertStateByClass('test', 'Nightly', last_build=1500)
+        self.db.state.assertStateByClass('test', 'Nightly',
+                                         last_build=1500 + self.localtime_offset)
         return self.sched.stopService()
 
     def test_iterations_onlyIfChanged_mixed_changes(self):
@@ -347,5 +363,6 @@ class Nightly(scheduler.SchedulerMixin, unittest.TestCase):
         # off-branch changes, and note that no build took place at 300s, as no important
         # changes had yet arrived
         self.assertEqual(self.events, [ 'B[3,5,6]@1500' ])
-        self.db.state.assertStateByClass('test', 'Nightly', last_build=1500)
+        self.db.state.assertStateByClass('test', 'Nightly',
+                                         last_build=1500 + self.localtime_offset)
         return self.sched.stopService()
