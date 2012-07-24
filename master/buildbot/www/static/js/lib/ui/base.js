2f4d186d67b8182e22e243ab7f69f4d7f61165d7
Pierre Tardy <tardyp@gmail.com>
2012-07-24 15:26:28 +0200

    www_haml: allow use of haml for templating
    Haml is a nice and simple templating langage, that avoids to write tons of
    markups, and has basic facility for loops, conditionnals, and macroing.
    
    Templates are fetched from the static server, compiled into a javascript
    function, cached, and executed on demand, with widget as the context.
    
    widgets can implement loadMoreContext() in order to load more context
    from the json api
    
    Signed-off-by: Pierre Tardy <tardyp@gmail.com>

--- master/buildbot/www/static/js/lib/ui/base.js
+++ master/buildbot/www/static/js/lib/ui/base.js
@@ -13,20 +13,43 @@
 //
 // Copyright Buildbot Team Members
 
-define(["dojo/_base/declare", "dijit/_Widget", "dijit/_Templated",
-       	"dojo/text!./templates/unimplemented.html"],
-       function(declare, _Widget, _Templated, template) {
-	   "use strict";
-	   return declare([_Widget, _Templated], {
-	       widgetsInTemplate: true,
+define(["dojo/_base/declare", "dijit/_Widget", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin",
+	"dojo/_base/Deferred",
+	"dojo/text!./templates/unimplemented.html"],
+       function(declare, _Widget, _TemplatedMixin, _WidgetsInTemplateMixin, Deferred, template) {
+	   return declare([_Widget, _TemplatedMixin, _WidgetsInTemplateMixin], {
 	       templateString: template,
                constructor: function(args){
 		   declare.safeMixin(this,args);
 	       },
+	       /* support for Haml as a template
+		  should be a MixIn, but it is so simple that
+		  we put it in the base widget
+		*/
+	       buildRendering: function(){
+		   /* just call the templateFunc, and go ahead with _TemplateMixin implementation */
+		   if(this.templateFunc){
+		       this.templateString = this.templateFunc(this);
+		   }
+		   this.inherited(arguments);
+	       },
+	       create: function(){
+		   var _arguments = arguments;
+		   var _this = this;
+		   var d = this.loadMoreContext();
+		   this.readyDeferred = dojo.when(d, function() {
+		       document.__base = _this;
+		       _this.inherited(_arguments);
+		   });
+	       },
+	       /* overide this function to get more context from json api
+		  Maybe return deferred
+		*/
+	       loadMoreContext: function(){
+	       },
 	       postCreate: function(){
-		   if (this.templateString==template) {
+		   if (this.templateString===template) {
 		       this.functionality.innerText=this.path_components.toString();
-		       console.log(this.url_args)
 		   }
                }
 	   });
