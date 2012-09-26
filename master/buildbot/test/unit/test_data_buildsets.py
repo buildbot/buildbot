d2d79cffde5a67466fa490768fb2d37666ad095d
Dustin J. Mitchell <dustin@mozilla.com>
2012-09-25 21:27:50 -0400

    Test fixes per tomprince's review
    * add docstrings for test utils in test_data_buildsets.py
    * use symbols for arbitrary epoch timestamps

--- master/buildbot/test/unit/test_data_buildsets.py
+++ master/buildbot/test/unit/test_data_buildsets.py
@@ -22,6 +22,9 @@ from buildbot.test.util import types, endpoint
 from buildbot.test.fake import fakedb, fakemaster
 from buildbot.status.results import SUCCESS, FAILURE
 
+A_TIMESTAMP = 1341700729
+EARLIER = 1248529376
+
 class Buildset(endpoint.EndpointMixin, unittest.TestCase):
 
     endpointClass = buildsets.BuildsetEndpoint
@@ -122,8 +125,19 @@ class BuildsetResourceType(unittest.TestCase):
 
     def do_test_addBuildset(self, kwargs, expectedReturn,
             expectedMessages, expectedBuildset):
+        """Run a test of addBuildset.
+
+        @param kwargs: kwargs to addBuildset
+        @param expectedReturn: expected return value - tuple of (bsid, brids)
+        @param expectedMessages: expected mq messages transmitted
+        @param expectedBuildset: expected buildset inserted into the db
+
+        The buildset is added at time A_TIMESTAMP.
+        Note that addBuildset does not add sourcestamps, so this method assumes
+        there are none in the db.
+        """
         clock = task.Clock()
-        clock.advance(1341700729)
+        clock.advance(A_TIMESTAMP)
         d = self.rtype.addBuildset(_reactor=clock, **kwargs)
         def check((bsid, brids)):
             self.assertEqual((bsid, brids), expectedReturn)
@@ -131,9 +145,37 @@ class BuildsetResourceType(unittest.TestCase):
             self.assertEqual(sorted(self.master.mq.productions),
                     sorted(expectedMessages))
             # and that the correct data was inserted into the db
-            self.master.db.buildsets.assertBuildset(200, expectedBuildset, {})
+            self.master.db.buildsets.assertBuildset(bsid, expectedBuildset, {})
         d.addCallback(check)
         return d
+
+    def _buildRequestMessage(self, brid, bsid, builderid, buildername):
+        return (
+            ('buildrequest', str(bsid), str(builderid), str(brid), 'new'),
+            dict(brid=brid, bsid=bsid, builderid=builderid,
+                 buildername=buildername))
+
+    def _buildsetMessage(self, bsid, external_idstring=u'extid',
+            reason=u'because', scheduler=u'fakesched', sourcestampsetid=234,
+            submitted_at=A_TIMESTAMP):
+        return (
+            ('buildset', str(bsid), 'new'),
+            dict(bsid=bsid, complete=False, complete_at=None,
+                 external_idstring=external_idstring, reason=reason,
+                 results=None, scheduler=scheduler,
+                 sourcestampsetid=sourcestampsetid,
+                 submitted_at=submitted_at))
+
+    def _buildsetCompleteMessage(self, bsid, complete_at=A_TIMESTAMP,
+            submitted_at=A_TIMESTAMP, external_idstring=u'extid',
+            reason=u'because', results=0, sourcestampsetid=234):
+        return (
+            ('buildset', str(bsid), 'complete'),
+            dict(bsid=bsid, complete=True, complete_at=complete_at,
+                 external_idstring=external_idstring, reason=reason,
+                 results=results, sourcestampsetid=sourcestampsetid,
+                 submitted_at=submitted_at))
+
 
     def test_addBuildset_two_builderNames(self):
         class FakeSched(object):
@@ -145,29 +187,9 @@ class BuildsetResourceType(unittest.TestCase):
                     builderNames=['a', 'b'])
         expectedReturn = (200, dict(a=1000, b=1001))
         expectedMessages = [
-        (('buildrequest', '200', '-1', '1000', 'new'), {
-            'brid': 1000,
-            'bsid': 200,
-            'builderid': -1,
-            'buildername': u'a',
-        }),
-        (('buildrequest', '200', '-1', '1001', 'new'), {
-            'brid': 1001,
-            'bsid': 200,
-            'builderid': -1,
-            'buildername': u'b',
-        }),
-        (('buildset', '200', 'new'), {
-            'bsid': 200,
-            'complete': False,
-            'complete_at': None,
-            'external_idstring': u'extid',
-            'reason': u'because',
-            'results': None,
-            'scheduler': u'fakesched',
-            'sourcestampsetid': 234,
-            'submitted_at': 1341700729,
-        }),
+            self._buildRequestMessage(1000, 200, -1, u'a'),
+            self._buildRequestMessage(1001, 200, -1, u'b'),
+            self._buildsetMessage(200),
         ]
         expectedBuildset = dict(reason=u'because',
                 properties={},
@@ -184,28 +206,10 @@ class BuildsetResourceType(unittest.TestCase):
         kwargs = dict(scheduler=FakeSched(), reason=u'because',
                             sourcestampsetid=234, external_idstring=u'extid')
         expectedReturn = (200, {})
-        expectedMessages = [(('buildset', '200', 'new'), {
-            'bsid': 200,
-            'complete': False,
-            'complete_at': None,
-            'external_idstring': u'extid',
-            'reason': u'because',
-            'results': None,
-            'scheduler': u'fakesched',
-            'sourcestampsetid': 234,
-            'submitted_at': 1341700729,
-        }),
-        (('buildset', '200', 'complete'), {
-            'bsid': 200,
-            'complete': True,
-            'complete_at': 1341700729,
-            'external_idstring': u'extid',
-            'reason': u'because',
-            'results': 0,
-            # note, no scheduler
-            'sourcestampsetid': 234,
-            'submitted_at': 1341700729,
-        }),
+        expectedMessages = [
+            self._buildsetMessage(200),
+            # with no builderNames, this is done already
+            self._buildsetCompleteMessage(200),
         ]
         expectedBuildset = dict(reason=u'because',
                 properties={},
@@ -222,15 +226,39 @@ class BuildsetResourceType(unittest.TestCase):
             expectComplete=False,
             expectMessage=False,
             expectSuccess=True):
+        """Test maybeBuildsetComplete.
+
+        @param buildRequestCompletions: dict mapping brid to True if complete,
+            else False (and defaulting to False)
+        @param buildRequestResults: dict mapping brid to result (defaulting
+            to SUCCESS)
+        @param buildsetComplete: true if the buildset is already complete
+        @param expectComplete: true if the buildset should be complete at exit
+        @param expectMessage: true if a buildset completion message is expected
+        @param expectSuccess: if expectComplete, whether to expect the buildset
+            to be complete
+
+        This first adds two buildsets to the database - 72 and 73.  Buildset 72
+        is already complete if buildsetComplete is true; 73 is not complete.
+        It adds four buildrequests - 42, 43, and 44 for buildset 72, and 45 for
+        buildset 73.  The completion and results are based on
+        buidlRequestCompletions and buildRequestResults.
+
+        Then, maybeBuildsetComplete is called for buildset 72, and the
+        expectations are checked.
+        """
+
         clock = task.Clock()
-        clock.advance(9876543)
+        clock.advance(A_TIMESTAMP)
         def mkbr(brid, bsid=72):
             return fakedb.BuildRequest(id=brid, buildsetid=bsid,
                 complete=buildRequestCompletions.get(brid),
                 results=buildRequestResults.get(brid, SUCCESS))
         yield self.master.db.insertTestData([
             fakedb.Buildset(id=72, sourcestampsetid=234,
-                complete=buildsetComplete),
+                submitted_at=EARLIER,
+                complete=buildsetComplete,
+                complete_at=A_TIMESTAMP if buildsetComplete else None),
             mkbr(42), mkbr(43), mkbr(44),
             fakedb.Buildset(id=73, sourcestampsetid=234,
                 complete=False),
@@ -241,21 +269,16 @@ class BuildsetResourceType(unittest.TestCase):
 
         self.master.db.buildsets.assertBuildsetCompletion(72, expectComplete)
         if expectMessage:
-            self.assertEqual(self.master.mq.productions, [ (
-                ('buildset', '72', 'complete'),
-                dict(bsid=72,
-                    complete=True,
-                    complete_at=9876543,
-                    external_idstring=u'extid',
-                    reason=u'because',
+            self.assertEqual(self.master.mq.productions, [
+                self._buildsetCompleteMessage(72,
                     results=SUCCESS if expectSuccess else FAILURE,
-                    sourcestampsetid=234,
-                    submitted_at=12345678))
+                    submitted_at=EARLIER),
             ])
         else:
             self.assertEqual(self.master.mq.productions, [])
 
     def test_maybeBuildsetComplete_not_yet(self):
+        # only brid 42 is complete, so the buildset is not complete
         return self.do_test_maybeBuildsetComplete(
                 buildRequestCompletions={42:True})
 
