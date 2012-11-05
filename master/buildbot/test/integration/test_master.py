74e74c9242f2dd095e6c911cb2dd8d0666f7565b
Dustin J. Mitchell <dustin@mozilla.com>
2012-11-05 07:40:49 -0500

    also run webstatus on an arbitrary port

--- master/buildbot/test/integration/test_master.py
+++ master/buildbot/test/integration/test_master.py
@@ -112,7 +112,7 @@ c['builders'].append(
       slavenames=["local1"],
       factory=f1))
 c['status'] = []
-c['status'].append(html.WebStatus(http_port=8010))
+c['status'].append(html.WebStatus(http_port=0))
 c['title'] = "test"
 c['titleURL'] = "test"
 c['buildbotURL'] = "http://localhost:8010/"
