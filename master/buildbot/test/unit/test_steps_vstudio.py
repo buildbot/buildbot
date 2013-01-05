661ab0f348e627070dbebcf36b77fb7ef58598f1
Cezar Elnazli <xlr3204s.ipod@gmail.com>
2013-01-05 21:12:08 +0200

    Update documentation to include VS2012 and add a test for it

--- master/buildbot/test/unit/test_steps_vstudio.py
+++ master/buildbot/test/unit/test_steps_vstudio.py
@@ -720,7 +720,7 @@ class TestVC9(VC8ExpectedEnvMixin, steps.BuildStepMixin, unittest.TestCase):
         return self.runStep()
 
 
-class TestVC10(VC8ExpectedEnvMixin, steps.BuildStepMixin, unittest.TestCase):
+class Test1C10(VC8ExpectedEnvMixin, steps.BuildStepMixin, unittest.TestCase):
 
     def setUp(self):
         return self.setUpBuildStep()
@@ -737,6 +737,30 @@ class TestVC10(VC8ExpectedEnvMixin, steps.BuildStepMixin, unittest.TestCase):
                                  'cfg', '/Project', 'pj' ],
                         env=self.getExpectedEnv(
                             r'C:\Program Files\Microsoft Visual Studio 10.0'))
+            + 0
+        )
+        self.expectOutcome(result=SUCCESS,
+                status_text=["compile", "0 projects", "0 files"])
+        return self.runStep()
+
+
+class TestVC11(VC8ExpectedEnvMixin, steps.BuildStepMixin, unittest.TestCase):
+
+    def setUp(self):
+        return self.setUpBuildStep()
+
+    def tearDown(self):
+        return self.tearDownBuildStep()
+
+    def test_installdir(self):
+        self.setupStep(vstudio.VC11(projectfile='pf', config='cfg',
+                                   project='pj'))
+        self.expectCommands(
+            ExpectShell(workdir='wkdir', usePTY='slave-config',
+                        command=['devenv.com', 'pf', '/Rebuild',
+                                 'cfg', '/Project', 'pj' ],
+                        env=self.getExpectedEnv(
+                            r'C:\Program Files\Microsoft Visual Studio 11.0'))
             + 0
         )
         self.expectOutcome(result=SUCCESS,
@@ -796,3 +820,5 @@ class Aliases(unittest.TestCase):
     def test_vs2010(self):
         self.assertIdentical(vstudio.VS2010, vstudio.VC10)
 
+    def test_vs2012(self):
+        self.assertIdentical(vstudio.VS2012, vstudio.VC11)
