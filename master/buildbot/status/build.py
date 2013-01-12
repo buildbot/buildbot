9a8fcf86ba90972afa5fc3e19d564d45fb1fd05d
Dustin J. Mitchell <dustin@mozilla.com>
2013-01-12 15:50:24 -0500

    fix comment typo

--- master/buildbot/status/build.py
+++ master/buildbot/status/build.py
@@ -1,4 +1,4 @@
-#.load and pickle.loads This file is part of Buildbot.  Buildbot is free software: you can
+# This file is part of Buildbot.  Buildbot is free software: you can
 # redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, version 2.
 #
