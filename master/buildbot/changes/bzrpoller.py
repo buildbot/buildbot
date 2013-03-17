b13780dc79933434d0e22099d452232a35c16e74
Georges Racinet <gracinet@anybox.fr>
2013-03-17 14:54:22 +0100

    removed hook code, made bzrlib import conditional, flake8 correction

--- master/buildbot/changes/bzrpoller.py
+++ master/buildbot/changes/bzrpoller.py
@@ -17,6 +17,8 @@
 bzr buildbot integration
 ========================
 
+TODO #708, to update for integration into core
+
 This file contains both bzr commit/change hooks and a bzr poller.
 
 ------------
@@ -29,65 +31,6 @@ should work in subsequent releases.
 For the hook to work, Twisted must be installed in the same Python that bzr
 uses.
 
------
-Hooks
------
-
-To install, put this file in a bzr plugins directory (e.g.,
-~/.bazaar/plugins). Then, in one of your bazaar conf files (e.g.,
-~/.bazaar/locations.conf), set the location you want to connect with buildbot
-with these keys:
-
-- buildbot_on: one of 'commit', 'push, or 'change'.  Turns the plugin on to
-  report changes via commit, changes via push, or any changes to the trunk.
-  'change' is recommended.
-
-- buildbot_server: (required to send to a buildbot master) the URL of the
-  buildbot master to which you will connect (as of this writing, the same
-  server and port to which slaves connect).
-
-- buildbot_port: (optional, defaults to 9989) the port of the buildbot master
-  to which you will connect (as of this writing, the same server and port to
-  which slaves connect)
-
-- buildbot_auth: (optional, defaults to change:changepw) the credentials
-  expected by the change source configuration in the master. Takes the
-  "user:password" form.
-
-- buildbot_pqm: (optional, defaults to not pqm) Normally, the user that
-  commits the revision is the user that is responsible for the change.  When
-  run in a pqm (Patch Queue Manager, see https://launchpad.net/pqm)
-  environment, the user that commits is the Patch Queue Manager, and the user
-  that committed the *parent* revision is responsible for the change.  To turn
-  on the pqm mode, set this value to any of (case-insensitive) "Yes", "Y",
-  "True", or "T".
-
-- buildbot_dry_run: (optional, defaults to not a dry run) Normally, the
-  post-commit hook will attempt to communicate with the configured buildbot
-  server and port. If this parameter is included and any of (case-insensitive)
-  "Yes", "Y", "True", or "T", then the hook will simply print what it would
-  have sent, but not attempt to contact the buildbot master.
-
-- buildbot_send_branch_name: (optional, defaults to not sending the branch
-  name) If your buildbot's bzr source build step uses a repourl, do
-  *not* turn this on. If your buildbot's bzr build step uses a baseURL, then
-  you may set this value to any of (case-insensitive) "Yes", "Y", "True", or
-  "T" to have the buildbot master append the branch name to the baseURL.
-
-Note: The bzr smart server (as of version 2.2.2) doesn't know how to resolve
-bzr:// urls into absolute paths so any paths in locations.conf won't match,
-hence no change notifications will be sent to Buildbot. Setting configuration
-parameters globally or in-branch might still work.
-
-When buildbot no longer has a hardcoded password, it will be a configuration
-option here as well.
-
-------
-Poller
-------
-
-See the Buildbot manual.
-
 -------------------
 Contact Information
 -------------------
@@ -95,17 +38,18 @@ Contact Information
 Maintainer/author: gary.poster@canonical.com
 """
 
+
+import buildbot.util
+import buildbot.changes.base
+import buildbot.changes.changes
+
 try:
-    import buildbot.util
-    import buildbot.changes.base
-    import buildbot.changes.changes
+    import bzrlib
 except ImportError:
-    DEFINE_POLLER = False
+    BZRLIB_PRESENT = False
 else:
-    DEFINE_POLLER = True
-import bzrlib.branch
-import bzrlib.errors
-import bzrlib.trace
+    BZRLIB_PRESENT = True
+
 import twisted.cred.credentials
 import twisted.internet.base
 import twisted.internet.defer
@@ -141,14 +85,14 @@ def generate_change(branch,
     identified as the "who", not the person who committed the branch itself.
     This is typically used for PQM.
     """
-    change = {} # files, who, comments, revision; NOT branch (= branch.nick)
+    change = {}  # files, who, comments, revision; NOT branch (= branch.nick)
     if new_revno is None:
         new_revno = branch.revno()
     if new_revid is None:
         new_revid = branch.get_rev_id(new_revno)
     # TODO: This falls over if this is the very first revision
     if old_revno is None:
-        old_revno = new_revno -1
+        old_revno = new_revno - 1
     if old_revid is None:
         old_revid = branch.get_rev_id(old_revno)
     repository = branch.repository
@@ -175,7 +119,7 @@ def generate_change(branch,
             files.append(' '.join([path, kind, name]))
     for info in changes.renamed:
         oldpath, newpath, id, kind, text_modified, meta_modified = info
-        elements = [oldpath, kind,'RENAMED', newpath]
+        elements = [oldpath, kind, 'RENAMED', newpath]
         if text_modified or meta_modified:
             elements.append('MODIFIED')
         files.append(' '.join(elements))
@@ -186,11 +130,11 @@ def generate_change(branch,
 
 # We don't want to make the hooks unnecessarily depend on buildbot being
 # installed locally, so we conditionally create the BzrPoller class.
-if DEFINE_POLLER:
+if True:  # was BUILDBOT_DEFINED,
+# TODO: don't want to change indentation right now
 
     FULL = object()
     SHORT = object()
-
 
     class BzrPoller(buildbot.changes.base.PollingChangeSource,
                     buildbot.util.ComparableMixin):
@@ -213,6 +157,8 @@ if DEFINE_POLLER:
             self.category = category
 
         def startService(self):
+            if not BZRLIB_PRESENT:
+                raise ImportError('bzrlib')
             twisted.python.log.msg("BzrPoller(%s) starting" % self.url)
             if self.branch_name is FULL:
                 ourbranch = self.url
@@ -268,13 +214,13 @@ if DEFINE_POLLER:
                 branch_name = self.url
             elif self.branch_name is SHORT:
                 branch_name = branch.nick
-            else: # presumably a string or maybe None
+            else:  # presumably a string or maybe None
                 branch_name = self.branch_name
             changes = []
             change = generate_change(
                 branch, blame_merge_author=self.blame_merge_author)
             if (self.last_revision is None or
-                change['revision'] > self.last_revision):
+                    change['revision'] > self.last_revision):
                 change['branch'] = branch_name
                 change['category'] = self.category
                 changes.append(change)
@@ -290,54 +236,29 @@ if DEFINE_POLLER:
 
         def addChange(self, change):
             d = twisted.internet.defer.Deferred()
+
             def _add_change():
                 d.callback(
                     self.parent.addChange(change, src='bzr'))
             twisted.internet.reactor.callLater(0, _add_change)
             return d
 
-#############################################################################
-# hooks
-
-HOOK_KEY = 'buildbot_on'
-SERVER_KEY = 'buildbot_server'
-PORT_KEY = 'buildbot_port'
-AUTH_KEY = 'buildbot_auth'
-DRYRUN_KEY = 'buildbot_dry_run'
-PQM_KEY = 'buildbot_pqm'
-SEND_BRANCHNAME_KEY = 'buildbot_send_branch_name'
-
-PUSH_VALUE = 'push'
-COMMIT_VALUE = 'commit'
-CHANGE_VALUE = 'change'
-
-def _is_true(config, key):
-    val = config.get_user_option(key)
-    return val is not None and val.lower().strip() in (
-        'y', 'yes', 't', 'true')
-
-def _installed_hook(branch):
-    value = branch.get_config().get_user_option(HOOK_KEY)
-    if value is not None:
-        value = value.strip().lower()
-        if value not in (PUSH_VALUE, COMMIT_VALUE, CHANGE_VALUE):
-            raise bzrlib.errors.BzrError(
-                '%s, if set, must be one of %s, %s, or %s' % (
-                    HOOK_KEY, PUSH_VALUE, COMMIT_VALUE, CHANGE_VALUE))
-    return value
-
 ##########################
 # Work around Twisted bug.
 # See http://twistedmatrix.com/trac/ticket/3591
+# TODO GR: merged 4 years ago ; check if current minimum required version
+# is more recent, and then, remove
 import operator
 import socket
 from twisted.internet import defer
 from twisted.python import failure
 
-# replaces twisted.internet.thread equivalent
+
 def _putResultInDeferred(reactor, deferred, f, args, kwargs):
     """
     Run a function and give results to a Deferred.
+
+    replaces twisted.internet.thread equivalent
     """
     try:
         result = f(*args, **kwargs)
@@ -347,18 +268,22 @@ def _putResultInDeferred(reactor, deferred, f, args, kwargs):
     else:
         reactor.callFromThread(deferred.callback, result)
 
-# would be a proposed addition.  deferToThread could use it
+
 def deferToThreadInReactor(reactor, f, *args, **kwargs):
     """
     Run function in thread and return result as Deferred.
+
+    would be a proposed addition.  deferToThread could use it
     """
     d = defer.Deferred()
     reactor.callInThread(_putResultInDeferred, reactor, d, f, args, kwargs)
     return d
 
-# uses its own reactor for the threaded calls, unlike Twisted's
+
 class ThreadedResolver(twisted.internet.base.ThreadedResolver):
-    def getHostByName(self, name, timeout = (1, 3, 11, 45)):
+    """Uses its own reactor for the threaded calls, unlike Twisted's."""
+
+    def getHostByName(self, name, timeout=(1, 3, 11, 45)):
         if timeout:
             timeoutDelay = reduce(operator.add, timeout)
         else:
@@ -371,98 +296,3 @@ class ThreadedResolver(twisted.internet.base.ThreadedResolver):
         self._runningQueries[lookupDeferred] = (userDeferred, cancelCall)
         lookupDeferred.addBoth(self._checkTimeout, name, lookupDeferred)
         return userDeferred
-##########################
-
-def send_change(branch, old_revno, old_revid, new_revno, new_revid, hook):
-    config = branch.get_config()
-    server = config.get_user_option(SERVER_KEY)
-    if not server:
-        bzrlib.trace.warning(
-            'bzr_buildbot: ERROR.  If %s is set, %s must be set',
-            HOOK_KEY, SERVER_KEY)
-        return
-    change = generate_change(
-        branch, old_revno, old_revid, new_revno, new_revid,
-        blame_merge_author=_is_true(config, PQM_KEY))
-    if _is_true(config, SEND_BRANCHNAME_KEY):
-        change['branch'] = branch.nick
-    # as of this writing (in Buildbot 0.7.9), 9989 is the default port when
-    # you make a buildbot master.
-    port = int(config.get_user_option(PORT_KEY) or 9989)
-    # if dry run, stop.
-    if _is_true(config, DRYRUN_KEY):
-        bzrlib.trace.note("bzr_buildbot DRY RUN "
-                          "(*not* sending changes to %s:%d on %s)",
-                          server, port, hook)
-        keys = change.keys()
-        keys.sort()
-        for k in keys:
-            bzrlib.trace.note("[%10s]: %s", k, change[k])
-        return
-    # We instantiate our own reactor so that this can run within a server.
-    reactor = twisted.internet.selectreactor.SelectReactor()
-    # See other reference to http://twistedmatrix.com/trac/ticket/3591
-    # above.  This line can go away with a release of Twisted that addresses
-    # this issue.
-    reactor.resolver = ThreadedResolver(reactor)
-    pbcf = twisted.spread.pb.PBClientFactory()
-    reactor.connectTCP(server, port, pbcf)
-    auth = config.get_user_option(AUTH_KEY)
-    if auth:
-        user, passwd = [s.strip() for s in auth.split(':', 1)]
-    else:
-        user, passwd = ('change', 'changepw')
-    deferred = pbcf.login(
-        twisted.cred.credentials.UsernamePassword(user, passwd))
-
-    def sendChanges(remote):
-        """Send changes to buildbot."""
-        bzrlib.trace.mutter("bzrbuildout sending changes: %s", change)
-        change['src'] = 'bzr'
-        return remote.callRemote('addChange', change)
-
-    deferred.addCallback(sendChanges)
-
-    def quit(ignore, msg):
-        bzrlib.trace.note("bzrbuildout: %s", msg)
-        reactor.stop()
-
-    def failed(failure):
-        bzrlib.trace.warning("bzrbuildout: FAILURE\n %s", failure)
-        reactor.stop()
-
-    deferred.addCallback(quit, "SUCCESS")
-    deferred.addErrback(failed)
-    reactor.callLater(60, quit, None, "TIMEOUT")
-    bzrlib.trace.note(
-        "bzr_buildbot: SENDING CHANGES to buildbot master %s:%d on %s",
-        server, port, hook)
-    reactor.run(installSignalHandlers=False) # run in a thread when in server
-
-def post_commit(local_branch, master_branch, # branch is the master_branch
-                old_revno, old_revid, new_revno, new_revid):
-    if _installed_hook(master_branch) == COMMIT_VALUE:
-        send_change(master_branch,
-                     old_revid, old_revid, new_revno, new_revid, COMMIT_VALUE)
-
-def post_push(result):
-    if _installed_hook(result.target_branch) == PUSH_VALUE:
-        send_change(result.target_branch,
-                     result.old_revid, result.old_revid,
-                     result.new_revno, result.new_revid, PUSH_VALUE)
-
-def post_change_branch_tip(result):
-    if _installed_hook(result.branch) == CHANGE_VALUE:
-        send_change(result.branch,
-                     result.old_revid, result.old_revid,
-                     result.new_revno, result.new_revid, CHANGE_VALUE)
-
-bzrlib.branch.Branch.hooks.install_named_hook(
-    'post_commit', post_commit,
-    'send change to buildbot master')
-bzrlib.branch.Branch.hooks.install_named_hook(
-    'post_push', post_push,
-    'send change to buildbot master')
-bzrlib.branch.Branch.hooks.install_named_hook(
-    'post_change_branch_tip', post_change_branch_tip,
-    'send change to buildbot master')
