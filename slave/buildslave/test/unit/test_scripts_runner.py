d8626361079a9e5a4fc32ed8619794e7ac9b4f82
Elmir Jagudin <elmir@axis.com>
2013-03-29 16:55:11 +0100

    added unit tests on buildslave cli Options classes

--- slave/buildslave/test/unit/test_scripts_runner.py
+++ slave/buildslave/test/unit/test_scripts_runner.py
@@ -200,6 +200,109 @@ class OptionsMixin(object):
                                (k, exp[k], opts[k]))
             self.fail("did not get expected options\n" + ("\n".join(msg)))
 
+class BaseDirTestsMixin:
+    """
+    Common tests for Options classes with 'basedir' parameter
+    """
+
+    GETCWD_PATH = "test-dir"
+    ABSPATH_PREFIX = "test-prefix-"
+    MY_BASEDIR = "my-basedir"
+
+    # the options class to instantiate for test cases
+    options_class = None
+
+    def setUp(self):
+        self.patch(os, "getcwd", lambda : self.GETCWD_PATH)
+        self.patch(os.path, "abspath", lambda path: self.ABSPATH_PREFIX + path)
+
+    def parse(self, *args):
+        assert self.options_class != None
+
+        opts = self.options_class()
+        opts.parseOptions(args)
+        return opts
+
+    def test_defaults(self):
+        opts = self.parse()
+        self.assertEqual(opts["basedir"],
+                         self.ABSPATH_PREFIX + self.GETCWD_PATH,
+                         "unexpected basedir path")
+
+    def test_basedir_arg(self):
+        opts = self.parse(self.MY_BASEDIR)
+        self.assertEqual(opts["basedir"],
+                         self.ABSPATH_PREFIX + self.MY_BASEDIR,
+                         "unexpected basedir path")
+
+    def test_too_many_args(self):
+        self.assertRaisesRegexp(usage.UsageError,
+                                "I wasn't expecting so many arguments",
+                                self.parse, "arg1", "arg2")
+
+
+class TestMakerBase(BaseDirTestsMixin, unittest.TestCase):
+    """
+    Test buildslave.scripts.runner.MakerBase class.
+    """
+    options_class = runner.MakerBase
+
+
+class TestStopOptions(BaseDirTestsMixin, unittest.TestCase):
+    """
+    Test buildslave.scripts.runner.StopOptions class.
+    """
+    options_class = runner.StopOptions
+
+    def test_synopsis(self):
+        opts = runner.StopOptions()
+        self.assertIn('buildslave stop', opts.getSynopsis())
+
+
+class TestStartOptions(OptionsMixin, BaseDirTestsMixin, unittest.TestCase):
+    """
+    Test buildslave.scripts.runner.StartOptions class.
+    """
+    options_class = runner.StartOptions
+
+    def test_synopsis(self):
+        opts = runner.StartOptions()
+        self.assertIn('buildslave start', opts.getSynopsis())
+
+    def test_all_args(self):
+        opts = self.parse("--quiet", "--nodaemon", self.MY_BASEDIR)
+        self.assertOptions(opts,
+                           dict(quiet=True, nodaemon=True,
+                                basedir=self.ABSPATH_PREFIX + self.MY_BASEDIR))
+
+
+class TestRestartOptions(OptionsMixin, BaseDirTestsMixin, unittest.TestCase):
+    """
+    Test buildslave.scripts.runner.RestartOptions class.
+    """
+    options_class = runner.RestartOptions
+
+    def test_synopsis(self):
+        opts = runner.RestartOptions()
+        self.assertIn('buildslave restart', opts.getSynopsis())
+
+    def test_all_args(self):
+        opts = self.parse("--quiet", "--nodaemon", self.MY_BASEDIR)
+        self.assertOptions(opts,
+                           dict(quiet=True, nodaemon=True,
+                                basedir=self.ABSPATH_PREFIX + self.MY_BASEDIR))
+
+
+class TestUpgradeSlaveOptions(BaseDirTestsMixin, unittest.TestCase):
+    """
+    Test buildslave.scripts.runner.UpgradeSlaveOptions class.
+    """
+    options_class = runner.UpgradeSlaveOptions
+
+    def test_synopsis(self):
+        opts = runner.UpgradeSlaveOptions()
+        self.assertIn('buildslave upgrade-slave', opts.getSynopsis())
+
 
 class TestCreateSlaveOptions(OptionsMixin, unittest.TestCase):
     """
