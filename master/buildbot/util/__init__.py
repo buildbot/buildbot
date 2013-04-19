6a04f1486f51a68150ef1cf94dc1c0579e0ac30d
Edmund Wong <ewongbb@pw-wspx.org>
2013-04-19 13:03:06 +0800

    - Fixed __cmp__ method.

--- master/buildbot/util/__init__.py
+++ master/buildbot/util/__init__.py
@@ -92,14 +92,14 @@ class ComparableMixin:
         acomp = []
         bcomp = []
         reflect.accumulateClassList(self.__class__, 'compare_attrs', acomp)
-        reflect.accumulateClassList(self.__class__, 'compare_attrs', bcomp)
+        reflect.accumulateClassList(them.__class__, 'compare_attrs', bcomp)
         result = cmp(acomp, bcomp)
         if result:
             return result
 
         self_list = [getattr(self, name, self._None)
                      for name in acomp]
-        them_list = [getattr(them, name, self._None)
+        them_list = [getattr(them, name, them._None)
                      for name in bcomp]
         return cmp(self_list, them_list)
 
