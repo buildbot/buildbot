1ef6e4aad19b651645bbdc658a8a3fafa303606c
Dustin J. Mitchell <dustin@mozilla.com>
2013-05-19 22:18:12 -0400

    remove testhooks support

--- master/buildbot/test/util/www.py
+++ master/buildbot/test/util/www.py
@@ -13,14 +13,11 @@
 #
 # Copyright Buildbot Team Members
 
-import os
 import pkg_resources
-from urlparse import urljoin
 from buildbot.util import json
-from twisted.internet import defer, reactor
+from twisted.internet import defer
 from twisted.web import server
 from buildbot.test.fake import fakemaster
-from twisted.python import failure, log
 from cStringIO import StringIO
 from uuid import uuid1
 
@@ -140,57 +137,3 @@ class WwwTestMixin(object):
                                     'content-disposition')
             exp['contentDisposition'] = [ contentDisposition ]
         self.assertEqual(got, exp)
-try:
-    from buildbot.test.util.txghost import Ghost
-    has_ghost= Ghost != None
-except ImportError:
-    no_ghost_message = "Need Ghost.py to run most of www_ui tests"
-    has_ghost=False
-    # if $REQUIRE_GHOST is set, then fail if it's not found
-    if os.environ.get('REQUIRE_GHOST'):
-        raise
-
-if not has_ghost and os.environ.get('REQUIRE_GHOST'):
-            raise Exception(no_ghost_message)
-
-
-class WwwGhostTestMixin(object):
-    if not has_ghost:
-        skip = no_ghost_message
-
-    @defer.inlineCallbacks
-    def setUp(self):
-        ## workaround twisted bug  http://twistedmatrix.com/trac/ticket/2386
-        import twisted
-        twisted.web.http._logDateTimeUsers = 1
-        twisted.internet.base.DelayedCall.debug = True
-        ## we cannot use self.patch, as _logDateTimeUsers is not present in all versions of twisted
-        self.master = yield fakemaster.make_master_for_uitest(0)
-        self.url = self.master.config.www["url"]
-        log.msg("listening on "+self.url)
-        self.ghost = Ghost()
-
-    @defer.inlineCallbacks
-    def tearDown(self):
-        from  twisted.internet.tcp import Server
-        del self.ghost
-        # webkit has the bad habbit on not closing the persistent
-        # connections, so we need to hack them away to make trial happy
-        for reader in reactor.getReaders():
-            if isinstance(reader, Server):
-                f = failure.Failure(Exception("test end"))
-                reader.connectionLost(f)
-        yield self.master.www.stopService()
-
-    @defer.inlineCallbacks
-    def doDohPageLoadRunnerTests(self, doh_tests="../../dojo/tests/colors"):
-        self.ghost.wait_timeout = 200
-        yield self.ghost.open(urljoin(self.url,"app/base/bb/tests/runner.html#"+doh_tests))
-        result_selector = "#testListContainer table tfoot tr.inProgress"
-        yield self.ghost.wait_for_selector(result_selector)
-        result, _ = self.ghost.evaluate("dojo.map(dojo.query('"+result_selector+" .failure'),function(a){return a.textContent;});")
-        errors, failures = map(int,result)
-        self.assertEqual(errors, 0,"there is at least one testsuite error")
-        self.assertEqual(failures, 0,"there is at least one testsuite failure")
-        result, _ = self.ghost.evaluate("dojo.map(dojo.query('"+result_selector+" td'),function(a){return a.textContent;});")
-        print "\n",str(result[1]).strip(),
