2bc2399d5ec2861ac8fda42402d73e55eedbb71a
Ludovic Chabant <ludovic@chabant.com>
2013-07-03 11:30:34 -0700

    Fixed P4 source unit tests on Windows
    This is because P4 normalizes slashes/backslashes, so we need to set our
    expectations right.

--- master/buildbot/test/unit/test_steps_source_p4.py
+++ master/buildbot/test/unit/test_steps_source_p4.py
@@ -23,7 +23,10 @@ from buildbot.test.util.properties import ConstantRenderable
 from buildbot.test.fake.remotecommand import ExpectShell, Expect
 from buildbot import config
 import textwrap
+import os.path
+import platform
 
+_is_windows = (platform.system() == 'Windows')
 
 class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
 
@@ -38,7 +41,10 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         self.build.getSourceStamp().revision = args.get('revision', None)
 
         # builddir propety used to create absolute path required in perforce client spec.
-        self.properties.setProperty('builddir', '/home/user/workspace', 'P4')
+        workspace_dir = '/home/user/workspace'
+        if _is_windows:
+            workspace_dir = r'C:\Users\username\Workspace'
+        self.properties.setProperty('builddir', workspace_dir, 'P4')
 
     def test_no_empty_step_config(self):
         self.assertRaises(config.ConfigErrors, lambda: P4())
@@ -80,6 +86,9 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
                           p4user='user', p4client='p4_client1', p4passwd='pass'),
                        dict(revision='100',))
 
+        root_dir = '/home/user/workspace/wkdir'
+        if _is_windows:
+            root_dir = r'C:\Users\username\Workspace\wkdir'
         client_spec = textwrap.dedent('''\
         Client: p4_client1
 
@@ -88,7 +97,7 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         Description:
         \tCreated by user
 
-        Root:\t/home/user/workspace/wkdir
+        Root:\t{0}
 
         Options:\tallwrite rmdir
 
@@ -96,7 +105,7 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
 
         View:
         \t//depot/trunk/... //p4_client1/...
-        ''');
+        '''.format(root_dir));
 
         self.expectCommands(
             ExpectShell(workdir='wkdir',  # defaults to this, only changes if it has a copy mode.
@@ -160,6 +169,9 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
                           p4base='//depot', p4branch='trunk',
                           p4user='user', p4client='p4_client1', p4passwd='pass'))
 
+        root_dir = '/home/user/workspace/wkdir'
+        if _is_windows:
+            root_dir = r'C:\Users\username\Workspace\wkdir'
         client_spec = textwrap.dedent('''\
         Client: p4_client1
 
@@ -168,7 +180,7 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         Description:
         \tCreated by user
 
-        Root:\t/home/user/workspace/wkdir
+        Root:\t{0}
 
         Options:\tallwrite rmdir
 
@@ -176,7 +188,7 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
 
         View:
         \t//depot/trunk/... //p4_client1/...
-        ''')
+        '''.format(root_dir))
         self._incremental(client_stdin=client_spec)
 
     def test_mode_incremental_p4base_with_p4extra_views(self):
@@ -185,6 +197,10 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
                           p4extra_views=[('-//depot/trunk/test', 'test'),
                                          ('-//depot/trunk/doc', 'doc')],
                           p4user='user', p4client='p4_client1', p4passwd='pass'))
+
+        root_dir = '/home/user/workspace/wkdir'
+        if _is_windows:
+            root_dir = r'C:\Users\username\Workspace\wkdir'
         client_spec = textwrap.dedent('''\
         Client: p4_client1
 
@@ -193,7 +209,7 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         Description:
         \tCreated by user
 
-        Root:\t/home/user/workspace/wkdir
+        Root:\t{0}
 
         Options:\tallwrite rmdir
 
@@ -203,13 +219,17 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         \t//depot/trunk/... //p4_client1/...
         \t-//depot/trunk/test/... //p4_client1/test/...
         \t-//depot/trunk/doc/... //p4_client1/doc/...
-        ''')
+        '''.format(root_dir))
         self._incremental(client_stdin=client_spec)
 
     def test_mode_incremental_p4viewspec(self):
         self.setupStep(P4(p4port='localhost:12000', mode='incremental',
                           p4viewspec=[('//depot/trunk/', '')],
                           p4user='user', p4client='p4_client1', p4passwd='pass'))
+
+        root_dir = '/home/user/workspace/wkdir'
+        if _is_windows:
+            root_dir = r'C:\Users\username\Workspace\wkdir'
         client_spec = textwrap.dedent('''\
         Client: p4_client1
 
@@ -218,7 +238,7 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         Description:
         \tCreated by user
 
-        Root:\t/home/user/workspace/wkdir
+        Root:\t{0}
 
         Options:\tallwrite rmdir
 
@@ -226,7 +246,7 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
 
         View:
         \t//depot/trunk/... //p4_client1/...
-        ''')
+        '''.format(root_dir))
         self._incremental(client_stdin=client_spec)
 
     def _full(self, client_stdin='', p4client='p4_client1', p4user='user'):
@@ -272,6 +292,9 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
                mode='full', p4base='//depot', p4branch='trunk',
                p4user='user', p4client='p4_client1', p4passwd='pass'))
 
+        root_dir = '/home/user/workspace/wkdir'
+        if _is_windows:
+            root_dir = r'C:\Users\username\Workspace\wkdir'
         client_stdin = textwrap.dedent('''\
         Client: p4_client1
 
@@ -280,14 +303,14 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         Description:
         \tCreated by user
 
-        Root:\t/home/user/workspace/wkdir
+        Root:\t{0}
 
         Options:\tallwrite rmdir
 
         LineEnd:\tlocal
 
         View:
-        \t//depot/trunk/... //p4_client1/...\n''')
+        \t//depot/trunk/... //p4_client1/...\n'''.format(root_dir))
         self._full(client_stdin=client_stdin)
 
     def test_mode_full_p4viewspec(self):
@@ -295,6 +318,10 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
             P4(p4port='localhost:12000',
                mode='full', p4viewspec=[('//depot/main/', '')],
                p4user='user', p4client='p4_client1', p4passwd='pass'))
+
+        root_dir = '/home/user/workspace/wkdir'
+        if _is_windows:
+            root_dir = r'C:\Users\username\Workspace\wkdir'
         client_stdin = textwrap.dedent('''\
         Client: p4_client1
 
@@ -303,14 +330,14 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         Description:
         \tCreated by user
 
-        Root:\t/home/user/workspace/wkdir
+        Root:\t{0}
 
         Options:\tallwrite rmdir
 
         LineEnd:\tlocal
 
         View:
-        \t//depot/main/... //p4_client1/...\n''')
+        \t//depot/main/... //p4_client1/...\n'''.format(root_dir))
 
         self._full(client_stdin=client_stdin)
 
@@ -322,6 +349,9 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
                p4branch='release/1.0', p4user='user', p4client='p4_client2',
                p4passwd='pass'))
 
+        root_dir = '/home/user/workspace/wkdir'
+        if _is_windows:
+            root_dir = r'C:\Users\username\Workspace\wkdir'
         client_stdin = textwrap.dedent('''\
         Client: p4_client2
 
@@ -330,14 +360,14 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         Description:
         \tCreated by user
 
-        Root:\t/home/user/workspace/wkdir
+        Root:\t{0}
 
         Options:\tallwrite rmdir
 
         LineEnd:\tlocal
 
         View:
-        \t//depot/release/1.0/... //p4_client2/...\n''')
+        \t//depot/release/1.0/... //p4_client2/...\n'''.format(root_dir))
 
         self._full(client_stdin=client_stdin, p4client='p4_client2')
 
@@ -349,6 +379,9 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
                p4user='user', p4client=ConstantRenderable('p4_client_render'),
                p4passwd='pass'))
 
+        root_dir = '/home/user/workspace/wkdir'
+        if _is_windows:
+            root_dir = r'C:\Users\username\Workspace\wkdir'
         client_stdin = textwrap.dedent('''\
         Client: p4_client_render
 
@@ -357,14 +390,14 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         Description:
         \tCreated by user
 
-        Root:\t/home/user/workspace/wkdir
+        Root:\t{0}
 
         Options:\tallwrite rmdir
 
         LineEnd:\tlocal
 
         View:
-        \t//depot/trunk/... //p4_client_render/...\n''')
+        \t//depot/trunk/... //p4_client_render/...\n'''.format(root_dir))
 
         self._full(client_stdin=client_stdin, p4client='p4_client_render')
 
@@ -376,6 +409,9 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
                p4branch=ConstantRenderable('render_branch'),
                p4user='user', p4client='p4_client1', p4passwd='pass'))
 
+        root_dir = '/home/user/workspace/wkdir'
+        if _is_windows:
+            root_dir = r'C:\Users\username\Workspace\wkdir'
         client_stdin = textwrap.dedent('''\
         Client: p4_client1
 
@@ -384,14 +420,14 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         Description:
         \tCreated by user
 
-        Root:\t/home/user/workspace/wkdir
+        Root:\t{0}
 
         Options:\tallwrite rmdir
 
         LineEnd:\tlocal
 
         View:
-        \t//depot/render_branch/... //p4_client1/...\n''')
+        \t//depot/render_branch/... //p4_client1/...\n'''.format(root_dir))
 
         self._full(client_stdin=client_stdin)
 
@@ -403,6 +439,9 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
                p4user='different_user', p4client='p4_client1',
                p4passwd='pass'))
 
+        root_dir = '/home/user/workspace/wkdir'
+        if _is_windows:
+            root_dir = r'C:\Users\username\Workspace\wkdir'
         client_stdin = textwrap.dedent('''\
         Client: p4_client1
 
@@ -411,13 +450,13 @@ class TestP4(sourcesteps.SourceStepMixin, unittest.TestCase):
         Description:
         \tCreated by different_user
 
-        Root:\t/home/user/workspace/wkdir
+        Root:\t{0}
 
         Options:\tallwrite rmdir
 
         LineEnd:\tlocal
 
         View:
-        \t//depot/render_trunk/... //p4_client1/...\n''')
+        \t//depot/render_trunk/... //p4_client1/...\n'''.format(root_dir))
 
         self._full(client_stdin=client_stdin, p4user='different_user')
