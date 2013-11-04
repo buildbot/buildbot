ba7dcc36f05be1e36e5a0da2aa7bb576bc16218a
Dustin J. Mitchell <dustin@mozilla.com>
2013-11-04 18:31:32 -0500

    Revert "set .master for buildsteps"
    This reverts commit 843ad6ad8a5656b3c8cef8cd415715a41ca6a1f4.

--- master/buildbot/process/buildstep.py
+++ master/buildbot/process/buildstep.py
@@ -512,7 +512,6 @@ class BuildStep(object, properties.PropertiesMixin):
 
         self._acquiringLock = None
         self.stopped = False
-        self.master = None
 
     def __new__(klass, *args, **kwargs):
         self = object.__new__(klass)
@@ -534,7 +533,6 @@ class BuildStep(object, properties.PropertiesMixin):
 
     def setBuild(self, build):
         self.build = build
-        self.master = self.build.master
 
     def setBuildSlave(self, buildslave):
         self.buildslave = buildslave
