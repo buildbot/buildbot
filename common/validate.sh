553a5277a4f484f35b1669d882ab5e295d7782e7
Dustin J. Mitchell <dustin@mozilla.com>
2013-11-06 20:16:28 -0500

    reorg

--- common/validate.sh
+++ common/validate.sh
@@ -98,27 +98,61 @@ for filename in ${py_files[@]}; do
 done
 $RES || warning "some import fixes failed -- not enforcing for now"
 
-statis "running autopep8"
+set -x
+status "running autopep8"
 if [[ -z `which autopep8` ]]; then
     warning "autopep8 is not installed"
+elif [[ ! -f common/pep8rc ]]; then
+    warning "common/pep8rc not found"
 else
+    changes_made=false
     for filename in ${py_files[@]}; do
-      if [[ -f common/pep8rc ]]; then
         LINEWIDTH=$(grep -E "max-line-length" common/pep8rc | sed 's/ //g' | cut -d'=' -f 2)
         # even if we dont enforce errors, if they can be fixed automatically, thats better..
         IGNORES=E501,W6
         # ignore is not None for SQLAlchemy code..
         if [[ "$filename" =~ "/db/" ]]; then
-          IGNORES=$IGNORES,E711,E712
+            IGNORES=$IGNORES,E711,E712
         fi
         autopep8 --in-place --max-line-length=$LINEWIDTH --ignore=$IGNORES "$filename"
-      else
-        warning "common/pep8rc not found"
-      fi
+        if ! git diff --quiet --exit-code "$filename"; then
+            changes_made=true
+        fi
     done
+    if ${changes_made}; then
+        not_ok "autopep8 made changes"
+    fi
 fi
 
-[[ `git diff --name-only HEAD | wc -l` -gt 0 ]] && not_ok "style fixes to be committed"
+status "running pep8"
+if [[ -z `which pep8` ]]; then
+    warning "pep8 is not installed"
+elif [[ ! -f common/pep8rc ]]; then
+    warning "common/pep8rc not found"
+else
+    pep8_ok=true
+    for filename in ${py_files[@]}; do
+        if ! pep8 --config=common/pep8rc "$filename"; then
+            pep8_ok=false
+        fi
+    done
+    $pep8_ok || not_ok "pep8 failed"
+fi
+
+status "running pylint"
+if [[ -z `which pylint` ]]; then
+    warning "pylint is not installed"
+elif [[ ! -f common/pylintrc ]]; then
+    warning "common/pylintrc not found"
+else
+    pylint_ok=true
+    for filename in ${py_files[@]}; do
+        if ! pylint --rcfile=common/pylintrc --disable=R,line-too-long --enable=W0611 --output-format=text --report=no "$filename"; then
+            pylint_ok=false
+        fi
+    done
+    $pylint_ok || not_ok "pylint failed"
+fi
 
 if git diff --name-only $REVRANGE | grep ^master/docs/ ; then
     status "building docs"
