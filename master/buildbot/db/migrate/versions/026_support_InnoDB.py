f7fce79cd11599daec9001821510437ddec024aa
maria marcano <mariangemarcano@gmail.com>
2014-01-16 15:01:18 +0100

    Creating the script to be able to switch to InnoDB

--- master/buildbot/db/migrate/versions/026_support_InnoDB.py
+++ master/buildbot/db/migrate/versions/026_support_InnoDB.py
@@ -47,6 +47,12 @@ def upgrade(migrate_engine):
         metadata=metadata,
         engine=migrate_engine)
 
+    changeset.alter_column(
+        sa.Column('name', sa.String(255), nullable=False),
+        table="object_state",
+        metadata=metadata,
+        engine=migrate_engine)
+
     migrate_engine.execute("Alter table buildrequests ENGINE=InnoDB")
     migrate_engine.execute("Alter table buildsets ENGINE=InnoDB")
     migrate_engine.execute("Alter table sourcestampsets ENGINE=InnoDB")
@@ -59,6 +65,14 @@ def upgrade(migrate_engine):
     migrate_engine.execute("Alter table change_properties ENGINE=InnoDB")
     migrate_engine.execute("Alter table change_users ENGINE=InnoDB")
     migrate_engine.execute("Alter table users ENGINE=InnoDB")
+    migrate_engine.execute("Alter table mastersconfig ENGINE=InnoDB")
+    migrate_engine.execute("Alter table migrate_version ENGINE=InnoDB")
+    migrate_engine.execute("Alter table object_state ENGINE=InnoDB")
+    migrate_engine.execute("Alter table users_info ENGINE=InnoDB")
+    migrate_engine.execute("Alter table patches ENGINE=InnoDB")
+    migrate_engine.execute("Alter table sourcestamps ENGINE=InnoDB")
+    migrate_engine.execute("Alter table sourcestamp_changes ENGINE=InnoDB")
+    migrate_engine.execute("Alter table scheduler_changes ENGINE=InnoDB")
 
     buildrequests_tbl = sa.Table('buildrequests', metadata, autoload=True)
     buildsets_tbl = sa.Table('buildsets', metadata, autoload=True)
@@ -72,68 +86,117 @@ def upgrade(migrate_engine):
     change_properties_tbl = sa.Table('change_properties', metadata, autoload=True)
     change_users_tbl = sa.Table('change_users', metadata, autoload=True)
     users_tbl = sa.Table('users', metadata, autoload=True)
+    mastersconfig_tbl = sa.Table('mastersconfig', metadata, autoload=True)
+    object_state_tbl = sa.Table('object_state', metadata, autoload=True)
+    users_info_tbl = sa.Table('users_info', metadata, autoload=True)
+    patches_tbl = sa.Table('patches', metadata, autoload=True)
+    sourcestamps_tbl = sa.Table('sourcestamps', metadata, autoload=True)
+    sourcestamp_changes_tbl = sa.Table('sourcestamp_changes', metadata, autoload=True)
+    scheduler_changes_tbl = sa.Table('scheduler_changes', metadata, autoload=True)
 
     # add missing FK  constraints
     # buildrequests table
     cons = constraint.ForeignKeyConstraint([buildrequests_tbl.c.mergebrid], [buildrequests_tbl.c.id])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
     cons = constraint.ForeignKeyConstraint([buildrequests_tbl.c.startbrid], [buildrequests_tbl.c.id])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
     cons = constraint.ForeignKeyConstraint([buildrequests_tbl.c.triggeredbybrid], [buildrequests_tbl.c.id])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
     cons = constraint.ForeignKeyConstraint([buildrequests_tbl.c.artifactbrid], [buildrequests_tbl.c.id])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
     cons = constraint.ForeignKeyConstraint([buildrequests_tbl.c.buildsetid], [buildsets_tbl.c.id])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
     # buildsets table
     cons = constraint.ForeignKeyConstraint([buildsets_tbl.c.sourcestampsetid], [sourcestampsets_tbl.c.id])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
     # buildrequest_claims table
     cons = constraint.ForeignKeyConstraint([buildrequest_claims_tbl.c.brid], [buildrequests_tbl.c.id])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
     cons = constraint.ForeignKeyConstraint([buildrequest_claims_tbl.c.objectid], [objects_tbl.c.id])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
     # builds table
     cons = constraint.ForeignKeyConstraint([builds_tbl.c.brid], [buildrequests_tbl.c.id])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
     # buildset_properties table
     cons = constraint.ForeignKeyConstraint([buildset_properties_tbl.c.buildsetid], [buildsets_tbl.c.id])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
     # change_files table
     cons = constraint.ForeignKeyConstraint([change_files_tbl.c.changeid], [changes_tbl.c.changeid])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
-    # change_files table
+    # change_properties table
     cons = constraint.ForeignKeyConstraint([change_properties_tbl.c.changeid], [changes_tbl.c.changeid])
-    # cons.drop()
+    cons.drop()
     cons.create()
 
     # change_users table
     cons = constraint.ForeignKeyConstraint([change_users_tbl.c.changeid], [changes_tbl.c.changeid])
-    #cons.drop()
+    cons.drop()
     cons.create()
 
     cons = constraint.ForeignKeyConstraint([change_users_tbl.c.uid], [users_tbl.c.uid])
-    #cons.drop()
+    cons.drop()
+    cons.create()
+
+    # mastersconfig table
+    cons = constraint.ForeignKeyConstraint([mastersconfig_tbl.c.objectid], [objects_tbl.c.id])
+    cons.drop()
+    cons.create()
+
+    # object_state table
+    cons = constraint.ForeignKeyConstraint([object_state_tbl.c.objectid], [objects_tbl.c.id])
+    cons.drop()
+    cons.create()
+
+    # users_info table
+    cons = constraint.ForeignKeyConstraint([users_info_tbl.c.uid], [users_tbl.c.uid])
+    cons.drop()
+    cons.create()
+
+    # sourcestamps table
+    cons = constraint.ForeignKeyConstraint([sourcestamps_tbl.c.patchid], [patches_tbl.c.id])
+    cons.drop()
+    cons.create()
+
+    cons = constraint.ForeignKeyConstraint([sourcestamps_tbl.c.sourcestampsetid], [sourcestampsets_tbl.c.id])
+    cons.drop()
+    cons.create()
+
+    # sourcestamp_changes table
+    cons = constraint.ForeignKeyConstraint([sourcestamp_changes_tbl.c.sourcestampid], [sourcestamps_tbl.c.id])
+    # cons.drop()
+    cons.create()
+
+    cons = constraint.ForeignKeyConstraint([sourcestamp_changes_tbl.c.changeid], [changes_tbl.c.changeid])
+    # cons.drop()
+    cons.create()
+
+    # scheduler_changes table
+    cons = constraint.ForeignKeyConstraint([scheduler_changes_tbl.c.objectid], [objects_tbl.c.id])
+    # cons.drop()
+    cons.create()
+
+    cons = constraint.ForeignKeyConstraint([scheduler_changes_tbl.c.changeid], [changes_tbl.c.changeid])
+    # cons.drop()
     cons.create()
