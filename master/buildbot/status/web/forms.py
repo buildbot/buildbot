384eb0b7ac6bc13cfd8aef65c9d74b3b722f8679
Daniel Hobley <danielh@unity3d.com>
2014-02-04 14:43:29 +0100

    Fixed the visual aspect of branches in the force build popup when the URL has encoded values in it

--- master/buildbot/status/web/forms.py
+++ master/buildbot/status/web/forms.py
@@ -12,6 +12,7 @@
 # Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 # Copyright Buildbot Team Members
+import urllib
 from twisted.web._responses import INTERNAL_SERVER_ERROR
 from twisted.web.resource import ErrorPage
 
@@ -38,10 +39,17 @@ class ForceBuildDialogPage(HtmlResource):
     def content(self, request, cxt):
         status = self.getStatus(request)
 
+        args = request.args.copy()
+
+        # decode all of the args
+        encoding = getRequestCharset(request)
+        for name, argl in args.iteritems():
+            args[name] = [ urllib.unquote(arg).decode(encoding) for arg in argl ]
+
         #Get builder info
         builder_status = None
-        if request.args.has_key("builder_name") and len(request.args["builder_name"]) == 1:
-            builder_status = status.getBuilder(request.args["builder_name"][0])
+        if args.has_key("builder_name") and len(args["builder_name"]) == 1:
+            builder_status = status.getBuilder(args["builder_name"][0])
             bm = self.getBuildmaster(request)
             builder = None
             for b in bm.botmaster.getBuilders():
@@ -58,19 +66,19 @@ class ForceBuildDialogPage(HtmlResource):
 
             #Get branches
             encoding = getRequestCharset(request)
-            branches = [ b.decode(encoding) for b in request.args.get("branch", []) if b ]
+            branches = [ b.decode(encoding) for b in args.get("branch", []) if b ]
             cxt['branches'] = branches
 
             # Add codebase info
             codebases = {}
             codebases_arg = getCodebasesArg(request=request, codebases=codebases)
-            cxt['codebases_arg'] = codebases_arg
+            cxt['codebases_arg'] = codebases_arg.encode(encoding)
 
             return_page = ""
-            if request.args.has_key("returnpage"):
-                return_page = request.args['returnpage']
+            if args.has_key("returnpage"):
+                return_page = args['returnpage']
                 if not isinstance(return_page, basestring):
-                    return_page = request.args['returnpage'][0]
+                    return_page = args['returnpage'][0]
 
                 if len(codebases_arg) > 0:
                     return_page = "&returnpage={0}".format(return_page)
@@ -82,8 +90,10 @@ class ForceBuildDialogPage(HtmlResource):
 
             #Add scheduler info
             buildForceContext(cxt, request, self.getBuildmaster(request), builder_status.getName())
-            cxt['fbuildsch'] = request.args
-            cxt['rt_update'] = request.args
+
+            cxt['fbuildsch'] = args
+            cxt['rt_update'] = args
+            request.args = args
 
             template = request.site.buildbot_service.templates.get_template("force_build_dialog.html")
             return template.render(**cxt)
