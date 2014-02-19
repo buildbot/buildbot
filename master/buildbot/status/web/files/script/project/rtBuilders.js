928092de9e95f0ebd41b1e0187a14a75f6c4faac
Daniel Hobley <danielh@unity3d.com>
2014-02-19 12:14:29 +0100

    Implemented first step of DRY for the realtimePages

--- master/buildbot/status/web/files/script/project/rtBuilders.js
+++ master/buildbot/status/web/files/script/project/rtBuilders.js
@@ -4,88 +4,9 @@ define(['jquery', 'project/realtimePages', 'helpers'], function ($, realtimePage
     
     rtBuilders = {
         init: function () {
-			
-			
-        	// Creating a new websocket
-	                
-	        // Global websocketvariabel        
-	        window.sock = null;
-
-	        var wsuri;
-
-	        var realTimeServer =  $('body').attr('data-realTimeServer');
-	         
-         	wsuri = realTimeServer;
-
-         	console.log($('body').attr('data-realTimeServer'));
-
-	         if ("WebSocket" in window) {
-	         	sock = new WebSocket(wsuri);
-	         } else if ("MozWebSocket" in window) {
-	         	sock = new MozWebSocket(wsuri);
-	         } else {
-	             log("Browser does not support WebSocket!");
-	             window.location = "http://autobahn.ws/unsupportedbrowser";
-	         }
-
-	         // if the socket connection is success
-	         if (sock) {
-	             sock.onopen = function() {
-	             	$('#bowlG').remove();
-		             // get the json url to parse
-		             console.log(helpers.getJsonUrl())
-		             broadcast(helpers.getJsonUrl());
-	         	    log("Connected to " + wsuri);
-	             }
-
-	             // when the connection closes
-	             sock.onclose = function(e) {
-	                 log("Connection closed (wasClean = " + e.wasClean + ", code = " + e.code + ", reason = '" + e.reason + "')");
-	                 sock = null;
-	                 console.log('closed')
-	             }
-
-	             // when the client recieves a message
-	             sock.onmessage = function(e) {
-	         		log(e.data);
-	             }
-	         }
-	        
-	        // send a message to the server
-	         function broadcast(msg) {
-	             if (sock) {
-	             	sock.send(msg);
-	            	 //log("Sent: " + msg);
-	             }		         
-	         };	        
-/*
-	         var oTable = $('.tablesorter-js').dataTable();
-	         /*
-			 $('.tablesorter-js').bind('sort', function () { 
-			 	//eventFired('Sort'); 	 	
-
-			 }).dataTable();
-			
-			function returnTr () {
-				 
-			}
-			
-			 $('.tablesorter-js tbody tr').click( function () {
-			  		var pos = oTable.fnGetPosition(this) 					
- 					return pos
-			  });
-				*/
-	        // send messages from the server to be parsed
-
-	        
-	        
-	        var tableRowList = $('.tablesorter-js tbody > tr');	        
-			//var diffTime = moment(moment(1390310146.791877).diff(1390309386.271075)).utc().format('mm hh SS');
-			//console.log(diffTime)
-	        function log(m) {											
-				realtimePages.buildersPage(m, tableRowList);	
-	        };
+            realtimePages.initRealtime(realtimePages.buildersPage);
         }
-    }
+    };
+
     return rtBuilders;
 });
--- master/buildbot/status/web/files/script/project/rtBuildqueue.js
+++ master/buildbot/status/web/files/script/project/rtBuildqueue.js
@@ -1,72 +1,11 @@
 define(['jquery', 'realtimePages', 'helpers'], function ($, realtimePages, helpers) {
          "use strict";
-    var rtBuildqueue;
-    
-    rtBuildqueue = {
-        init: function () {        	
-        	
-        	// Creating a new websocket
-	                
-	        // Global websocketvariabel        
-	        window.sock = null;
 
-	        var wsuri;
-
-	        var realTimeServer =  $('body').attr('data-realTimeServer');
-	         
-         	wsuri = realTimeServer;
-         		
-        	//tbsorter.fnAddData();						
-
-         	console.log($('body').attr('data-realTimeServer'));
-         	
-	         if ("WebSocket" in window) {
-	         	sock = new WebSocket(wsuri);
-	         } else if ("MozWebSocket" in window) {
-	         	sock = new MozWebSocket(wsuri);
-	         } else {
-	             log("Browser does not support WebSocket!");
-	             window.location = "http://autobahn.ws/unsupportedbrowser";
-	         }
-
-	         // if the socket connection is success
-	         if (sock) {
-	             sock.onopen = function() {
-	             	 $('#bowlG').remove();	  
-		             // get the json url to parse		             
-		             console.log(helpers.getJsonUrl())
-		             broadcast(helpers.getJsonUrl());
-	         	    log("Connected to " + wsuri);
-	             }
-
-	             // when the connection closes
-	             sock.onclose = function(e) {
-	                 log("Connection closed (wasClean = " + e.wasClean + ", code = " + e.code + ", reason = '" + e.reason + "')");
-	                 sock = null;
-	                 console.log('closed')
-	             }
-
-	             // when the client recieves a message
-	             sock.onmessage = function(e) {	  
-	                             	
-	         		log(e.data);
-	             }
-	         }
-	        
-	        // send a message to the server
-	         function broadcast(msg) {
-	             if (sock) {
-	             	sock.send(msg);
-	            	 //log("Sent: " + msg);
-	             }		         
-	         };	        
-	         		
-
-	        // send messages from the server to be parsed		
-	        function log(m) {											
-				realtimePages.rtBuildqueue(m);	
-	        };
+    var rtBuildqueue = {
+        init: function () {
+            realtimePages.initRealtime(realtimePages.rtBuildqueue);
         }
-    }
+    };
+
     return rtBuildqueue;
 });
