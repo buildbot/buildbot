6d441b2f9aedefde038f7c0bebbb5823809f13b9
Daniel Hobley <danielh@unity3d.com>
2014-03-06 13:59:16 +0100

    Updated production scripts

---
+++ master/buildbot/status/web/files/prod/script/project/extendMoment.js
@@ -0,0 +1,1 @@
+define(["moment"],function(e){var t;return t={init:function(){e.lang("progress-bar-en",{relativeTime:{future:"ETA: %s",past:"Overtime: %s",s:"%d seconds",m:"%d minute",mm:"%d minutes",h:"%d hour",hh:"%d hours",d:"a day",dd:"%d days",M:"a month",MM:"%d months",y:"a year",yy:"%d years"}}),e.lang("en")}},t});
