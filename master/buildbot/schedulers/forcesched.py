89eafe5e18bf519dd1f036909d24454cdeaa2c93
madazone <yasser.lebon@gmail.com>
2014-03-07 23:33:53 +0100

    Parameterize ForceScheduler button name

--- master/buildbot/schedulers/forcesched.py
+++ master/buildbot/schedulers/forcesched.py
@@ -481,6 +481,7 @@ class ForceScheduler(base.BaseScheduler):
                  username=UserNameParameter(),
                  reason=StringParameter(name="reason", default="force build", size=20),
                  reasonString="A build was forced by '%(owner)s': %(reason)s",
+                 buttonName="Force Build",
                  codebases=None,
 
                  properties=[
@@ -605,6 +606,7 @@ class ForceScheduler(base.BaseScheduler):
         self.all_fields.extend(self.forcedProperties)
 
         self.reasonString = reasonString
+        self.buttonName = buttonName
 
     def checkIfType(self, obj, chkType):
         return isinstance(obj, chkType)
