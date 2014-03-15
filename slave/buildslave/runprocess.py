91d92d2a537742c55152a78d562adec5da1f4810
Роман Донченко <dpb@corrigendum.ru>
2014-03-16 00:40:13 +0400

    Use win32_batch_quote in shell_quote

--- slave/buildslave/runprocess.py
+++ slave/buildslave/runprocess.py
@@ -63,18 +63,16 @@ def win32_batch_quote(cmd_list):
 
 def shell_quote(cmd_list):
     # attempt to quote cmd_list such that a shell will properly re-interpret
-    # it.  The pipes module is only available on UNIX, and Windows "shell"
-    # quoting is indescribably convoluted - so much so that it's not clear it's
-    # reversible.  Also, the quote function is undocumented (although it looks
-    # like it will be documented soon: http://bugs.python.org/issue9723).
-    # Finally, it has a nasty bug in some versions where an empty string is not
-    # quoted.
+    # it.  The pipes module is only available on UNIX; also, the quote
+    # function is undocumented (although it looks like it will be documented
+    # soon: http://bugs.python.org/issue9723). Finally, it has a nasty bug
+    # in some versions where an empty string is not quoted.
     #
     # So:
     #  - use pipes.quote on UNIX, handling '' as a special case
-    #  - use Python's repr() on Windows, as a best effort
+    #  - use our own custom function on Windows
     if runtime.platformType == 'win32':
-        return " ".join([repr(e) for e in cmd_list])
+        return win32_batch_quote(cmd_list)
     else:
         import pipes
 
