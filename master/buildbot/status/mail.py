2f360b7b5b7c0c88b1a096c30e611491a88a62e0
Varun Sharma <varunsharmalive@gmail.com>
2014-03-17 00:58:51 +0530

    added force mode issue #956 on buildbot tracker

--- master/buildbot/status/mail.py
+++ master/buildbot/status/mail.py
@@ -245,7 +245,8 @@ class MailNotifier(base.StatusReceiverMultiService):
                      "subject", "sendToInterestedUsers", "customMesg",
                      "messageFormatter", "extraHeaders"]
 
-    possible_modes = ("change", "failing", "passing", "problem", "warnings", "exception")
+    possible_modes = ("change", "failing", "passing", "problem", "warnings",
+                      "exception", "force")
 
     def __init__(self, fromaddr, mode=("failing", "passing", "warnings"),
                  categories=None, builders=None, addLogs=False,
@@ -290,6 +291,7 @@ class MailNotifier(base.StatusReceiverMultiService):
                                   when the previous build passed
                      - "warnings": send mail if a build contain warnings
                      - "exception": send mail if a build fails due to an exception
+                     - "force": send mail to the user who forces a build.
                      - "all": always send mail
                      Defaults to ("failing", "passing", "warnings").
 
@@ -398,6 +400,8 @@ class MailNotifier(base.StatusReceiverMultiService):
                 mode = ("failing", "passing", "warnings", "exception")
             elif mode == "warnings":
                 mode = ("failing", "warnings")
+            elif mode == "force":
+                mode = ("force",)
             else:
                 mode = (mode,)
         for m in mode:
@@ -506,6 +510,8 @@ class MailNotifier(base.StatusReceiverMultiService):
             return False  # ignore this build
 
         prev = self.getPreviousBuild(build)
+        if "force" in self.mode:
+            return True
         if "change" in self.mode:
             if prev and prev.getResults() != results:
                 return True
