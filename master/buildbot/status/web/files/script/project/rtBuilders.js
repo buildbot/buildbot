487209169f10813ce4ed77e837669c20d57a2f72
Daniel Hobley <danielh@unity3d.com>
2014-04-03 14:47:40 +0200

    Fixed pending build links with slashes in the builder names

--- master/buildbot/status/web/files/script/project/rtBuilders.js
+++ master/buildbot/status/web/files/script/project/rtBuilders.js
@@ -58,7 +58,7 @@ define(['jquery', 'realtimePages', 'helpers', 'dataTables', 'mustache', 'libs/jq
                             (type.currentBuilds === undefined || type.currentBuilds == 0)) {
                             noJobs = true;
                         }
-                        return mustache.render(builders, {showNoJobs: noJobs, pendingBuilds: type.pendingBuilds, currentBuilds: type.currentBuilds, builderName: type.name, projects: type.project});
+                        return mustache.render(builders, {showNoJobs: noJobs, pendingBuilds: type.pendingBuilds, currentBuilds: type.currentBuilds, builder_url: type.url});
                     },
                     "fnCreatedCell": function (nTd, sData, oData) {
                         if (oData.currentBuilds != undefined) {
