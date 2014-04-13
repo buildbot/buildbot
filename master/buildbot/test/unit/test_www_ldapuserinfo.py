e9af4078e73972581e226b9da685f6115f403fb9
Dustin J. Mitchell <dustin@buildbot.net>
2014-04-13 16:51:08 -0400

    update ldapuserinfo tests

--- master/buildbot/test/unit/test_www_ldapuserinfo.py
+++ master/buildbot/test/unit/test_www_ldapuserinfo.py
@@ -14,27 +14,37 @@
 # Copyright Buildbot Team Members
 
 import mock
+import new
 import sys
 
 from twisted.internet import defer
 from twisted.trial import unittest
 
+fake_ldap = new.module('ldap')
+fake_ldap.SCOPE_SUBTREE = 2
+with mock.patch.dict(sys.modules, {'ldap': fake_ldap}):
+    from buildbot.www import ldapuserinfo
+
 
 class FakeLdap(object):
-    initialize = mock.Mock()
-    simple_bind_s = mock.Mock()
-    search_s = mock.Mock()
-    SCOPE_SUBTREE = 2
+
+    def __init__(self):
+        def simple_bind_s(who, cred):
+            pass
+        self.simple_bind_s = mock.Mock(spec=simple_bind_s)
+
+        def search_s(base, scope, filterstr='f', attrlist=None, attrsonly=0):
+            pass
+        self.search_s = mock.Mock(spec=search_s)
 
 
 class LdapUserInfo(unittest.TestCase):
     # we completetly fake the python-ldap module, so no need to require
     # it to run the unit tests
+
     def setUp(self):
-        self.oldldap = sys.modules.get("ldap", None)
         self.ldap = FakeLdap()
-        sys.modules["ldap"] = self.ldap
-        from buildbot.www import ldapuserinfo
+        fake_ldap.initialize = lambda uri: self.ldap
 
         self.userInfoProvider = ldapuserinfo.LdapUserInfo(
             uri="ldap://uri", bind_user="user", bind_pw="pass",
@@ -46,34 +56,27 @@ class LdapUserInfo(unittest.TestCase):
             avatarPattern="avatar",
             avatarData="picture",
             accountExtraFields=["myfield"])
-        self.ldap.initialize = mock.Mock(side_effect=[self.ldap])
-        self.userInfoProvider.getLdap = lambda: self.ldap
-
-    def tearDown(self):
-        if self.oldldap is None:
-            del sys.modules["ldap"]
-        else:
-            sys.modules["ldap"] = self.oldldap
 
     @defer.inlineCallbacks
     def test_updateUserInfoNoResults(self):
-        self.ldap.search_s = mock.Mock(side_effect=
-                                       [[], [], []])
+        self.ldap.search_s.side_effect = [[], [], []]
         try:
             yield self.userInfoProvider.getUserInfo("me")
         except KeyError, e:
-            self.assertEqual(repr(e), "KeyError('ldap search \"accpattern\" returned 0 results',)")
+            self.assertEqual(
+                repr(e), "KeyError('ldap search \"accpattern\" returned 0 results',)")
         else:
             self.fail("should have raised a key error")
 
     @defer.inlineCallbacks
     def test_updateUserInfoNoGroups(self):
-        self.ldap.search_s = mock.Mock(side_effect=
-                                       [[("cn", {"accountFullName": ["me too"],
-                                                 "accountEmail": ["mee@too"]})], [], []])
+        self.ldap.search_s.side_effect = [[(
+            "cn", {"accountFullName": ["me too"],
+                   "accountEmail": ["mee@too"]})], [], []]
         res = yield self.userInfoProvider.getUserInfo("me")
         self.assertEqual(self.ldap.search_s.call_args_list, [
-            (('accbase', 2, 'accpattern', ['accountEmail', 'accountFullName', 'dn', 'myfield']), {}),
+            (('accbase', 2, 'accpattern',
+             ['accountEmail', 'accountFullName', 'dn', 'myfield']), {}),
             (('groupbase', 2, 'groupMemberPattern', ['groupName']), {}),
         ])
         self.assertEqual(res, {'email': 'mee@too', 'full_name': 'me too',
@@ -93,9 +96,8 @@ class LdapUserInfo(unittest.TestCase):
 
     @defer.inlineCallbacks
     def test_getUserAvatar(self):
-        self.ldap.search_s = mock.Mock(side_effect=
-                                       [[("cn", {"picture": ["\x89PNG lljklj"]})],
-                                        ])
+        self.ldap.search_s.side_effect = [
+            [("cn", {"picture": ["\x89PNG lljklj"]})]]
         res = yield self.userInfoProvider.getUserAvatar("me", 21, None)
         self.assertEqual(self.ldap.search_s.call_args_list, [
             (('accbase', 2, 'avatar', ['picture']), {}),
