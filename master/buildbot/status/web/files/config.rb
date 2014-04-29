fc3495448bdf20e003a81c311041c58471d66120
Simon Holm Nielsen <simon@unity3d.com>
2014-04-29 12:21:41 +0200

    Font face helper

--- master/buildbot/status/web/files/config.rb
+++ master/buildbot/status/web/files/config.rb
@@ -7,6 +7,7 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "script"
+fonts_dir = "fonts"
 
 
 # You can select your preferred output style here (can be overridden via the command line):
