0232c1d8d566f61e183bdfbeb468fdf4959faf47
Daniel Hobley <danielh@unity3d.com>
2014-06-10 10:09:13 +0200

    Fixed a bug where custom page JS was not being loaded correctly
    - Moved testresults-common into the project folder with all other JS files

--- master/buildbot/status/web/files/prod/script/project/buildLog.js.src.js
+++ master/buildbot/status/web/files/prod/script/project/buildLog.js.src.js
@@ -1,48 +1,50 @@
-/*global define*/
-define(["jquery", "helpers", "iFrameResize"], function ($) {
-
+/*global define, require*/
+define(["main"], function () {
     
 
-    function maybeScroll(checked) {
-        if (checked) {
-            setTimeout(function () {
-                window.scrollTo(0, document.body.scrollHeight);
-            }, 300);
+    require(["jquery", "helpers", "iFrameResize"], function ($) {
+
+        function maybeScroll(checked) {
+            if (checked) {
+                setTimeout(function () {
+                    window.scrollTo(0, document.body.scrollHeight);
+                }, 300);
+            }
         }
-    }
 
-    $(document).ready(function () {
+        $(document).ready(function () {
 
-        var $iFrame = $("#logIFrame"),
-            $scrollOpt = $("#scrollOpt"),
-            hasPressed = false;
+            var $iFrame = $("#logIFrame"),
+                $scrollOpt = $("#scrollOpt"),
+                hasPressed = false;
 
-        //Start auto resizer
-        $iFrame.iFrameResize({
-            "autoResize": true,
-            "sizeWidth": false,
-            "resizedCallback": function () {
-                maybeScroll($scrollOpt.prop("checked"));
-            }
-        });
-
-        $scrollOpt.click(function () {
-            window.scrollTo(0, document.body.scrollHeight);
-        });
-
-        $(document).keyup(function (event) {
-            if (event.which === 83) {
-                if (hasPressed === false) {
-                    var checked = !$scrollOpt.prop("checked");
-                    hasPressed = true;
-                    $scrollOpt.prop("checked", checked);
-                    maybeScroll(checked);
-                    setTimeout(function () {
-                        hasPressed = false;
-                    }, 300);
+            //Start auto resizer
+            $iFrame.iFrameResize({
+                "autoResize": true,
+                "sizeWidth": false,
+                "resizedCallback": function () {
+                    maybeScroll($scrollOpt.prop("checked"));
                 }
-            }
-        });
-    });
+            });
 
+            $scrollOpt.click(function () {
+                window.scrollTo(0, document.body.scrollHeight);
+            });
+
+            $(document).keyup(function (event) {
+                if (event.which === 83) {
+                    if (hasPressed === false) {
+                        var checked = !$scrollOpt.prop("checked");
+                        hasPressed = true;
+                        $scrollOpt.prop("checked", checked);
+                        maybeScroll(checked);
+                        setTimeout(function () {
+                            hasPressed = false;
+                        }, 300);
+                    }
+                }
+            });
+        });
+
+    });
 });
