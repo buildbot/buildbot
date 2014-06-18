19ad724fc59a083e9f7fc55fee0b780c225282c7
Daniel Hobley <danielh@unity3d.com>
2014-06-18 09:37:43 +0200

    Added failing test to test on katana

---
+++ master/buildbot/status/web/files/script/test/test_timeElements.js
@@ -0,0 +1,10 @@
+/*global define, describe, it, expect, beforeEach, afterEach*/
+define(["jquery"], function ($) {
+    "use strict";
+
+    describe("Time elements", function () {
+        it("should fail", function () {
+            expect(0).toEqual(1);
+        });
+    });
+});
