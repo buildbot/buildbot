85578fe638da9596d1840e66e9ec427af1035bd6
Mikhail Sobolev <mss@mawhrin.net>
2014-07-19 05:11:03 +0300

    add a decorator for todo tests

---
+++ master/buildbot/test/util/todo.py
@@ -0,0 +1,30 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+"""
+Mark a test as a todo one
+"""
+
+
+def todo(message):
+    """
+    decorator to mark a todo test
+    """
+    def wrap(func):
+        """
+        just mark the test
+        """
+        func.todo = message
+        return func
+    return wrap
