2dafe0f6d29000f56a22a69e08beaa1ca20b150b
Pierre Tardy <pierre.tardy@intel.com>
2014-08-06 22:16:24 +0200

    Simplify setup.py
    Current version is far too much boilerplate for a plugin
    Its obsolete with the change to gulp, and has proven to be very unreliable.
    
    We need to find something more simple that does not feel like black magic
    
    Delete old sample_plugin. It is obsolete, now we have good examples
    with waterfall and console
    
    Signed-off-by: Pierre Tardy <pierre.tardy@intel.com>

--- www/codeparameter/setup.py
+++ www/codeparameter/setup.py
@@ -16,129 +16,38 @@
 # Copyright Buildbot Team Members
 
 import os
-import setuptools.command.develop
-import setuptools.command.install
-import setuptools.command.sdist
-import subprocess
 
-from distutils.core import Command
 from setuptools import setup
 
-# This script can run either in a source checkout (e.g., to 'setup.py sdist')
-# or in an sdist tarball (to install)
+# For now, we only support develop, and manual npm install
+# plan is to use wheel for binary distribution: http://wheel.readthedocs.org/en/latest/
+
 MODE = 'SRC' if os.path.isdir('src') else 'SDIST'
 
-version = "0.1.0"
+if MODE == 'SRC':
+    # if we're in a source tree, use master's __init__ to determine the
+    # version, and update the version file as a by-product
+    master_init = os.path.abspath("../../master/buildbot/__init__.py")
+    globals = {"__file__": master_init}
+    execfile(master_init, globals)
+    version = globals['version']
+    open('VERSION', 'w').write(version + '\n')
+else:
+    # otherwise, use what the build left us
+    version = open('VERSION').read().strip()
 
-# command classes
-
-cmdclass = {}
-
-
-class buildjs(Command):
-    descripion = "Run everything needed to build frontend"
-
-    def initialize_options(self):
-        self.bindir = None
-
-    def finalize_options(self):
-        pass
-
-    def get_bindir(self):
-        if not self.bindir:
-            p = subprocess.Popen(["npm", "bin"], stdout=subprocess.PIPE)
-            self.bindir = p.communicate()[0].strip()
-        return self.bindir
-
-    def run(self):
-        # bail out if we're not in SRC mode, since buildjs won't work from
-        # an sdist tarball
-        if MODE != 'SRC':
-            return
-
-        os.environ['PATH'] = os.environ['PATH'] + ":" + self.get_bindir()
-        self.spawn(['npm', 'install'])
-        self.spawn(['bower', 'install'])
-        self.spawn(['gulp', 'prod'])
-
-cmdclass['buildjs'] = buildjs
-
-
-class install_www(Command):
-
-    description = "install WWW files"
-
-    def run(self):
-        cmd = self.get_finalized_command('install')
-        self.install_dir = os.path.join(cmd.install_lib, 'buildbot_www')
-        out = self.copy_tree('buildbot_www', self.install_dir)
-        self.outfiles.extend(out)
-        (out, _) = self.copy_file('VERSION', self.install_dir)
-        self.outfiles.append(out)
-
-    def get_outputs(self):
-        return self.outfiles
-
-cmdclass['install_www'] = install_www
-
-
-class sdist(setuptools.command.sdist.sdist):
-
-    """
-    Customize sdist to run buildjs first
-    """
-
-    def run(self):
-        if MODE == 'SRC':
-            self.run_command('buildjs')
-        setuptools.command.sdist.sdist.run(self)
-
-cmdclass['sdist'] = sdist
-
-
-class install(setuptools.command.install.install):
-
-    """
-    Customize install to run buildjs first, and to run install_js after.
-    """
-
-    sub_commands = setuptools.command.install.install.sub_commands + [
-        ('install_www', None)
-    ]
-
-    def run(self):
-        if MODE == 'SRC':
-            self.run_command('buildjs')
-        setuptools.command.install.install.run(self)
-
-cmdclass['install'] = install
-
-
-class develop(setuptools.command.develop.develop):
-
-    """
-    Customize develop to run npm/bower install.
-    """
-
-    def run(self):
-        if MODE == 'SRC':
-            self.run_command('buildjs')
-        setuptools.command.develop.develop.run(self)
-
-cmdclass['develop'] = develop
 
 setup(
     name='buildbot-codeparameter',
     version=version,
-    description='Buildbot Sample Plugin',
+    description='Buildbot parameter that use ace.js to display code',
     author=u'Pierre Tardy',
     author_email=u'tardyp@gmail.com',
     url='http://buildbot.net/',
     license='GNU GPL',
-    py_modules=['buildbot_codeparameter'],
-    cmdclass=cmdclass,
+    py_modules=['buildbot_www'],
     entry_points="""
         [buildbot.www]
-        codeparameter = buildbot_codeparameter:ep
+        base = buildbot_www:ep
     """,
 )
