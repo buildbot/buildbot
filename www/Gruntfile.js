7894df910e62378cadcb92a0a2e3f172724927f4
Daniel Hobley <danielh@unity3d.com>
2014-08-12 15:37:37 +0200

    Reduced the brightness of colorblind colors making them more appealing
    - Forcing css files to be updated when running production grunt tasks
    - Added white text to places where it makes sense
    - Added the ability to turn off the auto color change for color blind colors and replace with a manual color

--- www/Gruntfile.js
+++ www/Gruntfile.js
@@ -46,6 +46,7 @@ module.exports = function (grunt) {
             },
             prod: {
                 options: {
+                    force: true,
                     environment: "production",
                     outputStyle: "compressed"
                 }
