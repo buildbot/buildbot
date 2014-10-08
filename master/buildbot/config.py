5f7d49eff738e2c4f754e1a95f6e8459343db41c
Edmund Wong <ewongbb@pw-wspx.org>
2014-10-08 08:43:47 +0800

    Fixed config.py such that if Port is None, it uses just the domainname. Fixed test.

--- master/buildbot/config.py
+++ master/buildbot/config.py
@@ -582,7 +582,10 @@ class MasterConfig(util.ComparableMixin):
             if 'buildbotURL' in config_dict:
                 self.www['url'] = config_dict['buildbotURL']
             else:
-                self.www['url'] = 'http://localhost:%d/' % (www_cfg['port'],)
+                if www_cfg['port']:
+                   self.www['url'] = 'http://localhost:%d/' % (www_cfg['port'],)
+                else:
+                   self.www['url'] = 'http://localhost/'
 
         if not self.www['url'].endswith('/'):
             self.www['url'] += '/'
