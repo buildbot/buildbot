2add1d92b0643abf62621d71f4d1928326220c2f
Dustin J. Mitchell <dustin@buildbot.net>
2014-12-09 17:49:26 -0500

    Fix off-by-one error in log chunk handling
    The extra `- 1` here caused single blank lines to be skipped when
    chopping un-requested lines of the end of a response.  Earlier tests,
    even if they had actually been running, would not have discovered this
    as they did not test blank lines.  Fixes #3101.

--- master/buildbot/test/unit/test_db_logs.py
+++ master/buildbot/test/unit/test_db_logs.py
@@ -13,6 +13,7 @@
 #
 # Copyright Buildbot Team Members
 
+import base64
 import textwrap
 
 from buildbot.db import logs
@@ -49,17 +50,33 @@ class Tests(interfaces.InterfaceTests):
         fakedb.LogChunk(logid=201, first_line=2, last_line=4, compressed=0,
                         content=textwrap.dedent("""\
                     line TWO
-                    line 3
+
                     line 2**2""")),
         fakedb.LogChunk(logid=201, first_line=5, last_line=5, compressed=0,
                         content="another line"),
         fakedb.LogChunk(logid=201, first_line=6, last_line=6, compressed=0,
                         content="yet another line"),
     ]
+    bug3101Content = base64.b64decode("""
+        PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT0
+        9PT09PT09PT09PT09PT09PT09PT09PT09PT09PQpbU0tJUFBFRF0Kbm90IGEgd2luMz
+        IgcGxhdGZvcm0KCmJ1aWxkc2xhdmUudGVzdC51bml0LnRlc3RfcnVucHJvY2Vzcy5UZ
+        XN0UnVuUHJvY2Vzcy50ZXN0UGlwZVN0cmluZwotLS0tLS0tLS0tLS0tLS0tLS0tLS0t
+        LS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0
+        tLS0tLS0tClJhbiAyNjcgdGVzdHMgaW4gNS4zNzhzCgpQQVNTRUQgKHNraXBzPTEsIH
+        N1Y2Nlc3Nlcz0yNjYpCnByb2dyYW0gZmluaXNoZWQgd2l0aCBleGl0IGNvZGUgMAplb
+        GFwc2VkVGltZT04LjI0NTcwMg==""")
+
+    bug3101Rows = [
+        fakedb.Log(id=1470, stepid=101, name=u'problems', slug=u'problems',
+                   complete=1, num_lines=11, type=u't'),
+        fakedb.LogChunk(logid=1470, first_line=0, last_line=10, compressed=0,
+                        content=bug3101Content),
+    ]
 
     @defer.inlineCallbacks
     def checkTestLogLines(self):
-        expLines = ['line zero', 'line 1', 'line TWO', 'line 3', 'line 2**2',
+        expLines = ['line zero', 'line 1', 'line TWO', '', 'line 2**2',
                     'another line', 'yet another line']
         for first_line in range(0, 7):
             for last_line in range(first_line, 7):
@@ -191,6 +208,18 @@ class Tests(interfaces.InterfaceTests):
         ])
         self.assertEqual((yield self.db.logs.getLogLines(201, 9, 99)), '')
         self.assertEqual((yield self.db.logs.getLogLines(999, 9, 99)), '')
+
+    @defer.inlineCallbacks
+    def test_getLogLines_bug3101(self):
+        # regression test for #3101
+        content = self.bug3101Content
+        yield self.insertTestData(self.backgroundData + self.bug3101Rows)
+        # overall content is the same, with '\n' padding at the end
+        self.assertEqual((yield self.db.logs.getLogLines(1470, 0, 99)),
+                         self.bug3101Content + '\n')
+        # try to fetch just one line
+        self.assertEqual((yield self.db.logs.getLogLines(1470, 0, 0)),
+                         content.split('\n')[0] + '\n')
 
     @defer.inlineCallbacks
     def test_addLog_getLog(self):
