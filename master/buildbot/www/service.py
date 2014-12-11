43eb5c1709d9d34584537faf3813b60ffc2ac161
Dustin J. Mitchell <dustin@buildbot.net>
2014-12-10 21:35:01 -0500

    add default for www.get('plugins')

--- master/buildbot/www/service.py
+++ master/buildbot/www/service.py
@@ -126,7 +126,7 @@ class WWWService(service.ReconfigurableServiceMixin, service.AsyncMultiService):
             if key not in self.apps:
                 raise RuntimeError("could not find plugin %s; is it installed?" % (key,))
             root.putChild(key, self.apps.get(key).resource)
-        known_plugins = set(new_config.www.get('plugins')) | set(['base'])
+        known_plugins = set(new_config.www.get('plugins', {})) | set(['base'])
         for plugin_name in set(self.apps.names) - known_plugins:
             log.msg("NOTE: www plugin %r is installed but not "
                     "configured" % (plugin_name,))
