085942d83be50b020ad07369fad37caf27005640
Pierre Tardy <pierre.tardy@intel.com>
2014-12-29 14:43:19 +0100

    Integration base test improvements
    - add possibility of forcing a build with a change
    - fix issue with forcing a build that triggers other builds
    - add printBuild method for debugging in console

---
+++ master/buildbot/test/integration/test_trigger.py
@@ -0,0 +1,93 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+import StringIO
+import sys
+
+from buildbot.test.util.integration import RunMasterBase
+from twisted.internet import defer
+
+# This integration test creates a master and slave environment,
+# with two builders and a trigger step linking them
+
+expectedOutput = """\
+*** BUILD 1 *** ==> finished (success)
+    *** STEP shell *** ==> 'echo hello' (success)
+        log:stdio (45)
+    *** STEP trigger *** ==> triggered trigsched (success)
+       url:trigsched #2 (http://localhost:8080/#buildrequests/2)
+       url:success: build #1 (http://localhost:8080/#builders/2/builds/1)
+    *** STEP shell_1 *** ==> 'echo world' (success)
+        log:stdio (45)
+*** BUILD 2 *** ==> finished (success)
+    *** STEP shell *** ==> 'echo ola' (success)
+        log:stdio (45)
+"""
+
+
+class TriggeringMaster(RunMasterBase):
+
+    @defer.inlineCallbacks
+    def test_trigger(self):
+
+        change = dict(branch="master",
+                      files=["foo.c"],
+                      author="me@foo.com",
+                      comments="good stuff",
+                      revision="HEAD",
+                      project="none"
+                      )
+        build = yield self.doForceBuild(wantSteps=True, useChange=change, wantLogs=True)
+
+        self.assertEqual(build['steps'][1]['state_string'], 'triggered trigsched')
+        builds = yield self.master.data.get(("builds",))
+        self.assertEqual(len(builds), 2)
+        self.patch(sys, 'stdout', StringIO.StringIO())
+        for b in builds:
+            yield self.printBuild(b)
+        self.assertEqual(sys.stdout.getvalue(), expectedOutput)
+
+
+# master configuration
+def masterConfig():
+    c = {}
+    from buildbot.config import BuilderConfig
+    from buildbot.process.factory import BuildFactory
+    from buildbot.plugins import steps, schedulers
+
+    c['schedulers'] = [
+        schedulers.Triggerable(
+            name="trigsched",
+            builderNames=["build"]),
+        schedulers.AnyBranchScheduler(
+            name="sched",
+            builderNames=["testy"])]
+
+    f = BuildFactory()
+    f.addStep(steps.ShellCommand(command='echo hello'))
+    f.addStep(steps.Trigger(schedulerNames=['trigsched'],
+                            waitForFinish=True,
+                            updateSourceStamp=True))
+    f.addStep(steps.ShellCommand(command='echo world'))
+    f2 = BuildFactory()
+    f2.addStep(steps.ShellCommand(command='echo ola'))
+    c['builders'] = [
+        BuilderConfig(name="testy",
+                      slavenames=["local1"],
+                      factory=f),
+        BuilderConfig(name="build",
+                      slavenames=["local1"],
+                      factory=f2)]
+    return c
