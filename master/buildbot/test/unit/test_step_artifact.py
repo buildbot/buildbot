db0dc68799c6bef176eec38e30bff528163d03de
Maria Marcano <mariangemarcano@gmail.com>
2015-01-06 19:17:03 +0100

    Test CreateArtifactDirectory step it should create the directory using information of the current build (builder + brid + submitted datetime)

---
+++ master/buildbot/test/unit/test_step_artifact.py
@@ -0,0 +1,39 @@
+from twisted.trial import unittest
+from buildbot.test.util import steps
+
+from buildbot.steps import artifact
+from buildbot.status.results import SUCCESS
+from buildbot.test.fake.remotecommand import ExpectShell
+
+from buildbot.test.fake import fakedb
+
+class TestArtifactSteps(steps.BuildStepMixin, unittest.TestCase):
+
+    def setUp(self):
+        return self.setUpBuildStep()
+
+    def tearDown(self):
+        return self.tearDownBuildStep()
+
+    def setupStep(self, step):
+        steps.BuildStepMixin.setupStep(self, step)
+
+        fake_br = fakedb.BuildRequest(id=1, buildsetid=1, buildername="A", complete=1, results=0)
+        fake_br.submittedAt = 1418823086
+        self.build.requests = [fake_br]
+        self.build.builder.config.builddir = "build"
+
+
+
+    def test_create_artifact_directory(self):
+        self.setupStep(artifact.CreateArtifactDirectory(artifactDirectory="mydir",
+                                        artifactServer='usr@srv.com', artifactServerDir='/home/srv/web/dir'))
+        self.expectCommands(
+            ExpectShell(workdir='wkdir', usePTY='slave-config',
+                        command=['ssh','usr@srv.com', 'cd /home/srv/web/dir;', 'mkdir -p ',
+                                 'build_1_17_12_2014_13_31_26_+0000/mydir'])
+            + ExpectShell.log('stdio', stdout='')
+            + 0
+        )
+        self.expectOutcome(result=SUCCESS, status_text=['Remote artifact directory created.'])
+        return self.runStep()
