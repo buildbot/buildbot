5ed32c4f0537fb741ca2ab9d50189553c6be54ea
Mikhail Sobolev <mss@mawhrin.net>
2015-01-22 15:58:07 +0200

    re-format exclude patterns

--- master/docs/conf.py
+++ master/docs/conf.py
@@ -86,7 +86,10 @@ if release == 'latest':
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'release-notes/*.rst']
+exclude_patterns = [
+    '_build',
+    'release-notes/*.rst'
+]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 # default_role = None
