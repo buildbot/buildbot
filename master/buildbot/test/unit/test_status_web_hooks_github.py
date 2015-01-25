afe1ed3952a54619cda57c4f243f6c15a0fcd59a
Mikhail Sobolev <mss@mawhrin.net>
2015-01-25 15:28:27 +0200

    finalise test cases for github

--- master/buildbot/test/unit/test_status_web_hooks_github.py
+++ master/buildbot/test/unit/test_status_web_hooks_github.py
@@ -14,8 +14,13 @@
 # Copyright Buildbot Team Members
 
 from calendar import timegm
+from hashlib import sha1
+import hmac
 
 from buildbot.status.web.change_hook import ChangeHookResource
+from buildbot.status.web.hooks.github import _HEADER_CT
+from buildbot.status.web.hooks.github import _HEADER_EVENT
+from buildbot.status.web.hooks.github import _HEADER_SIGNATURE
 
 from buildbot.test.fake.web import FakeRequest
 from buildbot.test.util import compat
@@ -130,28 +135,44 @@ gitJsonPayloadEmpty = """
   "ref": "refs/heads/master"
 }
 """
+
 _CT_ENCODED = 'application/x-www-form-urlencoded'
 _CT_JSON = 'application/json'
 
 
-def _prepare_request(event, payload):
-    """
-    ...
-    """
+def _prepare_github_change_hook(**params):
+    return ChangeHookResource(dialects={
+        'github': params
+    })
+
+
+def _prepare_request(event, payload, _secret=None, headers=None):
+    if headers is None:
+        headers = dict()
+
     request = FakeRequest()
 
     request.uri = "/change_hook/github"
     request.method = "GET"
     request.received_headers = {
-        'X-GitHub-Event': event
+        _HEADER_EVENT: event
     }
 
     if isinstance(payload, str):
         request.content = StringIO(payload)
-        request.received_headers['Content-Type'] = _CT_JSON
+        request.received_headers[_HEADER_CT] = _CT_JSON
+
+        if _secret is not None:
+            signature = hmac.new(_secret, msg=payload, digestmod=sha1)
+            request.received_headers[_HEADER_SIGNATURE] = \
+                'sha1=%s' % (signature.hexdigest(),)
     else:
         request.args['payload'] = payload
-        request.received_headers['Content-Type'] = _CT_ENCODED
+        request.received_headers[_HEADER_CT] = _CT_ENCODED
+
+    request.received_headers.update(headers)
+
+    # print request.received_headers
 
     return request
 
@@ -159,11 +180,41 @@ def _prepare_request(event, payload):
 class TestChangeHookConfiguredWithGitChange(unittest.TestCase):
 
     def setUp(self):
-        self.changeHook = ChangeHookResource(dialects={
-            'github': {
-                'strict': False
-            }
+        self.changeHook = _prepare_github_change_hook(strict=False)
+
+    @compat.usesFlushLoggedErrors
+    def test_unknown_event(self):
+        bad_event = 'whatever'
+
+        self.request = _prepare_request(bad_event, gitJsonPayload)
+
+        d = self.request.test_render(self.changeHook)
+
+        @d.addCallback
+        def check_result(result):
+            expected = 'Unknown event: %r' % (bad_event,)
+            self.assertEqual(len(self.request.addedChanges), 0)
+            self.assertEqual(self.request.written, expected)
+
+        return d
+
+    @compat.usesFlushLoggedErrors
+    def test_unknown_content_type(self):
+        bad_content_type = 'application/x-useful'
+
+        self.request = _prepare_request('push', gitJsonPayload, headers={
+            _HEADER_CT: bad_content_type
         })
+
+        d = self.request.test_render(self.changeHook)
+
+        @d.addCallback
+        def check_result(result):
+            expected = 'Unknown content type: %r' % (bad_content_type,)
+            self.assertEqual(len(self.request.addedChanges), 0)
+            self.assertEqual(self.request.written, expected)
+
+        return d
 
     def _check_ping(self, payload):
         self.request = _prepare_request('ping', payload)
@@ -243,8 +294,8 @@ class TestChangeHookConfiguredWithGitChange(unittest.TestCase):
         self.request.uri = "/change_hook/github"
         self.request.method = "GET"
         self.request.received_headers = {
-            'Content-Type': _CT_ENCODED,
-            'X-GitHub-Event': 'push'
+            _HEADER_CT: _CT_ENCODED,
+            _HEADER_EVENT: 'push'
         }
 
         d = self.request.test_render(self.changeHook)
@@ -278,8 +329,8 @@ class TestChangeHookConfiguredWithGitChange(unittest.TestCase):
         self.request.uri = "/change_hook/github"
         self.request.method = "GET"
         self.request.received_headers = {
-            'Content-Type': _CT_ENCODED,
-            'X-GitHub-Event': 'push'
+            _HEADER_CT: _CT_ENCODED,
+            _HEADER_EVENT: 'push'
         }
 
         d = self.request.test_render(self.changeHook)
@@ -328,3 +379,142 @@ class TestChangeHookConfiguredWithGitChange(unittest.TestCase):
 
     def test_git_with_non_branch_changes_json(self):
         self._check_git_with_non_branch_changes(gitJsonPayloadNonBranch)
+
+
+class TestChangeHookConfiguredWithStrict(unittest.TestCase):
+
+    _SECRET = 'somethingreallysecret'
+
+    def setUp(self):
+        self.changeHook = _prepare_github_change_hook(strict=True,
+                                                      secret=self._SECRET)
+
+    def test_signature_ok(self):
+        self.request = _prepare_request('push', gitJsonPayload,
+                                        _secret=self._SECRET)
+        d = self.request.test_render(self.changeHook)
+
+        # Can it somehow be merged w/ the same code above in a different class?
+        @d.addCallback
+        def check_changes(_):
+            self.assertEquals(len(self.request.addedChanges), 2)
+            change = self.request.addedChanges[0]
+
+            self.assertEquals(change['files'], ['filepath.rb'])
+            self.assertEquals(change["repository"],
+                              "http://github.com/defunkt/github")
+            self.assertEquals(timegm(change["when_timestamp"].utctimetuple()),
+                              1203116237)
+            self.assertEquals(change["author"],
+                              "Fred Flinstone <fred@flinstone.org>")
+            self.assertEquals(change["revision"],
+                              '41a212ee83ca127e3c8cf465891ab7216a705f59')
+            self.assertEquals(change["comments"],
+                              "okay i give in")
+            self.assertEquals(change["branch"], "master")
+            self.assertEquals(change["revlink"],
+                              "http://github.com/defunkt/github/commit/"
+                              "41a212ee83ca127e3c8cf465891ab7216a705f59")
+
+            change = self.request.addedChanges[1]
+            self.assertEquals(change['files'], ['modfile', 'removedFile'])
+            self.assertEquals(change["repository"],
+                              "http://github.com/defunkt/github")
+            self.assertEquals(timegm(change["when_timestamp"].utctimetuple()),
+                              1203114994)
+            self.assertEquals(change["author"],
+                              "Fred Flinstone <fred@flinstone.org>")
+            self.assertEquals(change["src"], "git")
+            self.assertEquals(change["revision"],
+                              'de8251ff97ee194a289832576287d6f8ad74e3d0')
+            self.assertEquals(change["comments"], "update pricing a tad")
+            self.assertEquals(change["branch"], "master")
+            self.assertEquals(change["revlink"],
+                              "http://github.com/defunkt/github/commit/"
+                              "de8251ff97ee194a289832576287d6f8ad74e3d0")
+        return d
+
+    @compat.usesFlushLoggedErrors
+    def test_unknown_hash(self):
+        bad_hash_type = 'blah'
+
+        self.request = _prepare_request('push', gitJsonPayload, headers={
+            _HEADER_SIGNATURE: '%s=doesnotmatter' % (bad_hash_type,)
+        })
+
+        d = self.request.test_render(self.changeHook)
+
+        @d.addCallback
+        def check_result(_):
+            expected = 'Unknown hash type: %s' % (bad_hash_type,)
+            self.assertEqual(len(self.request.addedChanges), 0)
+            self.assertEqual(self.request.written, expected)
+
+        return d
+
+    @compat.usesFlushLoggedErrors
+    def test_signature_nok(self):
+        bad_signature = 'sha1=wrongstuff'
+
+        self.request = _prepare_request('push', gitJsonPayload, headers={
+            _HEADER_SIGNATURE: bad_signature
+        })
+
+        d = self.request.test_render(self.changeHook)
+
+        @d.addCallback
+        def check_result(_):
+            expected = 'Hash mismatch'
+            self.assertEqual(len(self.request.addedChanges), 0)
+            self.assertEqual(self.request.written, expected)
+
+        return d
+
+    @compat.usesFlushLoggedErrors
+    def test_missing_secret(self):
+        # override the value assigned in setUp
+        self.changeHook = _prepare_github_change_hook(strict=True)
+
+        self.request = _prepare_request('push', gitJsonPayload)
+
+        d = self.request.test_render(self.changeHook)
+
+        @d.addCallback
+        def check_result(_):
+            expected = 'Strict mode is requested while no secret is provided'
+            self.assertEqual(len(self.request.addedChanges), 0)
+            self.assertEqual(self.request.written, expected)
+
+        return d
+
+    @compat.usesFlushLoggedErrors
+    def test_wrong_signature_format(self):
+        bad_signature = 'hash=value=something'
+
+        self.request = _prepare_request('push', gitJsonPayload, headers={
+            _HEADER_SIGNATURE: bad_signature
+        })
+
+        d = self.request.test_render(self.changeHook)
+
+        @d.addCallback
+        def check_result(_):
+            expected = 'Wrong signature format: %r' % (bad_signature,)
+            self.assertEqual(len(self.request.addedChanges), 0)
+            self.assertEqual(self.request.written, expected)
+
+        return d
+
+    @compat.usesFlushLoggedErrors
+    def test_signature_missing(self):
+        self.request = _prepare_request('push', gitJsonPayload)
+
+        d = self.request.test_render(self.changeHook)
+
+        @d.addCallback
+        def check_result(result):
+            expected = 'Request has no required signature'
+            self.assertEqual(len(self.request.addedChanges), 0)
+            self.assertEqual(self.request.written, expected)
+
+        return d
