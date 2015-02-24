ce0d0b3240937f25ec82ba382d72ea6b6d37c11b
Maria Marcano <mariangemarcano@gmail.com>
2015-02-24 09:59:42 +0100

    Add test_generateFinishedBuildsUseBuildCache

--- master/buildbot/test/unit/test_status_builder.py
+++ master/buildbot/test/unit/test_status_builder.py
@@ -9,12 +9,8 @@ import datetime
 
 class TestBuilderStatus(unittest.TestCase):
 
-    def test_generateFinishedBuildsUseLatestBuildCache(self):
-        m = fakemaster.make_master()
-        b = builder.BuilderStatus(buildername="builder-01", category=None,
-                                    master=m)
-
-        codebases = {'katana-buildbot': 'katana'}
+    def setUp(self):
+        self.master = fakemaster.make_master()
 
         katana = {'katana-buildbot':
                       {'project': 'general',
@@ -26,27 +22,56 @@ class TestBuilderStatus(unittest.TestCase):
 
         self.project = ProjectConfig(name="Katana", codebases=[katana])
 
-        m.getProject = lambda x: self.project
-        m.getProjects = lambda: {'Katana': self.project}
-        b.nextBuildNumber = 39
+        self.master.getProject = lambda x: self.project
+        self.getProjects = lambda: {'Katana': self.project}
 
-        b.latestBuildCache['katana-buildbot=katana;'] = {'date': datetime.datetime.now(), 'build': 38}
-        b.buildCache = Mock()
+        self.builder_status = builder.BuilderStatus(buildername="builder-01", category=None,
+                                    master=self.master)
+
+        self.builder_status.nextBuildNumber = 39
+
+        self.builder_status.buildCache = Mock()
 
         def getCachedBuild(number):
-            build_status = BuildStatus(b, m, number)
+            build_status = BuildStatus(self.builder_status, self.master, number)
             build_status.finished = 1422441501.21
             build_status.reason ='A build was forced by user@localhost'
             build_status.slavename = 'build-slave-01'
             build_status.results = SUCCESS
             return build_status
 
-        b.buildCache.get = getCachedBuild
+        self.builder_status.buildCache.get = getCachedBuild
 
-        builds = list(b.generateFinishedBuilds(branches=[],
+    def test_generateFinishedBuildsUseLatestBuildCache(self):
+
+        codebases = {'katana-buildbot': 'katana'}
+
+        self.builder_status.latestBuildCache['katana-buildbot=katana;'] = {'date': datetime.datetime.now(),
+                                                                           'build': 38}
+
+        builds = list(self.builder_status.generateFinishedBuilds(branches=[],
                                                      codebases=codebases,
                                                      num_builds=1, max_search=200, useCache=True))
 
+        self.assertTrue(len(builds) > 0)
+        self.assertTrue(isinstance(builds[0], BuildStatus))
+        self.assertTrue(builds[0].number, 38)
+
+
+    def test_generateFinishedBuildsUseBuildCache(self):
+
+        codebases = {'katana-buildbot': 'katana'}
+
+        self.builder_status.saveYourself = lambda skipBuilds: True
+
+        self.assertEqual(self.builder_status.latestBuildCache, {})
+
+        builds = list(self.builder_status.generateFinishedBuilds(branches=[],
+                                                     codebases=codebases,
+                                                     num_builds=1, max_search=200, useCache=True))
+
+        self.assertTrue('katana-buildbot=katana;' in self.builder_status.latestBuildCache.keys())
+        self.assertTrue(self.builder_status.latestBuildCache['katana-buildbot=katana;']['build'], 38)
         self.assertTrue(len(builds) > 0)
         self.assertTrue(isinstance(builds[0], BuildStatus))
         self.assertTrue(builds[0].number, 38)
