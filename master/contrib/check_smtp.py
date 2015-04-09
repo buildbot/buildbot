8868f1e9d6af0250fe58dc230ab9480cda90c78b
Mikhail Sobolev <mss@mawhrin.net>
2015-04-10 00:22:41 +0300

    add a script to check smtp with starttls connectivity

---
+++ master/contrib/check_smtp.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python -tt
+"""
+This script helps to check that the SMTP_HOST (see below) would accept STARTTLS
+command, and if LOCAL_HOST is acceptable for it, would check the requested user
+name and password would allow to send e-mail through it.
+"""
+
+from smtplib import SMTP
+
+from getpass import getpass
+
+SMTP_HOST = 'the host you want to send e-mail through'
+LOCAL_HOST = 'hostname that the SMTP_HOST would accept'
+
+def main():
+    """
+    entry point
+    """
+
+    server = SMTP(SMTP_HOST)
+
+    server.starttls()
+
+    print server.ehlo(LOCAL_HOST)
+
+    user = raw_input('user: ')
+    password = getpass('password: ')
+
+    print server.login(user, password)
+    server.close()
+
+if __name__ == '__main__':
+    main()
+
+# vim:ts=4:sw=4:et:tw=80
