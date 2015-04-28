b02e1d479d61c1d5e7c9c8c23d55cadddfde13bb
Anton Lofgren <alofgren@op5.com>
2015-04-28 10:27:15 +0200

    status: URL-quote builder names
    Builders with special characters in them, such as '/', would break the
    JSON API example links. This patch corrects that by correctly URL
    encoding them.
    
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

--- master/buildbot/status/web/status_json.py
+++ master/buildbot/status/web/status_json.py
@@ -750,7 +750,7 @@ For help on any sub directory, use url /child/help
                 break
         if not builder:
             return
-        EXAMPLES = EXAMPLES.replace('<A_BUILDER>', builder.getName())
+        EXAMPLES = EXAMPLES.replace('<A_BUILDER>', urllib.quote(builder.getName(), ''))
         build = builder.getBuild(-1)
         if build:
             EXAMPLES = EXAMPLES.replace('<A_BUILD>', str(build.getNumber()))
