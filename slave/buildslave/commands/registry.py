85aac17f220151427e449d2aa0bf13feca20757d
Elmir Jagudin <elmir@unity3d.com>
2015-05-27 08:16:47 +0200

    drop bk slave command
    it is no longer used by the master

--- slave/buildslave/commands/registry.py
+++ slave/buildslave/commands/registry.py
@@ -21,7 +21,6 @@ commandRegistry = {
     "uploadFile": "buildslave.commands.transfer.SlaveFileUploadCommand",
     "uploadDirectory": "buildslave.commands.transfer.SlaveDirectoryUploadCommand",
     "downloadFile": "buildslave.commands.transfer.SlaveFileDownloadCommand",
-    "bk": "buildslave.commands.bk.BK",
     "darcs": "buildslave.commands.darcs.Darcs",
     "repo": "buildslave.commands.repo.Repo",
     "bzr": "buildslave.commands.bzr.Bzr",
