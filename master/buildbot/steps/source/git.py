0afaedb25a5b86514a7bd42927717c8710d4a0ab
Vitali Lovich <lovich@apple.com>
2015-06-01 13:43:16 -0700

    Switch _updateSubmodule to use inline callbacks

--- master/buildbot/steps/source/git.py
+++ master/buildbot/steps/source/git.py
@@ -524,14 +524,15 @@ class Git(Source):
             rc = yield self._dovccmd(['submodule', 'sync'])
         defer.returnValue(rc)
 
+    @defer.inlineCallbacks
     def _updateSubmodule(self, _=None):
+        rc = RC_SUCCESS
         if self.submodules:
             vccmd = ['submodule', 'update', '--init', '--recursive', '--force']
             if self.supportsSubmoduleCheckout:
                 vccmd.extend(['--checkout'])
-            return self._dovccmd(vccmd)
-        else:
-            return defer.succeed(RC_SUCCESS)
+            rc = yield self._dovccmd(vccmd)
+        defer.returnValue(rc)
 
     def _cleanSubmodule(self, _=None):
         if self.submodules:
