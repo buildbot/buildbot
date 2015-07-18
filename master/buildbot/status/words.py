a9254b889cc9469e5ba0db06af2e773da19b66d1
ravench <sim3213@gmail.com>
2015-07-18 20:40:20 +0200

    Removed tailing whitespace
    master/buildbot/status/words.py:875:100: W291 trailing whitespace
    
        command_HUSTLE.usage = "dondon on #qutebrowser: qutebrowser-bb needs to learn to do the hustle"
    
                                                                                                       ^
    
    1       W291 trailing whitespace
    
    1       W293 blank line contains whitespace
    
    2
    
    make: *** [pep8] Error 1

--- master/buildbot/status/words.py
+++ master/buildbot/status/words.py
@@ -872,7 +872,7 @@ class Contact(base.StatusReceiver):
         
     def command_HUSTLE(self, args):
         reactor.callLater(1.0, self.send, "/me does the hustle")
-    command_HUSTLE.usage = "dondon on #qutebrowser: qutebrowser-bb needs to learn to do the hustle"  
+    command_HUSTLE.usage = "dondon on #qutebrowser: qutebrowser-bb needs to learn to do the hustle"
 
     def command_SHUTDOWN(self, args):
         # FIXME: NEED TO THINK ABOUT!
