e5b3e062ffdf783cfe27f757351838f02126273f
ravench <sim3213@gmail.com>
2015-07-19 19:32:23 +0200

    typos...

--- master/buildbot/status/words.py
+++ master/buildbot/status/words.py
@@ -271,7 +271,7 @@ class Contact(base.StatusReceiver):
             args = args.encode('ascii')
         try:
             return shlex.split(args)
-        except ValueError as e:
+        except ValueError, e:
             raise UsageError(e)
 
     def command_HELLO(self, args):
