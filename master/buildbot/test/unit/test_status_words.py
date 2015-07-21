7f484d606bedce0b0423424d6cdf17a31b6a2d7d
ravench <sim3213@gmail.com>
2015-07-21 17:39:25 +0200

    Improved hello-test

--- master/buildbot/test/unit/test_status_words.py
+++ master/buildbot/test/unit/test_status_words.py
@@ -296,10 +296,11 @@ class TestContactChannel(unittest.TestCase):
 
     @defer.inlineCallbacks
     def test_command_hello(self):
+        greetings = ["ږغ كول ، هركلى كول ږغ، هركلى", "Goeie dag", "Tungjatjeta", "Yatasay", "Ahlan bik", "Voghdzuyin", "hola", "kaixo", "Horas", "Pryvitańnie", "Nomoskar", "Oki", "Selam", "Dez-mat", "Zdrávejte", "Mingala ba", "Hola", "Hafa dai", "Oh-see-YOH", "Nín hao", "Bonjou", "Zdravo", "Nazdar", "Hallo", "Hallo", "Iiti", "Kotáka", "Saluton", "Tere", "Hallo", "Hallo", "Bula", "Helo", "Hei", "Goede morgen", "Bonjour", "Hoi", "Ola", "Gamardžoba", "Guten Tag", "Mauri", "Geia!", "Inuugujoq", "Kem cho", "Sannu", "Aloha", "Shalóm", "Namasté", "Szia", "Halló", "Hai", "Kiana", "Dia is muire dhuit", "Buongiorno", "Kónnichi wa", "Salam", "Annyeonghaseyo", "Na", "Sabai dii", "Ave", "Es mīlu tevi", "Labas.", "Selamat petang", "Ni hao", "Kia ora", "Yokwe", "Kwe", "sain baina uu", "niltze", "Yá'át'ééh", "Namaste", "Hallo.", "Salâm", "Witajcie", "Olá", "Kâils", "Aroha", "Salut", "Privét", "Talofa", "Namo namah", "ćao", "Nazdar", "Zdravo", "Hola", "Jambo", "Hej", "Sälü", "Halo", "Selam", "Sàwàtdee kráp", "Dumela", "Merhaba", "Pryvít", "Adaab arz hai", "Chào", "Glidis", "Helo", "Sawubona", "Hoi"]
         yield self.do_test_command('hello', exp_usage=False)
         self.assertEqual(self.sent, ['yes?'])
         yield self.do_test_command('hello', exp_usage=False)
-        self.assertTrue(self.sent)
+        self.assertIn(self.sent, greetings)
 
     @defer.inlineCallbacks
     def test_command_list(self):
