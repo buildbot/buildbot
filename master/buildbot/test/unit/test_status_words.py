f409d99520d540b5d75e9150789915839cfdd4a5
ravench <sim3213@gmail.com>
2015-07-21 17:45:09 +0200

    Rememberd to define coding.

--- master/buildbot/test/unit/test_status_words.py
+++ master/buildbot/test/unit/test_status_words.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # This file is part of Buildbot.  Buildbot is free software: you can
 # redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, version 2.
