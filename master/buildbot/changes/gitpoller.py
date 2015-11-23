a6b9bd640277f30dc9774f2c4040ca904e67f35c
Jeremy Cornett <jeremy.cornett@venafi.com>
2015-11-23 09:25:58 -0500

    Force GitPoller revisions to be ascii
    This is a shot in the dark. Twisted keeps complaining about
    wanting strings instead of unicode when the gitpoller is running. It
    looks like the unicode strings are coming from that self.lastRev variable.

--- master/buildbot/changes/gitpoller.py
+++ master/buildbot/changes/gitpoller.py
@@ -268,7 +268,8 @@ class GitPoller(base.PollingChangeSource, StateMixin):
 
         # get the change list
         revListArgs = ([r'--format=%H', r'%s' % newRev] +
-                       [r'^%s' % rev for rev in itervalues(self.lastRev)] +
+                       [r'^%s' % rev.encode('ascii', 'ignore')
+                        for rev in itervalues(self.lastRev)] +
                        [r'--'])
         self.changeCount = 0
         results = yield self._dovccmd('log', revListArgs, path=self.workdir)
