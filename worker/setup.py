8ec11019b5d65ca6a3f7388941ba8c27fc9791ce
Vladimir Rutsky <vladimir@rutsky.org>
2016-03-06 01:38:55 +0300

    rename "buildslave.bat" to "buildbot_worker.bat"

--- worker/setup.py
+++ worker/setup.py
@@ -29,10 +29,10 @@ from distutils.core import setup
 from buildslave import version
 
 scripts = ["bin/buildbot-worker"]
-# sdist is usually run on a non-Windows platform, but the buildslave.bat file
-# still needs to get packaged.
+# sdist is usually run on a non-Windows platform, but the buildbot_worker.bat
+# file still needs to get packaged.
 if 'sdist' in sys.argv or sys.platform == 'win32':
-    scripts.append("contrib/windows/buildslave.bat")
+    scripts.append("contrib/windows/buildbot_worker.bat")
     scripts.append("contrib/windows/buildbot_service.py")
 
 
