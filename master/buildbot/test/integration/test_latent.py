10278805c64ae71fa317fc024dc573735467afe8
Tom Prince <tom.prince@twistedmatrix.com>
2016-04-28 22:03:10 -0600

    Add a test for multiple builders triggering a worker.

--- master/buildbot/test/integration/test_latent.py
+++ master/buildbot/test/integration/test_latent.py
@@ -14,6 +14,7 @@
 # Copyright Buildbot Team Members
 
 from buildbot.config import BuilderConfig
+from buildbot.process.results import SUCCESS
 from buildbot.process.factory import BuildFactory
 from buildbot.test.fake.reactor import NonThreadPool
 from buildbot.test.fake.reactor import TestReactor
@@ -23,7 +24,13 @@ from buildbot.worker.base import AbstractLatentWorker
 from twisted.internet.defer import Deferred
 from twisted.python import threadpool
 from twisted.python.failure import Failure
+from twisted.python.filepath import FilePath
 from twisted.trial.unittest import SynchronousTestCase
+
+try:
+    from buildbot_worker.bot import LocalWorker
+except ImportError:
+    LocalWorker = None
 
 
 class LatentController(object):
@@ -42,6 +49,9 @@ class LatentController(object):
         self.started = False
         d, self._start_deferred = self._start_deferred, None
         d.callback(result)
+
+    def connect_worker(self, workdir):
+        LocalWorker(self.worker.name, workdir.path, False).setServiceParent(self.worker)
 
 
 class ControllableLatentWorker(AbstractLatentWorker):
@@ -229,3 +239,65 @@ class Tests(SynchronousTestCase):
         # already kicked in to start a new build on this (the only) worker. We check that
         # a new instance was requested, which indicates that the worker accepted the build.
         self.assertEqual(controller.started, True)
+
+    def test_worker_multiple_substantiations_succeed(self):
+        """
+        If multiple builders trigger try to substantiate a worker at
+        the same time, if the substantiation succeeds then all of
+        the builds proceeed.
+        """
+        controller = LatentController('local')
+        config_dict = {
+            'builders': [
+                BuilderConfig(name="testy-1",
+                              workernames=["local"],
+                              factory=BuildFactory(),
+                              ),
+                BuilderConfig(name="testy-2",
+                              workernames=["local"],
+                              factory=BuildFactory(),
+                              ),
+            ],
+            'workers': [controller.worker],
+            'protocols': {'null': {}},
+            'multiMaster': True,
+        }
+        master = self.successResultOf(getMaster(self, self.reactor, config_dict))
+        builder_ids = [
+            self.successResultOf(master.data.updates.findBuilderId('testy-1')),
+            self.successResultOf(master.data.updates.findBuilderId('testy-2')),
+        ]
+
+        finished_builds = []
+        self.successResultOf(master.mq.startConsuming(
+            lambda key, build: finished_builds.append(build),
+            ('builds', None, 'finished')))
+
+        # Trigger a buildrequest
+        bsid, brids = self.successResultOf(
+            master.data.updates.addBuildset(
+                waited_for=False,
+                builderids=builder_ids,
+                sourcestamps=[
+                    {'codebase': '',
+                     'repository': '',
+                     'branch': None,
+                     'revision': None,
+                     'project': ''},
+                ],
+            )
+        )
+
+        # The worker fails to substantiate.
+        controller.start_instance(True)
+
+        local_workdir = FilePath(self.mktemp())
+        local_workdir.createDirectory()
+        controller.connect_worker(local_workdir)
+
+        # We check that there were two builds that finished, and
+        # that they both finished with success
+        self.assertEqual([build['results'] for build in finished_builds], [SUCCESS] * 2)
+
+    if LocalWorker is None:
+        test_worker_multiple_substantiations_succeed.skip = "buildbot-slave package is not installed"
