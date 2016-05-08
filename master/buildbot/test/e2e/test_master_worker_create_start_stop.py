b73cd8c0bafc25b3fb73eb805bbed356eddf4086
Vladimir Rutsky <vladimir@rutsky.org>
2016-05-08 03:36:28 +0300

    refactor e2e test + run command in async way

--- master/buildbot/test/e2e/test_master_worker_create_start_stop.py
+++ master/buildbot/test/e2e/test_master_worker_create_start_stop.py
@@ -17,37 +17,68 @@ from __future__ import print_function
 
 import os
 import re
-import subprocess
-import sys
+import socket
+
+from buildbot.test.util import dirs
 
 from twisted.internet import defer
+from twisted.internet import utils
 from twisted.python import log
 from twisted.trial import unittest
 
-from buildbot.test.util import dirs
+try:
+    from shutil import which
+except ImportError:
+    # Backport of shutil.which() from Python 3.3.
+    from shutilwhich import which
+
+
+def get_open_port():
+    # TODO: This is synchronous code which might be blocking, which is
+    # unacceptable in Twisted.
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", 0))
+    s.listen(1)
+    port = s.getsockname()[1]
+    s.close()
+    return port
 
 
 class TestMasterWorkerSetup(dirs.DirsMixin, unittest.TestCase):
 
     try:
-        import buildbot_worker as _  # noqa pylint: disable=unused-import
+        import buildbot_worker  # noqa pylint: disable=unused-import
     except ImportError:
         skip = "buildbot-worker package is not installed"
 
     @defer.inlineCallbacks
     def setUp(self):
-        self.origcwd = os.getcwd()
-        self.projectdir = os.path.abspath('project')
+        self._origcwd = os.getcwd()
+        test_name = self.id().rsplit('.', 1)[1]
+        self.projectdir = os.path.abspath('project_' + test_name)
         yield self.setUpDirs(self.projectdir)
+        os.chdir(self.projectdir)
+
+        self.master_port = get_open_port()
+        self.ui_port = get_open_port()
+
+        # Working directories of masters and workers.
+        # Populated when appropriate master or workers are created.
+        # Used for test cleanup in case of failure (force stop still running
+        # master/worker).
+        self.master_dir = None
+        self.workers_dirs = []
 
         self.logs = []
         self.success = False
 
     @defer.inlineCallbacks
     def tearDown(self):
-        os.chdir(self.origcwd)
+        os.chdir(self._origcwd)
 
         if not self.success:
+            yield self._force_stop()
+
             # Output ran command logs to stdout to help debugging in CI systems
             # where logs are not available (e.g. Travis).
             # Logs can be stored on AppVeyor and CircleCI, we can move
@@ -60,91 +91,137 @@ class TestMasterWorkerSetup(dirs.DirsMixin, unittest.TestCase):
             # Clean working directory only when test succeeded.
             yield self.tearDownDirs()
 
+    @defer.inlineCallbacks
+    def _force_stop(self):
+        """Force stop running master/workers"""
+        for worker_dir in self.workers_dirs:
+            try:
+                yield self._run_command(["buildbot-worker", "stop", worker_dir])
+            except Exception:
+                # Ignore errors.
+                pass
+
+        if self.master_dir is not None:
+            try:
+                yield self._run_command(["buildbot", "stop", self.master_dir])
+            except Exception:
+                # Ignore errors.
+                pass
+
     def _log(self, msg):
         self.logs.append(msg)
         log.msg(msg)
 
+    @defer.inlineCallbacks
     def _run_command(self, args):
         command_str = " ".join(args)
         self._log("Running command: '{0}'".format(command_str))
 
-        shell = False
-        if sys.platform == 'win32':
-            # There are issues finding Buildbot binaries on windows not in
-            # shell mode: "exceptions.WindowsError: [Error 2]
-            # The system cannot find the file specified"
-            shell = True
+        executable, args = args[0], args[1:]
 
-        process = subprocess.Popen(
-            args, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-            shell=shell)
-        stdout, stderr = process.communicate()
+        # Find executable in path.
+        executable_path = which(executable)
+        if executable_path is None:
+            raise RuntimeError(
+                "Can't find '{0}' in path.".format(executable))
+
+        stdout, stderr, exitcode = yield utils.getProcessOutputAndValue(
+            executable_path, args)
+
         if stderr:
             self._log("stderr:\n{0}".format(stderr))
         if stdout:
             self._log("stdout:\n{0}".format(stdout))
-        self._log("Process finished with code {0}".format(process.returncode))
-        assert process.returncode == 0, "command failed: '{0}'".format(
-            command_str)
-        return stdout, stderr
+        self._log("Process finished with code {0}".format(exitcode))
+        assert exitcode == 0, "command failed: '{0}'".format(command_str)
 
-    def test_master_worker_setup(self):
-        os.chdir(self.projectdir)
+        defer.returnValue((stdout, stderr))
 
-        master_dir = "master-dir"
-        worker_dir = "worker-dir"
-
-        # Create master.
-        stdout = self._run_command(
-            ["buildbot", "create-master", master_dir])[0]
+    @defer.inlineCallbacks
+    def _buildbot_create_master(self, master_dir):
+        """Runs "buildbot create-master" and checks result"""
+        assert self.master_dir is None
+        self.master_dir = master_dir
+        stdout, _ = yield self._run_command(
+            ["buildbot", "create-master", master_dir])
         self.assertIn("buildmaster configured in", stdout)
 
-        # Copy master.cfg with disabling www plugins (they are not installed
-        # on Travis).
-        with open(os.path.join(master_dir, "master.cfg.sample"), "rt") as f:
-            master_cfg = f.read()
-        master_cfg = re.sub(r"plugins=dict\([^)]+\)", "plugins={}", master_cfg)
-        with open(os.path.join(master_dir, "master.cfg"), "wt") as f:
-            f.write(master_cfg)
-
-        # Create worker.
-        stdout = self._run_command([
-            "buildbot-worker", "create-worker", worker_dir, "localhost",
-            "example-worker", "pass"])[0]
+    @defer.inlineCallbacks
+    def _buildbot_worker_create_worker(self, worker_dir):
+        self.workers_dirs.append(worker_dir)
+        master_addr = "localhost:{port}".format(port=self.master_port)
+        stdout, _ = yield self._run_command([
+            "buildbot-worker", "create-worker", worker_dir, master_addr,
+            "example-worker", "pass"])
         self.assertIn("worker configured in", stdout)
 
-        try:
-            # Start master.
-            stdout = self._run_command(["buildbot", "start", master_dir])[0]
-        except:
-            # Try to stop master in case it's process is not actually stopped.
-            self._run_command(["buildbot", "stop", master_dir])
-            raise
-
+    @defer.inlineCallbacks
+    def _buildbot_start(self, master_dir):
+        stdout, _ = yield self._run_command(["buildbot", "start", master_dir])
         self.assertIn(
             "The buildmaster appears to have (re)started correctly",
             stdout)
 
-        try:
-            # Start worker.
-            stdout = self._run_command([
-                "buildbot-worker", "start", worker_dir])[0]
-        except:
-            # Try to stop master in case it's process is not actually stopped.
-            self._run_command(["buildbot-worker", "stop", worker_dir])
-            raise
+    @defer.inlineCallbacks
+    def _buildbot_stop(self, master_dir):
+        stdout, _ = yield self._run_command(["buildbot", "stop", master_dir])
+        self.assertRegexpMatches(stdout, r"buildbot process \d+ is dead")
+
+    @defer.inlineCallbacks
+    def _buildbot_worker_start(self, worker_dir):
+        # Start worker.
+        stdout, _ = yield self._run_command([
+            "buildbot-worker", "start", worker_dir])
 
         self.assertIn(
-            "The buildbot-worker appears to have (re)started "
-            "correctly",
+            "The buildbot-worker appears to have (re)started correctly",
             stdout)
 
-        # Stop worker.
-        stdout = self._run_command(["buildbot-worker", "stop", worker_dir])[0]
+    @defer.inlineCallbacks
+    def _buildbot_worker_stop(self, worker_dir):
+        stdout, _ = yield self._run_command(
+            ["buildbot-worker", "stop", worker_dir])
         self.assertRegexpMatches(stdout, r"worker process \d+ is dead")
 
+    @defer.inlineCallbacks
+    def test_master_worker_setup(self):
+        """Create master and worker (with default pyflakes configuration),
+        start them, stop them.
+        """
+
+        # Create master.
+        master_dir = "master-dir"
+        yield self._buildbot_create_master(master_dir)
+
+        # Create master.cfg based on sample file.
+        sample_config = os.path.join(master_dir, "master.cfg.sample")
+        with open(sample_config, "rt") as f:
+            master_cfg = f.read()
+
+        # Disable www plugins (they are not installed on Travis).
+        master_cfg = re.sub(r"plugins=dict\([^)]+\)", "plugins={}", master_cfg)
+
+        # Substitute ports to listen.
+        master_cfg = master_cfg.replace('9989', str(self.master_port))
+        master_cfg = master_cfg.replace('8010', str(self.ui_port))
+
+        with open(os.path.join(master_dir, "master.cfg"), "wt") as f:
+            f.write(master_cfg)
+
+        # Create worker.
+        worker_dir = "worker-dir"
+        yield self._buildbot_worker_create_worker(worker_dir)
+
+        # Start master.
+        yield self._buildbot_start(master_dir)
+
+        # Start worker.
+        yield self._buildbot_worker_start(worker_dir)
+
+        # Stop worker.
+        yield self._buildbot_worker_stop(worker_dir)
+
         # Stop master.
-        stdout = self._run_command(["buildbot", "stop", master_dir])[0]
-        self.assertRegexpMatches(stdout, r"buildbot process \d+ is dead")
+        yield self._buildbot_stop(master_dir)
 
         self.success = True
