77f1291f45643c9a0deb9330ce5782661395bf1b
Vladimir Rutsky <vladimir@rutsky.org>
2016-05-12 13:57:28 +0300

    add unit test for _wrapRemoteException

--- master/buildbot/test/unit/test_worker_protocols_pb.py
+++ master/buildbot/test/unit/test_worker_protocols_pb.py
@@ -256,3 +256,24 @@ class TestConnection(unittest.TestCase):
         conn.perspective_keepalive()
 
         conn.worker.messageReceivedFromWorker.assert_called_with()
+
+
+class Test_wrapRemoteException(unittest.TestCase):
+
+    def test_raises_NoSuchMethod(self):
+        def f():
+            with pb._wrapRemoteException():
+                raise twisted_pb.RemoteError(
+                    'twisted.spread.flavors.NoSuchMethod', None, None)
+
+        self.assertRaises(pb._NoSuchMethod, f)
+
+    def test_raises_unknown(self):
+        class Error(Exception):
+            pass
+
+        def f():
+            with pb._wrapRemoteException():
+                raise Error()
+
+        self.assertRaises(Error, f)
