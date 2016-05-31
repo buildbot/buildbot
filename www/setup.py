50b3f93a120b1ecfcbf0a8b373b46a3050d7e010
Christian Kjær Larsen <christianden@gmail.com>
2016-05-31 08:47:27 +0200

    Moved www into seperate setup

---
+++ www/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+"""
+Standard setup script.
+"""
+
+from distutils.core import setup
+
+
+setup_args = {
+    'name': "www",
+    'description': "Katana frontend",
+    'long_description': "JavaScript frontend for katana",
+    'package_dir': {'www': ''},
+    'packages': ["www"],
+    'include_package_data': True
+}
+
+setup(**setup_args)
+
+# Local Variables:
+# fill-column: 71
+# End:
