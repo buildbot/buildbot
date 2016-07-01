0fc406cb5c00cdbdbf8667827a35d7479e1091e8
Christopher Chedeau <vjeuxx@gmail.com>
2016-07-01 12:42:02 -0700

    Testing mention-bot double notification fix
    Please ignore this pull request

--- master/buildbot/reporters/utils.py
+++ master/buildbot/reporters/utils.py
@@ -1,3 +1,5 @@
+Testing mention bot
+
 # This file is part of Buildbot.  Buildbot is free software: you can
 # redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, version 2.
