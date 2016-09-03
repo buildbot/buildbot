1a4085ced0ae05f4478c6030e414016ab446f362
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-09-03 10:58:40 -0700

    Replace types.StringTypes with future.utils.string_types.
    types.StringTypes is gone in Python 3.

--- master/buildbot/steps/source/p4.py
+++ master/buildbot/steps/source/p4.py
@@ -16,7 +16,6 @@
 from future.utils import string_types
 
 import re
-from types import StringType
 
 from twisted.internet import defer
 from twisted.python import log
@@ -92,7 +91,7 @@ class P4(Source):
             config.error(
                 "Either provide p4viewspec or p4base and p4branch (and optionally p4extra_views")
 
-        if p4viewspec and isinstance(p4viewspec, StringType):
+        if p4viewspec and isinstance(p4viewspec, string_types):
             config.error(
                 "p4viewspec must not be a string, and should be a sequence of 2 element sequences")
 
