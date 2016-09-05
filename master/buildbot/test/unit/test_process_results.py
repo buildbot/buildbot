29d96c69dae2bd25160090d4cc2dc717f29a644b
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-09-04 17:33:49 -0700

    Replace dict.sort(cmp=) with dict.sort(key=).
    dict.sort(cmp=) is gone in Python 3.
    Test worst_status() in a slightly different way,
    and create a new test test_sort_worst_status()
    to sort the list of statuses, with SKIPPED coming first in the
    list.

--- master/buildbot/test/unit/test_process_results.py
+++ master/buildbot/test/unit/test_process_results.py
@@ -27,9 +27,15 @@ class TestResults(unittest.TestCase):
             self.assertEqual(results.Results[i], r)
 
     def test_worst_status(self):
+        self.assertEqual(results.WARNINGS,
+            results.worst_status(results.SUCCESS, results.WARNINGS))
+        self.assertEqual(results.CANCELLED,
+            results.worst_status(results.SKIPPED, results.CANCELLED))
+
+    def test_sort_worst_status(self):
         res = lrange(len(results.Results))
         res.sort(
-            cmp=lambda a, b: 1 if (results.worst_status(a, b) == a) else -1)
+            key=lambda a: a if a != results.SKIPPED else -1)
         self.assertEqual(res, [
             results.SKIPPED,
             results.SUCCESS,
