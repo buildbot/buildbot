efd5f776146fb5cb978135e56e16691a8b3b6e97
BarryUnity <barry@unity3d.com>
2016-09-06 18:40:52 +0200

    Fixing unit test

--- master/buildbot/test/unit/test_status_web_status_json.py
+++ master/buildbot/test/unit/test_status_web_status_json.py
@@ -217,7 +217,7 @@ class TestBuilderSlavesJsonResources(unittest.TestCase):
                              'version': None, 'connected': False, 'eid': -1, 'lastMessage': 0,
                              'health': 0,
                              'fqdn': None,
-                             'managerUrl': 'No Slave Manager URL Configured',
+                             'managerUrl': None,
                              'paused': False,
                              'graceful_shutdown': False,
                              'builders': [
