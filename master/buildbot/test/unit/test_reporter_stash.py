40cbceb4aca13e124052d5bdb4be124ee4ff14b3
Pierre Tardy <tardyp@gmail.com>
2016-10-26 22:17:09 +0200

    port reporterstash to httpclientservice

--- master/buildbot/test/unit/test_reporter_stash.py
+++ master/buildbot/test/unit/test_reporter_stash.py
@@ -22,6 +22,7 @@ from buildbot import config
 from buildbot.process.results import FAILURE
 from buildbot.process.results import SUCCESS
 from buildbot.reporters.stash import StashStatusPush
+from buildbot.test.fake import httpclientservice as fakehttpclientservice
 from buildbot.test.fake import fakemaster
 from buildbot.test.util.reporter import ReporterTestMixin
 
@@ -31,20 +32,20 @@ class TestStashStatusPush(unittest.TestCase, ReporterTestMixin):
     @defer.inlineCallbacks
     def setUp(self):
         # ignore config error if txrequests is not installed
-        config._errors = Mock()
+        self.patch(config, '_errors', Mock())
         self.master = fakemaster.make_master(testcase=self,
                                              wantData=True, wantDb=True, wantMq=True)
 
+        self.http = yield fakehttpclientservice.HTTPClientService.getFakeService(
+            self.master, self,
+            'serv', auth=('username', 'passwd'))
         self.sp = sp = StashStatusPush("serv", "username", "passwd")
-        sp.sessionFactory = Mock(return_value=Mock())
         yield sp.setServiceParent(self.master)
-        yield sp.startService()
+        yield self.master.startService()
 
     @defer.inlineCallbacks
     def tearDown(self):
-        yield self.sp.stopService()
-        self.assertEqual(self.sp.session.close.call_count, 1)
-        config._errors = None
+        yield self.master.stopService()
 
     @defer.inlineCallbacks
     def setupBuildResults(self, buildResults):
@@ -55,24 +56,25 @@ class TestStashStatusPush(unittest.TestCase, ReporterTestMixin):
     @defer.inlineCallbacks
     def test_basic(self):
         build = yield self.setupBuildResults(SUCCESS)
+        # we make sure proper calls to txrequests have been made
+        self.http.expect(
+            'post',
+            u'/rest/build-status/1.0/commits/d34db33fd43db33f',
+            json={'url': 'http://localhost:8080/#builders/79/builds/0',
+                  'state': 'INPROGRESS', 'key': u'Builder0'})
+        self.http.expect(
+            'post',
+            u'/rest/build-status/1.0/commits/d34db33fd43db33f',
+            json={'url': 'http://localhost:8080/#builders/79/builds/0',
+                  'state': 'SUCCESSFUL', 'key': u'Builder0'})
+        self.http.expect(
+            'post',
+            u'/rest/build-status/1.0/commits/d34db33fd43db33f',
+            json={'url': 'http://localhost:8080/#builders/79/builds/0',
+                  'state': 'FAILED', 'key': u'Builder0'})
         build['complete'] = False
         self.sp.buildStarted(("build", 20, "started"), build)
         build['complete'] = True
         self.sp.buildFinished(("build", 20, "finished"), build)
         build['results'] = FAILURE
         self.sp.buildFinished(("build", 20, "finished"), build)
-        # we make sure proper calls to txrequests have been made
-        self.assertEqual(
-            self.sp.session.post.mock_calls,
-            [call(u'serv/rest/build-status/1.0/commits/d34db33fd43db33f',
-                  json={'url': 'http://localhost:8080/#builders/79/builds/0',
-                        'state': 'INPROGRESS', 'key': u'Builder0'},
-                  auth=('username', 'passwd')),
-             call(u'serv/rest/build-status/1.0/commits/d34db33fd43db33f',
-                  json={'url': 'http://localhost:8080/#builders/79/builds/0',
-                        'state': 'SUCCESSFUL', 'key': u'Builder0'},
-                  auth=('username', 'passwd')),
-             call(u'serv/rest/build-status/1.0/commits/d34db33fd43db33f',
-                  json={'url': 'http://localhost:8080/#builders/79/builds/0',
-                        'state': 'FAILED', 'key': u'Builder0'},
-                  auth=('username', 'passwd'))])
