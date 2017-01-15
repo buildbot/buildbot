c7ee81aa77ca51f3adb3e5765195a599b5fc5f6b
Vladimir Rutsky <vladimir@rutsky.org>
2017-01-16 02:28:47 +0300

    reorder imports according to pylint

--- master/buildbot/db/model.py
+++ master/buildbot/db/model.py
@@ -18,8 +18,8 @@ from __future__ import print_function
 
 import migrate
 import migrate.versioning.repository
-import sqlalchemy as sa
 from migrate import exceptions
+import sqlalchemy as sa
 
 from twisted.python import log
 from twisted.python import util
