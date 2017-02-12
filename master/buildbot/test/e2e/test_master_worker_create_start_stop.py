f651fef68f6c02988b690d5401c0496770086454
Vladimir Rutsky <vladimir@rutsky.org>
2017-02-12 19:37:20 +0300

    move indent() backport to buildbot.util.indent

--- master/buildbot/test/e2e/test_master_worker_create_start_stop.py
+++ master/buildbot/test/e2e/test_master_worker_create_start_stop.py
@@ -36,28 +36,7 @@ from twisted.trial import unittest
 from buildbot.test.util import dirs
 from buildbot.test.util.decorators import skipIf
 from buildbot.test.util.decorators import skipUnlessPlatformIs
-
-try:
-    from textwrap import indent
-except ImportError:
-    # textwrap.indent() implementation from Python 3.3
-    def indent(text, prefix, predicate=None):
-        """Adds 'prefix' to the beginning of selected lines in 'text'.
-
-        If 'predicate' is provided, 'prefix' will only be added to the lines
-        where 'predicate(line)' is True. If 'predicate' is not provided,
-        it will default to adding 'prefix' to all non-empty lines that do not
-        consist solely of whitespace characters.
-        """
-        if predicate is None:
-            def predicate(line):  # pylint: disable=function-redefined
-                return line.strip()
-
-        def prefixed_lines():
-            for line in text.splitlines(True):
-                yield (prefix + line if predicate(line) else line)
-        return ''.join(prefixed_lines())
-
+from buildbot.util.indent import indent
 
 try:
     from shutil import which
