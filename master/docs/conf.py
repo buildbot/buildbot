54758fb2fa22c84e0b9e3c0f9badee85573da9af
Pierre Tardy <tardyp@gmail.com>
2017-03-31 11:35:49 +0200

    newsfragment

--- master/docs/conf.py
+++ master/docs/conf.py
@@ -146,8 +146,9 @@ intersphinx_mapping = {
 }
 
 extlinks = {
-    'bug': ('http://trac.buildbot.net/ticket/%s', 'bug #'),
     'pull': ('https://github.com/buildbot/buildbot/pull/%s', 'pull request '),
+    'issue': ('https://github.com/buildbot/buildbot/issue/%s', 'issue # '),
+    'bug': ('http://trac.buildbot.net/ticket/%s', 'bug #'),  # deprecated. Use issue instead, and point to GH
     # Renders as link with whole url, e.g.
     #   :src-link:`master`
     # renders as
