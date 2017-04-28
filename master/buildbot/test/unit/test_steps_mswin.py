cb71f8752b3a09b696448eca440e7b3ca23c2c34
Steve Hoelzer <shoelzer@gmail.com>
2017-04-28 09:12:13 -0500

    Fix Robocopy verbose arguments and add test

--- master/buildbot/test/unit/test_steps_mswin.py
+++ master/buildbot/test/unit/test_steps_mswin.py
@@ -112,6 +112,12 @@ class TestRobocopySimple(steps.BuildStepMixin, unittest.TestCase):
             expected_args=['blah*', '/XD', 'foo', 'bar']
         )
 
+    def test_verbose_output(self):
+        return self._run_simple_test(
+            r'D:\source', r'E:\dest', files=['*.foo'], verbose=True,
+            expected_args=['*.foo', '/V', '/TS', '/FP']
+        )
+
     @defer.inlineCallbacks
     def test_codes(self):
         # Codes that mean uneventful copies (including no copy at all).
