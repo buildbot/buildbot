9f4d1b363cf7adfcdedf99e1fc343115a63b9ed2
Steve Hoelzer <shoelzer@gmail.com>
2017-05-03 12:20:17 -0500

    Add test for Robocopy custom_opts

--- master/buildbot/test/unit/test_steps_mswin.py
+++ master/buildbot/test/unit/test_steps_mswin.py
@@ -112,6 +112,12 @@ class TestRobocopySimple(steps.BuildStepMixin, unittest.TestCase):
             expected_args=['blah*', '/XD', 'foo', 'bar']
         )
 
+    def test_custom_opts(self):
+        return self._run_simple_test(
+            r'D:\source', r'E:\dest', files=['*.foo'], custom_opts=['/R:10', '/W:60'],
+            expected_args=['*.foo', '/R:10', '/W:60']
+        )
+
     def test_verbose_output(self):
         return self._run_simple_test(
             r'D:\source', r'E:\dest', files=['*.foo'], verbose=True,
