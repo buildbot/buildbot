0bfd96cafecdfeefe978f0edc73d4e035ac44144
Remy  Protat <remy.protat@intel.com>
2017-05-15 14:37:31 +0200

    filterwarning uses repr string format

--- master/buildbot/test/__init__.py
+++ master/buildbot/test/__init__.py
@@ -134,4 +134,4 @@ warnings.filterwarnings('ignore',
                         "deprecated, use Unicode filenames instead")
 # moto warning v1.0.0
 warnings.filterwarnings('ignore', "Flags not at the start of the expression")
-warnings.filterwarnings('ignore', "object() takes no parameters")
+warnings.filterwarnings('ignore', r"object\(\) takes no parameters")
