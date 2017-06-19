buildbot/buildbot

commit 395425eeb1ff03ac05df8cb69cbd69b9aef9a9f1

added optional VENV_PY_VERSION arg for virtualenv make target

Makefile
@@ -5,6 +5,9 @@ DOCKERBUILD := docker build --build-arg http_proxy=$$http_proxy --build-arg http
 
 PIP?=pip
 
+VENV_NAME:=.venv$(VENV_PY_VERSION)
+VENV_PY_VERSION?=python
+
 # build rst documentation
 docs:
 	$(MAKE) -C master/docs
@@ -60,27 +63,27 @@ docker-buildbot-master:
 docker-buildbot-master-ubuntu:
 	$(DOCKERBUILD) -t buildbot/buildbot-master-ubuntu:master -f master/Dockerfile.ubuntu master
 
-.venv:
-		virtualenv .venv
-		.venv/bin/pip install -U pip setuptools
-		.venv/bin/pip install -e pkg \
-			-e 'master[tls,test,docs]' \
-			-e 'worker[test]' \
-			buildbot_www \
-			'git+https://github.com/tardyp/towncrier'
+$(VENV_NAME):
+	virtualenv -p $(VENV_PY_VERSION) $(VENV_NAME)
+	$(VENV_NAME)/bin/pip install -U pip
+	$(VENV_NAME)/bin/pip install -e pkg \
+		-e 'master[tls,test,docs]' \
+		-e 'worker[test]' \
+		buildbot_www \
+		'git+https://github.com/tardyp/towncrier'
 
 # helper for virtualenv creation
-virtualenv: .venv
+virtualenv: $(VENV_NAME)   # usage: make virtaulenv VENV_PY_VERSION=python3.4
 	@echo now you can type following command  to activate your virtualenv
-	@echo . .venv/bin/activate
+	@echo . $(VENV_NAME)/bin/activate
 
-release_notes: .venv
+release_notes: $(VENV_NAME)
 	test ! -z "$(VERSION)"  #  usage: make release_notes VERSION=0.9.2
 	yes | towncrier --version $(VERSION) --date `date -u  +%F`
 	git commit -m "relnotes for $(VERSION)"
 
 # helper for release creation
-release: .venv
+release: $(VENV_NAME)
 	test ! -z "$(VERSION)"  #  usage: make release VERSION=0.9.2
 	GPG_TTY=`tty` git tag -a -sf v$(VERSION) -m "TAG $(VERSION)"
 	./common/maketarballs.sh

commit ff3f865280b61fb5d0c6eb9f11c49803ebd727e9

virtualenv: upgrade setuptools to avoid errors
This problem was fixed in commit d2b9a0b6eb0e ("virtualenv: upgrade
setuptools to avoid errors") but was re-introduced recently.

Fixes: 395425eeb1ff ("added optional VENV_PY_VERSION arg for virtualenv make target")

Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

Makefile
@@ -65,7 +65,7 @@ docker-buildbot-master-ubuntu:
 
 $(VENV_NAME):
 	virtualenv -p $(VENV_PY_VERSION) $(VENV_NAME)
-	$(VENV_NAME)/bin/pip install -U pip
+	$(VENV_NAME)/bin/pip install -U pip setuptools
 	$(VENV_NAME)/bin/pip install -e pkg \
 		-e 'master[tls,test,docs]' \
 		-e 'worker[test]' \