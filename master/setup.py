e14e5f88d9bb1591cd0e7a54d8cd1c1f69e73d6c
Craig Rodrigues <rodrigc@freebsd.org>
2017-06-23 22:57:10 -0700

    Add Python 3.4 to setuptools metadata for Pypi

--- master/setup.py
+++ master/setup.py
@@ -158,6 +158,7 @@ setup_args = {
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ],
--- worker/setup.py
+++ worker/setup.py
@@ -89,6 +89,7 @@ setup_args = {
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
