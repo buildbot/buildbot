20cbec271909290a97024f18acfd8a8cdcbe012b
Craig Rodrigues <rodrigc@freebsd.org>
2017-06-23 22:23:06 -0700

    Clean up some bytes/unicode issues
    - get rid of _decode() method, since _dovccmd() already decodes
    - replace bytes2NativeString with bytes2unicode
    - no need to decode args in _convert_nonzero_to_failure()

--- master/buildbot/changes/gitpoller.py
+++ master/buildbot/changes/gitpoller.py
@@ -29,7 +29,6 @@ from twisted.python import log
 from buildbot import config
 from buildbot.changes import base
 from buildbot.util import ascii2unicode
-from buildbot.util import bytes2NativeString
 from buildbot.util import bytes2unicode
 from buildbot.util import unicode2bytes
 from buildbot.util.state import StateMixin
@@ -122,7 +121,7 @@ class GitPoller(base.PollingChangeSource, StateMixin):
 
     def describe(self):
         str = ('GitPoller watching the remote git repository ' +
-               bytes2NativeString(self.repourl, self.encoding))
+               bytes2unicode(self.repourl, self.encoding))
 
         if self.branches:
             if self.branches is True:
@@ -198,7 +197,7 @@ class GitPoller(base.PollingChangeSource, StateMixin):
             try:
                 rev = yield self._dovccmd(
                     'rev-parse', [self._trackerBranch(branch)], path=self.workdir)
-                revs[branch] = bytes2NativeString(rev, self.encoding)
+                revs[branch] = bytes2unicode(rev, self.encoding)
                 yield self._process_changes(revs[branch], branch)
             except Exception:
                 log.err(_why="trying to poll branch %s of %s"
@@ -207,13 +206,9 @@ class GitPoller(base.PollingChangeSource, StateMixin):
         self.lastRev.update(revs)
         yield self.setState('lastRev', self.lastRev)
 
-    def _decode(self, git_output):
-        return bytes2unicode(git_output, self.encoding)
-
     def _get_commit_comments(self, rev):
         args = ['--no-walk', r'--format=%s%n%b', rev, '--']
         d = self._dovccmd('log', args, path=self.workdir)
-        d.addCallback(self._decode)
         return d
 
     def _get_commit_timestamp(self, rev):
@@ -243,7 +238,7 @@ class GitPoller(base.PollingChangeSource, StateMixin):
             match = re.match('^"(.*)"$', file)
             if match:
                 file = bytes2unicode(match.groups()[0], 'unicode_escape')
-            return self._decode(file)
+            return bytes2unicode(file)
 
         @d.addCallback
         def process(git_output):
@@ -259,7 +254,6 @@ class GitPoller(base.PollingChangeSource, StateMixin):
 
         @d.addCallback
         def process(git_output):
-            git_output = self._decode(git_output)
             if not git_output:
                 raise EnvironmentError('could not get commit author for rev')
             return git_output
@@ -361,9 +355,8 @@ class GitPoller(base.PollingChangeSource, StateMixin):
                                         path):
             "utility to handle the result of getProcessOutputAndValue"
             (stdout, stderr, code) = res
-            stdout = bytes2NativeString(stdout, self.encoding)
-            stderr = bytes2NativeString(stderr, self.encoding)
-            args = bytes2NativeString(args, self.encoding)
+            stdout = bytes2unicode(stdout, self.encoding)
+            stderr = bytes2unicode(stderr, self.encoding)
             if code != 0:
                 if code == 128:
                     raise GitError('command %s %s in %s on repourl %s failed with exit code %d: %s'
