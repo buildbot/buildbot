buildbot/buildbot

commit f098bdd5d7fbefd8e4567b8052a90e119c5f12a8

www: add grid_view plugin
Add a grid view plugin.

It is possible to display changes in all branches or restrict to a
specific branch. It is also possible to filter builders by clicking on
builder tags.

In the settings page, the user can specify how many changes are
displayed at max plus other more internal stuff.

Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

www/grid_view/src/module/main.module.coffee
@@ -1,0 +1,76 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+# Register new module
+class BuildbotGridView extends App
+    constructor: ->
+        return [
+            'ui.router'
+            'ui.bootstrap'
+            'ngAnimate'
+            'guanlecoja.ui'
+            'bbData'
+        ]
+
+# Register new state
+class State extends Config
+    constructor: ($stateProvider, glMenuServiceProvider, bbSettingsServiceProvider) ->
+        # Menu configuration
+        glMenuServiceProvider.addGroup(
+            name: 'grid'
+            caption: 'Grid View'
+            icon: 'cubes'
+            order: 4
+        )
+
+        # Register URL routing
+        $stateProvider
+            .state(
+                name: 'grid'
+                controller: 'gridController'
+                controllerAs: 'C'
+                templateUrl: 'buildbot_grid_view/views/grid.html'
+                url: '/grid?branch&tag'
+                reloadOnSearch: false
+                data:
+                    group: 'grid'
+                    caption: 'Grid View'
+            )
+
+        bbSettingsServiceProvider.addSettingsGroup(
+            name: 'Grid'
+            caption: 'Grid related settings'
+            items: [
+                    type: 'bool'
+                    name: 'compactChanges'
+                    caption: 'Hide avatar and time ago from change details'
+                    defaultValue: true
+                ,
+                    type: 'integer'
+                    name: 'revisionLimit'
+                    caption: 'Maximum number of revisions to display'
+                    default_value: 5
+                ,
+                    type: 'integer'
+                    name: 'changeFetchLimit'
+                    caption: 'Maximum number of changes to fetch'
+                    default_value: 100
+                ,
+                    type: 'integer'
+                    name: 'buildFetchLimit'
+                    caption: 'Maximum number of builds to fetch'
+                    default_value: 1000
+            ]
+        )

Makefile
@@ -34,7 +34,7 @@ flake8:
 frontend:
 	$(PIP) install -e pkg
 	$(PIP) install mock
-	for i in base wsgi_dashboards codeparameter console_view waterfall_view nestedexample; do $(PIP) install -e www/$$i || exit 1; done
+	for i in base wsgi_dashboards codeparameter console_view grid_view waterfall_view nestedexample; do $(PIP) install -e www/$$i || exit 1; done
 
 # do installation tests. Test front-end can build and install for all install methods
 frontend_install_tests:

common/maketarballs.sh
@@ -4,7 +4,7 @@ rm -rf dist
 mkdir dist
 pip install mock wheel
 set -e
-for pkg in pkg master worker www/base www/console_view www/waterfall_view www/wsgi_dashboards
+for pkg in pkg master worker www/base www/console_view www/grid_view www/waterfall_view www/wsgi_dashboards
 do
   pip install -e ${pkg}
   (

common/validate.sh
@@ -143,7 +143,7 @@ echo "${MAGENTA}Validating the following commits:${NORM}"
 git log "$REVRANGE" --pretty=oneline || exit 1
 
 if ! $quick && ! $no_js; then
-    for module in www/base www/console_view www/waterfall_view www/codeparameter www/wsgi_dashboards;
+    for module in www/base www/console_view www/grid_view www/waterfall_view www/codeparameter www/wsgi_dashboards;
     do
         status "running 'setup.py develop' for $module"
         if ! (cd $module; python setup.py develop >/dev/null ); then

www/grid_view/buildbot_grid_view/__init__.py
@@ -1,0 +1,19 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+from buildbot.www.plugin import Application
+
+# create the interface for the setuptools entry point
+ep = Application(__name__, "Buildbot Grid View plugin")

www/grid_view/guanlecoja/config.coffee
@@ -1,0 +1,34 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+module.exports =
+    name: 'grid_view'
+    dir: build: 'buildbot_grid_view/static'
+    bower:
+        testdeps:
+            # vendors.js includes jquery, angularjs, etc in the right order
+            "guanlecoja-ui":
+                version: '~1.6.0'
+                files: ['vendors.js', 'scripts.js']
+            "angular-mocks":
+                version: '~1.5.3'
+                files: "angular-mocks.js"
+            'buildbot-data':
+                version: '~2.2.0'
+                files: 'dist/buildbot-data.js'
+
+    karma:
+        # we put tests first, so that we have angular, and fake app defined
+        files: ["tests.js", "scripts.js", 'fixtures.js']

www/grid_view/gulpfile.js
@@ -1,0 +1,1 @@
+require('guanlecoja')(require('gulp'));

www/grid_view/setup.cfg
@@ -1,0 +1,2 @@
+[bdist_wheel]
+universal=1

commit 60e54481bba52741c0f3078d4aeba22dcca42dd3

grid_view: fix templateUrl
Fixes: f098bdd5d7fb ("www: add grid_view plugin")
Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

www/grid_view/src/module/main.module.coffee
@@ -41,7 +41,7 @@ class State extends Config
                 name: 'grid'
                 controller: 'gridController'
                 controllerAs: 'C'
-                templateUrl: 'buildbot_grid_view/views/grid.html'
+                templateUrl: 'grid_view/views/grid.html'
                 url: '/grid?branch&tag'
                 reloadOnSearch: false
                 data: