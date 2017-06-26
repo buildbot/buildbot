buildbot/buildbot

commit f098bdd5d7fbefd8e4567b8052a90e119c5f12a8

www: add grid_view plugin
Add a grid view plugin.

It is possible to display changes in all branches or restrict to a
specific branch. It is also possible to filter builders by clicking on
builder tags.

In the settings page, the user can specify how many changes are
displayed at max plus other more internal stuff.

Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

www/grid_view/src/module/grid.controller.coffee
@@ -1,0 +1,167 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+class Grid extends Controller
+    constructor: (@$scope, @$stateParams, @$state, resultsService, dataService, bbSettingsService) ->
+        _.mixin(@$scope, resultsService)
+        @data = dataService.open().closeOnDestroy(@$scope)
+
+        @branch = @$stateParams.branch
+        @tags = @$stateParams.tag ? []
+        if not angular.isArray(@tags)
+            @tags = [@tags]
+
+        settings = bbSettingsService.getSettingsGroup('Grid')
+        @revisionLimit = settings.revisionLimit.value
+        @changeFetchLimit = settings.changeFetchLimit.value
+        @buildFetchLimit = settings.buildFetchLimit.value
+        @compactChanges = settings.compactChanges.value
+
+        @buildsets = @data.getBuildsets(
+            limit: @changeFetchLimit
+            order: '-bsid'
+            property: ['bsid', 'sourcestamps']
+        )
+        @changes = @data.getChanges(
+            limit: @changeFetchLimit
+            order: '-changeid'
+        )
+        @builders = @data.getBuilders(
+            property: ['builderid', 'name']
+        )
+        @buildrequests = @data.getBuildrequests(
+            limit: @buildFetchLimit
+            order: '-buildrequestid'
+            property: ['buildrequestid', 'buildsetid', 'builderid']
+        )
+        @builds = @data.getBuilds(
+            limit: @buildFetchLimit
+            order: '-buildrequestid'
+        )
+
+        @buildsets.onChange = @changes.onChange = @builders.onChange = \
+            @buildrequests.change = @builds.onChange = @onChange
+
+    dataReady: ->
+        for collection in [@buildsets, @changes, @builders, @buildrequests, @builds]
+            if not (collection.$resolved and collection.length > 0)
+                return false
+        return true
+
+    dataFetched: ->
+        for collection in [@buildsets, @changes, @builders, @buildrequests, @builds]
+            if not collection.$resolved
+                return false
+        return true
+
+    onChange: =>
+        if not @dataReady()
+            return
+
+        changesByBSID = {}
+        @$scope.changes = []
+        branches = {}
+
+        changesBySSID = {}
+        for c in @changes
+            changesBySSID[c.sourcestamp.ssid] = c
+
+        for bset in @buildsets
+            change = changesBySSID[_.last(bset.sourcestamps).ssid]
+            unless change?
+                continue
+            change.bsid = bset.bsid
+            change.branch ?= 'master'
+            branches[change.branch] = true
+
+            if @$scope.changes.length > @revisionLimit
+                continue
+            if @branch and change.branch != @branch
+                continue
+
+            @$scope.changes.push(change)
+            changesByBSID[bset.bsid] = change
+
+        @$scope.changes.sort((a, b) -> a.bsid - b.bsid)
+
+        @$scope.branches = (br for br of branches)
+
+        requestsByBSID = {}
+        for req in @buildrequests
+            (requestsByBSID[req.buildsetid] ?= []).push(req)
+        buildByReqID = {}
+        for build in @builds
+            buildByReqID[build.buildrequestid] = build
+
+        for builder in @builders
+            builder.builds = {}
+
+        buildersById = {}
+        # find builds for the selected changes and associate them to builders
+        for c in @$scope.changes
+            requests = requestsByBSID[c.bsid]
+            unless requests?
+                continue
+            for req in requests
+                build = buildByReqID[req.buildrequestid]
+                unless build?
+                    continue
+                builder = @builders.get(build.builderid)
+                unless @isBuilderDisplayed(builder)
+                    continue
+                buildersById[builder.builderid] = builder
+                builder.builds[c.bsid] = build
+
+        @$scope.builders = (buildersById[i] for i of buildersById)
+
+    changeBranch: (branch) =>
+        @branch = branch
+        @refresh()
+
+    toggleTag: (tag) =>
+        i = @tags.indexOf(tag)
+        if i < 0
+            @tags.push(tag)
+        else
+            @tags.splice(i, 1)
+        @refresh()
+
+    resetTags: =>
+        @tags = []
+        @refresh()
+
+    refresh: =>
+        @$stateParams.branch = @branch
+        if @tags.length == 0
+            @$stateParams.tag = undefined
+        else
+            @$stateParams.tag = @tags
+
+        params =
+            branch: @$stateParams.branch
+            tag: @$stateParams.tag
+
+        # change URL without reloading page
+        @$state.transitionTo(@$state.current, params, {notify: false})
+        @onChange()
+
+    isBuilderDisplayed: (builder) =>
+        for tag in @tags
+            if builder.tags.indexOf(tag) < 0
+                return false
+        return true
+
+    isTagToggled: (tag) =>
+        return @tags.indexOf(tag) >= 0

Makefile
@@ -34,7 +34,7 @@ flake8:
 frontend:
 	$(PIP) install -e pkg
 	$(PIP) install mock
-	for i in base wsgi_dashboards codeparameter console_view waterfall_view nestedexample; do $(PIP) install -e www/$$i || exit 1; done
+	for i in base wsgi_dashboards codeparameter console_view grid_view waterfall_view nestedexample; do $(PIP) install -e www/$$i || exit 1; done
 
 # do installation tests. Test front-end can build and install for all install methods
 frontend_install_tests:

common/maketarballs.sh
@@ -4,7 +4,7 @@ rm -rf dist
 mkdir dist
 pip install mock wheel
 set -e
-for pkg in pkg master worker www/base www/console_view www/waterfall_view www/wsgi_dashboards
+for pkg in pkg master worker www/base www/console_view www/grid_view www/waterfall_view www/wsgi_dashboards
 do
   pip install -e ${pkg}
   (

common/validate.sh
@@ -143,7 +143,7 @@ echo "${MAGENTA}Validating the following commits:${NORM}"
 git log "$REVRANGE" --pretty=oneline || exit 1
 
 if ! $quick && ! $no_js; then
-    for module in www/base www/console_view www/waterfall_view www/codeparameter www/wsgi_dashboards;
+    for module in www/base www/console_view www/grid_view www/waterfall_view www/codeparameter www/wsgi_dashboards;
     do
         status "running 'setup.py develop' for $module"
         if ! (cd $module; python setup.py develop >/dev/null ); then

www/grid_view/buildbot_grid_view/__init__.py
@@ -1,0 +1,19 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+from buildbot.www.plugin import Application
+
+# create the interface for the setuptools entry point
+ep = Application(__name__, "Buildbot Grid View plugin")

www/grid_view/guanlecoja/config.coffee
@@ -1,0 +1,34 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+module.exports =
+    name: 'grid_view'
+    dir: build: 'buildbot_grid_view/static'
+    bower:
+        testdeps:
+            # vendors.js includes jquery, angularjs, etc in the right order
+            "guanlecoja-ui":
+                version: '~1.6.0'
+                files: ['vendors.js', 'scripts.js']
+            "angular-mocks":
+                version: '~1.5.3'
+                files: "angular-mocks.js"
+            'buildbot-data':
+                version: '~2.2.0'
+                files: 'dist/buildbot-data.js'
+
+    karma:
+        # we put tests first, so that we have angular, and fake app defined
+        files: ["tests.js", "scripts.js", 'fixtures.js']

www/grid_view/gulpfile.js
@@ -1,0 +1,1 @@
+require('guanlecoja')(require('gulp'));

www/grid_view/setup.cfg
@@ -1,0 +1,2 @@
+[bdist_wheel]
+universal=1

commit 5b0eb2db4ce992d4f31572e9cffa73aef5bb0778

grid_view: remove wrongfully used 'property' API parameter
Fixes: f098bdd5d7fb ("www: add grid_view plugin")
Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

www/grid_view/src/module/grid.controller.coffee
@@ -32,23 +32,18 @@ class Grid extends Controller
         @buildsets = @data.getBuildsets(
             limit: @changeFetchLimit
             order: '-bsid'
-            property: ['bsid', 'sourcestamps']
         )
         @changes = @data.getChanges(
             limit: @changeFetchLimit
             order: '-changeid'
         )
-        @builders = @data.getBuilders(
-            property: ['builderid', 'name']
-        )
+        @builders = @data.getBuilders()
         @buildrequests = @data.getBuildrequests(
             limit: @buildFetchLimit
             order: '-buildrequestid'
-            property: ['buildrequestid', 'buildsetid', 'builderid']
         )
         @builds = @data.getBuilds(
             limit: @buildFetchLimit
-            order: '-buildrequestid'
         )
 
         @buildsets.onChange = @changes.onChange = @builders.onChange = \