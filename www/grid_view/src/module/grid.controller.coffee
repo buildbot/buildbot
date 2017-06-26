buildbot/buildbot

commit f098bdd5d7fbefd8e4567b8052a90e119c5f12a8

www: add grid_view plugin
Add a grid view plugin.

It is possible to display changes in all branches or restrict to a
specific branch. It is also possible to filter builders by clicking on
builder tags.

In the settings page, the user can specify how many changes are
displayed at max plus other more internal stuff.

Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

www/grid_view/src/module/grid.controller.coffee
@@ -1,0 +1,167 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+class Grid extends Controller
+    constructor: (@$scope, @$stateParams, @$state, resultsService, dataService, bbSettingsService) ->
+        _.mixin(@$scope, resultsService)
+        @data = dataService.open().closeOnDestroy(@$scope)
+
+        @branch = @$stateParams.branch
+        @tags = @$stateParams.tag ? []
+        if not angular.isArray(@tags)
+            @tags = [@tags]
+
+        settings = bbSettingsService.getSettingsGroup('Grid')
+        @revisionLimit = settings.revisionLimit.value
+        @changeFetchLimit = settings.changeFetchLimit.value
+        @buildFetchLimit = settings.buildFetchLimit.value
+        @compactChanges = settings.compactChanges.value
+
+        @buildsets = @data.getBuildsets(
+            limit: @changeFetchLimit
+            order: '-bsid'
+            property: ['bsid', 'sourcestamps']
+        )
+        @changes = @data.getChanges(
+            limit: @changeFetchLimit
+            order: '-changeid'
+        )
+        @builders = @data.getBuilders(
+            property: ['builderid', 'name']
+        )
+        @buildrequests = @data.getBuildrequests(
+            limit: @buildFetchLimit
+            order: '-buildrequestid'
+            property: ['buildrequestid', 'buildsetid', 'builderid']
+        )
+        @builds = @data.getBuilds(
+            limit: @buildFetchLimit
+            order: '-buildrequestid'
+        )
+
+        @buildsets.onChange = @changes.onChange = @builders.onChange = \
+            @buildrequests.change = @builds.onChange = @onChange
+
+    dataReady: ->
+        for collection in [@buildsets, @changes, @builders, @buildrequests, @builds]
+            if not (collection.$resolved and collection.length > 0)
+                return false
+        return true
+
+    dataFetched: ->
+        for collection in [@buildsets, @changes, @builders, @buildrequests, @builds]
+            if not collection.$resolved
+                return false
+        return true
+
+    onChange: =>
+        if not @dataReady()
+            return
+
+        changesByBSID = {}
+        @$scope.changes = []
+        branches = {}
+
+        changesBySSID = {}
+        for c in @changes
+            changesBySSID[c.sourcestamp.ssid] = c
+
+        for bset in @buildsets
+            change = changesBySSID[_.last(bset.sourcestamps).ssid]
+            unless change?
+                continue
+            change.bsid = bset.bsid
+            change.branch ?= 'master'
+            branches[change.branch] = true
+
+            if @$scope.changes.length > @revisionLimit
+                continue
+            if @branch and change.branch != @branch
+                continue
+
+            @$scope.changes.push(change)
+            changesByBSID[bset.bsid] = change
+
+        @$scope.changes.sort((a, b) -> a.bsid - b.bsid)
+
+        @$scope.branches = (br for br of branches)
+
+        requestsByBSID = {}
+        for req in @buildrequests
+            (requestsByBSID[req.buildsetid] ?= []).push(req)
+        buildByReqID = {}
+        for build in @builds
+            buildByReqID[build.buildrequestid] = build
+
+        for builder in @builders
+            builder.builds = {}
+
+        buildersById = {}
+        # find builds for the selected changes and associate them to builders
+        for c in @$scope.changes
+            requests = requestsByBSID[c.bsid]
+            unless requests?
+                continue
+            for req in requests
+                build = buildByReqID[req.buildrequestid]
+                unless build?
+                    continue
+                builder = @builders.get(build.builderid)
+                unless @isBuilderDisplayed(builder)
+                    continue
+                buildersById[builder.builderid] = builder
+                builder.builds[c.bsid] = build
+
+        @$scope.builders = (buildersById[i] for i of buildersById)
+
+    changeBranch: (branch) =>
+        @branch = branch
+        @refresh()
+
+    toggleTag: (tag) =>
+        i = @tags.indexOf(tag)
+        if i < 0
+            @tags.push(tag)
+        else
+            @tags.splice(i, 1)
+        @refresh()
+
+    resetTags: =>
+        @tags = []
+        @refresh()
+
+    refresh: =>
+        @$stateParams.branch = @branch
+        if @tags.length == 0
+            @$stateParams.tag = undefined
+        else
+            @$stateParams.tag = @tags
+
+        params =
+            branch: @$stateParams.branch
+            tag: @$stateParams.tag
+
+        # change URL without reloading page
+        @$state.transitionTo(@$state.current, params, {notify: false})
+        @onChange()
+
+    isBuilderDisplayed: (builder) =>
+        for tag in @tags
+            if builder.tags.indexOf(tag) < 0
+                return false
+        return true
+
+    isTagToggled: (tag) =>
+        return @tags.indexOf(tag) >= 0

www/grid_view/src/module/grid.tpl.jade
@@ -1,0 +1,47 @@
+.container.grid
+    .load-indicator(ng-hide='C.dataFetched()')
+        .spinner
+            i.fa.fa-circle-o-notch.fa-spin.fa-2x
+            p loading
+    p(ng-show="C.dataFetched() && C.changes.length == 0")
+        | No changes. Grid View needs a changesource to be setup, and
+        a(href="#/changes")
+            |  changes
+        |  to be in the system.
+    .form-inline(ng-show="C.dataReady()")
+        label
+            | Branch &nbsp;
+        select.form-control(ng-model="C.branch", ng-change="C.changeBranch(C.branch)", ng-options="br for br in branches | orderBy")
+            option(value="")
+                | (all)
+    table.table.table-condensed.table-striped.table-hover(ng-show="C.dataReady()")
+        thead
+            tr
+                th
+                    | Builder
+                th
+                    span(ng-show="C.tags.length == 0")
+                        | Tags
+                    span(ng-show="C.tags.length < 5", ng-repeat="tag in C.tags")
+                        span.builder-tag.label.label-success(ng-click="C.toggleTag(tag)")
+                          | {{ tag }}
+                    span(ng-show="C.tags.length >= 5")
+                        span.label.label-success
+                            | {{ C.tags.length }} tags
+                    span(ng-show="C.tags.length > 0")
+                        span.label.label-danger.clickable(ng-click="C.resetTags()", uib-tooltip="Reset tags filter") x
+                th.change(ng-repeat="ch in changes")
+                    changedetails(change="ch", compact="C.compactChanges")
+        tbody
+            tr(ng-repeat="b in builders | orderBy: 'name'")
+                th
+                    a(ui-sref="builder({builder: b.builderid})")
+                        | {{ b.name }}
+                td
+                    span(ng-repeat="tag in b.tags")
+                        span.builder-tag.label(ng-click="C.toggleTag(tag)", ng-class="C.isTagToggled(tag) ? 'label-success': 'label-default'")
+                            | {{ tag }}
+                td(ng-repeat="ch in changes")
+                    a(ng-if="b.builds[ch.bsid]", ui-sref="build({builder: b.builderid, build: b.builds[ch.bsid].number})")
+                        span.badge-status(ng-class="results2class(b.builds[ch.bsid], 'pulse')")
+                            | {{ b.builds[ch.bsid].number }}

www/grid_view/src/module/main.module.coffee
@@ -1,0 +1,76 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+# Register new module
+class BuildbotGridView extends App
+    constructor: ->
+        return [
+            'ui.router'
+            'ui.bootstrap'
+            'ngAnimate'
+            'guanlecoja.ui'
+            'bbData'
+        ]
+
+# Register new state
+class State extends Config
+    constructor: ($stateProvider, glMenuServiceProvider, bbSettingsServiceProvider) ->
+        # Menu configuration
+        glMenuServiceProvider.addGroup(
+            name: 'grid'
+            caption: 'Grid View'
+            icon: 'cubes'
+            order: 4
+        )
+
+        # Register URL routing
+        $stateProvider
+            .state(
+                name: 'grid'
+                controller: 'gridController'
+                controllerAs: 'C'
+                templateUrl: 'buildbot_grid_view/views/grid.html'
+                url: '/grid?branch&tag'
+                reloadOnSearch: false
+                data:
+                    group: 'grid'
+                    caption: 'Grid View'
+            )
+
+        bbSettingsServiceProvider.addSettingsGroup(
+            name: 'Grid'
+            caption: 'Grid related settings'
+            items: [
+                    type: 'bool'
+                    name: 'compactChanges'
+                    caption: 'Hide avatar and time ago from change details'
+                    defaultValue: true
+                ,
+                    type: 'integer'
+                    name: 'revisionLimit'
+                    caption: 'Maximum number of revisions to display'
+                    default_value: 5
+                ,
+                    type: 'integer'
+                    name: 'changeFetchLimit'
+                    caption: 'Maximum number of changes to fetch'
+                    default_value: 100
+                ,
+                    type: 'integer'
+                    name: 'buildFetchLimit'
+                    caption: 'Maximum number of builds to fetch'
+                    default_value: 1000
+            ]
+        )

Makefile
@@ -34,7 +34,7 @@ flake8:
 frontend:
 	$(PIP) install -e pkg
 	$(PIP) install mock
-	for i in base wsgi_dashboards codeparameter console_view waterfall_view nestedexample; do $(PIP) install -e www/$$i || exit 1; done
+	for i in base wsgi_dashboards codeparameter console_view grid_view waterfall_view nestedexample; do $(PIP) install -e www/$$i || exit 1; done
 
 # do installation tests. Test front-end can build and install for all install methods
 frontend_install_tests:

common/maketarballs.sh
@@ -4,7 +4,7 @@ rm -rf dist
 mkdir dist
 pip install mock wheel
 set -e
-for pkg in pkg master worker www/base www/console_view www/waterfall_view www/wsgi_dashboards
+for pkg in pkg master worker www/base www/console_view www/grid_view www/waterfall_view www/wsgi_dashboards
 do
   pip install -e ${pkg}
   (

common/validate.sh
@@ -143,7 +143,7 @@ echo "${MAGENTA}Validating the following commits:${NORM}"
 git log "$REVRANGE" --pretty=oneline || exit 1
 
 if ! $quick && ! $no_js; then
-    for module in www/base www/console_view www/waterfall_view www/codeparameter www/wsgi_dashboards;
+    for module in www/base www/console_view www/grid_view www/waterfall_view www/codeparameter www/wsgi_dashboards;
     do
         status "running 'setup.py develop' for $module"
         if ! (cd $module; python setup.py develop >/dev/null ); then

www/grid_view/buildbot_grid_view/__init__.py
@@ -1,0 +1,19 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+from buildbot.www.plugin import Application
+
+# create the interface for the setuptools entry point
+ep = Application(__name__, "Buildbot Grid View plugin")

www/grid_view/guanlecoja/config.coffee
@@ -1,0 +1,34 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+module.exports =
+    name: 'grid_view'
+    dir: build: 'buildbot_grid_view/static'
+    bower:
+        testdeps:
+            # vendors.js includes jquery, angularjs, etc in the right order
+            "guanlecoja-ui":
+                version: '~1.6.0'
+                files: ['vendors.js', 'scripts.js']
+            "angular-mocks":
+                version: '~1.5.3'
+                files: "angular-mocks.js"
+            'buildbot-data':
+                version: '~2.2.0'
+                files: 'dist/buildbot-data.js'
+
+    karma:
+        # we put tests first, so that we have angular, and fake app defined
+        files: ["tests.js", "scripts.js", 'fixtures.js']

commit 908683af0e28f91058872c2323b79cac6946f317

grid_view: fix error when multiple buildsets for one change
When using trigger buildsteps, there can be multiple buildsets for the
same change object.

Modify the code so that builds are identified by change id.

Fixes: f098bdd5d7fb ("www: add grid_view plugin")
Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

www/grid_view/src/module/grid.controller.coffee
@@ -28,6 +28,7 @@ class Grid extends Controller
         @changeFetchLimit = settings.changeFetchLimit.value
         @buildFetchLimit = settings.buildFetchLimit.value
         @compactChanges = settings.compactChanges.value
+        @rightToLeft = settings.rightToLeft.value
 
         @buildsets = @data.getBuildsets(
             limit: @changeFetchLimit
@@ -44,6 +45,7 @@ class Grid extends Controller
         )
         @builds = @data.getBuilds(
             limit: @buildFetchLimit
+            order: '-buildrequestid'
         )
 
         @buildsets.onChange = @changes.onChange = @builders.onChange = \
@@ -65,31 +67,41 @@ class Grid extends Controller
         if not @dataReady()
             return
 
-        changesByBSID = {}
-        @$scope.changes = []
+        changes = {}
         branches = {}
 
+        # map changes by source stamp id
         changesBySSID = {}
         for c in @changes
             changesBySSID[c.sourcestamp.ssid] = c
+            c.buildsets = {}  # there can be multiple buildsets by change
 
+        # associate buildsets to each change and remember existing branches
         for bset in @buildsets
             change = changesBySSID[_.last(bset.sourcestamps).ssid]
             unless change?
                 continue
-            change.bsid = bset.bsid
+
+            change.buildsets[bset.bsid] = bset
             change.branch ?= 'master'
             branches[change.branch] = true
 
-            if @$scope.changes.length > @revisionLimit
-                continue
             if @branch and change.branch != @branch
                 continue
 
-            @$scope.changes.push(change)
-            changesByBSID[bset.bsid] = change
+            changes[change.changeid] = change
 
-        @$scope.changes.sort((a, b) -> a.bsid - b.bsid)
+        # only keep the @revisionLimit most recent changes for display
+        changes = (change for own cid, change of changes)
+        if @rightToLeft
+            changes.sort((a, b) -> b.changeid - a.changeid)
+            if changes.length > @revisionLimit
+                changes = changes.slice(0, @revisionLimit)
+        else
+            changes.sort((a, b) -> a.changeid - b.changeid)
+            if changes.length > @revisionLimit
+                changes = changes.slice(changes.length - @revisionLimit)
+        @$scope.changes = changes
 
         @$scope.branches = (br for br of branches)
 
@@ -106,20 +118,21 @@ class Grid extends Controller
         buildersById = {}
         # find builds for the selected changes and associate them to builders
         for c in @$scope.changes
-            requests = requestsByBSID[c.bsid]
-            unless requests?
-                continue
-            for req in requests
-                build = buildByReqID[req.buildrequestid]
-                unless build?
+            for own bsid, bset of c.buildsets
+                requests = requestsByBSID[bsid]
+                unless requests?
                     continue
-                builder = @builders.get(build.builderid)
-                unless @isBuilderDisplayed(builder)
-                    continue
-                buildersById[builder.builderid] = builder
-                builder.builds[c.bsid] = build
+                for req in requests
+                    build = buildByReqID[req.buildrequestid]
+                    unless build?
+                        continue
+                    builder = @builders.get(build.builderid)
+                    unless @isBuilderDisplayed(builder)
+                        continue
+                    buildersById[builder.builderid] = builder
+                    builder.builds[c.changeid] = build
 
-        @$scope.builders = (buildersById[i] for i of buildersById)
+        @$scope.builders = (builder for own i, builder of buildersById)
 
     changeBranch: (branch) =>
         @branch = branch

www/grid_view/src/module/grid.tpl.jade
@@ -30,7 +30,7 @@
                             | {{ C.tags.length }} tags
                     span(ng-show="C.tags.length > 0")
                         span.label.label-danger.clickable(ng-click="C.resetTags()", uib-tooltip="Reset tags filter") x
-                th.change(ng-repeat="ch in changes")
+                th.change(ng-repeat="ch in changes track by ch.changeid")
                     changedetails(change="ch", compact="C.compactChanges")
         tbody
             tr(ng-repeat="b in builders | orderBy: 'name'")
@@ -41,7 +41,7 @@
                     span(ng-repeat="tag in b.tags")
                         span.builder-tag.label(ng-click="C.toggleTag(tag)", ng-class="C.isTagToggled(tag) ? 'label-success': 'label-default'")
                             | {{ tag }}
-                td(ng-repeat="ch in changes")
-                    a(ng-if="b.builds[ch.bsid]", ui-sref="build({builder: b.builderid, build: b.builds[ch.bsid].number})")
-                        span.badge-status(ng-class="results2class(b.builds[ch.bsid], 'pulse')")
-                            | {{ b.builds[ch.bsid].number }}
+                td(ng-repeat="ch in changes track by ch.changeid")
+                    a(ng-if="b.builds[ch.changeid]", ui-sref="build({builder: b.builderid, build: b.builds[ch.changeid].number})")
+                        span.badge-status(ng-class="results2class(b.builds[ch.changeid], 'pulse')")
+                            | {{ b.builds[ch.changeid].number }}

www/grid_view/src/module/main.module.coffee
@@ -58,6 +58,11 @@ class State extends Config
                     caption: 'Hide avatar and time ago from change details'
                     defaultValue: true
                 ,
+                    type: 'bool'
+                    name: 'rightToLeft'
+                    caption: 'Show most recent changes on the left'
+                    defaultValue: true
+                ,
                     type: 'integer'
                     name: 'revisionLimit'
                     caption: 'Maximum number of revisions to display'