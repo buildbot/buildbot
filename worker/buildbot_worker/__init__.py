171b00c0cac5aca89af82cec6bd437ba84cdee08
Pierre Tardy <tardyp@gmail.com>
2017-07-02 14:15:44 +0200

    py2.6 compat

--- worker/buildbot_worker/__init__.py
+++ worker/buildbot_worker/__init__.py
@@ -45,10 +45,10 @@ def gitDescribeToPep440(version):
         if v.group('dev'):
             patch += 1
             dev = int(v.group('dev'))
-            return "{}.{}.{}-dev{}".format(major, minor, patch, dev)
+            return "{0}.{1}.{2}-dev{3}".format(major, minor, patch, dev)
         if v.group('post'):
-            return "{}.{}.{}.post{}".format(major, minor, patch, v.group('post'))
-        return "{}.{}.{}".format(major, minor, patch)
+            return "{0}.{1}.{2}.post{3}".format(major, minor, patch, v.group('post'))
+        return "{0}.{1}.{2}".format(major, minor, patch)
 
     return v
 
