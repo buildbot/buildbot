686dab59da7c590a6fabe286d54255d9be87a364
Robin Jarry <robin.jarry@6wind.com>
2017-08-17 11:08:33 +0200

    properties: make changes info available
    In addition of the master attribute, add new sourcestamps and changes
    attributes to be used when the build attribute is not set.
    
    This will be useful when calling Properties.render() outside of a build
    (in schedulers for instance).
    
    Properties.sourcestamps and Properties.changes must of course be set for
    this to work. If they are unset and build is also unset, an
    AttributeError will be risen when they are accessed.
    
    Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

--- master/buildbot/test/fake/fakebuild.py
+++ master/buildbot/test/fake/fakebuild.py
@@ -15,6 +15,7 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
+from future.utils import itervalues
 
 import posixpath
 
@@ -100,8 +101,20 @@ class FakeBuild(properties.PropertiesMixin):
             return self.sources[codebase]
         return None
 
+    def getAllSourceStamps(self):
+        return list(itervalues(self.sources))
+
+    def allChanges(self):
+        for s in itervalues(self.sources):
+            for c in s.changes:
+                yield c
+
     def allFiles(self):
-        return []
+        files = []
+        for c in self.allChanges():
+            for f in c.files:
+                files.append(f)
+        return files
 
     def getBuilder(self):
         return self.builder
