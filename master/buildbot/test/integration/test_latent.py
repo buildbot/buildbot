31259b085795e220641f1842e0adaea19b2289f2
Tim Bradt <tjbradt@mtu.edu>
2017-09-08 11:48:53 -0400

    Fixed isort stuff

--- master/buildbot/test/integration/test_latent.py
+++ master/buildbot/test/integration/test_latent.py
@@ -32,9 +32,9 @@ from buildbot.interfaces import LatentWorkerSubstantiatiationCancelled
 from buildbot.process.buildstep import BuildStep
 from buildbot.process.factory import BuildFactory
 from buildbot.process.results import CANCELLED
+from buildbot.process.results import EXCEPTION
 from buildbot.process.results import RETRY
 from buildbot.process.results import SUCCESS
-from buildbot.process.results import EXCEPTION
 from buildbot.test.fake.latent import LatentController
 from buildbot.test.fake.reactor import NonThreadPool
 from buildbot.test.fake.reactor import TestReactor
