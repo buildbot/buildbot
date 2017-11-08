93e78edd0fac59f458c3b4d4b4858f85749d32aa
Pierre Tardy <tardyp@gmail.com>
2017-11-08 12:14:03 +0100

    unit tests for worker actions

--- master/buildbot/test/unit/test_worker_base.py
+++ master/buildbot/test/unit/test_worker_base.py
@@ -526,6 +526,29 @@ class TestAbstractWorker(unittest.TestCase):
         self.assertEqual(worker.missing_timer, None)
         self.assertEqual(len(self.master.data.updates.missingWorkers), 0)
 
+    @defer.inlineCallbacks
+    def test_worker_actions_stop(self):
+        worker = self.createWorker(attached=False)
+        yield worker.startService()
+        worker.controlWorker(("worker", 1, "stop"), {'reason': "none"})
+        self.assertEqual(worker._graceful, True)
+
+    @defer.inlineCallbacks
+    def test_worker_actions_kill(self):
+        worker = self.createWorker(attached=False)
+        yield worker.startService()
+        worker.controlWorker(("worker", 1, "kill"), {'reason': "none"})
+        self.assertEqual(worker.conn, None)
+
+    @defer.inlineCallbacks
+    def test_worker_actions_pause(self):
+        worker = self.createWorker(attached=False)
+        yield worker.startService()
+        worker.controlWorker(("worker", 1, "pause"), {'reason': "none"})
+        self.assertEqual(worker._paused, True)
+
+        worker.controlWorker(("worker", 1, "unpause"), {'reason': "none"})
+        self.assertEqual(worker._paused, False)
 
 class TestAbstractLatentWorker(unittest.SynchronousTestCase):
 
