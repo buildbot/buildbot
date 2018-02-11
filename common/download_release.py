8461ed2f7550b79e6712e1eff11d8e80794c9f6c
Pierre Tardy <tardyp@gmail.com>
2018-02-11 21:43:10 +0100

    update release scripts
    - update smokedist to use tarballs starting with 1
    - add gpg signing of the tag tarball

--- common/download_release.py
+++ common/download_release.py
@@ -6,8 +6,17 @@ import os
 import requests
 import yaml
 
+def download(url, fn):
+    print(url, fn)
+    if os.path.exists(fn):
+        return
+    with open(fn, 'w') as f:
+        r = s.get(url, stream=True)
+        for c in r.iter_content(1024):
+            f.write(c)
 
 def main():
+    global s
     with open(os.path.expanduser("~/.config/hub")) as f:
         conf = yaml.load(f)
         token = conf['github.com'][0]['oauth_token']
@@ -17,16 +26,30 @@ def main():
     r = s.get("https://api.github.com/repos/buildbot/buildbot/releases/latest")
     r.raise_for_status()
     r = r.json()
+    tag = r['name']
+    upload_url = r['upload_url'].split('{')[0]
     assets = s.get("https://api.github.com/repos/buildbot/buildbot/releases/{id}/assets".format(id=r['id']))
     assets.raise_for_status()
     assets = assets.json()
-    os.system("rm -rf dist")
     os.system("mkdir -p dist")
     for url in (a['browser_download_url'] for a in assets):
         if url.endswith(".whl") or url.endswith(".tar.gz"):
-            print(url)
-            os.system("wget -P dist/ {url}".format(url=url))
-
-
+            fn = os.path.join('dist', url.split('/')[-1])
+            download(url, fn)
+    # download tag archive
+    url = "https://github.com/buildbot/buildbot/archive/{tag}.tar.gz".format(tag=tag)
+    fn = os.path.join('dist', url.split('/')[-1])
+    download(url, fn)
+    sigfn = fn + ".sig"
+    if os.path.exists(sigfn):
+        os.unlink(sigfn)
+    # sign the tag archive for debian
+    os.system("gpg --output {} -b {}".format(sigfn, fn))
+    sigfnbase = os.path.basename(sigfn)
+    r = s.post(upload_url,
+               headers={'Content-Type': "application/pgp-signature"},
+               params={"name": sigfnbase},
+               data=open(sigfn, 'rb'))
+    print(r.content)
 if __name__ == '__main__':
     main()
