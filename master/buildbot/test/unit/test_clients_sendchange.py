89f0eeb8378722ca9c271566048a6cb12c570ba2
Monika Kairaityte <monika@kibit.lt>
2018-07-07 19:13:36 +0300

    test_clients_sendchange: Use @defer.inlineCallbacks where possible

--- master/buildbot/test/unit/test_clients_sendchange.py
+++ master/buildbot/test/unit/test_clients_sendchange.py
@@ -75,77 +75,68 @@ class Sender(unittest.TestCase):
                           self.creds.username, self.creds.password,
                           self.added_changes])
 
+    @defer.inlineCallbacks
     def test_send_minimal(self):
         s = sendchange.Sender('localhost:1234')
-        d = s.send('branch', 'rev', 'comm', ['a'])
+        yield s.send('branch', 'rev', 'comm', ['a'])
 
-        def check(_):
-            self.assertProcess('localhost', 1234, 'change', 'changepw', [
-                dict(project='', repository='', who=None, files=['a'],
-                     comments='comm', branch='branch', revision='rev',
-                     category=None, when=None, properties={}, revlink='',
-                     src=None)])
-        d.addCallback(check)
-        return d
+        self.assertProcess('localhost', 1234, 'change', 'changepw', [
+            dict(project='', repository='', who=None, files=['a'],
+                 comments='comm', branch='branch', revision='rev',
+                 category=None, when=None, properties={}, revlink='',
+                 src=None)])
 
+    @defer.inlineCallbacks
     def test_send_auth(self):
         s = sendchange.Sender('localhost:1234', auth=('me', 'sekrit'))
-        d = s.send('branch', 'rev', 'comm', ['a'])
+        yield s.send('branch', 'rev', 'comm', ['a'])
 
-        def check(_):
-            self.assertProcess('localhost', 1234, 'me', 'sekrit', [
-                dict(project='', repository='', who=None, files=['a'],
-                     comments='comm', branch='branch', revision='rev',
-                     category=None, when=None, properties={}, revlink='',
-                     src=None)])
-        d.addCallback(check)
-        return d
+        self.assertProcess('localhost', 1234, 'me', 'sekrit', [
+            dict(project='', repository='', who=None, files=['a'],
+                 comments='comm', branch='branch', revision='rev',
+                 category=None, when=None, properties={}, revlink='',
+                 src=None)])
 
+    @defer.inlineCallbacks
     def test_send_full(self):
         s = sendchange.Sender('localhost:1234')
-        d = s.send('branch', 'rev', 'comm', ['a'], who='me', category='cats',
+        yield s.send('branch', 'rev', 'comm', ['a'], who='me', category='cats',
                    when=1234, properties={'a': 'b'}, repository='r', vc='git',
                    project='p', revlink='rl')
 
-        def check(_):
-            self.assertProcess('localhost', 1234, 'change', 'changepw', [
-                dict(project='p', repository='r', who='me', files=['a'],
-                     comments='comm', branch='branch', revision='rev',
-                     category='cats', when=1234, properties={'a': 'b'},
-                     revlink='rl', src='git')])
-        d.addCallback(check)
-        return d
+        self.assertProcess('localhost', 1234, 'change', 'changepw', [
+            dict(project='p', repository='r', who='me', files=['a'],
+                 comments='comm', branch='branch', revision='rev',
+                 category='cats', when=1234, properties={'a': 'b'},
+                 revlink='rl', src='git')])
 
+    @defer.inlineCallbacks
     def test_send_files_tuple(self):
         # 'buildbot sendchange' sends files as a tuple, rather than a list..
         s = sendchange.Sender('localhost:1234')
-        d = s.send('branch', 'rev', 'comm', ('a', 'b'))
+        yield s.send('branch', 'rev', 'comm', ('a', 'b'))
 
-        def check(_):
-            self.assertProcess('localhost', 1234, 'change', 'changepw', [
-                dict(project='', repository='', who=None, files=['a', 'b'],
-                     comments='comm', branch='branch', revision='rev',
-                     category=None, when=None, properties={}, revlink='',
-                     src=None)])
-        d.addCallback(check)
-        return d
+        self.assertProcess('localhost', 1234, 'change', 'changepw', [
+            dict(project='', repository='', who=None, files=['a', 'b'],
+                 comments='comm', branch='branch', revision='rev',
+                 category=None, when=None, properties={}, revlink='',
+                 src=None)])
 
+    @defer.inlineCallbacks
     def test_send_codebase(self):
         s = sendchange.Sender('localhost:1234')
-        d = s.send('branch', 'rev', 'comm', ['a'], codebase='mycb')
+        yield s.send('branch', 'rev', 'comm', ['a'], codebase='mycb')
 
-        def check(_):
-            self.assertProcess('localhost', 1234, 'change', 'changepw', [
-                dict(project='', repository='', who=None, files=['a'],
-                     comments='comm', branch='branch', revision='rev',
-                     category=None, when=None, properties={}, revlink='',
-                     src=None, codebase='mycb')])
-        d.addCallback(check)
-        return d
+        self.assertProcess('localhost', 1234, 'change', 'changepw', [
+            dict(project='', repository='', who=None, files=['a'],
+                 comments='comm', branch='branch', revision='rev',
+                 category=None, when=None, properties={}, revlink='',
+                 src=None, codebase='mycb')])
 
+    @defer.inlineCallbacks
     def test_send_unicode(self):
         s = sendchange.Sender('localhost:1234')
-        d = s.send(u'\N{DEGREE SIGN}',
+        yield s.send(u'\N{DEGREE SIGN}',
                    u'\U0001f49e',
                    u'\N{POSTAL MARK FACE}',
                    [u'\U0001F4C1'],
@@ -157,27 +148,25 @@ class Sender(unittest.TestCase):
                    properties={u'\N{LATIN SMALL LETTER A WITH MACRON}': 'b'},
                    revlink=u'\U0001F517')
 
-        def check(_):
-            self.assertProcess('localhost', 1234, 'change', 'changepw', [
-                dict(project=u'\N{SKULL AND CROSSBONES}',
-                     repository=u'\N{SNOWMAN}',
-                     who=u'\N{THAI CHARACTER KHOMUT}',
-                     files=[u'\U0001F4C1'],  # FILE FOLDER
-                     comments=u'\N{POSTAL MARK FACE}',
-                     branch=u'\N{DEGREE SIGN}',
-                     revision=u'\U0001f49e',  # REVOLVING HEARTS
-                     category=u'\U0001F640',  # WEARY CAT FACE
-                     when=1234,
-                     properties={u'\N{LATIN SMALL LETTER A WITH MACRON}': 'b'},
-                     revlink=u'\U0001F517',  # LINK SYMBOL
-                     src=None)])
-        d.addCallback(check)
-        return d
+        self.assertProcess('localhost', 1234, 'change', 'changepw', [
+            dict(project=u'\N{SKULL AND CROSSBONES}',
+                 repository=u'\N{SNOWMAN}',
+                 who=u'\N{THAI CHARACTER KHOMUT}',
+                 files=[u'\U0001F4C1'],  # FILE FOLDER
+                 comments=u'\N{POSTAL MARK FACE}',
+                 branch=u'\N{DEGREE SIGN}',
+                 revision=u'\U0001f49e',  # REVOLVING HEARTS
+                 category=u'\U0001F640',  # WEARY CAT FACE
+                 when=1234,
+                 properties={u'\N{LATIN SMALL LETTER A WITH MACRON}': 'b'},
+                 revlink=u'\U0001F517',  # LINK SYMBOL
+                 src=None)])
 
+    @defer.inlineCallbacks
     def test_send_unicode_utf8(self):
         s = sendchange.Sender('localhost:1234')
 
-        d = s.send(u'\N{DEGREE SIGN}'.encode('utf8'),
+        yield s.send(u'\N{DEGREE SIGN}'.encode('utf8'),
                    u'\U0001f49e'.encode('utf8'),
                    u'\N{POSTAL MARK FACE}'.encode('utf8'),
                    [u'\U0001F4C1'.encode('utf8')],
@@ -190,30 +179,28 @@ class Sender(unittest.TestCase):
                        u'\N{LATIN SMALL LETTER A WITH MACRON}'.encode('utf8'): 'b'},
                    revlink=u'\U0001F517'.encode('utf8'))
 
-        def check(_):
-            self.assertProcess('localhost', 1234, 'change', 'changepw', [
-                dict(project=u'\N{SKULL AND CROSSBONES}',
-                     repository=u'\N{SNOWMAN}',
-                     who=u'\N{THAI CHARACTER KHOMUT}',
-                     files=[u'\U0001F4C1'],  # FILE FOLDER
-                     comments=u'\N{POSTAL MARK FACE}',
-                     branch=u'\N{DEGREE SIGN}',
-                     revision=u'\U0001f49e',  # REVOLVING HEARTS
-                     category=u'\U0001F640',  # WEARY CAT FACE
-                     when=1234,
-                     # NOTE: not decoded!
-                     properties={b'\xc4\x81': 'b'},
-                     revlink=u'\U0001F517',  # LINK SYMBOL
-                     src=None)])
-        d.addCallback(check)
-        return d
+        self.assertProcess('localhost', 1234, 'change', 'changepw', [
+            dict(project=u'\N{SKULL AND CROSSBONES}',
+                 repository=u'\N{SNOWMAN}',
+                 who=u'\N{THAI CHARACTER KHOMUT}',
+                 files=[u'\U0001F4C1'],  # FILE FOLDER
+                 comments=u'\N{POSTAL MARK FACE}',
+                 branch=u'\N{DEGREE SIGN}',
+                 revision=u'\U0001f49e',  # REVOLVING HEARTS
+                 category=u'\U0001F640',  # WEARY CAT FACE
+                 when=1234,
+                 # NOTE: not decoded!
+                 properties={b'\xc4\x81': 'b'},
+                 revlink=u'\U0001F517',  # LINK SYMBOL
+                 src=None)])
 
+    @defer.inlineCallbacks
     def test_send_unicode_latin1(self):
         # hand send() a bunch of latin1 strings, and expect them recoded
         # to unicode
         s = sendchange.Sender('localhost:1234', encoding='latin1')
 
-        d = s.send(u'\N{YEN SIGN}'.encode('latin1'),
+        yield s.send(u'\N{YEN SIGN}'.encode('latin1'),
                    u'\N{POUND SIGN}'.encode('latin1'),
                    u'\N{BROKEN BAR}'.encode('latin1'),
                    [u'\N{NOT SIGN}'.encode('latin1')],
@@ -226,20 +213,17 @@ class Sender(unittest.TestCase):
                        u'\N{SUPERSCRIPT ONE}'.encode('latin1'): 'b'},
                    revlink=u'\N{INVERTED QUESTION MARK}'.encode('latin1'))
 
-        def check(_):
-            self.assertProcess('localhost', 1234, 'change', 'changepw', [
-                dict(project=u'\N{DEGREE SIGN}',
-                     repository=u'\N{SECTION SIGN}',
-                     who=u'\N{MACRON}',
-                     files=[u'\N{NOT SIGN}'],
-                     comments=u'\N{BROKEN BAR}',
-                     branch=u'\N{YEN SIGN}',
-                     revision=u'\N{POUND SIGN}',
-                     category=u'\N{PILCROW SIGN}',
-                     when=1234,
-                     # NOTE: not decoded!
-                     properties={b'\xb9': 'b'},
-                     revlink=u'\N{INVERTED QUESTION MARK}',
-                     src=None)])
-        d.addCallback(check)
-        return d
+        self.assertProcess('localhost', 1234, 'change', 'changepw', [
+            dict(project=u'\N{DEGREE SIGN}',
+                 repository=u'\N{SECTION SIGN}',
+                 who=u'\N{MACRON}',
+                 files=[u'\N{NOT SIGN}'],
+                 comments=u'\N{BROKEN BAR}',
+                 branch=u'\N{YEN SIGN}',
+                 revision=u'\N{POUND SIGN}',
+                 category=u'\N{PILCROW SIGN}',
+                 when=1234,
+                 # NOTE: not decoded!
+                 properties={b'\xb9': 'b'},
+                 revlink=u'\N{INVERTED QUESTION MARK}',
+                 src=None)])
