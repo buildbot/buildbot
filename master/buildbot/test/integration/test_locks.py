00026d4a8c2e90f86f4736a6e62e3223ab27886f
Povilas Kanapickas <povilas@radix.lt>
2019-01-24 21:38:15 +0200

    test: Remove unused imports

--- master/buildbot/test/integration/test_locks.py
+++ master/buildbot/test/integration/test_locks.py
@@ -14,9 +14,6 @@
 # Copyright Buildbot Team Members
 
 
-from __future__ import absolute_import
-from __future__ import print_function
-
 from twisted.internet import defer
 from twisted.python import log
 from twisted.python.filepath import FilePath
