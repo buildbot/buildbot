8e74cd4aba25cadb89ad171e62db1d96d5db8868
Josh Bialkowski <josh.bialkowski@gmail.com>
2019-01-30 12:48:12 -0800

    singleline newsfrag, add serialize to test

--- master/buildbot/test/unit/test_www_auth.py
+++ master/buildbot/test/unit/test_www_auth.py
@@ -13,6 +13,7 @@
 #
 # Copyright Buildbot Team Members
 
+import json
 import mock
 
 from twisted.cred.checkers import InMemoryUsernamePasswordDatabaseDontUse
@@ -117,6 +118,7 @@ class RemoteUserAuth(www.WwwTestMixin, unittest.TestCase):
                          'username': 'rachel',
                          'realm': 'foo.com',
                          'email': 'rachel'})
+        _ = json.dumps(self.request.session.user_info)
 
     @defer.inlineCallbacks
     def test_maybeAutoLogin_no_header(self):
