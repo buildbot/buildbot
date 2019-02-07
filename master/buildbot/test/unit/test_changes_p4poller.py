0707d290f741bcc8e2a7c0ebe12d946c76d6b448
Alexandre Isoard <alexandre.isoard@gmail.com>
2019-02-06 19:18:06 -0800

    Fix issue with p4poller ticket parsing
    In recent versions of Perforce CLI the output of `p4 login -p` changed.
    The ticket number is still on the last line of output (was 3rd line, is
    now 2nd line).

--- master/buildbot/test/unit/test_changes_p4poller.py
+++ master/buildbot/test/unit/test_changes_p4poller.py
@@ -346,6 +346,36 @@ class TestP4Poller(changesource.ChangeSourceMixin,
             self.changesource._ticket_passwd, 'TICKET_ID_GOES_HERE')
 
     @defer.inlineCallbacks
+    def test_acquire_ticket_auth2(self):
+        self.attachChangeSource(
+            P4Source(p4port=None, p4user=None, p4passwd='pass',
+                     p4base='//depot/myproject/',
+                     split_file=lambda x: x.split('/', 1),
+                     use_tickets=True))
+        self.expectCommands(
+            gpo.Expect('p4', '-P', 'TICKET_ID_GOES_HERE',
+                       'changes', '-m', '1', '//depot/myproject/...').stdout(first_p4changes)
+        )
+
+        transport = FakeTransport()
+
+        # p4poller uses only those arguments at the moment
+        def spawnProcess(pp, cmd, argv, env):
+            self.assertEqual([cmd, argv],
+                             ['p4', [b'p4', b'login', b'-p']])
+            pp.makeConnection(transport)
+            self.assertEqual('pass\n', transport.msg)
+            pp.outReceived('Enter password:\nTICKET_ID_GOES_HERE\n')
+            so = error.ProcessDone(None)
+            pp.processEnded(failure.Failure(so))
+        self.patch(reactor, 'spawnProcess', spawnProcess)
+
+        yield self.changesource.poll()
+
+        self.assertEqual(
+            self.changesource._ticket_passwd, 'TICKET_ID_GOES_HERE')
+
+    @defer.inlineCallbacks
     def test_poll_split_file(self):
         """Make sure split file works on branch only changes"""
         self.attachChangeSource(
