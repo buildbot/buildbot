buildbot/buildbot

commit be5d917fc39ee17febc96bc97f980d1a0f476700

schedulers must call parent de/activate methods
# Please enter the commit message for your changes. Lines starting
# with '#' will be kept; you may remove them yourself if you want to.
# An empty message aborts the commit.
# Not currently on any branch.
# You are currently editing a commit while rebasing branch 'nine' on '924c5e3'.
#
# Changes to be committed:
#   (use "git reset HEAD^1 <file>..." to unstage)
#
#	modified:   master/buildbot/schedulers/base.py
#	modified:   master/buildbot/schedulers/basic.py
#	modified:   master/buildbot/schedulers/dependent.py
#	modified:   master/buildbot/schedulers/timed.py
#	modified:   master/buildbot/schedulers/trysched.py
#	modified:   master/buildbot/test/util/scheduler.py
#	modified:   master/docs/developer/schedulers.rst
#

master/buildbot/schedulers/dependent.py
@@ -38,7 +38,10 @@ class Dependent(base.BaseScheduler):
         # complete.
         self._subscription_lock = defer.DeferredLock()
 
+    @defer.inlineCallbacks
     def activate(self):
+        yield base.BaseScheduler.deactivate(self)
+
         self._buildset_new_consumer = self.master.data.startConsuming(
                     self._buildset_new_cb,
                     {}, ('buildset',))
@@ -49,15 +52,18 @@ class Dependent(base.BaseScheduler):
                     ('buildset', None, 'complete'))
 
         # check for any buildsets completed before we started
-        return self._checkCompletedBuildsets(None, )
+        yield self._checkCompletedBuildsets(None, )
 
+    @defer.inlineCallbacks
     def deactivate(self):
+        # the base deactivate will unsubscribe from new changes
+        yield base.BaseScheduler.deactivate(self)
+
         if self._buildset_new_consumer:
             self._buildset_new_consumer.stopConsuming()
         if self._buildset_complete_consumer:
             self._buildset_complete_consumer.stopConsuming()
         self._cached_upstream_bsids = None
-        return defer.succeed(None)
 
     @util.deferredLocked('_subscription_lock')
     def _buildset_new_cb(self, key, msg):

master/buildbot/schedulers/base.py
@@ -159,7 +159,7 @@ class BaseScheduler(ClusteredService, StateMixin):
         d.addErrback(log.err, 'while processing change')
 
     def _stopConsumingChanges(self):
-        # (note: called automatically in stopService)
+        # (note: called automatically in deactivate)
 
         # acquire the lock change consumption lock to ensure that any change
         # consumption is complete before we are done stopping consumption

master/buildbot/schedulers/basic.py
@@ -69,9 +69,11 @@ class BaseBasicScheduler(base.BaseScheduler):
         raise NotImplementedError
 
     def activate(self):
-        d = self.startConsumingChanges(fileIsImportant=self.fileIsImportant,
+        d = base.BaseScheduler.activate(self)
+        d.addCallback(lambda _ :
+            self.startConsumingChanges(fileIsImportant=self.fileIsImportant,
                                        change_filter=self.change_filter,
-                                       onlyImportant=self.onlyImportant)
+                                       onlyImportant=self.onlyImportant))
 
         # if treeStableTimer is False, then we don't care about classified
         # changes, so get rid of any hanging around from previous
@@ -89,15 +91,18 @@ class BaseBasicScheduler(base.BaseScheduler):
 
         return d
 
+    @defer.inlineCallbacks
     def deactivate(self):
-        # the base stopService will unsubscribe from new changes
+        # the base deactivate will unsubscribe from new changes
+        yield base.BaseScheduler.deactivate(self)
+
         @util.deferredLocked(self._stable_timers_lock)
         def cancel_timers():
             for timer in self._stable_timers.values():
                 if timer:
                     timer.cancel()
             self._stable_timers.clear()
-        return cancel_timers()
+        yield cancel_timers()
 
     @util.deferredLocked('_stable_timers_lock')
     def gotChange(self, change, important):

master/buildbot/schedulers/timed.py
@@ -59,11 +59,14 @@ class Timed(base.BaseScheduler):
         self._reactor = reactor # patched by tests
 
     def activate(self):
+        d = base.BaseScheduler.activate(self)
+
         # no need to lock this; nothing else can run before the service is started
         self.actuateOk = True
 
         # get the scheduler's last_build time (note: only done at startup)
-        d = self.getState('last_build', None)
+        d.addCallback(lambda _ :
+            self.getState('last_build', None))
         def set_last(lastActuated):
             self.lastActuated = lastActuated
         d.addCallback(set_last)
@@ -79,7 +82,10 @@ class Timed(base.BaseScheduler):
         """Hook for subclasses to participate in the L{activate} process;
         can return a Deferred"""
 
+    @defer.inlineCallbacks
     def deactivate(self):
+        yield base.BaseScheduler.deactivate(self)
+
         # shut down any pending actuation, and ensure that we wait for any
         # current actuation to complete by acquiring the lock.  This ensures
         # that no build will be scheduled after deactivate is complete.
@@ -89,7 +95,7 @@ class Timed(base.BaseScheduler):
             if self.actuateAtTimer:
                 self.actuateAtTimer.cancel()
             self.actuateAtTimer = None
-        return self.actuationLock.run(stop_actuating)
+        yield self.actuationLock.run(stop_actuating)
 
     ## Scheduler methods
 

master/buildbot/schedulers/trysched.py
@@ -95,7 +95,10 @@ class Try_Jobdir(TryBase):
 
     # activation handlers
 
+    @defer.inlineCallbacks
     def activate(self):
+        yield TryBase.activate(self)
+
         # set the watcher's basedir now that we have a master
         jobdir = os.path.join(self.master.basedir, self.jobdir)
         self.watcher.setBasedir(jobdir)
@@ -103,11 +106,15 @@ class Try_Jobdir(TryBase):
             if not os.path.exists(os.path.join(jobdir, subdir)):
                 os.mkdir(os.path.join(jobdir, subdir))
 
-        # bridge the activate/deactivate to a startService/stopService on the child service
+        # bridge the activate/deactivate to a startService/stopService on the
+        # child service
         self.watcher.startService()
 
+    @defer.inlineCallbacks
     def deactivate(self):
-        # bridge the activate/deactivate to a startService/stopService on the child service
+        yield TryBase.deactivate(self)
+        # bridge the activate/deactivate to a startService/stopService on the
+        # child service
         self.watcher.stopService()
 
     def parseJob(self, f):
@@ -292,7 +299,10 @@ class Try_Userpass(TryBase):
         self.port = port
         self.userpass = userpass
 
+    @defer.inlineCallbacks
     def activate(self):
+        yield TryBase.activate(self)
+
         # register each user/passwd with the pbmanager
         def factory(mind, username):
             return Try_Userpass_Perspective(self, username)
@@ -302,6 +312,8 @@ class Try_Userpass(TryBase):
                 self.master.pbmanager.register(
                     self.port, user, passwd, factory))
 
+    @defer.inlineCallbacks
     def deactivate(self):
-        return defer.gatherResults(
+        yield TryBase.deactivate(self)
+        yield defer.gatherResults(
             [reg.unregister() for reg in self.registrations])

master/buildbot/test/util/scheduler.py
@@ -14,6 +14,7 @@
 # Copyright Buildbot Team Members
 
 from twisted.internet import defer
+from buildbot.schedulers import base
 from buildbot.test.fake import fakemaster, fakedb
 from buildbot.test.util import interfaces
 
@@ -101,6 +102,30 @@ class SchedulerMixin(interfaces.InterfaceTests):
                 self.addedSourceStampSets.append([])
                 return defer.succeed(400 + len(self.addedSourceStampSets) - 1)
             self.db.sourcestamps.addSourceStampSet = fake_addSourceStampSet
+
+        # patch methods to detect a failure to upcall the activate and
+        # deactivate methods .. unless we're testing BaseScheduler
+        def patch(meth):
+            oldMethod = getattr(scheduler, meth)
+            def newMethod():
+                self._parentMethodCalled = False
+                d = defer.maybeDeferred(oldMethod)
+                @d.addCallback
+                def check(rv):
+                    self.assertTrue(self._parentMethodCalled,
+                        "'%s' did not call its parent" % meth)
+                    return rv
+                return d
+            setattr(scheduler, meth, newMethod)
+
+            oldParent = getattr(base.BaseScheduler, meth)
+            def newParent(self_):
+                self._parentMethodCalled = True
+                return oldParent(self_)
+            self.patch(base.BaseScheduler, meth, newParent)
+        if scheduler.__class__ != base.BaseScheduler:
+            patch('activate')
+            patch('deactivate')
 
         self.sched = scheduler
         return scheduler

commit 3d9e6278720712cfecdea35941d9f0b8337dc9b1

schedulers/dependent: call the correct activate parent method
Fix the tests accordingly.

Fixes: be5d917fc39e ("schedulers must call parent de/activate methods")
Signed-off-by: Robin Jarry <robin@jarry.cc>

master/buildbot/schedulers/dependent.py
@@ -45,7 +45,7 @@ class Dependent(base.BaseScheduler):
 
     @defer.inlineCallbacks
     def activate(self):
-        yield base.BaseScheduler.deactivate(self)
+        yield base.BaseScheduler.activate(self)
 
         if not self.enabled:
             return

master/buildbot/test/unit/test_schedulers_dependent.py
@@ -80,11 +80,13 @@ class Dependent(scheduler.SchedulerMixin, unittest.TestCase):
 
         self.assertEqual(
             sorted([q.filter for q in sched.master.mq.qrefs]),
-            [('buildsets', None, 'complete',), ('buildsets', None, 'new',)])
+            [('buildsets', None, 'complete',), ('buildsets', None, 'new',),
+             ('schedulers', '133', 'updated')])
 
         yield sched.deactivate()
 
-        self.assertEqual([q.filter for q in sched.master.mq.qrefs], [])
+        self.assertEqual([q.filter for q in sched.master.mq.qrefs],
+                         [('schedulers', '133', 'updated')])
 
     def sendBuildsetMessage(self, scheduler_name=None, results=-1,
                             complete=False):