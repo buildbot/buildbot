016d2aea4ee8d38ed9514f691e000ae227e593e3
Pierre Tardy <pierre.tardy@renault.com>
2019-03-11 10:42:23 +0100

    remove vendor hack

--- www/guanlecoja/gulpfile.js
+++ www/guanlecoja/gulpfile.js
@@ -14,27 +14,8 @@ gulp.task('scripts', function(){
 });
 
 gulp.task('vendors', function(){
-	return gulp.src('vendors.coffee',  { read: false })
-    .pipe(browserify({
-      transform: ['coffeeify'],
-      extensions: ['.coffee'],
-	  builtins: false,
-	  commondir: false,
-	  detectGlobals: false,
-	  insertGlobals : false,
-      // those packages need to access files from the package
-	  // readline is required in a deadcode path
-	  // body-parser use dynamic parsing of its source code
-	  // consolidate requires tons of template package deps and is a dep of gulp-wrap > 0.8, and thus gulp-wrap should not be upgraded
-	  exclude: ['karma', 'readline','v8'],
-      ignore: ['./lib-cov/connect'],
-	}))
-	.on('error', gutil.log)
-	/* hack in order to have bower browserified */
-	.pipe(replace(/var pkg = require\(path/, 'var pkg = {version:"1"}; //'))
-	.pipe(replace(/require\(".\/streams"\)\(iconv\);/, ''))
-	.pipe(replace(/require\(".\/extend-node"\)\(iconv\);/, ''))
-    .pipe(rename('vendors.js'))
+	return gulp.src('vendors.coffee')
+	.pipe(coffee()).on('error', gutil.log)
 	.pipe(gulp.dest('.'));
 });
 
