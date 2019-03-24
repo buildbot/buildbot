5eea78227ba2e5ad06bc923a0529ef3aa949757e
Povilas Kanapickas <povilas@radix.lt>
2019-03-24 12:16:53 +0200

    test: Use fake reactor in secter tests

--- master/buildbot/test/unit/test_fake_secrets_manager.py
+++ master/buildbot/test/unit/test_fake_secrets_manager.py
@@ -6,12 +6,14 @@ from buildbot.secrets.manager import SecretManager
 from buildbot.secrets.secret import SecretDetails
 from buildbot.test.fake import fakemaster
 from buildbot.test.fake.secrets import FakeSecretStorage
+from buildbot.test.util.misc import TestReactorMixin
 
 
-class TestSecretsManager(unittest.TestCase):
+class TestSecretsManager(TestReactorMixin, unittest.TestCase):
 
     def setUp(self):
-        self.master = fakemaster.make_master()
+        self.setUpTestReactor()
+        self.master = fakemaster.make_master(self.reactor)
         self.master.config.secretsProviders = [FakeSecretStorage(secretdict={"foo": "bar",
                                                                              "other": "value"})]
 
--- master/buildbot/test/unit/test_secret_in_vault.py
+++ master/buildbot/test/unit/test_secret_in_vault.py
@@ -20,15 +20,19 @@ from buildbot.secrets.providers.vault import HashiCorpVaultSecretProvider
 from buildbot.test.fake import fakemaster
 from buildbot.test.fake import httpclientservice as fakehttpclientservice
 from buildbot.test.util.config import ConfigErrorsMixin
+from buildbot.test.util.misc import TestReactorMixin
 
 
-class TestSecretInVaultHttpFakeBase(ConfigErrorsMixin, unittest.TestCase):
+class TestSecretInVaultHttpFakeBase(ConfigErrorsMixin, TestReactorMixin,
+                                    unittest.TestCase):
 
     def setUp(self, version):
+        self.setUpTestReactor()
         self.srvcVault = HashiCorpVaultSecretProvider(vaultServer="http://vaultServer",
                                                       vaultToken="someToken",
                                                       apiVersion=version)
-        self.master = fakemaster.make_master(testcase=self, wantData=True)
+        self.master = fakemaster.make_master(self.reactor, testcase=self,
+                                             wantData=True)
         self._http = self.successResultOf(
             fakehttpclientservice.HTTPClientService.getFakeService(
                 self.master, self, 'http://vaultServer', headers={'X-Vault-Token': "someToken"}))
