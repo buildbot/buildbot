9794411f67af794cf9109f2ece683c0163940a85
Mayank Madan <maddiemadan@gmail.com>
2019-03-31 05:16:01 +0530

    Several variable name changes and other fixes

--- master/buildbot/reporters/zulip.py
+++ master/buildbot/reporters/zulip.py
@@ -1,22 +1,39 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+
 from twisted.internet import defer
 
 from buildbot import config
-from buildbot.reporters import utils
 from buildbot.reporters.http import HttpStatusPushBase
 from buildbot.util import httpclientservice
 from buildbot.util.logger import Logger
 
-logger = Logger()
+log = Logger()
 
 
 class ZulipStatusPush(HttpStatusPushBase):
     name = "ZulipStatusPush"
+    neededDetails = dict(wantProperties=True)
 
     def checkConfig(self, endpoint, token, stream=None, **kwargs):
         if not isinstance(endpoint, str):
             config.error("Endpoint must be a string")
         if not isinstance(token, str):
             config.error("Token must be a string")
+        super().checkConfig(**kwargs)
 
     @defer.inlineCallbacks
     def reconfigService(self, endpoint, token, stream=None, **kwargs):
@@ -27,15 +44,9 @@ class ZulipStatusPush(HttpStatusPushBase):
         self.token = token
         self.stream = stream
 
-    def buildStarted(self, key, build):
-        return self.send(build, key[2])
-
-    def buildFinished(self, key, build):
-        return self.send(build, key[2])
-
     @defer.inlineCallbacks
-    def getBuildDetails(self, build, event):
-        yield utils.getDetailsForBuild(self.master, build, wantProperties=True)
+    def send(self, build):
+        event = ("new", "finished")[0 if build["complete_at"] is None else 1]
         jsondata = dict(event=event, buildid=build["buildid"], buildername=build["builder"]["name"], url=build["url"],
                         project=build["properties"]["project"][0])
         if event == "new":
@@ -43,16 +54,12 @@ class ZulipStatusPush(HttpStatusPushBase):
         elif event == "finished":
             jsondata["timestamp"] = build["complete_at"]
             jsondata["results"] = build["results"]
-        return jsondata
 
-    @defer.inlineCallbacks
-    def send(self, build, event):
-        jsondata = yield self.getBuildDetails(build, event)
         if self.stream is not None:
             url = "/api/v1/external/buildbot?api_key={}&stream={}".format(self.token, self.stream)
         else:
             url = "/api/v1/external/buildbot?api_key={}".format(self.token)
-        res = yield self._http.post(url, json=jsondata)
-        if res.code != 200:
-            content = yield res.content()
-            logger.error("{}: Error pushing build status to Zulip: {}".format(res.code, content))
+        response = yield self._http.post(url, json=jsondata)
+        if response.code != 200:
+            content = yield response.content()
+            log.error("{}: Error pushing build status to Zulip: {}".format(response.code, content))
