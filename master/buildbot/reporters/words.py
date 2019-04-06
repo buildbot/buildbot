30f3ddea2d7777d0fc042f65a601a1ec92fe9147
Povilas Kanapickas <povilas@radix.lt>
2019-04-06 23:15:51 +0300

    test: Use fake reactor in reporters tests

--- master/buildbot/reporters/words.py
+++ master/buildbot/reporters/words.py
@@ -162,7 +162,7 @@ class Contact(service.AsyncService):
     'broadcast contact' (chat rooms, IRC channels as a whole).
     """
 
-    def __init__(self, bot, user=None, channel=None):
+    def __init__(self, bot, user=None, channel=None, _reactor=reactor):
         """
         :param StatusBot bot: StatusBot this Contact belongs to
         :param user: User ID representing this contact
@@ -190,6 +190,8 @@ class Contact(service.AsyncService):
         self.channel = channel
         self._next_HELLO = 'yes?'
 
+        self.reactor = _reactor
+
     # silliness
 
     silly = {
@@ -213,7 +215,7 @@ class Contact(service.AsyncService):
         response = self.silly[message]
         when = 0.5
         for r in response:
-            reactor.callLater(when, self.send, r)
+            self.reactor.callLater(when, self.send, r)
             when += 2.5
 
     def builderMatchesAnyTag(self, builder_tags):
@@ -776,7 +778,8 @@ class Contact(service.AsyncService):
                     complete_at = lastBuild['complete_at']
                     if complete_at:
                         complete_at = util.datetime2epoch(complete_at)
-                        ago = self.convertTime(int(util.now() - complete_at))
+                        ago = self.convertTime(int(self.reactor.seconds() -
+                                                   complete_at))
                     else:
                         ago = "??"
                     status = lastBuild['state_string']
@@ -821,7 +824,8 @@ class Contact(service.AsyncService):
                 complete_at = lastBuild['complete_at']
                 if complete_at:
                     complete_at = util.datetime2epoch(complete_at)
-                    ago = self.convertTime(int(util.now() - complete_at))
+                    ago = self.convertTime(int(self.reactor.seconds() -
+                                               complete_at))
                 else:
                     ago = "??"
                 status = lastBuild['state_string']
@@ -905,11 +909,11 @@ class Contact(service.AsyncService):
             self.act("readies phasers")
 
     def command_DANCE(self, args):
-        reactor.callLater(1.0, self.send, "<(^.^<)")
-        reactor.callLater(2.0, self.send, "<(^.^)>")
-        reactor.callLater(3.0, self.send, "(>^.^)>")
-        reactor.callLater(3.5, self.send, "(7^.^)7")
-        reactor.callLater(5.0, self.send, "(>^.^<)")
+        self.reactor.callLater(1.0, self.send, "<(^.^<)")
+        self.reactor.callLater(2.0, self.send, "<(^.^)>")
+        self.reactor.callLater(3.0, self.send, "(>^.^)>")
+        self.reactor.callLater(3.5, self.send, "(7^.^)7")
+        self.reactor.callLater(5.0, self.send, "(>^.^<)")
 
     def command_HUSTLE(self, args):
         self.act("does the hustle")
@@ -945,7 +949,7 @@ class Contact(service.AsyncService):
                 botmaster.cancelCleanShutdown()
         elif args == 'now':
             self.send("Stopping buildbot")
-            reactor.stop()
+            self.reactor.stop()
     command_SHUTDOWN.usage = {
         None: "shutdown check|start|stop|now - shutdown the buildbot master",
         "check": "shutdown check - check if the buildbot master is running or shutting down",
