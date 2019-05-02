37e5a22884a098e33c52c60c4a1c3a0592162208
Jan Marker <jan.marker@kdab.com>
2019-05-02 13:37:02 +0200

    Fix reconfiguration of statistics service
    Before the consumers were not correctly unregistered, so each
    reconfig would create new consumers *additional* to the old ones.

--- master/buildbot/statistics/stats_service.py
+++ master/buildbot/statistics/stats_service.py
@@ -26,6 +26,10 @@ class StatsService(service.BuildbotService):
     A middleware for passing on statistics data to all storage backends.
     """
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.consumers = []
+
     def checkConfig(self, storage_backends):
         for wfb in storage_backends:
             if not isinstance(wfb, StatsStorageBase):
@@ -43,12 +47,11 @@ class StatsService(service.BuildbotService):
         for svc in storage_backends:
             self.registeredStorageServices.append(svc)
 
-        self.consumers = []
+        self.removeConsumers()
         self.registerConsumers()
 
     @defer.inlineCallbacks
     def registerConsumers(self):
-        self.removeConsumers()  # remove existing consumers and add new ones
         self.consumers = []
 
         for svc in self.registeredStorageServices:
