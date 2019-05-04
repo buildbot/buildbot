44a1aafa5a213c086f54b60f9061b62e720a26f3
Povilas Kanapickas <povilas@radix.lt>
2019-05-04 12:18:18 +0300

    www/waterfall_view: Setup webpack to output a library

--- www/waterfall_view/webpack.config.js
+++ www/waterfall_view/webpack.config.js
@@ -1,38 +1,62 @@
 'use strict';
 
 // Modules
+var path = require('path');
 var webpack = require('webpack');
+const env = require('yargs').argv.env;
 var autoprefixer = require('autoprefixer');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var PeerDepsExternalsPlugin = require('peer-deps-externals-webpack-plugin');
+const pkg = require('./package.json');
 
-var ENV = process.env.npm_lifecycle_event;
-var isTest = ENV === 'test' || ENV === 'test-watch';
-var isProd = ENV === 'build';
+var event = process.env.npm_lifecycle_event;
+
+let libraryName = pkg.name;
+
+var isTest = event === 'test' || event === 'test-watch';
+var isProd = env === 'prod';
+
+let outputFile, mode;
+
+if (isProd) {
+    mode = 'production';
+    outputFile = libraryName + '.min.js';
+} else {
+    mode = 'development';
+    outputFile = libraryName + '.js';
+}
 
 module.exports = function makeWebpackConfig() {
 
     var config = {};
 
-    config.entry = isTest ? void 0 : {
-        bbdata: './src/data.module.js'
+    config.mode = mode;
+
+    config.entry = {
+        waterfall: './src/module/main.module.js'
     };
 
     config.output = isTest ? {} : {
         path: __dirname + '/dist',
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js'
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+        globalObject: "typeof self !== 'undefined' ? self : this",
     };
 
     if (isTest) {
         config.devtool = 'inline-source-map';
-    }
-    else if (isProd) {
+    } else {
         config.devtool = 'source-map';
     }
-    else {
-        config.devtool = 'eval-source-map';
+
+    config.plugins = []
+
+    if (!isTest) {
+        config.plugins.push(new PeerDepsExternalsPlugin());
     }
 
     config.plugins = [
@@ -54,10 +78,6 @@ module.exports = function makeWebpackConfig() {
         }]
     };
 
-    // config.plugins = [
-    //   new webpack.optimize.UglifyJsPlugin({minimize: true})
-    // ]
-
     if (isTest) {
         config.module.rules.push({
             enforce: 'pre',
@@ -70,7 +90,14 @@ module.exports = function makeWebpackConfig() {
             query: {
                 esModules: true
             }
-        })
+        });
+
+        // avoid duplicate load of angular
+        config.resolve = {
+            alias: {
+              'angular': path.resolve(path.join(__dirname, 'node_modules', 'angular'))
+            },
+        };
     }
 
     return config;
