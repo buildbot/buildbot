f8b9595ecfee4c82c0e0c90dd94839f53730c135
Povilas Kanapickas <povilas@radix.lt>
2019-05-04 12:18:04 +0300

    www/data_module: Setup webpack to output a library

--- www/data_module/webpack.config.js
+++ www/data_module/webpack.config.js
@@ -2,37 +2,54 @@
 
 // Modules
 var webpack = require('webpack');
+const env = require('yargs').argv.env;
 var autoprefixer = require('autoprefixer');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var PeerDepsExternalsPlugin = require('peer-deps-externals-webpack-plugin');
+const pkg = require('./package.json');
 
-var ENV = process.env.npm_lifecycle_event;
-var isTest = ENV === 'test' || ENV === 'test-watch';
-var isProd = ENV === 'build';
+let libraryName = pkg.name;
+
+var event = process.env.npm_lifecycle_event;
+
+var isTest = event === 'test' || event === 'test-watch';
+var isProd = env === 'prod';
+
+let outputFile, mode;
+
+if (isProd) {
+    mode = 'production';
+    outputFile = libraryName + '.min.js';
+} else {
+    mode = 'development';
+    outputFile = libraryName + '.js';
+}
 
 module.exports = function makeWebpackConfig() {
 
     var config = {};
 
-    config.entry = isTest ? void 0 : {
+    config.mode = mode;
+
+    config.entry = {
         bbdata: './src/data.module.js'
     };
 
     config.output = isTest ? {} : {
         path: __dirname + '/dist',
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js'
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+        globalObject: "typeof self !== 'undefined' ? self : this",
     };
 
     if (isTest) {
         config.devtool = 'inline-source-map';
-    }
-    else if (isProd) {
+    } else {
         config.devtool = 'source-map';
-    }
-    else {
-        config.devtool = 'eval-source-map';
     }
 
     config.module = {
@@ -43,9 +60,11 @@ module.exports = function makeWebpackConfig() {
         }]
     };
 
-    // config.plugins = [
-    //   new webpack.optimize.UglifyJsPlugin({minimize: true})
-    // ]
+    config.plugins = [];
+
+    if (!isTest) {
+        config.plugins.push(new PeerDepsExternalsPlugin());
+    }
 
     if (isTest) {
         config.module.rules.push({
