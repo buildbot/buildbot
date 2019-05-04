a89877632973981df1a74ebff8394b72d7c2ae7f
Povilas Kanapickas <povilas@radix.lt>
2019-05-04 12:18:05 +0300

    www/guanlecoja-ui: Setup webpack and karma tests

--- www/guanlecoja-ui/src/module/main.module.js
+++ www/guanlecoja-ui/src/module/main.module.js
@@ -1,1 +1,9 @@
+import 'angular-animate';
+import 'angular-ui-bootstrap';
+import 'lodash';
+import '@uirouter/angularjs';
+
 angular.module("guanlecoja.ui", ["ui.bootstrap", "ui.router", "ngAnimate"]);
+
+const context = require.context('./', true, /^(?!.*(?:module|spec|webpack.js$)).*\.js$/);
+context.keys().forEach(context);
---
+++ www/guanlecoja-ui/webpack.config.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// Modules
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var ENV = process.env.npm_lifecycle_event;
+var isTest = ENV === 'test' || ENV === 'test-watch';
+var isProd = ENV === 'build';
+
+module.exports = function makeWebpackConfig() {
+
+    var config = {};
+
+    config.entry = isTest ? void 0 : {
+        bbdata: './src/data.module.js'
+    };
+
+    config.output = isTest ? {} : {
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js'
+    };
+
+    if (isTest) {
+        config.devtool = 'inline-source-map';
+    }
+    else if (isProd) {
+        config.devtool = 'source-map';
+    }
+    else {
+        config.devtool = 'eval-source-map';
+    }
+
+    config.module = {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    };
+    // config.plugins = [
+    //   new webpack.optimize.UglifyJsPlugin({minimize: true})
+    // ]
+
+    if (isTest) {
+        config.module.rules.push({
+            enforce: 'pre',
+            test: /\.js$/,
+            exclude: [
+                /node_modules/,
+                /\.spec\.js$/
+            ],
+            loader: 'istanbul-instrumenter-loader',
+            query: {
+                esModules: true
+            }
+        })
+    }
+
+    return config;
+}();
