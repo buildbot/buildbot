85c6fda7195fc96c603ee8e81cfc2fd383fbb050
Povilas Kanapickas <povilas@radix.lt>
2019-05-04 12:18:38 +0300

    www/base: Add reference to test mocks

--- www/base/src/tests.webpack.js
+++ www/base/src/tests.webpack.js
@@ -4,6 +4,7 @@
 import 'angular';
 import 'angular-mocks/angular-mocks';
 import './app/app.module.js'
+import '../test/scripts/mocks/config.mock.js'
 
 const context = require.context('./', true, /\.spec.js$/);
 context.keys().forEach(context);
