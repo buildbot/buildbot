349f17782908c57fd46c25e929c0dd0336611696
Povilas Kanapickas <povilas@radix.lt>
2019-05-04 12:18:37 +0300

    www/base: Setup webpack to output a library

--- www/base/webpack.config.js
+++ www/base/webpack.config.js
@@ -1,19 +1,38 @@
 'use strict';
 
 // Modules
+var path = require('path');
 var webpack = require('webpack');
+const env = require('yargs').argv.env;
 var autoprefixer = require('autoprefixer');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var PeerDepsExternalsPlugin = require('peer-deps-externals-webpack-plugin');
+const pkg = require('./package.json');
 
-var ENV = process.env.npm_lifecycle_event;
-var isTest = ENV === 'test' || ENV === 'test-watch';
-var isProd = ENV === 'build';
+var event = process.env.npm_lifecycle_event;
+
+let libraryName = pkg.name;
+
+var isTest = event === 'test' || event === 'test-watch';
+var isProd = env === 'prod';
+
+let outputFile, mode;
+
+if (isProd) {
+    mode = 'production';
+    outputFile = libraryName + '.min.js';
+} else {
+    mode = 'development';
+    outputFile = libraryName + '.js';
+}
 
 module.exports = function makeWebpackConfig() {
 
     var config = {};
+
+    config.mode = mode;
 
     config.entry = isTest ? void 0 : {
         base: './src/app/app.module.js'
@@ -21,18 +40,23 @@ module.exports = function makeWebpackConfig() {
 
     config.output = isTest ? {} : {
         path: __dirname + '/dist',
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js'
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+        globalObject: "typeof self !== 'undefined' ? self : this",
     };
 
     if (isTest) {
         config.devtool = 'inline-source-map';
-    }
-    else if (isProd) {
+    } else {
         config.devtool = 'source-map';
     }
-    else {
-        config.devtool = 'eval-source-map';
+
+    config.plugins = []
+
+    if (!isTest) {
+        config.plugins.push(new PeerDepsExternalsPlugin());
     }
 
     config.module = {
@@ -42,10 +66,6 @@ module.exports = function makeWebpackConfig() {
             exclude: /node_modules/
         }]
     };
-
-    // config.plugins = [
-    //   new webpack.optimize.UglifyJsPlugin({minimize: true})
-    // ]
 
     if (isTest) {
         config.module.rules.push({
