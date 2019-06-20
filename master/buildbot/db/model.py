c6c09494b3a8f6761e5ca276972524d42143f67a
Bertrand Demiddelaer <bertrand.demiddelaer@scality.com>
2019-06-20 11:41:29 +0200

    add an index to steps.started_at

--- master/buildbot/db/model.py
+++ master/buildbot/db/model.py
@@ -759,6 +759,8 @@ class Model(base.DBConnectorComponent):
              unique=True)
     sa.Index('steps_name', steps.c.buildid, steps.c.name,
              unique=True)
+    sa.Index('steps_started_at',
+             steps.c.started_at)
     sa.Index('logs_slug', logs.c.stepid, logs.c.slug, unique=True)
     sa.Index('logchunks_firstline', logchunks.c.logid, logchunks.c.first_line)
     sa.Index('logchunks_lastline', logchunks.c.logid, logchunks.c.last_line)
