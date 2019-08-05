3b4a0ccee98f98bab3d4c178d17ddfe68907572d
Rajdeep Bharati <rajdeep@macports.org>
2019-08-05 16:30:15 +0530

    Create new changebuilds page
    Displays all builds for the selected change.

--- www/base/src/app/changes/changes.route.js
+++ www/base/src/app/changes/changes.route.js
@@ -15,8 +15,9 @@ class ChangesState {
             controller: `${name}Controller`,
             template: require('./changes.tpl.jade'),
             name,
-            url: '/changes',
-            data: cfg
+            url: '/changes?id',
+            data: cfg,
+            reloadOnSearch: false
         };
 
         $stateProvider.state(state);
@@ -29,8 +30,7 @@ class ChangesState {
                 name:'changesFetchLimit',
                 caption:'Maximum number of changes to fetch',
                 default_value: 50
-            }
-            ]});
+            }]});
     }
 }
 
