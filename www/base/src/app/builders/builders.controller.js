a1181bf43e71023fe3397c277ef4fe7944f94a19
Rajdeep Bharati <rajdeep@macports.org>
2019-08-11 00:09:58 +0530

    Add pagination to builders page

--- www/base/src/app/builders/builders.controller.js
+++ www/base/src/app/builders/builders.controller.js
@@ -35,6 +35,9 @@ class Builders {
         , true);
         const buildFetchLimit = $scope.settings.buildFetchLimit.value;
 
+        $scope.page_size = $scope.settings.page_size.value;
+        $scope.currentPage = 1;
+
         const updateTagsFilterFromLocation = function() {
             $scope.tags_filter = $location.search()["tags"];
             if ($scope.tags_filter == null) { $scope.tags_filter = []; }
