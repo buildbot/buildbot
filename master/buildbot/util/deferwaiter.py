21f0e5ed4fd5c73a6350de52ec07a81182010794
Povilas Kanapickas <povilas@radix.lt>
2019-12-29 17:49:17 +0200

    deferwaiter: Implement waiter for repeated actions

--- master/buildbot/util/deferwaiter.py
+++ master/buildbot/util/deferwaiter.py
@@ -14,6 +14,7 @@
 # Copyright Buildbot Team Members
 
 from twisted.internet import defer
+from twisted.python import log
 
 from buildbot.util import Notifier
 
@@ -42,3 +43,61 @@ class DeferWaiter:
         if not self._waited:
             return
         yield self._finish_notifier.wait()
+
+
+class RepeatedActionHandler:
+    """ This class handles a repeated action such as submitting keepalive requests. It integrates
+        with DeferWaiter to correctly control shutdown of such process.
+    """
+
+    def __init__(self, reactor, waiter, interval, action,
+                 start_timer_after_action_completes=False):
+        self._reactor = reactor
+        self._waiter = waiter
+        self._interval = interval
+        self._action = action
+        self._enabled = False
+        self._timer = None
+        self._start_timer_after_action_completes = start_timer_after_action_completes
+
+    def setInterval(self, interval):
+        self._interval = interval
+
+    def start(self):
+        if self._enabled:
+            return
+        self._enabled = True
+        self._start_timer()
+
+    def stop(self):
+        if not self._enabled:
+            return
+
+        self._enabled = False
+        if self._timer and self._timer.active():
+            self._timer.cancel()
+            self._timer = None
+
+    def _start_timer(self):
+        self._timer = self._reactor.callLater(self._interval, self._handle_timeout)
+
+    @defer.inlineCallbacks
+    def _do_action(self):
+        try:
+            yield self._action()
+        except Exception as e:
+            log.err(e, 'Got exception in RepeatedActionHandler')
+
+    def _handle_timeout(self):
+        self._waiter.add(self._handle_action())
+
+    @defer.inlineCallbacks
+    def _handle_action(self):
+        if self._start_timer_after_action_completes:
+            yield self._do_action()
+
+        if self._enabled:
+            self._start_timer()
+
+        if not self._start_timer_after_action_completes:
+            yield self._do_action()
