689cd3d16c986d29e83e3bb49066cc652f59730f
bschatt <44769431+bschatt@users.noreply.github.com>
2020-01-04 19:26:15 +0100

    P4: allow P4TICKETS env var

--- master/buildbot/changes/p4poller.py
+++ master/buildbot/changes/p4poller.py
@@ -102,7 +102,7 @@ class P4Source(base.PollingChangeSource, util.ComparableMixin):
                      "server_tz")
 
     env_vars = ["P4CLIENT", "P4PORT", "P4PASSWD", "P4USER",
-                "P4CHARSET", "P4CONFIG", "PATH", "HOME"]
+                "P4CHARSET", "P4CONFIG", "P4TICKETS", "PATH", "HOME"]
 
     changes_line_re = re.compile(
         r"Change (?P<num>\d+) on \S+ by \S+@\S+ '.*'$")
