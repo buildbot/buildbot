91ed09964e91742e8430177fbf76e56777742ed7
Povilas Kanapickas <povilas@radix.lt>
2020-05-30 23:51:07 +0300

    Bump minimum requirement of SQLAlchemy to 1.2.0

--- master/setup.py
+++ master/setup.py
@@ -476,7 +476,7 @@ setup_args['install_requires'] = [
     'Jinja2 >= 2.1',
     # required for tests, but Twisted requires this anyway
     'zope.interface >= 4.1.1',
-    'sqlalchemy>=1.1.0',
+    'sqlalchemy>=1.2.0',
     'sqlalchemy-migrate>=0.9',
     'python-dateutil>=1.5',
     'txaio ' + txaio_ver,
