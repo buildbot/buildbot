a7350c6e57d9ed723adcdddf2f94d22b87446205
Povilas Kanapickas <povilas@radix.lt>
2020-06-21 17:47:24 +0300

    data: Add data APIs for build data functionality

---
+++ master/buildbot/data/build_data.py
@@ -0,0 +1,92 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+
+
+from twisted.internet import defer
+
+from buildbot.data import base
+from buildbot.data import types
+
+
+class Db2DataMixin:
+
+    def db2data(self, dbdict):
+        data = {
+            'buildid': dbdict['buildid'],
+            'name': dbdict['name'],
+            'value': dbdict['value'],
+            'source': dbdict['source'],
+        }
+        return defer.succeed(data)
+
+
+class BuildDatasNoValueEndpoint(Db2DataMixin, base.BuildNestingMixin, base.Endpoint):
+
+    isCollection = True
+    pathPatterns = """
+        /builders/n:builderid/builds/n:build_number/data
+        /builders/i:buildername/builds/n:build_number/data
+        /builds/n:buildid/data
+        """
+
+    @defer.inlineCallbacks
+    def get(self, resultSpec, kwargs):
+        buildid = yield self.getBuildid(kwargs)
+
+        build_datadicts = yield self.master.db.build_data.getAllBuildDataNoValues(buildid)
+
+        results = []
+        for dbdict in build_datadicts:
+            results.append((yield self.db2data(dbdict)))
+        return results
+
+
+class BuildDataEndpoint(Db2DataMixin, base.BuildNestingMixin, base.Endpoint):
+
+    isCollection = False
+    pathPatterns = """
+        /builders/n:builderid/builds/n:build_number/data/i:name
+        /builders/i:buildername/builds/n:build_number/data/i:name
+        /builds/n:buildid/data/i:name
+    """
+
+    @defer.inlineCallbacks
+    def get(self, resultSpec, kwargs):
+        buildid = yield self.getBuildid(kwargs)
+        name = kwargs['name']
+
+        build_datadict = yield self.master.db.build_data.getBuildData(buildid, name)
+
+        return (yield self.db2data(build_datadict)) if build_datadict else None
+
+
+class BuildData(base.ResourceType):
+
+    name = "build_data"
+    plural = "build_data"
+    endpoints = [BuildDatasNoValueEndpoint, BuildDataEndpoint]
+    keyFields = []
+
+    class EntityType(types.Entity):
+        buildid = types.Integer()
+        name = types.String()
+        value = types.NoneOk(types.Binary())
+        source = types.String()
+    entityType = EntityType(name)
+
+    @base.updateMethod
+    @defer.inlineCallbacks
+    def setBuildData(self, buildid, name, value, source):
+        yield self.master.db.build_data.setBuildData(buildid, name, value, source)
