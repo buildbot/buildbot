dfee6d6d02f872f0c32bd17f3f12705dc8e894f3
Bart Smink <bartsmink@gmail.com>
2020-09-07 19:24:15 +0200

    fixes buildbot issues with buildbot try on python3, see issue #4488

--- master/buildbot/process/properties.py
+++ master/buildbot/process/properties.py
@@ -190,6 +190,8 @@ class Properties(util.ComparableMixin):
 
     def setProperty(self, name, value, source, runtime=False):
         name = util.bytes2unicode(name)
+        if isinstance(value, bytes):
+            value = util.bytes2unicode(value)
         if not IRenderable.providedBy(value):
             json.dumps(value)  # Let the exception propagate ...
         source = util.bytes2unicode(source)
