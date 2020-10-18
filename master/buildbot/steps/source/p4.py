2ddb69a68d55be9cc3db96882e3c7a4e3828c4b5
Arseniy Terekhin <senyai@gmail.com>
2020-10-18 23:48:08 +0300

    p4 step: mark password renderable
    closes 3954

--- master/buildbot/steps/source/p4.py
+++ master/buildbot/steps/source/p4.py
@@ -44,7 +44,7 @@ class P4(Source):
 
     name = 'p4'
 
-    renderables = ['mode', 'p4base', 'p4client', 'p4viewspec', 'p4branch']
+    renderables = ['mode', 'p4base', 'p4client', 'p4viewspec', 'p4branch', 'p4passwd']
     possible_modes = ('incremental', 'full')
 
     def __init__(self, mode='incremental',
