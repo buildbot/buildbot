buildbot/buildbot

commit f65df841935275f8d2e0921cc0b04f95ae40cfd4

gerrit: Deduplicate only source-related gerrit events

master/buildbot/changes/gerritchangesource.py
@@ -163,7 +163,7 @@ class GerritChangeSourceBase(base.ChangeSource):
         return func(properties, event)
 
     @defer.inlineCallbacks
-    def addChange(self, chdict):
+    def addChange(self, event_type, chdict):
         stampdict = {
             "branch": chdict["branch"],
             "revision": chdict["revision"],
@@ -177,7 +177,7 @@ class GerritChangeSourceBase(base.ChangeSource):
         stampid, found_existing = yield(
              self.master.db.sourcestamps.findOrCreateId(**stampdict))
 
-        if found_existing:
+        if found_existing and event_type in ("patchset-created", "ref-updated"):
             if self.debug or True:
                 eventstr = "{}/{} -- {}:{}".format(
                     self.gitBaseURL, chdict["project"], chdict["branch"],
@@ -236,7 +236,7 @@ class GerritChangeSourceBase(base.ChangeSource):
                 patchset=event["patchSet"]["number"]
             )
 
-        yield self.addChange({
+        yield self.addChange(event['type'], {
             'author': _gerrit_user_to_author(event_change["owner"]),
             'project': util.bytes2unicode(event_change["project"]),
             'repository': "{}/{}".format(

master/buildbot/test/unit/changes/test_gerritchangesource.py
@@ -200,6 +200,39 @@ class TestGerritChangeSource(changesource.ChangeSourceMixin,
         self.assertEqual(len(self.master.data.updates.changesAdded), 1)
 
     @defer.inlineCallbacks
+    def test_duplicate_non_source_events_not_ignored(self):
+        s = self.newChangeSource('somehost', 'someuser',
+                                 handled_events=['patchset-created', 'ref-updated',
+                                                 'change-merged', 'comment-added'])
+        yield s.lineReceived(json.dumps({
+            'type': "comment-added",
+            'change': {
+                'branch': "br",
+                'project': "pr",
+                'number': "4321",
+                'owner': {'name': "Dustin", 'email': "dustin@mozilla.com"},
+                'url': "http://buildbot.net",
+                'subject': "fix 1234"
+            },
+            'patchSet': {'revision': "abcdef", 'number': "12"}
+        }))
+        self.assertEqual(len(self.master.data.updates.changesAdded), 1)
+
+        yield s.lineReceived(json.dumps({
+            'type': "comment-added",
+            'change': {
+                'branch': "br",
+                'project': "pr",
+                'number': "4321",
+                'owner': {'name': "Dustin", 'email': "dustin@mozilla.com"},
+                'url': "http://buildbot.net",
+                'subject': "fix 1234"
+            },
+            'patchSet': {'revision': "abcdef", 'number': "12"}
+        }))
+        self.assertEqual(len(self.master.data.updates.changesAdded), 2)
+
+    @defer.inlineCallbacks
     def test_malformed_events_ignored(self):
         s = self.newChangeSource('somehost', 'someuser')
         # "change" not in event

commit 995dd25ffe23677ae4a0a8cce07e14ec5cd2a321

gerrit: Fix crash when refupdated event is sent to GerritChangeSource
This fixes a regression introduced in
f65df841935275f8d2e0921cc0b04f95ae40cfd4.

master/buildbot/changes/gerritchangesource.py
@@ -257,7 +257,7 @@ class GerritChangeSourceBase(base.ChangeSource):
         if "submitter" in event:
             author = _gerrit_user_to_author(event["submitter"], author)
 
-        return self.addChange(dict(
+        return self.addChange(event['type'], dict(
             author=author,
             project=ref["project"],
             repository="{}/{}".format(self.gitBaseURL, ref["project"]),

master/buildbot/test/unit/changes/test_gerritchangesource.py
@@ -168,6 +168,54 @@ class TestGerritChangeSource(changesource.ChangeSourceMixin,
             self.assertEqual(self.expected_change[k], v)
 
     @defer.inlineCallbacks
+    def test_lineReceived_ref_updated(self):
+        s = self.newChangeSource('somehost', 'someuser')
+        yield s.lineReceived(json.dumps({
+            'type': 'ref-updated',
+            'submitter': {
+                'name': 'tester',
+                'email': 'tester@example.com',
+                'username': 'tester'
+            },
+            'refUpdate': {
+                'oldRev': '12341234',
+                'newRev': '56785678',
+                'refName': 'refs/heads/master',
+                'project': 'test'
+            },
+            'eventCreatedOn': 1614528683
+        }))
+        self.assertEqual(len(self.master.data.updates.changesAdded), 1)
+        c = self.master.data.updates.changesAdded[0]
+        self.assertEqual(c, {
+            'files': ['unknown'],
+            'comments': 'Gerrit: patchset(s) merged.',
+            'author': 'tester <tester@example.com>',
+            'committer': None,
+            'revision': '56785678',
+            'when_timestamp': None,
+            'branch': 'refs/heads/master',
+            'category': 'ref-updated',
+            'revlink': '',
+            'properties': {
+                'event.type': 'ref-updated',
+                'event.submitter.name': 'tester',
+                'event.submitter.email':
+                    'tester@example.com',
+                    'event.submitter.username': 'tester',
+                    'event.refUpdate.oldRev': '12341234',
+                    'event.refUpdate.newRev': '56785678',
+                    'event.refUpdate.refName': 'refs/heads/master',
+                    'event.refUpdate.project': 'test',
+                    'event.source': 'GerritChangeSource'
+                },
+            'repository': 'ssh://someuser@somehost:29418/test',
+            'codebase': None,
+            'project': 'test',
+            'src': None
+        })
+
+    @defer.inlineCallbacks
     def test_duplicate_events_ignored(self):
         s = self.newChangeSource('somehost', 'someuser')
         yield s.lineReceived(json.dumps(dict(