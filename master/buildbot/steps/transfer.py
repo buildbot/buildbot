buildbot/buildbot

commit 5c03c53a8cb245d9b288d2f371e829c545d1a1e3

steps: Convert FileDownload to new style

master/buildbot/steps/transfer.py
@@ -425,9 +425,10 @@ class FileDownload(_TransferBuildStep):
                 'mode must be an integer or None')
         self.mode = mode
 
-    def start(self):
+    @defer.inlineCallbacks
+    def run(self):
         self.checkWorkerHasCommand("downloadFile")
-        self.stdio_log = self.addLog("stdio")
+        self.stdio_log = yield self.addLog("stdio")
 
         # we are currently in the buildmaster's basedir, so any non-absolute
         # paths will be interpreted relative to that
@@ -436,19 +437,16 @@ class FileDownload(_TransferBuildStep):
         log.msg("FileDownload started, from master %r to worker %r" %
                 (source, workerdest))
 
-        self.descriptionDone = "downloading to {}".format(os.path.basename(workerdest))
+        self.descriptionDone = ["downloading to", os.path.basename(workerdest)]
 
         # setup structures for reading the file
         try:
             fp = open(source, 'rb')
         except IOError:
             # if file does not exist, bail out with an error
-            self.addCompleteLog('stderr',
-                                'File %r not available at master' % source)
-            # TODO: once BuildStep.start() gets rewritten to use
-            # maybeDeferred, just re-raise the exception here.
-            eventually(BuildStep.finished, self, FAILURE)
-            return
+            yield self.addCompleteLog('stderr', 'File {!r} not available at master'.format(source))
+            raise
+
         fileReader = remotetransfer.FileReader(fp)
 
         # default arguments
@@ -466,8 +464,8 @@ class FileDownload(_TransferBuildStep):
             args['workerdest'] = workerdest
 
         cmd = makeStatusRemoteCommand(self, 'downloadFile', args)
-        d = self.runTransferCommand(cmd)
-        d.addCallback(self.finished).addErrback(self.failed)
+        res = yield self.runTransferCommand(cmd)
+        return res
 
 
 class StringDownload(_TransferBuildStep):

commit 3d333e68285245957aecf3b9a6c1f7a4d19b7f79

Fix exception in FileDownload when file doesn't exist on master
Raising an exception breaks use cases such as FileDownload(haltOnFailure=False).
Also, the behavior deviated unnecessarily from FileUpload. This regression was
made in 5c03c53a8cb245d9b288d2f371e829c545d1a1e3. The test didn't catch the
behavior change because it wasn't there yet.

The test, introduced in 86dfd8269b84ad0beb57dfeefbcd9b4adf18818e, was partly
broken due to misplaced and wrong expectLogfile call. This is now fixed too.

master/buildbot/steps/transfer.py
@@ -460,7 +460,7 @@ class FileDownload(_TransferBuildStep):
         except IOError:
             # if file does not exist, bail out with an error
             yield self.addCompleteLog('stderr', 'File {!r} not available at master'.format(source))
-            raise
+            return FAILURE
 
         fileReader = remotetransfer.FileReader(fp)
 

master/buildbot/test/unit/steps/test_transfer.py
@@ -998,13 +998,12 @@ class TestFileDownload(steps.BuildStepMixin, TestReactorMixin,
 
         self.expectCommands()
 
-        self.expectOutcome(result=EXCEPTION,
-                           state_string="downloading to {0} (exception)".format(
+        self.expectOutcome(result=FAILURE,
+                           state_string="downloading to {0} (failure)".format(
                                os.path.basename(self.destfile)))
-        yield self.runStep()
         self.expectLogfile('stderr',
-                           "File wkdir/srcdir not available at worker")
-        self.assertEqual(len(self.flushLoggedErrors(FileNotFoundError)), 1)
+                           "File 'not existing file' not available at master")
+        yield self.runStep()
 
 
 class TestStringDownload(steps.BuildStepMixin, TestReactorMixin,