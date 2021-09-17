2cb0c471094fffb7260a52b537a9926f45ef1318
Povilas Kanapickas <povilas@radix.lt>
2021-09-17 12:11:27 +0300

    test: Fix worker http proxy log handling when running concurrently

--- master/buildbot/test/integration/test_worker_proxy.py
+++ master/buildbot/test/integration/test_worker_proxy.py
@@ -15,6 +15,7 @@
 
 import asyncio
 import multiprocessing
+import os
 import signal
 import socket
 
@@ -32,8 +33,17 @@ from .interop.test_worker_reconnect import WorkerReconnect
 
 # This integration test puts HTTP proxy in between the master and worker.
 
-import os
-CURRENT = os.path.dirname(os.path.abspath(__file__))
+
+def get_log_path():
+    return f'test_worker_proxy_stdout_{os.getpid()}.txt'
+
+
+def write_to_log(msg, with_traceback=False):
+    with open(get_log_path(), 'a') as outfile:
+        outfile.write(msg)
+        if with_traceback:
+            import traceback
+            traceback.print_exc(file=outfile)
 
 
 async def handle_client(local_reader, local_writer):
@@ -51,8 +61,7 @@ async def handle_client(local_reader, local_writer):
         request = await local_reader.read(2048)
         lines = request.split(b"\r\n")
         if not lines[0].startswith(b"CONNECT "):
-            with open(os.path.join(CURRENT, "stdout.txt"), "a") as outfile:
-                outfile.write(f"bad request {request.decode()}\n")
+            write_to_log(f"bad request {request.decode()}\n")
             local_writer.write(b"HTTP/1.1 407 Only CONNECT allowed\r\n\r\n")
             return
         host, port = lines[0].split(b" ")[1].split(b":")
@@ -61,13 +70,11 @@ async def handle_client(local_reader, local_writer):
                 host.decode(), int(port)
             )
         except socket.gaierror:
-            with open(os.path.join(CURRENT, "stdout.txt"), "a") as outfile:
-                outfile.write(f"failed to relay to {host} {port}\n")
+            write_to_log(f"failed to relay to {host} {port}\n")
             local_writer.write(b"HTTP/1.1 404 Not Found\r\n\r\n")
             return
 
-        with open(os.path.join(CURRENT, "stdout.txt"), "a") as outfile:
-            outfile.write(f"relaying to {host} {port}\n")
+        write_to_log(f"relaying to {host} {port}\n")
         local_writer.write(b"HTTP/1.1 200 Connection established\r\n\r\n")
         pipe1 = pipe(local_reader, remote_writer)
         pipe2 = pipe(remote_reader, local_writer)
@@ -78,8 +85,7 @@ async def handle_client(local_reader, local_writer):
 
 
 def run_proxy():
-    with open(os.path.join(CURRENT, "stdout.txt"), "a") as outfile:
-        outfile.write("run_proxy\n")
+    write_to_log("run_proxy\n")
 
     try:
         loop = asyncio.get_event_loop()
@@ -91,50 +97,43 @@ def run_proxy():
 
         signal.signal(signal.SIGTERM, signal_handler)
 
-        with open(os.path.join(CURRENT, "stdout.txt"), "a") as outfile:
-            outfile.write("Serving on {}\n".format(server.sockets[0].getsockname()))
+        write_to_log("Serving on {}\n".format(server.sockets[0].getsockname()))
         try:
-            with open(os.path.join(CURRENT, "stdout.txt"), "a") as outfile:
-                outfile.write("Running forever\n")
+            write_to_log("Running forever\n")
             loop.run_forever()
         except KeyboardInterrupt:
-            with open(os.path.join(CURRENT, "stdout.txt"), "a") as outfile:
-                outfile.write("End\n")
-            pass
+            write_to_log("End\n")
 
         server.close()
         loop.run_until_complete(server.wait_closed())
         loop.close()
 
     except BaseException as e:
-        with open(os.path.join(CURRENT, "stdout.txt"), "a") as outfile:
-            outfile.write(f"Exception Raised: {str(e)}\n")
-            import traceback
-            traceback.print_exc(file=outfile)
+        write_to_log(f"Exception Raised: {str(e)}\n", with_traceback=True)
 
 
 class RunMasterBehindProxy(RunMasterBase):
     # we need slightly longer timeout for proxy related tests
     timeout = 30
+    debug = False
 
     def setUp(self):
-        with open(os.path.join(CURRENT, "stdout.txt"), "w") as outfile:
-            outfile.write("setUp\n")
+        write_to_log("setUp\n")
         self.proxy_process = multiprocessing.Process(target=run_proxy)
         self.proxy_process.start()
-        with open(os.path.join(CURRENT, "stdout.txt"), "a") as outfile:
-            outfile.write(f"X {self.proxy_process.exitcode}\n")
-            outfile.write(f"Y {self.proxy_process.is_alive()}\n")
+        self.target_port = self.queue.get()
+        write_to_log(f"got target_port {self.target_port}\n")
 
     def tearDown(self):
-        with open(os.path.join(CURRENT, "stdout.txt"), "a") as outfile:
-            outfile.write("tearDown\n")
+        write_to_log("tearDown\n")
         self.proxy_process.terminate()
         self.proxy_process.join()
-        print("---- stdout ----")
-        with open(os.path.join(CURRENT, "stdout.txt")) as file:
-            print(file.read())
-        print("---- ------ ----")
+        if self.debug:
+            print("---- stdout ----")
+            with open(get_log_path()) as file:
+                print(file.read())
+            print("---- ------ ----")
+            os.unlink(get_log_path())
 
     @defer.inlineCallbacks
     def setupConfig(self, config_dict, startWorker=True):
