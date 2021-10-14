buildbot/buildbot

commit a3c770a60e619aef827ac60b5ca07a39ebee9a6b

make properties a proper graphql subresource, fetched on demand
As of now, the changes properties are not a subresources
It would need a lot of changes in the dbapi, and I doubt this really deserves it.

master/buildbot/data/builds.py
@@ -172,13 +172,16 @@ class BuildsEndpoint(Db2DataMixin, base.BuildNestingMixin, base.Endpoint):
         buildscol = []
         for b in builds:
             data = yield self.db2data(b)
-            # Avoid to request DB for Build's properties if not specified
-            if filters:  # pragma: no cover
-                props = yield self.master.db.builds.getBuildProperties(b['id'])
-                filtered_properties = self._generate_filtered_properties(
-                    props, filters)
-                if filtered_properties:
-                    data['properties'] = filtered_properties
+            if kwargs.get('graphql'):
+                # let the graphql engine manage the properties
+                del data['properties']
+            else:
+                # Avoid to request DB for Build's properties if not specified
+                if filters:  # pragma: no cover
+                    props = yield self.master.db.builds.getBuildProperties(b["buildid"])
+                    filtered_properties = self._generate_filtered_properties(props, filters)
+                    if filtered_properties:
+                        data["properties"] = filtered_properties
 
             buildscol.append(data)
         return buildscol
@@ -195,7 +198,7 @@ class Build(base.ResourceType):
         /builds/:buildid
         /workers/:workerid/builds/:buildid
     """
-    subresources = ["Step"]
+    subresources = ["Step", "Property"]
 
     class EntityType(types.Entity):
         buildid = types.Integer()

master/buildbot/data/base.py
@@ -131,7 +131,7 @@ class Endpoint:
                 )
         else:
             parentid = self.parentMapping[parent_type]
-        ret = {}
+        ret = {'graphql': True}
         ret[parentid] = parent[parentid]
         return ret
 
@@ -141,8 +141,8 @@ class Endpoint:
                 return self.get_kwargs_from_graphql_parent(
                     parent, resolve_info.parent_type.name
                 )
-            return {}
-        ret = {}
+            return {'graphql': True}
+        ret = {'graphql': True}
         k = self.rtype.keyField
         v = args.pop(k)
         if v is not None:

master/buildbot/data/buildsets.py
@@ -132,6 +132,7 @@ class Buildset(base.ResourceType):
         parent_buildid = types.NoneOk(types.Integer())
         parent_relationship = types.NoneOk(types.String())
     entityType = EntityType(name)
+    subresources = ["Property"]
 
     @base.updateMethod
     @defer.inlineCallbacks

master/buildbot/data/changes.py
@@ -14,6 +14,7 @@
 # Copyright Buildbot Team Members
 
 import copy
+import json
 
 from twisted.internet import defer
 from twisted.python import log
@@ -30,15 +31,21 @@ from buildbot.util import epoch2datetime
 class FixerMixin:
 
     @defer.inlineCallbacks
-    def _fixChange(self, change):
+    def _fixChange(self, change, is_graphql):
         # TODO: make these mods in the DB API
         if change:
             change = change.copy()
             change['when_timestamp'] = datetime2epoch(change['when_timestamp'])
-
-            sskey = ('sourcestamps', str(change['sourcestampid']))
-            change['sourcestamp'] = yield self.master.data.get(sskey)
-            del change['sourcestampid']
+            if is_graphql:
+                props = change['properties']
+                change['properties'] = [
+                    {'name': k, 'source': v[1], 'value': json.dumps(v[0])}
+                    for k, v in props.items()
+                ]
+            else:
+                sskey = ('sourcestamps', str(change['sourcestampid']))
+                change['sourcestamp'] = yield self.master.data.get(sskey)
+                del change['sourcestampid']
         return change
     fieldMapping = {
         'changeid': 'changes.changeid',
@@ -54,7 +61,7 @@ class ChangeEndpoint(FixerMixin, base.Endpoint):
 
     def get(self, resultSpec, kwargs):
         d = self.master.db.changes.getChange(kwargs['changeid'])
-        d.addCallback(self._fixChange)
+        d.addCallback(self._fixChange, is_graphql='graphql' in kwargs)
         return d
 
 
@@ -89,7 +96,7 @@ class ChangesEndpoint(FixerMixin, base.BuildNestingMixin, base.Endpoint):
                 changes = yield self.master.db.changes.getChanges(resultSpec=resultSpec)
         results = []
         for ch in changes:
-            results.append((yield self._fixChange(ch)))
+            results.append((yield self._fixChange(ch, is_graphql='graphql' in kwargs)))
         return results
 
 
@@ -102,6 +109,7 @@ class Change(base.ResourceType):
         /changes/:changeid
     """
     keyField = "changeid"
+    subresources = ["Build", "Property"]
 
     class EntityType(types.Entity):
         changeid = types.Integer()
@@ -121,7 +129,6 @@ class Change(base.ResourceType):
         codebase = types.String()
         sourcestamp = sourcestamps.SourceStamp.entityType
     entityType = EntityType(name)
-    subresources = ["Build"]
 
     @base.updateMethod
     @defer.inlineCallbacks

master/buildbot/data/graphql.py
@@ -32,6 +32,12 @@ except ImportError:  # pragma: no cover
     graphql = None
 
 
+def _enforce_list(v):
+    if isinstance(v, list):
+        return v
+    return [v]
+
+
 class GraphQLConnector(service.AsyncService):
     """Mixin class to separate the GraphQL traits for the data connector
 
@@ -134,14 +140,22 @@ class GraphQLConnector(service.AsyncService):
             add_dependent_types(typ)
             query_fields = []
             # build the queriable parameters, via query_fields
-            for field in sorted(rtype.entityType.fields.keys()):
-                field_type = rtype.entityType.fields[field]
+            for field, field_type in sorted(rtype.entityType.fields.items()):
+                # in graphql, we handle properties as queriable sub resources
+                # instead of hardcoded attributes like in rest api
+                if field == 'properties':
+                    continue
                 field_type_graphql = field_type.getGraphQLInputType()
                 if field_type_graphql is None:
                     continue
                 query_fields.append(f"{field}: {field_type_graphql}")
                 for op in sorted(operators):
-                    query_fields.append(f"{field}__{op}: {field_type_graphql}")
+                    if op in ["in", "notin"]:
+                        if field_type_graphql in ["String", "Int"]:
+                            query_fields.append(
+                                f"{field}__{op}: [{field_type_graphql}]")
+                    else:
+                        query_fields.append(f"{field}__{op}: {field_type_graphql}")
 
             query_fields.extend(["order: String", "limit: Int", "offset: Int"])
             ep = self.data.getEndPointForResourceName(rtype.plural)
@@ -210,13 +224,19 @@ class GraphQLConnector(service.AsyncService):
         async def field_resolver(parent, resolve_info, **args):
             field = resolve_info.field_name
             if parent is not None and field in parent:
-                return default_field_resolver(parent, resolve_info, **args)
+                res = default_field_resolver(parent, resolve_info, **args)
+                if isinstance(res, list) and args:
+                    ep = self.data.getEndPointForResourceName(field)
+                    args = {k: _enforce_list(v) for k, v in args.items()}
+                    rspec = self.data.resultspec_from_jsonapi(args, ep.rtype.entityType, True)
+                    res = rspec.apply(res)
+                return res
             ep = self.data.getEndPointForResourceName(field)
             rspec = None
             kwargs = ep.get_kwargs_from_graphql(parent, resolve_info, args)
 
             if ep.isCollection or ep.isPseudoCollection:
-                args = {k: [v] for k, v in args.items()}
+                args = {k: _enforce_list(v) for k, v in args.items()}
                 rspec = self.data.resultspec_from_jsonapi(args, ep.rtype.entityType, True)
 
             return await self._aio_ep_get(ep, kwargs, rspec)

master/buildbot/data/properties.py
@@ -13,6 +13,8 @@
 #
 # Copyright Buildbot Team Members
 
+import json
+
 from twisted.internet import defer
 
 from buildbot.data import base
@@ -41,14 +43,69 @@ class BuildPropertiesEndpoint(base.Endpoint):
     def get(self, resultSpec, kwargs):
         buildid = kwargs.get("buildid", None)
         if buildid is None:
+            # fixme: this cannot work...
             buildid = kwargs.get("build_number")
         return self.master.db.builds.getBuildProperties(buildid)
 
 
-class Properties(base.ResourceType):
+class PropertiesListEndpoint(base.Endpoint):
+
+    isCollection = True
+    pathPatterns = """
+        /builds/n:buildid/property_list
+        /buildsets/n:bsid/properties_list
+        /changes/n:changeid/properties_list
+    """
+    buildFieldMapping = {
+        "name": "build_properties.name",
+        "source": "build_properties.source",
+        "value": "build_properties.value",
+    }
+    buildsetFieldMapping = {
+        "name": "buildset_properties.name",
+        "source": "buildset_properties.source",
+        "value": "buildset_properties.value",
+    }
+    changeFieldMapping = {
+        "name": "change_properties.name",
+        "source": "change_properties.source",
+        "value": "change_properties.value",
+    }
+
+    @defer.inlineCallbacks
+    def get(self, resultSpec, kwargs):
+        buildid = kwargs.get("buildid", None)
+        bsid = kwargs.get("bsid", None)
+        changeid = kwargs.get("changeid", None)
+        if buildid is not None:
+            if resultSpec is not None:
+                resultSpec.fieldMapping = self.buildFieldMapping
+            props = yield self.master.db.builds.getBuildProperties(buildid, resultSpec)
+        elif bsid is not None:
+            if resultSpec is not None:
+                resultSpec.fieldMapping = self.buildsetFieldMapping
+            props = yield self.master.db.buildsets.getBuildsetProperties(bsid)
+        elif changeid is not None:
+            if resultSpec is not None:
+                resultSpec.fieldMapping = self.buildsetFieldMapping
+            props = yield self.master.db.changes.getChangeProperties(changeid)
+        return [{'name': k, 'source': v[1], 'value': json.dumps(v[0])} for k, v in props.items()]
+
+
+class Property(base.ResourceType):
 
     name = "property"
     plural = "properties"
+    endpoints = [PropertiesListEndpoint]
+    keyField = "name"
+
+    entityType = types.PropertyEntityType(name)
+
+
+class Properties(base.ResourceType):
+
+    name = "_property"
+    plural = "_properties"
     endpoints = [BuildsetPropertiesEndpoint, BuildPropertiesEndpoint]
     keyField = "name"
 

master/buildbot/data/resultspec.py
@@ -37,6 +37,8 @@ class FieldBase:
         'gt': lambda d, v: d > v[0],
         'ge': lambda d, v: d >= v[0],
         'contains': lambda d, v: v[0] in d,
+        'in': lambda d, v: d in v,
+        'notin': lambda d, v: d not in v,
     }
 
     singular_operators_sql = {
@@ -46,20 +48,26 @@ class FieldBase:
         'le': lambda d, v: d <= v[0],
         'gt': lambda d, v: d > v[0],
         'ge': lambda d, v: d >= v[0],
-        'contains': lambda d, v: d.contains(v[0])
+        'contains': lambda d, v: d.contains(v[0]),
         # only support string values, because currently there are no queries against lists in SQL
+        'in': lambda d, v: d.in_(v),
+        'notin': lambda d, v: d.notin_(v),
     }
 
     plural_operators = {
         'eq': lambda d, v: d in v,
         'ne': lambda d, v: d not in v,
         'contains': lambda d, v: len(set(v).intersection(set(d))) > 0,
+        'in': lambda d, v: d in v,
+        'notin': lambda d, v: d not in v,
     }
 
     plural_operators_sql = {
         'eq': lambda d, v: d.in_(v),
         'ne': lambda d, v: d.notin_(v),
         'contains': lambda d, vs: sa.or_(*[d.contains(v) for v in vs]),
+        'in': lambda d, v: d.in_(v),
+        'notin': lambda d, v: d.notin_(v),
         # sqlalchemy v0.8's or_ cannot take generator arguments, so this has to be manually expanded
         # only support string values, because currently there are no queries against lists in SQL
     }

master/buildbot/data/types.py
@@ -418,6 +418,9 @@ class Entity(Type):
                     fields=[dict(name=k,
                                  type=v.toGraphQL())
                             for k, v in self.fields.items()
+                            # in graphql, we handle properties as queriable sub resources
+                            # instead of hardcoded attributes like in rest api
+                            if k != "properties"
                             ])
 
     def toGraphQLTypeName(self):

commit fa1504e51f31c2ba4714698711a5fab5183934f4

data: Fix incorrect invocation of getBuildProperties()
This fixes regression introduced in
a3c770a60e619aef827ac60b5ca07a39ebee9a6b.

master/buildbot/data/builds.py
@@ -178,7 +178,7 @@ class BuildsEndpoint(Db2DataMixin, base.BuildNestingMixin, base.Endpoint):
             else:
                 # Avoid to request DB for Build's properties if not specified
                 if filters:  # pragma: no cover
-                    props = yield self.master.db.builds.getBuildProperties(b["buildid"])
+                    props = yield self.master.db.builds.getBuildProperties(data["buildid"])
                     filtered_properties = self._generate_filtered_properties(props, filters)
                     if filtered_properties:
                         data["properties"] = filtered_properties