7a2fc80b3ba1c6e873b1913898cc7c66f24b5ba1
Chih-Hsuan Yen <yan12125@gmail.com>
2021-12-18 10:33:21 +0800

    Remove a compatibility trick for builtins
    The trick is for Python 2.x, and buildbot master already requires Python
    >= 3.6 [1].
    
    [1] https://github.com/buildbot/buildbot/blob/v3.5.0/master/setup.py#L467

--- master/buildbot/test/unit/config/test_master.py
+++ master/buildbot/test/unit/config/test_master.py
@@ -13,11 +13,7 @@
 #
 # Copyright Buildbot Team Members
 
-# We cannot use the builtins module here from Python-Future.
-# We need to use the native __builtin__ module on Python 2,
-# and builtins module on Python 3, because we need to override
-# the actual native open method.
-
+import builtins
 import os
 import re
 import textwrap
@@ -50,13 +46,6 @@ from buildbot.test.util.warnings import assertProducesWarning
 from buildbot.util import service
 from buildbot.warnings import ConfigWarning
 from buildbot.warnings import DeprecatedApiWarning
-
-try:
-    # Python 2
-    import __builtin__ as builtins
-except ImportError:
-    # Python 3
-    import builtins
 
 global_defaults = dict(
     title='Buildbot',
