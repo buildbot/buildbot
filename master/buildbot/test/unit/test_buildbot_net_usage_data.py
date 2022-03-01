9fda0439436c098249aa44e6ff7943bdc93e75a1
Ralf Mueller <ralf.mueller@dkrz.de>
2022-03-01 16:48:04 +0100

    support Arch spinoff: antergos

--- master/buildbot/test/unit/test_buildbot_net_usage_data.py
+++ master/buildbot/test/unit/test_buildbot_net_usage_data.py
@@ -151,5 +151,5 @@ class Tests(unittest.TestCase):
         self.assertEqual(len(distro), 2)
         self.assertNotIn("unknown", distro[0])
         # Rolling distributions like Arch Linux (arch) does not have VERSION_ID
-        if distro[0] not in ["arch", "gentoo"]:
+        if distro[0] not in ["arch", "gentoo", "antergos"]:
             self.assertNotIn("unknown", distro[1])
