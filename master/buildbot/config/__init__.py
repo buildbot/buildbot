1206b1c1a17337ea5c6e829122f41ca854dac1a8
Povilas Kanapickas <povilas@radix.lt>
2022-03-06 03:18:16 +0200

    config: Remove unused indirect import of config.get_is_in_unit_tests()

--- master/buildbot/config/__init__.py
+++ master/buildbot/config/__init__.py
@@ -17,4 +17,3 @@
 from .builder import BuilderConfig  # noqa pylint: disable=unused-import
 from .errors import ConfigErrors  # noqa pylint: disable=unused-import
 from .errors import error  # noqa pylint: disable=unused-import
-from .master import get_is_in_unit_tests  # noqa pylint: disable=unused-import
