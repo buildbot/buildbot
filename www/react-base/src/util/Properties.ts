313bd8c5da56de8489660edf7c3a96c6c602bb45
Povilas Kanapickas <povilas@radix.lt>
2022-08-24 00:00:26 +0300

    www/react: Implement several utilities for accessing properties

---
+++ www/react-base/src/util/Properties.ts
@@ -0,0 +1,44 @@
+/*
+  This file is part of Buildbot.  Buildbot is free software: you can
+  redistribute it and/or modify it under the terms of the GNU General Public
+  License as published by the Free Software Foundation, version 2.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51
+  Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+  Copyright Buildbot Team Members
+*/
+
+export function getPropertyValueArrayOrEmpty(props: {[key: string]: any}, key: string) {
+  if (!(key in props)) {
+    return [];
+  }
+  const prop = props[key];
+  // each property is an array of two elements: property value and source
+  if (!Array.isArray(prop) || prop.length !== 2) {
+    return [];
+  }
+  const value = prop[0];
+  if (!Array.isArray(value)) {
+    return [];
+  }
+  return value;
+}
+
+export function getPropertyValueOrDefault(props: {[key: string]: any}, key: string, def: any) {
+  if (!(key in props)) {
+    return def;
+  }
+  const prop = props[key];
+  // each property is an array of two elements: property value and source
+  if (!Array.isArray(prop) || prop.length !== 2) {
+    return def;
+  }
+  return prop[0];
+}
