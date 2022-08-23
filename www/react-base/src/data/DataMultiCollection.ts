815d08e6cfbe59f870ce6906e0367439033a2254
Povilas Kanapickas <povilas@radix.lt>
2022-08-24 00:00:58 +0300

    www/react: Extract collection-independent code out of multi collection

--- www/react-base/src/data/DataMultiCollection.ts
+++ www/react-base/src/data/DataMultiCollection.ts
@@ -16,109 +16,18 @@
 */
 
 import BaseClass from "./classes/BaseClass";
-import {
-  action,
-  autorun,
-  computed,
-  IObservableArray,
-  makeObservable,
-  observable,
-  ObservableMap
-} from "mobx";
-import DataCollection, {IDataCollection} from "./DataCollection";
-import {IReactionDisposer} from "mobx";
+import {IObservableArray, ObservableMap} from "mobx";
+import DataCollection from "./DataCollection";
+import {BasicDataMultiCollection} from "./BasicDataMultiCollection";
 
-/* This class wraps multiple DataCollections of the same thing.
- */
 export default class DataMultiCollection<ParentDataType extends BaseClass,
-    DataType extends BaseClass> implements IDataCollection {
-
-  parentArray: IObservableArray<ParentDataType> | null;
-  parentArrayMap: ObservableMap<string, DataCollection<ParentDataType>> | null;
-  parentFilteredIds: IObservableArray<string>;
-
-  @observable byParentId = observable.map<string, DataCollection<DataType>>();
-  @observable sortedParentIds = observable.array<string>();
-  callback: (child: ParentDataType) => DataCollection<DataType>;
-  private disposer: IReactionDisposer;
+    DataType extends BaseClass> extends BasicDataMultiCollection<ParentDataType, DataCollection<DataType>> {
 
   constructor(parentArray: IObservableArray<ParentDataType> | null,
               parentArrayMap: ObservableMap<string, DataCollection<ParentDataType>> | null,
               parentFilteredIds: IObservableArray<string> | null,
               callback: (child: ParentDataType) => DataCollection<DataType>) {
-    makeObservable(this);
-    this.parentArray = parentArray;
-    this.parentArrayMap = parentArrayMap;
-    this.parentFilteredIds = parentFilteredIds ?? observable([]);
-
-    this.callback = callback;
-    if (parentArray !== null) {
-      this.disposer = autorun(() => {
-        const newParentIds = new Set<string>();
-        for (let parent of this.parentArray!) {
-          if (!this.parentFilteredIds.indexOf(parent.id)) {
-            continue;
-          }
-
-          newParentIds.add(parent.id);
-          if (!this.byParentId.has(parent.id)) {
-            this.addByParentId(parent.id, this.callback(parent));
-          }
-        }
-
-        for (let key of this.byParentId.keys()) {
-          if (!newParentIds.has(key)) {
-            this.removeByParentId(key);
-          }
-        }
-      });
-    } else if (parentArrayMap !== null) {
-      this.disposer = autorun(() => {
-        const newParentIds = new Set<string>();
-        for (const parentList of this.parentArrayMap!.values()) {
-          for (const parent of parentList.array) {
-            if (!this.parentFilteredIds.indexOf(parent.id)) {
-              continue;
-            }
-
-            newParentIds.add(parent.id);
-            if (!this.byParentId.has(parent.id)) {
-              this.addByParentId(parent.id, this.callback(parent));
-            }
-          }
-        }
-        for (let key of this.byParentId.keys()) {
-          if (!newParentIds.has(key)) {
-            this.removeByParentId(key);
-          }
-        }
-      });
-    } else {
-      throw Error("Either parentArray or parentArrayMap must not be null");
-    }
-  }
-
-  subscribe() {
-    return Promise.resolve();
-  }
-
-  initial(data: any[]) {}
-
-  close() : Promise<void> {
-    this.disposer();
-    return Promise.all(Object.values(this.byParentId).map((collection => collection.close()))).then();
-  }
-
-  @action addByParentId(id: string, collection: DataCollection<DataType>) {
-    this.byParentId.set(id, collection);
-    this.sortedParentIds.push(id);
-    this.sortedParentIds.sort();
-  }
-
-  @action removeByParentId(id: string) {
-    this.byParentId.get(id)?.close();
-    this.byParentId.delete(id);
-    this.sortedParentIds.remove(id);
+    super(parentArray, parentArrayMap, parentFilteredIds, callback);
   }
 
   getRelated<ChildDataType extends BaseClass>(
