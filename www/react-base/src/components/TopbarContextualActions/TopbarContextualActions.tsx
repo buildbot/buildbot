d982523581848cf776846f1dd8315a6fcf7d90e7
Povilas Kanapickas <povilas@radix.lt>
2022-08-24 00:00:05 +0300

    www/react: Rewrite TopbarContextualActions component to React

---
+++ www/react-base/src/components/TopbarContextualActions/TopbarContextualActions.tsx
@@ -0,0 +1,47 @@
+/*
+  This file is part of Buildbot.  Buildbot is free software: you can
+  redistribute it and/or modify it under the terms of the GNU General Public
+  License as published by the Free Software Foundation, version 2.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51
+  Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+  Copyright Buildbot Team Members
+*/
+
+export type TopbarAction = {
+  caption: string;
+  icon?: string;
+  help?: string;
+  extraClass?: string;
+  action: () => void;
+}
+
+const TopbarContextualActions = (actions: TopbarAction[]) => {
+  const elements = actions.map(action => {
+    return (
+      <div className="form-group">
+        <button className={"btn btn-default " + (action.extraClass ?? "")} type="button"
+                onClick={action.action} title={action.help ?? ""}>
+          {action.icon ?  <><i className={"fa fa-" + action.icon}></i><span>&nbsp;</span></> : <></> }
+          {action.caption}
+        </button>
+        &nbsp;
+      </div>
+    );
+  });
+
+  return (
+    <form className="navbar-form navbar-left">
+      {elements}
+    </form>
+  );
+}
+
+export default TopbarContextualActions;
