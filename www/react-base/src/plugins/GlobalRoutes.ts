c94cdc0eeaf129a50832d9bc16458a5411519e96
Povilas Kanapickas <povilas@radix.lt>
2022-08-24 00:00:15 +0300

    www/react: Implement plugin system to register routes

---
+++ www/react-base/src/plugins/GlobalRoutes.ts
@@ -0,0 +1,39 @@
+/*
+  This file is part of Buildbot.  Buildbot is free software: you can
+  redistribute it and/or modify it under the terms of the GNU General Public
+  License as published by the Free Software Foundation, version 2.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51
+  Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+  Copyright Buildbot Team Members
+*/
+
+import {TopbarItem} from "../components/Topbar/Topbar";
+import {action, makeObservable, observable} from "mobx";
+
+export type RouteConfig = {
+  route: string;
+  group: string | null;
+  element: () => JSX.Element;
+}
+
+export class GlobalRoutes {
+  @observable routes : RouteConfig[] = [];
+
+  constructor() {
+    makeObservable(this);
+  }
+
+  @action addRoute(config: RouteConfig) {
+    this.routes.push(config);
+  }
+}
+
+export const globalRoutes = new GlobalRoutes();
