8fbbf81640b198c9e1d751a80d884035cf941f37
Povilas Kanapickas <povilas@radix.lt>
2022-08-24 00:00:18 +0300

    www/react: Copy guanlecoja-ui css files to React project

--- www/react-base/src/App.tsx
+++ www/react-base/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './globals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.less';
 import 'bootstrap';
 
 function App() {
