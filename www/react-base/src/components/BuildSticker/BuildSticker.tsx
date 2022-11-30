41c81716165a951c33fdf0c1139d51a19fd3b3dd
Povilas Kanapickas <povilas@radix.lt>
2022-11-30 23:49:21 +0200

    www/react: Convert BuildSticker component to react-bootstrap

--- www/react-base/src/components/BuildSticker/BuildSticker.tsx
+++ www/react-base/src/components/BuildSticker/BuildSticker.tsx
@@ -21,6 +21,7 @@ import {Builder} from "../../data/classes/Builder";
 import {results2class, results2text} from "../../util/Results";
 import {Link} from "react-router-dom";
 import {durationFormat, useCurrentTime} from "../../util/Moment";
+import { Card } from 'react-bootstrap';
 
 type BuildStickerProps = {
   build: Build;
@@ -31,24 +32,24 @@ const BuildSticker = ({build, builder}: BuildStickerProps) => {
   const now = useCurrentTime();
 
   return (
-    <div className={"panel panel-default buildsticker " + results2class(build, null)}>
-      <div className="panel-body no-select">
-        <div className="row">
-          <span className={"pull-right label " + results2class(build, null)}>
+    <Card className={"bb-buildsticker " + results2class(build, null)}>
+      <Card.Body>
+        <div className="bb-buildsticker-left">
+          <span className={"pull-right label bb-results " + results2class(build, null)}>
             {results2text(build)}
           </span>
           <Link to={`builders/${builder.builderid}/builds/${build.number}`}>
             {builder.name}/{build.number}
           </Link>
         </div>
-        <div className="row">
+        <div className="bb-buildsticker-left">
           <span className="pull-right">
             {durationFormat((build.complete ? build.complete_at! : now) - build.started_at)}
           </span>
           <span>{build.state_string}</span>
         </div>
-      </div>
-    </div>
+      </Card.Body>
+    </Card>
   );
 }
 
