41c5cbf80daf2fa16e223eac2bd74fb59507dd6b
Povilas Kanapickas <povilas@radix.lt>
2022-12-04 16:01:10 +0200

    www/react: Fix column-based field layout in force build modal

--- www/react-base/src/components/ForceBuildModal/Fields/FieldNested.tsx
+++ www/react-base/src/components/ForceBuildModal/Fields/FieldNested.tsx
@@ -73,7 +73,7 @@ const FieldNested = observer(({field, fieldsState}: FieldNestedProps) => {
       <Card>
         { field.label !== null && field.label !== '' ? <Card.Header>{field.label}</Card.Header> : <></> }
         <Card.Body>
-          <div className="form-horizontal">
+          <div className="row">
             {
               field.fields.filter(f => !shouldHideField(f)).map(f => (
                 <div className={columnClass}>
@@ -89,7 +89,7 @@ const FieldNested = observer(({field, fieldsState}: FieldNestedProps) => {
 
   // layout === simple
   return (
-    <div className="form-horizontal">
+    <div className="row">
       {
         field.fields.filter(f => !shouldHideField(f)).map(f => (
           <div className={columnClass}>
