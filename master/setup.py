7bebaa84e509ba137fe132acf011ff544b7bc2c3
Povilas Kanapickas <povilas@radix.lt>
2023-03-06 18:24:23 +0200

    Mark master and worker as compatible with Python 3.11

--- master/setup.py
+++ master/setup.py
@@ -155,6 +155,8 @@ setup_args = {
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 
     'packages': [
--- worker/setup.py
+++ worker/setup.py
@@ -103,6 +103,9 @@ setup_args = {
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 
     'packages': [
