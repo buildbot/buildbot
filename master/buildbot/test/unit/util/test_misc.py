7220a2ab2d38ca4513bb6c252693b7396582e725
Povilas Kanapickas <povilas@radix.lt>
2023-07-02 19:57:37 +0300

    util: Add function to chunkify list

--- master/buildbot/test/unit/util/test_misc.py
+++ master/buildbot/test/unit/util/test_misc.py
@@ -125,3 +125,44 @@ class TestCancelAfter(TestReactorMixin, unittest.TestCase):
         self.d.errback(RuntimeError("oh noes"))  # ignored
         self.assertTrue(d.called)
         yield self.assertFailure(d, defer.CancelledError)
+
+
+class TestChunkifyList(unittest.TestCase):
+    def test_all(self):
+        self.assertEqual(list(misc.chunkify_list([], 0)), [])
+        self.assertEqual(list(misc.chunkify_list([], 1)), [])
+        self.assertEqual(list(misc.chunkify_list([1], 0)), [[1]])
+        self.assertEqual(list(misc.chunkify_list([1], 1)), [[1]])
+        self.assertEqual(list(misc.chunkify_list([1], 2)), [[1]])
+        self.assertEqual(list(misc.chunkify_list([1, 2], 0)), [[1], [2]])
+        self.assertEqual(list(misc.chunkify_list([1, 2], 1)), [[1], [2]])
+        self.assertEqual(list(misc.chunkify_list([1, 2], 2)), [[1, 2]])
+        self.assertEqual(list(misc.chunkify_list([1, 2], 3)), [[1, 2]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3], 0)), [[1], [2], [3]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3], 1)), [[1], [2], [3]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3], 2)), [[1, 2], [3]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3], 3)), [[1, 2, 3]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3], 4)), [[1, 2, 3]])
+
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)),
+                         [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)),
+                         [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)),
+                         [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)),
+                         [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4)),
+                         [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)),
+                         [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 6)),
+                         [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7)),
+                         [[1, 2, 3, 4, 5, 6, 7], [8, 9, 10]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8)),
+                         [[1, 2, 3, 4, 5, 6, 7, 8], [9, 10]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9)),
+                         [[1, 2, 3, 4, 5, 6, 7, 8, 9], [10]])
+        self.assertEqual(list(misc.chunkify_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)),
+                         [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
