7567f8fc5ac71d6a41d588a3fb4dfa27fceb1bd0
Monika Kairaityte <monika@kibit.lt>
2023-07-18 13:10:56 +0300

    schedulers: Simplify changes evaluation code

--- master/buildbot/db/schedulers.py
+++ master/buildbot/db/schedulers.py
@@ -110,8 +110,7 @@ class SchedulersConnectorComponent(base.DBConnectorComponent):
             q = sa.select(
                 [sch_ch_tbl.c.changeid, sch_ch_tbl.c.important],
                 whereclause=wc)
-            return {r.changeid: [False, True][r.important]
-                    for r in conn.execute(q)}
+            return {r.changeid: bool(r.important) for r in conn.execute(q)}
         return self.db.pool.do(thd)
 
     def findSchedulerId(self, name):
