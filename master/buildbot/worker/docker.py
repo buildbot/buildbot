f4a3eddeacf5a787ca525ef381a01e8c5973cc9f
Aki Tuomi <cmouse@cmouse.fi>
2023-11-02 12:27:15 +0200

    DockerLatentWorker: Explicitly disconnect when stopping instance
    Seems to fix #4678

--- master/buildbot/worker/docker.py
+++ master/buildbot/worker/docker.py
@@ -379,3 +379,6 @@ class DockerLatentWorker(CompatibleLatentWorkerMixin,
             except docker.errors.APIError as e:
                 log.msg('Error while removing the image: %s', e)
         docker_client.close()
+        conn = self.conn
+        self.conn = None
+        self._disconnect(conn)
