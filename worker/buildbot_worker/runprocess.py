a54c5a4f223735a84d8e42d732e9bda687f428c9
YSLu <hydrogencl@gmail.com>
2023-11-21 17:18:50 +0100

    another tryout for the pylint wording

--- worker/buildbot_worker/runprocess.py
+++ worker/buildbot_worker/runprocess.py
@@ -305,7 +305,7 @@ class RunProcess(object):
             """
             The arrPattern is only for small request now. The list of pattern
             is open for future.
-            The command-lines find the sensitive information from the commandlines
+            The function finds the sensitive information from the command-lines
             and prevent it revealing in the stdout.
             """
             arrPattern = [r"(?<=https\:\/\/)(.*?)(?=\@)"]
