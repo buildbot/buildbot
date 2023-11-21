115d5037068c99fab6e2708bd203b65c93e61858
YSLu <hydrogencl@gmail.com>
2023-11-21 15:38:01 +0100

    fix code style

--- worker/buildbot_worker/runprocess.py
+++ worker/buildbot_worker/runprocess.py
@@ -300,8 +300,7 @@ class RunProcess(object):
         """
         self.command_id = command
         command_tmp = command
-        def mosaic_sensitive_info(cmd, arrPattern = [r"(?<=https\:\/\/)(.*?)(?=\@)"],
-                               strMosaic="######"):
+        def mosaic_sensitive_info(cmd, arrPattern = [r"(?<=https\:\/\/)(.*?)(?=\@)"], strMosaic="######"):
             strTmp = ""
             for pattern in arrPattern:
                 try:
