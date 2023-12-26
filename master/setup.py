2dd116acb7b17cd5fa4587d2aea99bb5d9c3adfa
Pavol Misik <pmisik@gmail.com>
2023-12-26 19:12:00 +0100

    Declare Python3.12 compatibility in generated packages of master and worker.

--- master/setup.py
+++ master/setup.py
@@ -154,6 +154,7 @@ setup_args = {
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 
     'packages': [
