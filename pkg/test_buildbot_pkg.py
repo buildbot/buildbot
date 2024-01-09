301f04cec775a3d591c83c9f1a590dc8588061f7
Pavol Misik <pmisik@gmail.com>
2024-01-09 22:28:39 +0100

    pkg: test react packages and remove tests with direct invocation os setup.py

--- pkg/test_buildbot_pkg.py
+++ pkg/test_buildbot_pkg.py
@@ -24,20 +24,31 @@ from twisted.trial import unittest
 
 
 class BuildbotWWWPkg(unittest.TestCase):
-    pkgName = "buildbot_www"
-    pkgPaths = ["www", "base"]
-    epName = "base"
+    pkgName = "buildbot_www_react"
+    pkgPaths = ["www", "react-base"]
+    epName = "base_react"
 
     loadTestScript = dedent(
         """
         from importlib.metadata import entry_points
         import re
         apps = {}
-        for ep in entry_points().get('buildbot.www', []):
+
+        eps = entry_points()
+        entry_point = "buildbot.www"
+        if hasattr(eps, "select"):
+            entry_point_group = eps.select(group=entry_point)
+        else:
+            entry_point_group = eps.get(entry_point, [])
+
+        for ep in entry_point_group:
             apps[ep.name] = ep.load()
 
         print(apps["%(epName)s"])
-        assert("scripts.js" in apps["%(epName)s"].resource.listNames())
+        expected_file = "scripts.js"
+        if "%(epName)s" == "base_react":
+            expected_file = "index.html"
+        assert(expected_file in apps["%(epName)s"].resource.listNames())
         assert(re.match(r'\d+\.\d+\.\d+', apps["%(epName)s"].version) is not None)
         assert(apps["%(epName)s"].description is not None)
         """
@@ -57,25 +68,17 @@ class BuildbotWWWPkg(unittest.TestCase):
         self.rmtree(os.path.join(self.path, "dist"))
         self.rmtree(os.path.join(self.path, "static"))
 
-    def run_setup(self, cmd):
-        check_call([sys.executable, 'setup.py', cmd], cwd=self.path)
+    def run_build(self, kind):
+        check_call([sys.executable, "-m", "build", "--no-isolation", f"--{kind}"], cwd=self.path)
 
     def check_correct_installation(self):
         # assert we can import buildbot_www
-        # and that it has an endpoint with resource containing file "script.js"
+        # and that it has an endpoint with resource containing file "script.js" or "index.html" (in case of buildbot_www_react)
         check_call([sys.executable, '-c', self.loadTestScript % dict(epName=self.epName)])
 
-    def test_install(self):
-        self.run_setup("install")
-        self.check_correct_installation()
-
     def test_wheel(self):
-        self.run_setup("bdist_wheel")
+        self.run_build("wheel")
         check_call("pip install dist/*.whl", shell=True, cwd=self.path)
-        self.check_correct_installation()
-
-    def test_develop(self):
-        self.run_setup("develop")
         self.check_correct_installation()
 
     def test_develop_via_pip(self):
@@ -83,24 +86,18 @@ class BuildbotWWWPkg(unittest.TestCase):
         self.check_correct_installation()
 
     def test_sdist(self):
-        self.run_setup("sdist")
+        self.run_build("sdist")
         check_call("pip install dist/*.tar.gz", shell=True, cwd=self.path)
         self.check_correct_installation()
 
 
 class BuildbotConsolePkg(BuildbotWWWPkg):
-    pkgName = "buildbot-console-view"
-    pkgPaths = ["www", "console_view"]
-    epName = "console_view"
+    pkgName = "buildbot-react-console-view"
+    pkgPaths = ["www", "react-console_view"]
+    epName = "react_console_view"
 
 
 class BuildbotWaterfallPkg(BuildbotWWWPkg):
-    pkgName = "buildbot-waterfall-view"
-    pkgPaths = ["www", "waterfall_view"]
-    epName = "waterfall_view"
-
-
-class BuildbotCodeparameterPkg(BuildbotWWWPkg):
-    pkgName = "buildbot-codeparameter"
-    pkgPaths = ["www", "codeparameter"]
-    epName = "codeparameter"
+    pkgName = "buildbot-react-waterfall-view"
+    pkgPaths = ["www", "react-waterfall_view"]
+    epName = "react_waterfall_view"
