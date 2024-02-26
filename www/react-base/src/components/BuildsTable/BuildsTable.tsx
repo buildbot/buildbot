ad1e14e23cd512b061aec0e5395c42714a9e0773
Yngve N. Pettersen <yngve@vivaldi.com>
2024-02-26 21:25:41 +0100

    Add revision info column in BuildsTable.
    Fetch branch and revision properties for views using it.
    
    (Issue #4516)

--- www/react-base/src/components/BuildsTable/BuildsTable.tsx
+++ www/react-base/src/components/BuildsTable/BuildsTable.tsx
@@ -83,6 +83,9 @@ export const BuildsTable = observer(({builds, builders}: BuildsTableProps) => {
         <td>
           {buildCompleteInfoElement}
         </td>
+        <td>
+          {getPropertyValueOrDefault(build.properties, "revision", "(unknown)")}
+        </td>
         <td>{getPropertyValueArrayOrEmpty(build.properties, 'owners').map((owner, index) => (
           <span key={index}>{owner}</span>
         ))}
@@ -110,6 +113,7 @@ export const BuildsTable = observer(({builds, builders}: BuildsTableProps) => {
             <td width="100px">#</td>
             <td width="150px">Completed At</td>
             <td width="150px">Duration</td>
+            <td width='150px'>Revision</td>
             <td width="200px">Owners</td>
             <td width="150px">Worker</td>
             <td>Status</td>
