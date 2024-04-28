2e888da65b46c150c43d5497f713eac43518df3c
Monika Kairaityte <monika@kibit.lt>
2024-04-28 23:44:37 +0300

    smokes: Increase tests timeout to improve reliability

--- smokes-react/playwright.config.ts
+++ smokes-react/playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  timeout: 90000,
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
