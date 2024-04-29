buildbot/buildbot

commit 02bbfc44812ea01abddb8b66602a03b943f63a94

process: Add checks for types of args passed to ShellMixin

master/buildbot/config/checks.py
@@ -41,6 +41,10 @@ def check_param_type(value, default_value, class_inst, name, types, types_msg):
     return default_value
 
 
+def check_param_bool(value, class_inst, name):
+    return check_param_type(value, False, class_inst, name, (bool,), "bool")
+
+
 def check_param_str(value, class_inst, name):
     return check_param_type(value, "(unknown)", class_inst, name, (str,), "str")
 
@@ -55,6 +59,10 @@ def check_param_int(value, class_inst, name):
 
 def check_param_int_none(value, class_inst, name):
     return check_param_type(value, None, class_inst, name, (int, type(None)), "int or None")
+
+
+def check_param_number_none(value, class_inst, name):
+    return check_param_type(value, 0, class_inst, name, (int, float, None), "int or float or None")
 
 
 def check_markdown_support(class_inst):

master/buildbot/process/buildstep.py
@@ -34,7 +34,11 @@ from zope.interface import implementer
 from buildbot import config
 from buildbot import interfaces
 from buildbot import util
+from buildbot.config.checks import check_param_bool
 from buildbot.config.checks import check_param_length
+from buildbot.config.checks import check_param_number_none
+from buildbot.config.checks import check_param_str
+from buildbot.config.checks import check_param_str_none
 from buildbot.db.model import Model
 from buildbot.interfaces import IRenderable
 from buildbot.interfaces import WorkerSetupError
@@ -955,24 +959,24 @@ class ShellMixin:
     initialStdin = None
     decodeRC = {0: SUCCESS}
 
-    _shellMixinArgs = [
-        'command',
-        'workdir',
-        'env',
-        'want_stdout',
-        'want_stderr',
-        'usePTY',
-        'logfiles',
-        'lazylogfiles',
-        'timeout',
-        'maxTime',
-        'logEnviron',
-        'interruptSignal',
-        'sigtermTime',
-        'initialStdin',
-        'decodeRC',
+    _shell_mixin_arg_config = [
+        ('command', None),
+        ('workdir', check_param_str),
+        ('env', None),
+        ('want_stdout', check_param_bool),
+        ('want_stderr', check_param_bool),
+        ('usePTY', check_param_bool),
+        ('logfiles', None),
+        ('lazylogfiles', check_param_bool),
+        ('timeout', check_param_number_none),
+        ('maxTime', check_param_number_none),
+        ('logEnviron', check_param_bool),
+        ('interruptSignal', check_param_str_none),
+        ('sigtermTime', check_param_number_none),
+        ('initialStdin', check_param_str_none),
+        ('decodeRC', None),
     ]
-    renderables = _shellMixinArgs
+    renderables = [arg for arg, _ in _shell_mixin_arg_config]
 
     def setupShellMixin(self, constructorArgs, prohibitArgs=None):
         constructorArgs = constructorArgs.copy()
@@ -983,12 +987,16 @@ class ShellMixin:
         def bad(arg):
             config.error(f"invalid {self.__class__.__name__} argument {arg}")
 
-        for arg in self._shellMixinArgs:
+        for arg, check in self._shell_mixin_arg_config:
             if arg not in constructorArgs:
                 continue
             if arg in prohibitArgs:
                 bad(arg)
             else:
+                value = constructorArgs[arg]
+                if check is not None and not IRenderable.providedBy(value):
+                    check(value, self.__class__, arg)
+
                 setattr(self, arg, constructorArgs[arg])
             del constructorArgs[arg]
         for arg in list(constructorArgs):
@@ -1001,7 +1009,7 @@ class ShellMixin:
     def makeRemoteShellCommand(
         self, collectStdout=False, collectStderr=False, stdioLogName='stdio', **overrides
     ):
-        kwargs = {arg: getattr(self, arg) for arg in self._shellMixinArgs}
+        kwargs = {arg: getattr(self, arg) for arg, _ in self._shell_mixin_arg_config}
         kwargs.update(overrides)
         stdio = None
         if stdioLogName is not None:

master/buildbot/test/unit/process/test_buildstep.py
@@ -1244,6 +1244,13 @@ class TestShellMixin(
         self.expect_outcome(result=SUCCESS)
         yield self.run_step()
 
+    def test_bad_arg_type(self):
+        mixin = SimpleShellCommand()
+        with self.assertRaisesConfigError(
+            "SimpleShellCommand argument usePTY must be an instance of bool"
+        ):
+            mixin.setupShellMixin({'usePTY': 13})
+
     @defer.inlineCallbacks
     def test_no_default_workdir(self):
         self.setup_step(SimpleShellCommand(command=['cmd', 'arg']), want_default_work_dir=False)

commit e9ececc5ff20d6f6cbf3db06a0bb08678e62189e

config: Fix check_param_number_none() check
Fixes: 02bbfc44812ea01abddb8b66602a03b943f63a94.

master/buildbot/config/checks.py
@@ -62,7 +62,9 @@ def check_param_int_none(value, class_inst, name):
 
 
 def check_param_number_none(value, class_inst, name):
-    return check_param_type(value, 0, class_inst, name, (int, float, None), "int or float or None")
+    return check_param_type(
+        value, 0, class_inst, name, (int, float, type(None)), "int or float or None"
+    )
 
 
 def check_markdown_support(class_inst):