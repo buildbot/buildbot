466014de92877c7bc4822a47ada5b4896ca2c6e8
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2024-05-01 16:35:31 +0200

    www: make BuilderView work with either buildernumber or buildername

--- www/react-base/src/views/BuilderView/BuilderView.tsx
+++ www/react-base/src/views/BuilderView/BuilderView.tsx
@@ -39,6 +39,7 @@ import {FaStop, FaSpinner} from "react-icons/fa";
 import {buildTopbarItemsForBuilder} from "../../util/TopbarUtils";
 import { Tab, Tabs } from "react-bootstrap";
 import { LoadingSpan } from "../../components/LoadingSpan/LoadingSpan";
+import { UrlNotFoundView } from "../UrlNotFoundView/UrlNotFoundView";
 
 const anyCancellableBuilds = (builds: DataCollection<Build>,
                               buildrequests: DataCollection<Buildrequest>) => {
@@ -93,14 +94,14 @@ const buildTopbarActions = (builds: DataCollection<Build>,
 }
 
 export const BuilderView = observer(() => {
-  const builderid = Number.parseInt(useParams<"builderid">().builderid ?? "");
+  const builderid = useParams<"builderid">().builderid
   const navigate = useNavigate();
 
   const accessor = useDataAccessor([builderid]);
 
   const numBuilds = 200;
 
-  const buildersQuery = useDataApiQuery(() => Builder.getAll(accessor, {id: builderid.toString()}));
+  const buildersQuery = useDataApiQuery(() => Builder.getAll(accessor, {id: builderid}));
   const buildsQuery = useDataApiQuery(() =>
     buildersQuery.getRelated(builder => Build.getAll(accessor, {query: {
         builderid: builder.builderid,
@@ -131,11 +132,11 @@ export const BuilderView = observer(() => {
   })));
 
   const builder = buildersQuery.getNthOrNull(0);
-  const builds = buildsQuery.getParentCollectionOrEmpty(builderid.toString());
-  const buildrequests = buildrequestsQuery.getParentCollectionOrEmpty(builderid.toString());
-  const forceschedulers = forceSchedulersQuery.getParentCollectionOrEmpty(builderid.toString());
+  const builds = buildsQuery.getParentCollectionOrEmpty(builder?.id ?? "");
+  const buildrequests = buildrequestsQuery.getParentCollectionOrEmpty(builder?.id ?? "");
+  const forceschedulers = forceSchedulersQuery.getParentCollectionOrEmpty(builder?.id ?? "");
   const project = projectsQuery.getNthOrNull(0);
-  const workers = workersQuery.getParentCollectionOrEmpty(builderid.toString());
+  const workers = workersQuery.getParentCollectionOrEmpty(builder?.id ?? "");
 
   const [isCancelling, setIsCancelling] = useState(false);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
@@ -211,6 +212,10 @@ export const BuilderView = observer(() => {
     </ul>);
   };
 
+  if (builder === null) {
+    return <UrlNotFoundView />
+  }
+
   return (
     <div className="container">
       <AlertNotification text={errorMsg}/>
@@ -231,7 +236,7 @@ export const BuilderView = observer(() => {
 
       <BuildsTable builds={builds} builders={null}/>
       {shownForceScheduler !== null
-        ? <ForceBuildModal scheduler={shownForceScheduler} builderid={builderid}
+        ? <ForceBuildModal scheduler={shownForceScheduler} builderid={builder.builderid}
                            onClose={onForceBuildModalClose}/>
         : <></>
       }
