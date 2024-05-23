5a7339f24e83de6317002ed8cc755055ed4e31b6
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2024-05-23 14:29:53 +0200

    test: fix fakedb to use master reactor
    This would prevent to run test with a realReactor and DB

--- master/buildbot/test/fakedb/connector.py
+++ master/buildbot/test/fakedb/connector.py
@@ -48,50 +48,52 @@ class FakeDBConnector(AbstractDBConnector):
     see their documentation for more.
     """
 
-    def __init__(self, testcase):
+    def __init__(self, testcase, reactor=None):
+        if reactor is None:
+            reactor = testcase.reactor
         super().__init__()
         # reset the id generator, for stable id's
         Row._next_id = 1000
         self.t = testcase
         self.checkForeignKeys = False
         self._components = []
-        self.changes = comp = FakeChangesComponent(self, testcase)
+        self.changes = comp = FakeChangesComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.changesources = comp = FakeChangeSourcesComponent(self, testcase)
+        self.changesources = comp = FakeChangeSourcesComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.schedulers = comp = FakeSchedulersComponent(self, testcase)
+        self.schedulers = comp = FakeSchedulersComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.sourcestamps = comp = FakeSourceStampsComponent(self, testcase)
+        self.sourcestamps = comp = FakeSourceStampsComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.buildsets = comp = FakeBuildsetsComponent(self, testcase)
+        self.buildsets = comp = FakeBuildsetsComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.workers = comp = FakeWorkersComponent(self, testcase)
+        self.workers = comp = FakeWorkersComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.state = comp = FakeStateComponent(self, testcase)
+        self.state = comp = FakeStateComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.buildrequests = comp = FakeBuildRequestsComponent(self, testcase)
+        self.buildrequests = comp = FakeBuildRequestsComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.builds = comp = FakeBuildsComponent(self, testcase)
+        self.builds = comp = FakeBuildsComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.build_data = comp = FakeBuildDataComponent(self, testcase)
+        self.build_data = comp = FakeBuildDataComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.steps = comp = FakeStepsComponent(self, testcase)
+        self.steps = comp = FakeStepsComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.logs = comp = FakeLogsComponent(self, testcase)
+        self.logs = comp = FakeLogsComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.users = comp = FakeUsersComponent(self, testcase)
+        self.users = comp = FakeUsersComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.masters = comp = FakeMastersComponent(self, testcase)
+        self.masters = comp = FakeMastersComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.projects = comp = FakeProjectsComponent(self, testcase)
+        self.projects = comp = FakeProjectsComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.builders = comp = FakeBuildersComponent(self, testcase)
+        self.builders = comp = FakeBuildersComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.tags = comp = FakeTagsComponent(self, testcase)
+        self.tags = comp = FakeTagsComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.test_results = comp = FakeTestResultsComponent(self, testcase)
+        self.test_results = comp = FakeTestResultsComponent(self, testcase, reactor)
         self._components.append(comp)
-        self.test_result_sets = comp = FakeTestResultSetsComponent(self, testcase)
+        self.test_result_sets = comp = FakeTestResultSetsComponent(self, testcase, reactor)
         self._components.append(comp)
 
     @defer.inlineCallbacks
