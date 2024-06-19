6a7bc331d4b21c9c08d9c18330388d406800a2f5
Povilas Kanapickas <povilas@radix.lt>
2024-06-20 02:25:48 +0300

    test: Increase timeout in all integration tests

--- master/buildbot/test/util/integration.py
+++ master/buildbot/test/util/integration.py
@@ -181,7 +181,7 @@ class RunMasterBase(unittest.TestCase):
 
     # All tests that start master need higher timeout due to test runtime variability on
     # oversubscribed hosts.
-    timeout = 20
+    timeout = 40
 
     if Worker is None:
         skip = "buildbot-worker package is not installed"
