b0019884a32d0073e926306daaedc85154679c8a
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2024-07-17 10:56:01 +0200

    GitPoller: Fix _tracker_ref when repourl port is specified
    Fixes #7822
    
    `:` is used in the command line to separate {remote_ref}:{local_ref}.
    Replace it with it's url encoded value of `%3A`

--- master/buildbot/test/unit/changes/test_gitpoller.py
+++ master/buildbot/test/unit/changes/test_gitpoller.py
@@ -2521,7 +2521,7 @@ class TestGitPollerUtils(unittest.TestCase):
             gitpoller.GitPoller._tracker_ref(
                 "https://example.org:1234/owner/repo.git", "refs/heads/branch_name"
             ),
-            "refs/buildbot/https/example.org:1234/owner/repo/heads/branch_name",
+            "refs/buildbot/https/example.org%3A1234/owner/repo/heads/branch_name",
         )
 
     def test_tracker_ref_tag(self):
@@ -2529,7 +2529,7 @@ class TestGitPollerUtils(unittest.TestCase):
             gitpoller.GitPoller._tracker_ref(
                 "https://example.org:1234/owner/repo.git", "refs/tags/v1"
             ),
-            "refs/buildbot/https/example.org:1234/owner/repo/tags/v1",
+            "refs/buildbot/https/example.org%3A1234/owner/repo/tags/v1",
         )
 
     def test_tracker_ref_with_credentials(self):
@@ -2537,7 +2537,7 @@ class TestGitPollerUtils(unittest.TestCase):
             gitpoller.GitPoller._tracker_ref(
                 "https://user:password@example.org:1234/owner/repo.git", "refs/heads/branch_name"
             ),
-            "refs/buildbot/https/example.org:1234/owner/repo/heads/branch_name",
+            "refs/buildbot/https/example.org%3A1234/owner/repo/heads/branch_name",
         )
 
     def test_tracker_ref_sub_branch(self):
@@ -2545,7 +2545,7 @@ class TestGitPollerUtils(unittest.TestCase):
             gitpoller.GitPoller._tracker_ref(
                 "https://user:password@example.org:1234/owner/repo.git", "refs/heads/branch_name"
             ),
-            "refs/buildbot/https/example.org:1234/owner/repo/heads/branch_name",
+            "refs/buildbot/https/example.org%3A1234/owner/repo/heads/branch_name",
         )
 
     def test_tracker_ref_not_ref_collision(self):
