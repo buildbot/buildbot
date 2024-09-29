buildbot/buildbot

commit d788b5aaaa7b6c883f411b844c363b45d2250706

www: Show human readable time in the debug tabs

www/base/src/components/RawData/RawData.tsx
@@ -16,6 +16,7 @@
 */
 
 import './RawData.scss';
+import moment from "moment";
 import {useState} from "react";
 import _ from "underscore";
 import {isObservableArray, isObservableObject} from "mobx";
@@ -32,11 +33,35 @@ const isArrayOfObjectsRaw = (v: any) => {
   return isArrayRaw(v) && v.length > 0 && isObjectRaw(v[0]);
 }
 
-type RawDataProps = {
-  data: {[key: string]: any};
+export const displayBuildRequestEntry = (key: string, value: any) : string => {
+  if (key === "claimed_at" || key === "complete_at" || key === "submitted_at") {
+    try {
+      return `${value} (${moment(Number.parseInt(value.toString()) * 1000).format()})`;
+    } catch (error) {
+      return value.toString();
+    }
+  }
+  return value.toString();
 }
 
-export const RawData = ({data}: RawDataProps) => {
+export const displayBuildsetEntry = (key: string, value: any) : string => {
+  if (key === "complete_at" || key === "created_at") {
+    // created_at is for dictionaries in sourcestamps list
+    try {
+      return `${value} (${moment(Number.parseInt(value.toString()) * 1000).format()})`;
+    } catch (error) {
+      return value.toString();
+    }
+  }
+  return value.toString();
+}
+
+type RawDataProps = {
+  data: {[key: string]: any};
+  displayCallback?: (key: string, value: any) => string;
+}
+
+export const RawData = ({data, displayCallback}: RawDataProps) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const renderArrayElements = (value: any[]) => {
@@ -51,7 +76,7 @@ export const RawData = ({data}: RawDataProps) => {
     );
   }
 
-  const renderDataElement = (value: any) => {
+  const renderDataElement = (key: string, value: any) => {
     if (value === null) {
       return <dd>{"null"}&nbsp;</dd>;
     }
@@ -70,12 +95,14 @@ export const RawData = ({data}: RawDataProps) => {
       return (
         <dd>
           <ArrowExpander isExpanded={isExpanded} setIsExpanded={setIsExpanded}/>
-          {isExpanded ? <div><RawData data={value}/></div> : <></>}
+          {isExpanded ? <div><RawData data={value} displayCallback={displayCallback}/></div> : <></>}
         </dd>
       )
     }
 
-    return <dd>{value.toString()}&nbsp;</dd>;
+    const displayValue = displayCallback === undefined ? value.toString() : displayCallback(key, value);
+
+    return <dd>{displayValue}&nbsp;</dd>;
   }
 
   const renderElements = () => {
@@ -89,7 +116,7 @@ export const RawData = ({data}: RawDataProps) => {
       return (
         <div className={'bb-raw-data-key-value'} key={key}>
           <dt>{key}</dt>
-          {renderDataElement(value)}
+          {renderDataElement(key, value)}
         </div>
       );
     });

www/base/src/views/BuildRequestView/BuildRequestView.tsx
@@ -33,7 +33,11 @@ import {
 import {useNavigate, useParams, useSearchParams} from "react-router-dom";
 import {Tab, Tabs} from "react-bootstrap";
 import {TopbarAction, TopbarItem, useTopbarActions, useTopbarItems} from "buildbot-ui";
-import {RawData} from "../../components/RawData/RawData";
+import {
+  displayBuildRequestEntry,
+  displayBuildsetEntry,
+  RawData
+} from "../../components/RawData/RawData";
 import {AlertNotification} from "../../components/AlertNotification/AlertNotification";
 import {BuildSummary} from "../../components/BuildSummary/BuildSummary";
 import {PropertiesTable} from "../../components/PropertiesTable/PropertiesTable";
@@ -163,9 +167,9 @@ export const BuildRequestView = observer(() => {
         </Tab>
         <Tab eventKey="debug" title="Debug">
           <TableHeading>Buildrequest</TableHeading>
-          <RawData data={buildRequest !== null ? buildRequest.toObject() : {}}/>
+          <RawData data={buildRequest !== null ? buildRequest.toObject() : {}} displayCallback={displayBuildRequestEntry}/>
           <TableHeading>Buildset</TableHeading>
-          <RawData data={buildset !== null ? buildset.toObject() : {}}/>
+          <RawData data={buildset !== null ? buildset.toObject() : {}} displayCallback={displayBuildsetEntry}/>
           <TableHeading>Builder</TableHeading>
           <RawData data={builder !== null ? builder.toObject() : {}}/>
           {buildRawData}

www/base/src/views/BuildView/BuildViewDebugTab.tsx
@@ -26,7 +26,11 @@ import {
 } from "buildbot-data-js";
 import {BuildLinkWithSummaryTooltip} from "buildbot-ui";
 import {LoadingDiv} from "../../components/LoadingDiv/LoadingDiv";
-import {RawData} from "../../components/RawData/RawData";
+import {
+  RawData,
+  displayBuildRequestEntry,
+  displayBuildsetEntry
+} from "../../components/RawData/RawData";
 import {TableHeading} from "../../components/TableHeading/TableHeading";
 
 export type BuildViewDebugTabProps = {
@@ -76,9 +80,9 @@ export const BuildViewDebugTab = observer(({build, buildset, buildrequest}:
       <TableHeading>
         <Link to={`/buildrequests/${buildrequest.id}`}>Buildrequest:</Link>
       </TableHeading>
-      <RawData data={buildrequest.toObject()}/>
+      <RawData data={buildrequest.toObject()} displayCallback={displayBuildRequestEntry}/>
       <TableHeading>Buildset:</TableHeading>
-      <RawData data={buildset.toObject()}/>
+      <RawData data={buildset.toObject()} displayCallback={displayBuildsetEntry}/>
       <TableHeading>Previous builds on the same build directory:</TableHeading>
       {renderPrevBuildOnBuildDir()}
     </>

commit a832c1895f059fe973cf2fc64265e5bce8fd0be4

www: Add more properties to expand human readable times
Fixes: d788b5aaaa7b6c883f411b844c363b45d2250706

www/base/src/components/RawData/RawData.tsx
@@ -45,7 +45,7 @@ export const displayBuildRequestEntry = (key: string, value: any) : string => {
 }
 
 export const displayBuildsetEntry = (key: string, value: any) : string => {
-  if (key === "complete_at" || key === "created_at") {
+  if (key === "complete_at" || key === "created_at" || key === "submitted_at") {
     // created_at is for dictionaries in sourcestamps list
     try {
       return `${value} (${moment(Number.parseInt(value.toString()) * 1000).format()})`;
@@ -69,7 +69,7 @@ export const RawData = ({data, displayCallback}: RawDataProps) => {
       <ul>
         {value.map((v, index) => (
           <li key={index}>
-            <RawData data={v}/>
+            <RawData data={v} displayCallback={displayCallback}/>
           </li>
         ))}
       </ul>