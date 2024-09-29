fa3ea6a5431f80294db7a1e52d91fabdd6a53f63
Povilas Kanapickas <povilas@radix.lt>
2024-09-29 14:42:35 +0300

    www: Extract LoadMoreTableRow component

--- www/base/src/components/BuildsTable/BuildsTable.tsx
+++ www/base/src/components/BuildsTable/BuildsTable.tsx
@@ -17,7 +17,7 @@
 
 import './BuildsTable.scss';
 import {observer} from "mobx-react";
-import {Button, Table} from "react-bootstrap";
+import {Table} from "react-bootstrap";
 import {
   Build,
   Builder,
@@ -34,6 +34,7 @@ import {
 } from "buildbot-ui";
 import {Link} from "react-router-dom";
 import {LoadingSpan} from "../LoadingSpan/LoadingSpan";
+import {LoadMoreTableRow} from "../LoadMoreTableRow/LoadMoreTableRow";
 import {TableHeading} from "../TableHeading/TableHeading";
 import {buildbotGetSettings, buildbotSetupPlugin} from "buildbot-plugin-support";
 
@@ -121,13 +122,7 @@ export const BuildsTable = observer(({builds, builders, fetchLimit, onLoadMore}:
     if (!builds.isResolved() || onLoadMore === null || builds.array.length < fetchLimit) {
       return <></>;
     }
-    return (
-      <tr>
-        <td colSpan={builders !== null ? 8 : 7}>
-          <Button variant="outline-secondary" onClick={onLoadMore}>Load more</Button>
-        </td>
-      </tr>
-    );
+    return <LoadMoreTableRow colSpan={builders !== null ? 8 : 7} onLoadMore={onLoadMore}/>;
   };
 
   const tableElement = () => {
