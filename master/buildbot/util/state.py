fe1a3e8d99e22dbdb22e4e3825f38c07bf1a7377
Jörg Thalheim <joerg@thalheim.io>
2024-10-06 16:22:00 +0200

    master/buildbot/util/state.py: use async/await instead of inlineCallbacks

--- master/buildbot/util/state.py
+++ master/buildbot/util/state.py
@@ -14,31 +14,26 @@
 # Copyright Buildbot Team Members
 
 
-from twisted.internet import defer
-
-
 class StateMixin:
     # state management
 
     _objectid = None
 
-    @defer.inlineCallbacks
-    def getState(self, *args, **kwargs):
+    async def getState(self, *args, **kwargs):
         # get the objectid, if not known
         if self._objectid is None:
-            self._objectid = yield self.master.db.state.getObjectId(
+            self._objectid = await self.master.db.state.getObjectId(
                 self.name, self.__class__.__name__
             )
 
-        rv = yield self.master.db.state.getState(self._objectid, *args, **kwargs)
+        rv = await self.master.db.state.getState(self._objectid, *args, **kwargs)
         return rv
 
-    @defer.inlineCallbacks
-    def setState(self, key, value):
+    async def setState(self, key, value):
         # get the objectid, if not known
         if self._objectid is None:
-            self._objectid = yield self.master.db.state.getObjectId(
+            self._objectid = await self.master.db.state.getObjectId(
                 self.name, self.__class__.__name__
             )
 
-        yield self.master.db.state.setState(self._objectid, key, value)
+        await self.master.db.state.setState(self._objectid, key, value)
