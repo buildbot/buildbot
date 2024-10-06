20c0fb1bedb85a9893be5e6231762491976a5047
Jörg Thalheim <joerg@thalheim.io>
2024-10-06 16:22:16 +0200

    master/buildbot/test/unit/data/test_sourcestamps.py: use async/await instead of inlineCallbacks

--- master/buildbot/test/unit/data/test_sourcestamps.py
+++ master/buildbot/test/unit/data/test_sourcestamps.py
@@ -13,7 +13,6 @@
 #
 # Copyright Buildbot Team Members
 
-from twisted.internet import defer
 from twisted.trial import unittest
 
 from buildbot.data import sourcestamps
@@ -43,17 +42,15 @@ class SourceStampEndpoint(endpoint.EndpointMixin, unittest.TestCase):
     def tearDown(self):
         self.tearDownEndpoint()
 
-    @defer.inlineCallbacks
-    def test_get_existing(self):
-        sourcestamp = yield self.callGet(('sourcestamps', 13))
+    async def test_get_existing(self):
+        sourcestamp = await self.callGet(('sourcestamps', 13))
 
         self.validateData(sourcestamp)
         self.assertEqual(sourcestamp['branch'], 'oak')
         self.assertEqual(sourcestamp['patch'], None)
 
-    @defer.inlineCallbacks
-    def test_get_existing_patch(self):
-        sourcestamp = yield self.callGet(('sourcestamps', 14))
+    async def test_get_existing_patch(self):
+        sourcestamp = await self.callGet(('sourcestamps', 14))
 
         self.validateData(sourcestamp)
         self.assertEqual(sourcestamp['branch'], 'poplar')
@@ -69,9 +66,8 @@ class SourceStampEndpoint(endpoint.EndpointMixin, unittest.TestCase):
             },
         )
 
-    @defer.inlineCallbacks
-    def test_get_missing(self):
-        sourcestamp = yield self.callGet(('sourcestamps', 99))
+    async def test_get_missing(self):
+        sourcestamp = await self.callGet(('sourcestamps', 99))
 
         self.assertEqual(sourcestamp, None)
 
@@ -94,23 +90,20 @@ class SourceStampsEndpoint(endpoint.EndpointMixin, unittest.TestCase):
     def tearDown(self):
         self.tearDownEndpoint()
 
-    @defer.inlineCallbacks
-    def test_get(self):
-        sourcestamps = yield self.callGet(('sourcestamps',))
+    async def test_get(self):
+        sourcestamps = await self.callGet(('sourcestamps',))
 
         for m in sourcestamps:
             self.validateData(m)
 
         self.assertEqual(sorted([m['ssid'] for m in sourcestamps]), [13, 14, 15])
 
-    @defer.inlineCallbacks
-    def test_get_by_buildsetid_no_buildset(self):
-        sourcestamps = yield self.callGet(("buildsets", 101, "sourcestamps"))
+    async def test_get_by_buildsetid_no_buildset(self):
+        sourcestamps = await self.callGet(("buildsets", 101, "sourcestamps"))
         self.assertEqual(sourcestamps, [])
 
-    @defer.inlineCallbacks
-    def test_get_by_buildsetid(self):
-        sourcestamps = yield self.callGet(("buildsets", 30, "sourcestamps"))
+    async def test_get_by_buildsetid(self):
+        sourcestamps = await self.callGet(("buildsets", 30, "sourcestamps"))
 
         for m in sourcestamps:
             self.validateData(m)
