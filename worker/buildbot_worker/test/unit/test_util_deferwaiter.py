2815b424bc19ce15170d8de5194ae57766e89afb
Jörg Thalheim <joerg@thalheim.io>
2024-10-06 16:22:03 +0200

    worker/buildbot_worker/test/unit/test_util_deferwaiter.py: use async/await instead of inlineCallbacks

--- worker/buildbot_worker/test/unit/test_util_deferwaiter.py
+++ worker/buildbot_worker/test/unit/test_util_deferwaiter.py
@@ -54,22 +54,20 @@ class WaiterTests(unittest.TestCase):
         self.assertFalse(w.has_waited())
         self.assertTrue(d.called)
 
-    @defer.inlineCallbacks
-    def test_passes_result(self):
+    async def test_passes_result(self):
         w = DeferWaiter()
 
         d1 = defer.Deferred()
         w.add(d1)
 
         d1.callback(123)
-        res = yield d1
+        res = await d1
         self.assertEqual(res, 123)
 
         d = w.wait()
         self.assertTrue(d.called)
 
-    @defer.inlineCallbacks
-    def test_cancel_not_called(self):
+    async def test_cancel_not_called(self):
         w = DeferWaiter()
 
         d1 = defer.Deferred()
@@ -82,12 +80,11 @@ class WaiterTests(unittest.TestCase):
         d = w.wait()
         self.assertTrue(d.called)
         with self.assertRaises(defer.CancelledError):
-            yield d1
+            await d1
 
         self.flushLoggedErrors(defer.CancelledError)
 
-    @defer.inlineCallbacks
-    def test_cancel_called(self):
+    async def test_cancel_called(self):
         w = DeferWaiter()
 
         d1_waited = defer.Deferred()
@@ -102,6 +99,6 @@ class WaiterTests(unittest.TestCase):
         self.assertTrue(d1.called)
         self.assertTrue(d1_waited.called)
         with self.assertRaises(defer.CancelledError):
-            yield d1
+            await d1
 
         self.flushLoggedErrors(defer.CancelledError)
