04c8986aa8e1067cd83f84f0a933ba89bec6223a
Jörg Thalheim <joerg@thalheim.io>
2024-10-06 16:22:08 +0200

    master/buildbot/test/unit/reporters/test_gitlab.py: use async/await instead of inlineCallbacks

--- master/buildbot/test/unit/reporters/test_gitlab.py
+++ master/buildbot/test/unit/reporters/test_gitlab.py
@@ -36,8 +36,7 @@ from buildbot.test.util.reporter import ReporterTestMixin
 class TestGitLabStatusPush(
     TestReactorMixin, ConfigErrorsMixin, unittest.TestCase, ReporterTestMixin, logging.LoggingMixin
 ):
-    @defer.inlineCallbacks
-    def setUp(self):
+    async def setUp(self):
         self.setup_test_reactor()
 
         self.setup_reporter_test()
@@ -46,8 +45,8 @@ class TestGitLabStatusPush(
 
         self.master = fakemaster.make_master(self, wantData=True, wantDb=True, wantMq=True)
 
-        yield self.master.startService()
-        self._http = yield fakehttpclientservice.HTTPClientService.getService(
+        await self.master.startService()
+        self._http = await fakehttpclientservice.HTTPClientService.getService(
             self.master,
             self,
             HOSTED_BASE_URL,
@@ -56,7 +55,7 @@ class TestGitLabStatusPush(
             verify=None,
         )
         self.sp = GitLabStatusPush(Interpolate('XXYYZZ'))
-        yield self.sp.setServiceParent(self.master)
+        await self.sp.setServiceParent(self.master)
 
         def setup_properties(props):
             props.setProperty("buildername", "Builder0", "Builder")
@@ -71,9 +70,8 @@ class TestGitLabStatusPush(
     def tearDown(self):
         return self.master.stopService()
 
-    @defer.inlineCallbacks
-    def test_buildrequest(self):
-        buildrequest = yield self.insert_buildrequest_new()
+    async def test_buildrequest(self):
+        buildrequest = await self.insert_buildrequest_new()
         self._http.expect('get', '/api/v4/projects/buildbot%2Fbuildbot', content_json={"id": 1})
         self._http.expect(
             'post',
@@ -98,12 +96,11 @@ class TestGitLabStatusPush(
             },
         )
 
-        yield self.sp._got_event(('buildrequests', 11, 'new'), buildrequest)
-        yield self.sp._got_event(('buildrequests', 11, 'cancel'), buildrequest)
+        await self.sp._got_event(('buildrequests', 11, 'new'), buildrequest)
+        await self.sp._got_event(('buildrequests', 11, 'cancel'), buildrequest)
 
-    @defer.inlineCallbacks
-    def test_basic(self):
-        build = yield self.insert_build_new()
+    async def test_basic(self):
+        build = await self.insert_build_new()
         self._http.expect('get', '/api/v4/projects/buildbot%2Fbuildbot', content_json={"id": 1})
         self._http.expect(
             'post',
@@ -150,19 +147,18 @@ class TestGitLabStatusPush(
             },
         )
 
-        yield self.sp._got_event(('builds', 20, 'new'), build)
+        await self.sp._got_event(('builds', 20, 'new'), build)
         build['complete'] = True
         build['results'] = SUCCESS
-        yield self.sp._got_event(('builds', 20, 'finished'), build)
+        await self.sp._got_event(('builds', 20, 'finished'), build)
         build['results'] = FAILURE
-        yield self.sp._got_event(('builds', 20, 'finished'), build)
+        await self.sp._got_event(('builds', 20, 'finished'), build)
         build['results'] = CANCELLED
-        yield self.sp._got_event(('builds', 20, 'finished'), build)
+        await self.sp._got_event(('builds', 20, 'finished'), build)
 
-    @defer.inlineCallbacks
-    def test_sshurl(self):
+    async def test_sshurl(self):
         self.reporter_test_repo = 'git@gitlab:buildbot/buildbot.git'
-        build = yield self.insert_build_new()
+        build = await self.insert_build_new()
         self._http.expect('get', '/api/v4/projects/buildbot%2Fbuildbot', content_json={"id": 1})
         self._http.expect(
             'post',
@@ -176,13 +172,12 @@ class TestGitLabStatusPush(
             },
         )
         build['complete'] = False
-        yield self.sp._got_event(('builds', 20, 'new'), build)
+        await self.sp._got_event(('builds', 20, 'new'), build)
 
-    @defer.inlineCallbacks
-    def test_merge_request_forked(self):
+    async def test_merge_request_forked(self):
         self.reporter_test_repo = 'git@gitlab:buildbot/buildbot.git'
         self.reporter_test_props['source_project_id'] = 20922342342
-        build = yield self.insert_build_new()
+        build = await self.insert_build_new()
         self._http.expect(
             'post',
             '/api/v4/projects/20922342342/statuses/d34db33fd43db33f',
@@ -195,15 +190,14 @@ class TestGitLabStatusPush(
             },
         )
         build['complete'] = False
-        yield self.sp._got_event(('builds', 20, 'new'), build)
+        await self.sp._got_event(('builds', 20, 'new'), build)
         # Don't run these tests in parallel!
         del self.reporter_test_props['source_project_id']
 
-    @defer.inlineCallbacks
-    def test_noproject(self):
+    async def test_noproject(self):
         self.reporter_test_repo = 'git@gitlab:buildbot/buildbot.git'
         self.setUpLogging()
-        build = yield self.insert_build_new()
+        build = await self.insert_build_new()
         self._http.expect(
             'get',
             '/api/v4/projects/buildbot%2Fbuildbot',
@@ -211,24 +205,22 @@ class TestGitLabStatusPush(
             code=404,
         )
         build['complete'] = False
-        yield self.sp._got_event(('builds', 20, 'new'), build)
+        await self.sp._got_event(('builds', 20, 'new'), build)
         self.assertLogged(
             r"Unknown \(or hidden\) gitlab projectbuildbot%2Fbuildbot: project not found"
         )
 
-    @defer.inlineCallbacks
-    def test_nourl(self):
+    async def test_nourl(self):
         self.reporter_test_repo = ''
-        build = yield self.insert_build_new()
+        build = await self.insert_build_new()
         build['complete'] = False
-        yield self.sp._got_event(('builds', 20, 'new'), build)
+        await self.sp._got_event(('builds', 20, 'new'), build)
         # implicit check that no http request is done
         # nothing is logged as well
 
-    @defer.inlineCallbacks
-    def test_senderror(self):
+    async def test_senderror(self):
         self.setUpLogging()
-        build = yield self.insert_build_new()
+        build = await self.insert_build_new()
         self._http.expect('get', '/api/v4/projects/buildbot%2Fbuildbot', content_json={"id": 1})
         self._http.expect(
             'post',
@@ -244,20 +236,19 @@ class TestGitLabStatusPush(
             code=404,
         )
         build['complete'] = False
-        yield self.sp._got_event(('builds', 20, 'new'), build)
+        await self.sp._got_event(('builds', 20, 'new'), build)
         self.assertLogged(
             "Could not send status \"running\" for"
             " http://gitlab/buildbot/buildbot at d34db33fd43db33f:"
             " sha1 not found for branch master"
         )
 
-    @defer.inlineCallbacks
-    def test_badchange(self):
+    async def test_badchange(self):
         self.setUpLogging()
-        build = yield self.insert_build_new()
+        build = await self.insert_build_new()
         self._http.expect('get', '/api/v4/projects/buildbot%2Fbuildbot', content_json={"id": 1})
         build['complete'] = False
-        yield self.sp._got_event(('builds', 20, 'new'), build)
+        await self.sp._got_event(('builds', 20, 'new'), build)
         self.assertLogged(
             "Failed to send status \"running\" for"
             " http://gitlab/buildbot/buildbot at d34db33fd43db33f\n"
