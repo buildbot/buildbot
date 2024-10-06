a77cb06e0e31f3c34f1335f649b255c9a36adaef
Jörg Thalheim <joerg@thalheim.io>
2024-10-06 16:21:58 +0200

    master/buildbot/steps/source/mercurial.py: use async/await instead of inlineCallbacks

--- master/buildbot/steps/source/mercurial.py
+++ master/buildbot/steps/source/mercurial.py
@@ -98,19 +98,18 @@ class Mercurial(Source):
         if errors:
             raise ConfigErrors(errors)
 
-    @defer.inlineCallbacks
-    def run_vc(self, branch, revision, patch):
+    async def run_vc(self, branch, revision, patch):
         self.revision = revision
         self.method = self._getMethod()
-        self.stdio_log = yield self.addLogForRemoteCommands("stdio")
+        self.stdio_log = await self.addLogForRemoteCommands("stdio")
 
-        installed = yield self.checkHg()
+        installed = await self.checkHg()
 
         if not installed:
             raise WorkerSetupError("Mercurial is not installed on worker")
 
         # FIXME: this does not do anything
-        yield self.sourcedirIsPatched()
+        await self.sourcedirIsPatched()
 
         if self.branchType == 'dirname':
             self.repourl = self.repourl + (branch or '')
@@ -119,74 +118,67 @@ class Mercurial(Source):
         elif self.branchType == 'inrepo':
             self.update_branch = branch or 'default'
 
-        yield self._getAttrGroupMember('mode', self.mode)()
+        await self._getAttrGroupMember('mode', self.mode)()
 
         if patch:
-            yield self.patch(patch)
+            await self.patch(patch)
 
-        yield self.parseGotRevision()
+        await self.parseGotRevision()
         return results.SUCCESS
 
-    @defer.inlineCallbacks
-    def mode_full(self):
+    async def mode_full(self):
         if self.method == 'clobber':
-            yield self.clobber()
+            await self.clobber()
             return
 
-        updatable = yield self._sourcedirIsUpdatable()
+        updatable = await self._sourcedirIsUpdatable()
         if not updatable:
-            yield self._clone()
-            yield self._update()
+            await self._clone()
+            await self._update()
         elif self.method == 'clean':
-            yield self.clean()
+            await self.clean()
         elif self.method == 'fresh':
-            yield self.fresh()
+            await self.fresh()
         else:
             raise ValueError("Unknown method, check your configuration")
 
-    @defer.inlineCallbacks
-    def mode_incremental(self):
+    async def mode_incremental(self):
         if self.method is not None:
             raise ValueError(self.method)
 
-        updatable = yield self._sourcedirIsUpdatable()
+        updatable = await self._sourcedirIsUpdatable()
 
         if updatable:
-            yield self._dovccmd(self.getHgPullCommand())
+            await self._dovccmd(self.getHgPullCommand())
         else:
-            yield self._clone()
+            await self._clone()
 
-        yield self._checkBranchChange()
+        await self._checkBranchChange()
 
-    @defer.inlineCallbacks
-    def clean(self):
+    async def clean(self):
         command = ['--config', 'extensions.purge=', 'purge']
-        yield self._dovccmd(command)
-        yield self._pullUpdate()
+        await self._dovccmd(command)
+        await self._pullUpdate()
 
-    @defer.inlineCallbacks
-    def _clobber(self):
+    async def _clobber(self):
         cmd = remotecommand.RemoteCommand(
             'rmdir', {'dir': self.workdir, 'logEnviron': self.logEnviron}
         )
         cmd.useLog(self.stdio_log, False)
-        yield self.runCommand(cmd)
+        await self.runCommand(cmd)
 
-    @defer.inlineCallbacks
-    def clobber(self):
-        yield self._clobber()
-        yield self._clone()
-        yield self._update()
+    async def clobber(self):
+        await self._clobber()
+        await self._clone()
+        await self._update()
 
-    @defer.inlineCallbacks
-    def fresh(self):
+    async def fresh(self):
         command = ['--config', 'extensions.purge=', 'purge', '--all']
-        yield self._dovccmd(command)
-        yield self._pullUpdate()
+        await self._dovccmd(command)
+        await self._pullUpdate()
 
-    @defer.inlineCallbacks
-    def parseGotRevision(self):
-        stdout = yield self._dovccmd(['parents', '--template', '{node}\\n'], collectStdout=True)
+    async def parseGotRevision(self):
+        stdout = await self._dovccmd(['parents', '--template', '{node}\\n'], collectStdout=True)
 
         revision = stdout.strip()
         if len(revision) != 40:
@@ -194,9 +186,8 @@ class Mercurial(Source):
         log.msg(f"Got Mercurial revision {revision}")
         self.updateSourceProperty('got_revision', revision)
 
-    @defer.inlineCallbacks
-    def _checkBranchChange(self):
-        current_branch = yield self._getCurrentBranch()
+    async def _checkBranchChange(self):
+        current_branch = await self._getCurrentBranch()
         msg = (
             f"Working dir is on in-repo branch '{current_branch}' and build needs "
             f"'{self.update_branch}'."
@@ -204,11 +195,11 @@ class Mercurial(Source):
         if current_branch != self.update_branch and self.clobberOnBranchChange:
             msg += ' Clobbering.'
             log.msg(msg)
-            yield self.clobber()
+            await self.clobber()
             return
         msg += ' Updating.'
         log.msg(msg)
-        yield self._removeAddedFilesAndUpdate(None)
+        await self._removeAddedFilesAndUpdate(None)
 
     def getHgPullCommand(self):
         command = ['pull', self.repourl]
@@ -218,14 +209,12 @@ class Mercurial(Source):
             command.extend(['--rev', self.update_branch])
         return command
 
-    @defer.inlineCallbacks
-    def _pullUpdate(self):
+    async def _pullUpdate(self):
         command = self.getHgPullCommand()
-        yield self._dovccmd(command)
-        yield self._checkBranchChange()
+        await self._dovccmd(command)
+        await self._checkBranchChange()
 
-    @defer.inlineCallbacks
-    def _dovccmd(
+    async def _dovccmd(
         self, command, collectStdout=False, initialStdin=None, decodeRC=None, abandonOnFailure=True
     ):
         if not command:
@@ -244,7 +233,7 @@ class Mercurial(Source):
             decodeRC=decodeRC,
         )
         cmd.useLog(self.stdio_log, False)
-        yield self.runCommand(cmd)
+        await self.runCommand(cmd)
 
         if abandonOnFailure and cmd.didFail():
             log.msg(f"Source step failed while running command {cmd}")
@@ -268,11 +257,10 @@ class Mercurial(Source):
             )
         return changes[-1].revision
 
-    @defer.inlineCallbacks
-    def _getCurrentBranch(self):
+    async def _getCurrentBranch(self):
         if self.branchType == 'dirname':
             return self.branch
-        stdout = yield self._dovccmd(['identify', '--branch'], collectStdout=True)
+        stdout = await self._dovccmd(['identify', '--branch'], collectStdout=True)
         return stdout.strip()
 
     def _getMethod(self):
@@ -287,10 +275,9 @@ class Mercurial(Source):
     def _sourcedirIsUpdatable(self):
         return self.pathExists(self.build.path_module.join(self.workdir, '.hg'))
 
-    @defer.inlineCallbacks
-    def _removeAddedFilesAndUpdate(self, _):
+    async def _removeAddedFilesAndUpdate(self, _):
         command = ['locate', 'set:added()']
-        stdout = yield self._dovccmd(command, collectStdout=True, decodeRC={0: SUCCESS, 1: SUCCESS})
+        stdout = await self._dovccmd(command, collectStdout=True, decodeRC={0: SUCCESS, 1: SUCCESS})
 
         files = []
         for filename in stdout.splitlines():
@@ -298,7 +285,7 @@ class Mercurial(Source):
             files.append(filename)
         if files:
             if self.workerVersionIsOlderThan('rmdir', '2.14'):
-                yield self.removeFiles(files)
+                await self.removeFiles(files)
             else:
                 cmd = remotecommand.RemoteCommand(
                     'rmdir',
@@ -308,12 +295,11 @@ class Mercurial(Source):
                     },
                 )
                 cmd.useLog(self.stdio_log, False)
-                yield self.runCommand(cmd)
+                await self.runCommand(cmd)
 
-        yield self._update()
+        await self._update()
 
-    @defer.inlineCallbacks
-    def removeFiles(self, files):
+    async def removeFiles(self, files):
         for filename in files:
             cmd = remotecommand.RemoteCommand(
                 'rmdir',
@@ -323,19 +309,18 @@ class Mercurial(Source):
                 },
             )
             cmd.useLog(self.stdio_log, False)
-            yield self.runCommand(cmd)
+            await self.runCommand(cmd)
             if cmd.rc != 0:
                 return cmd.rc
         return 0
 
-    @defer.inlineCallbacks
-    def _update(self):
+    async def _update(self):
         command = ['update', '--clean']
         if self.revision:
             command += ['--rev', self.revision]
         elif self.branchType == 'inrepo':
             command += ['--rev', self.update_branch]
-        yield self._dovccmd(command)
+        await self._dovccmd(command)
 
     def _clone(self):
         if self.retry:
