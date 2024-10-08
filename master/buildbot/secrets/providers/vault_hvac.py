92c94a3b2ee038d33992f7d97413cd375347121f
Jörg Thalheim <joerg@thalheim.io>
2024-10-08 22:46:12 +0200

    buildbot.secrets.providers.vault_hvac: fix type errors

--- master/buildbot/secrets/providers/vault_hvac.py
+++ master/buildbot/secrets/providers/vault_hvac.py
@@ -17,6 +17,7 @@ HVAC based providers
 """
 
 import importlib.metadata
+from typing import Optional
 
 from packaging.version import parse as parse_version
 from twisted.internet import defer
@@ -66,7 +67,7 @@ class HashiCorpVaultKvSecretProvider(SecretProviderBase):
     In case more secret engines are going to be supported, each engine should have it's own class.
     """
 
-    name = 'SecretInVaultKv'
+    name: Optional[str] = 'SecretInVaultKv'  # type: ignore[assignment]
 
     def checkConfig(
         self,
