6257e19b397836e1816ea5fae9121e1daefd3fda
Jörg Thalheim <joerg@thalheim.io>
2024-10-08 09:26:46 +0200

    buildbot.process.build: fix type errors

--- master/buildbot/process/build.py
+++ master/buildbot/process/build.py
@@ -44,6 +44,7 @@ from buildbot.util import bytes2unicode
 from buildbot.util.eventual import eventually
 
 if TYPE_CHECKING:
+    from buildbot.locks import BaseLockId
     from buildbot.process.builder import Builder
     from buildbot.process.workerforbuilder import AbstractWorkerForBuilder
 
@@ -89,8 +90,11 @@ class Build(properties.PropertiesMixin):
         self.master = builder.master
         self.workerforbuilder: AbstractWorkerForBuilder | None = None
 
-        self.locks = []  # list of lock accesses
-        self._locks_to_acquire = []  # list of (real_lock, access) tuples
+        self.locks: list[BaseLockId] = []  # list of lock accesses
+
+        self._locks_to_acquire: list[
+            tuple[BaseLockId, str]
+        ] = []  # list of (real_lock, access) tuples
         # build a source stamp
         self.sources = requests[0].mergeSourceStampsWith(requests[1:])
         self.reason = requests[0].mergeReasons(requests[1:])
@@ -99,12 +103,12 @@ class Build(properties.PropertiesMixin):
         self._locks_acquire_step = None
         self.currentStep = None
 
-        self.workerEnvironment = {}
+        self.workerEnvironment: dict[str, str] = {}
         self.buildid = None
         self._buildid_notifier = Notifier()
         self.number = None
-        self.executedSteps = []
-        self.stepnames = {}
+        self.executedSteps: list[buildstep.BuildStep] = []
+        self.stepnames: dict[str, int] = {}
 
         self.terminate = False
 
@@ -314,6 +318,7 @@ class Build(properties.PropertiesMixin):
         self.conn = None
 
         worker = workerforbuilder.worker
+        assert worker is not None
 
         # Cache the worker information as variables instead of accessing via worker, as the worker
         # will disappear during disconnection and some of these properties may still be needed.
@@ -326,11 +331,14 @@ class Build(properties.PropertiesMixin):
         # then we just assign the build to the first buildrequest
         brid = self.requests[0].id
         builderid = yield self.getBuilderId()
+        assert self.master is not None
+        assert self.master.data is not None
         self.buildid, self.number = yield self.master.data.updates.addBuild(
             builderid=builderid, buildrequestid=brid, workerid=worker.workerid
         )
         self._buildid_notifier.notify(self.buildid)
 
+        assert self.master.mq is not None
         self.stopBuildConsumer = yield self.master.mq.startConsuming(
             self.controlStopBuild, ("control", "builds", str(self.buildid), "stop")
         )
@@ -349,8 +357,10 @@ class Build(properties.PropertiesMixin):
         self._preparation_step = buildstep.create_step_from_step_or_factory(
             buildstep.BuildStep(name="worker_preparation")
         )
+        assert self._preparation_step is not None
         self._preparation_step.setBuild(self)
         yield self._preparation_step.addStep()
+        assert self.master.data.updates is not None
         yield self.master.data.updates.startStep(self._preparation_step.stepid, locks_acquired=True)
 
         Build.setupBuildProperties(self.getProperties(), self.requests, self.sources, self.number)
