b6ce3ca5975657321db0f08942ebd76ab5c7cf8e
Povilas Kanapickas <povilas@radix.lt>
2024-10-21 16:28:43 +0300

    test: Mark FakeSchedulersComponent.enable() as component method

--- master/buildbot/test/fakedb/schedulers.py
+++ master/buildbot/test/fakedb/schedulers.py
@@ -83,6 +83,11 @@ class FakeSchedulersComponent(FakeDBComponent):
 
     # component methods
 
+    def enable(self, schedulerid, v):
+        assert schedulerid in self.schedulers
+        self.enabled[schedulerid] = v
+        return defer.succeed(None)
+
     def classifyChanges(self, schedulerid, classifications):
         self.classifications.setdefault(schedulerid, {}).update(classifications)
         return defer.succeed(None)
@@ -193,10 +198,3 @@ class FakeSchedulersComponent(FakeDBComponent):
     def get_scheduler_master(self, schedulerid):
         current_masterid = self.scheduler_masters.get(schedulerid, None)
         return defer.succeed(current_masterid)
-
-    # assertions
-
-    def enable(self, schedulerid, v):
-        assert schedulerid in self.schedulers
-        self.enabled[schedulerid] = v
-        return defer.succeed((('control', 'schedulers', schedulerid, 'enable'), {'enabled': v}))
