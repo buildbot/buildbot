5520900ffc50c1a816362d57fb5439e3537f4566
Povilas Kanapickas <povilas@radix.lt>
2024-11-26 06:39:56 +0200

    test: Fix missing rows in buildrequest distributor tests

--- master/buildbot/test/unit/process/test_buildrequestdistributor.py
+++ master/buildbot/test/unit/process/test_buildrequestdistributor.py
@@ -67,6 +67,7 @@ class TestBRDBase(TestReactorMixin, unittest.TestCase):
 
         # a collection of rows that would otherwise clutter up every test
         self.base_rows = [
+            fakedb.Master(id=fakedb.FakeDBConnector.MASTER_ID),
             fakedb.SourceStamp(id=21),
             fakedb.Builder(id=77, name='A'),
             fakedb.Buildset(id=11, reason='because'),
@@ -437,7 +438,9 @@ class TestMaybeStartBuilds(TestBRDBase):
     @defer.inlineCallbacks
     def test_no_workerforbuilders(self):
         rows = [
+            fakedb.Master(id=fakedb.FakeDBConnector.MASTER_ID),
             fakedb.Builder(id=78, name='bldr'),
+            fakedb.Buildset(id=10),
             fakedb.BuildRequest(id=11, buildsetid=10, builderid=78),
         ]
         yield self.do_test_maybeStartBuildsOnBuilder(rows=rows, exp_claims=[], exp_builds=[])
@@ -745,6 +748,7 @@ class TestMaybeStartBuilds(TestBRDBase):
         self.addWorkers({'test-worker1': 1, 'test-worker2': 1})
         rows = [
             *self.base_rows,
+            fakedb.Master(id=9999),
             fakedb.BuildRequest(
                 id=10, buildsetid=11, builderid=77, submitted_at=130000
             ),  # will turn out to be claimed!
@@ -779,6 +783,7 @@ class TestMaybeStartBuilds(TestBRDBase):
             self.addWorkers({f'test-worker{i}': 1})
 
         rows = [
+            fakedb.Master(id=fakedb.FakeDBConnector.MASTER_ID),
             fakedb.SourceStamp(id=21),
             fakedb.Builder(id=78, name='B'),
             fakedb.Buildset(id=12, reason='because'),
