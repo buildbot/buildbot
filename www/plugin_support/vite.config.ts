e3af97af2789951c12405e727c7f4b79d6d54314
Monika Kairaityte <monika@kibit.lt>
2024-12-18 16:18:19 +0200

    www/plugin_support: Setup vite-plugin-checker for type errors

--- www/plugin_support/vite.config.ts
+++ www/plugin_support/vite.config.ts
@@ -1,12 +1,14 @@
 import {resolve} from "path";
 import {defineConfig} from "vite";
 import dts from 'vite-plugin-dts'
+import checker from 'vite-plugin-checker';
 
 const outDir = 'dist';
 
 export default defineConfig({
   plugins: [
     dts(),
+    checker({typescript: true}),
   ],
   build: {
     lib: {
