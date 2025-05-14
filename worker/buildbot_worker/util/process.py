29178c64bde50aba85411b073894f00cc151f525
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2025-05-14 20:41:17 +0200

    worker: early-out of compute_environ if nothing to do

--- worker/buildbot_worker/util/process.py
+++ worker/buildbot_worker/util/process.py
@@ -22,41 +22,41 @@ import re
 def compute_environ(
     environ: dict[str, str | list[str] | int | None] | None = None,
 ) -> dict[str, str]:
-    if environ:
-        for key, v in environ.items():
-            if isinstance(v, list):
-                # Need to do os.pathsep translation.  We could either do that
-                # by replacing all incoming ':'s with os.pathsep, or by
-                # accepting lists.  I like lists better.
-                # If it's not a string, treat it as a sequence to be
-                # turned in to a string.
-
-                # TODO: replace `os.pathsep.join(environ[key])` -> `os.pathsep.join(v)`
-                environ[key] = os.pathsep.join(environ[key])  # type: ignore[arg-type]
-
-        if "PYTHONPATH" in environ:
-            environ['PYTHONPATH'] += os.pathsep + "${PYTHONPATH}"  # type: ignore[operator]
-
-        # do substitution on variable values matching pattern: ${name}
-        p = re.compile(r'\${([0-9a-zA-Z_]*)}')
-
-        def subst(match: re.Match[str]) -> str:
-            return os.environ.get(match.group(1), "")
-
-        newenv = {}
-        for key in os.environ:
-            # setting a key to None will delete it from the worker
-            # environment
-            if key not in environ or environ[key] is not None:
-                newenv[key] = os.environ[key]
-        for key, v in environ.items():
-            if v is not None:
-                if not isinstance(v, str):
-                    raise RuntimeError(
-                        f"'env' values must be strings or lists; key '{key}' is incorrect"
-                    )
-                newenv[key] = p.sub(subst, v)
-
-        return newenv
-    else:  # not environ
+    if not environ:
         return os.environ.copy()
+
+    for key, v in environ.items():
+        if isinstance(v, list):
+            # Need to do os.pathsep translation.  We could either do that
+            # by replacing all incoming ':'s with os.pathsep, or by
+            # accepting lists.  I like lists better.
+            # If it's not a string, treat it as a sequence to be
+            # turned in to a string.
+
+            # TODO: replace `os.pathsep.join(environ[key])` -> `os.pathsep.join(v)`
+            environ[key] = os.pathsep.join(environ[key])  # type: ignore[arg-type]
+
+    if "PYTHONPATH" in environ:
+        environ['PYTHONPATH'] += os.pathsep + "${PYTHONPATH}"  # type: ignore[operator]
+
+    # do substitution on variable values matching pattern: ${name}
+    p = re.compile(r'\${([0-9a-zA-Z_]*)}')
+
+    def subst(match: re.Match[str]) -> str:
+        return os.environ.get(match.group(1), "")
+
+    newenv = {}
+    for key in os.environ:
+        # setting a key to None will delete it from the worker
+        # environment
+        if key not in environ or environ[key] is not None:
+            newenv[key] = os.environ[key]
+    for key, v in environ.items():
+        if v is not None:
+            if not isinstance(v, str):
+                raise RuntimeError(
+                    f"'env' values must be strings or lists; key '{key}' is incorrect"
+                )
+            newenv[key] = p.sub(subst, v)
+
+    return newenv
