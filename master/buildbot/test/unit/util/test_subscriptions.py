617ba2c7801b612bcb5f0a983cbdd496c8352a13
Thomas Desveaux <desveaux.thomas@gmail.com>
2025-05-19 16:10:15 +0200

    util: type SubscriptionPoint

--- master/buildbot/test/unit/util/test_subscriptions.py
+++ master/buildbot/test/unit/util/test_subscriptions.py
@@ -13,11 +13,23 @@
 #
 # Copyright Buildbot Team Members
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from typing import Any
+from typing import Callable
+from typing import NoReturn
+
 from twisted.internet import defer
 from twisted.python import failure
 from twisted.trial import unittest
 
 from buildbot.util import subscription
+
+if TYPE_CHECKING:
+    from twisted.internet.defer import Deferred
+
+    from buildbot.util.twisted import InlineCallbacksType
 
 
 class TestException(Exception):
@@ -25,16 +37,16 @@ class TestException(Exception):
 
 
 class subscriptions(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.subpt = subscription.SubscriptionPoint('test_sub')
 
-    def test_str(self):
+    def test_str(self) -> None:
         self.assertIn('test_sub', str(self.subpt))
 
-    def test_subscribe_unsubscribe(self):
-        state = []
+    def test_subscribe_unsubscribe(self) -> None:
+        state: list[tuple[tuple[Any, ...], dict[str, Any]]] = []
 
-        def cb(*args, **kwargs):
+        def cb(*args: Any, **kwargs: Any) -> None:
             state.append((args, kwargs))
 
         # subscribe
@@ -54,14 +66,15 @@ class subscriptions(unittest.TestCase):
         self.subpt.deliver(3, 4)
         self.assertEqual(state, [])
 
-    def test_exception(self):
-        def cb(*args, **kwargs):
+    def test_exception(self) -> None:
+        def cb(*args: Any, **kwargs: Any) -> NoReturn:
             raise RuntimeError('mah bucket!')
 
         self.subpt.subscribe(cb)
         self.subpt.deliver()  # should not raise
 
         exceptions = self.subpt.pop_exceptions()
+        assert exceptions is not None
         self.assertEqual(len(exceptions), 1)
         self.assertIsInstance(exceptions[0], RuntimeError)
 
@@ -69,11 +82,11 @@ class subscriptions(unittest.TestCase):
         # we clean it up
         self.assertEqual(1, len(self.flushLoggedErrors(RuntimeError)))
 
-    def test_deferred_exception(self):
-        d = defer.Deferred()
+    def test_deferred_exception(self) -> None:
+        d: Deferred[None] = defer.Deferred()
 
         @defer.inlineCallbacks
-        def cb_deferred(*args, **kwargs):
+        def cb_deferred(*args: Any, **kwargs: Any) -> InlineCallbacksType[NoReturn]:
             yield d
             raise RuntimeError('msg')
 
@@ -83,18 +96,19 @@ class subscriptions(unittest.TestCase):
         d.callback(None)
 
         exceptions = self.subpt.pop_exceptions()
+        assert exceptions is not None
         self.assertEqual(len(exceptions), 1)
         self.assertIsInstance(exceptions[0], failure.Failure)
 
         self.assertEqual(1, len(self.flushLoggedErrors(RuntimeError)))
 
-    def test_deferred_exception_after_pop_exceptions(self):
+    def test_deferred_exception_after_pop_exceptions(self) -> None:
         # waitForDeliveriesToFinish is forgotten to be called and exception happens after
         # pop_exceptions.
-        d = defer.Deferred()
+        d: Deferred[None] = defer.Deferred()
 
         @defer.inlineCallbacks
-        def cb_deferred(*args, **kwargs):
+        def cb_deferred(*args: Any, **kwargs: Any) -> InlineCallbacksType[NoReturn]:
             yield d
             raise TestException('msg')
 
@@ -105,18 +119,19 @@ class subscriptions(unittest.TestCase):
 
         d.callback(None)
 
+        assert exceptions is not None
         self.assertEqual(len(exceptions), 0)
         self.assertEqual(2, len(self.flushLoggedErrors(TestException)))
 
-    def test_multiple_exceptions(self):
-        d = defer.Deferred()
+    def test_multiple_exceptions(self) -> None:
+        d: Deferred[None] = defer.Deferred()
 
         @defer.inlineCallbacks
-        def cb_deferred(*args, **kwargs):
+        def cb_deferred(*args: Any, **kwargs: Any) -> InlineCallbacksType[NoReturn]:
             yield d
             raise RuntimeError('msg')
 
-        def cb(*args, **kwargs):
+        def cb(*args: Any, **kwargs: Any) -> NoReturn:
             raise RuntimeError('msg')
 
         self.subpt.subscribe(cb_deferred)
@@ -126,24 +141,25 @@ class subscriptions(unittest.TestCase):
         d.callback(None)
 
         exceptions = self.subpt.pop_exceptions()
+        assert exceptions is not None
         self.assertEqual(len(exceptions), 2)
         self.assertIsInstance(exceptions[0], RuntimeError)
         self.assertIsInstance(exceptions[1], failure.Failure)
 
         self.assertEqual(2, len(self.flushLoggedErrors(RuntimeError)))
 
-    def test_deliveries_finished(self):
-        state = []
+    def test_deliveries_finished(self) -> None:
+        state: list[tuple[Any, ...]] = []
 
-        def create_cb(d):
-            def cb(*args):
+        def create_cb(d: Deferred[None]) -> Callable[..., Deferred[None]]:
+            def cb(*args: Any) -> Deferred[None]:
                 state.append(args)
                 return d
 
             return cb
 
-        d1 = defer.Deferred()
-        d2 = defer.Deferred()
+        d1: Deferred[None] = defer.Deferred()
+        d2: Deferred[None] = defer.Deferred()
         self.subpt.subscribe(create_cb(d1))
         self.subpt.subscribe(create_cb(d2))
         self.assertEqual(state, [])
@@ -164,10 +180,10 @@ class subscriptions(unittest.TestCase):
         d = self.subpt.waitForDeliveriesToFinish()
         self.assertTrue(d.called)
 
-    def test_deliveries_not_finished_within_callback(self):
-        state = []
+    def test_deliveries_not_finished_within_callback(self) -> None:
+        state: list[tuple[Any, ...]] = []
 
-        def cb(*args):
+        def cb(*args: Any) -> None:
             state.append(args)
             d = self.subpt.waitForDeliveriesToFinish()
             self.assertFalse(d.called)
--- master/buildbot/util/subscription.py
+++ master/buildbot/util/subscription.py
@@ -14,29 +14,39 @@
 # Copyright Buildbot Team Members
 
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from typing import Any
+from typing import Callable
+
 from twisted.internet import defer
 from twisted.python import log
 
 from buildbot.util import Notifier
 
+if TYPE_CHECKING:
+    from twisted.internet.defer import Deferred
+    from twisted.python.failure import Failure
+
 
 class SubscriptionPoint:
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = name
-        self.subscriptions = set()
-        self._unfinished_deliveries = []
-        self._unfinished_notifier = Notifier()
-        self._got_exceptions = []
+        self.subscriptions: set[Subscription] = set()
+        self._unfinished_deliveries: list[Deferred[None] | SubscriptionPoint] = []
+        self._unfinished_notifier: Notifier[None] = Notifier()
+        self._got_exceptions: list[Exception | Failure] | None = []
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"<SubscriptionPoint '{self.name}'>"
 
-    def subscribe(self, callback):
+    def subscribe(self, callback: Callable) -> Subscription:
         sub = Subscription(self, callback)
         self.subscriptions.add(sub)
         return sub
 
-    def deliver(self, *args, **kwargs):
+    def deliver(self, *args: Any, **kwargs: Any) -> None:
         self._unfinished_deliveries.append(self)
         for sub in list(self.subscriptions):
             try:
@@ -51,21 +61,24 @@ class SubscriptionPoint:
 
         self._notify_delivery_finished(None, self)
 
-    def waitForDeliveriesToFinish(self):
-        # returns a deferred
+    def waitForDeliveriesToFinish(self) -> Deferred[None]:
         if not self._unfinished_deliveries:
             return defer.succeed(None)
         return self._unfinished_notifier.wait()
 
-    def pop_exceptions(self):
+    def pop_exceptions(self) -> list[Exception | Failure] | None:
         exceptions = self._got_exceptions
         self._got_exceptions = None  # we no longer expect any exceptions
         return exceptions
 
-    def _unsubscribe(self, subscription):
+    def _unsubscribe(self, subscription: Subscription) -> None:
         self.subscriptions.remove(subscription)
 
-    def _notify_delivery_exception(self, e, sub):
+    def _notify_delivery_exception(
+        self,
+        e: Exception | Failure,
+        sub: Subscription,
+    ) -> None:
         log.err(e, f'while invoking callback {sub.callback} to {self}')
         if self._got_exceptions is None:
             log.err(
@@ -76,16 +89,16 @@ class SubscriptionPoint:
             return
         self._got_exceptions.append(e)
 
-    def _notify_delivery_finished(self, _, d):
+    def _notify_delivery_finished(self, _: None, d: SubscriptionPoint | Deferred[None]) -> None:
         self._unfinished_deliveries.remove(d)
         if not self._unfinished_deliveries:
             self._unfinished_notifier.notify(None)
 
 
 class Subscription:
-    def __init__(self, subpt, callback):
+    def __init__(self, subpt: SubscriptionPoint, callback: Callable) -> None:
         self.subpt = subpt
         self.callback = callback
 
-    def unsubscribe(self):
+    def unsubscribe(self) -> None:
         self.subpt._unsubscribe(self)
