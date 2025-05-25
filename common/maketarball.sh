f62bba0f39061defca63e37a32cbf3fc7c3c2e18
Thomas Desveaux <desveaux.thomas@gmail.com>
2025-05-25 11:18:04 +0200

    pkg: convert setup.cfg to pyproject.toml

--- common/maketarball.sh
+++ common/maketarball.sh
@@ -4,12 +4,7 @@ pkg=$1
 (
     cd ${pkg}
     rm -rf MANIFEST dist
-    if [ ${pkg} == "pkg" ]; then
-        python -m build --no-isolation --sdist
-        # wheels must be build separately in order to properly omit tests
-        python -m build --no-isolation --wheel
-    else
-        python -m build
-    fi
+
+    python -m build
 )
 cp ${pkg}/dist/* dist/
