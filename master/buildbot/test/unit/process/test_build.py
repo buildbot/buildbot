buildbot/buildbot

commit 739cc044a9b4d7c708fb17bbc74b56e5d1f760f4

process.build: fix potential issue with Build.setLocks keeping ref to passed arg

master/buildbot/process/build.py
@@ -164,10 +164,8 @@ class Build(properties.PropertiesMixin):
     def getProperties(self) -> IProperties:
         return self.properties
 
-    def setLocks(self, lockList: list[BaseLockId]) -> None:
-        # FIXME: should take Iterable and create a list
-        # to avoid keeping same ref as caller and caller potentially mutating it
-        self.locks = lockList
+    def setLocks(self, lockList: Iterable[BaseLockId]) -> None:
+        self.locks = list(lockList)
 
     @defer.inlineCallbacks
     def _setup_locks(self) -> InlineCallbacksType[None]:

commit dd3caf657164f26231850d5caa9cdb25e7880e56

process: Fix crash with renderable locks
setLocks() must not assume that the argument will always be iterable.

Fixes: 739cc044a9b4d7c708fb17bbc74b56e5d1f760f4

master/buildbot/interfaces.py
@@ -27,9 +27,15 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 from typing import Any
+from typing import Generic
+from typing import Protocol
+from typing import TypeVar
+from typing import Union
 
 from zope.interface import Attribute
 from zope.interface import Interface
+
+T = TypeVar('T')
 
 if TYPE_CHECKING:
     from twisted.internet.defer import Deferred
@@ -223,6 +229,13 @@ class IMachineAction(Interface):
 
 class ILatentMachine(IMachine):
     """A machine that is not always running, but can be started when requested."""
+
+
+class IRenderableType(Protocol, Generic[T]):
+    def getRenderingFor(self, iprops: IProperties) -> Deferred[T]: ...
+
+
+IMaybeRenderableType = Union[IRenderableType[T], T]
 
 
 class IRenderable(Interface):

master/buildbot/process/build.py
@@ -15,6 +15,7 @@
 
 from __future__ import annotations
 
+from collections.abc import Iterable
 from copy import deepcopy
 from functools import reduce
 from typing import TYPE_CHECKING
@@ -50,7 +51,6 @@ from buildbot.util import bytes2unicode
 from buildbot.util.eventual import eventually
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
     from collections.abc import Iterator
     from collections.abc import Sequence
     from pathlib import PurePath
@@ -59,6 +59,7 @@ if TYPE_CHECKING:
 
     from buildbot.interfaces import IBuildStep
     from buildbot.interfaces import IBuildStepFactory
+    from buildbot.interfaces import IMaybeRenderableType
     from buildbot.interfaces import IProperties
     from buildbot.locks import BaseLock
     from buildbot.locks import BaseLockId
@@ -116,7 +117,7 @@ class Build(properties.PropertiesMixin):
         self.master = builder.master
         self.workerforbuilder: AbstractWorkerForBuilder | None = None
 
-        self.locks: list[BaseLockId] = []  # list of lock accesses
+        self.locks: IMaybeRenderableType[Iterable[BaseLockId]] = []  # list of lock accesses
 
         # list of (real_lock, access) tuples
         self._locks_to_acquire: list[tuple[BaseLock, str]] = []
@@ -164,8 +165,12 @@ class Build(properties.PropertiesMixin):
     def getProperties(self) -> IProperties:
         return self.properties
 
-    def setLocks(self, lockList: Iterable[BaseLockId]) -> None:
-        self.locks = list(lockList)
+    def setLocks(self, locks: IMaybeRenderableType[Iterable[BaseLockId]]) -> None:
+        if isinstance(locks, Iterable):
+            # copy the passed list so that changes by the caller do not modify stored instance
+            self.locks = list(locks)
+        else:
+            self.locks = locks
 
     @defer.inlineCallbacks
     def _setup_locks(self) -> InlineCallbacksType[None]:

master/buildbot/test/unit/process/test_build.py
@@ -31,6 +31,7 @@ from buildbot.process.buildstep import create_step_from_step_or_factory
 from buildbot.process.locks import get_real_locks_from_accesses
 from buildbot.process.metrics import MetricLogObserver
 from buildbot.process.properties import Properties
+from buildbot.process.properties import renderer
 from buildbot.process.results import CANCELLED
 from buildbot.process.results import EXCEPTION
 from buildbot.process.results import FAILURE
@@ -400,6 +401,32 @@ class TestBuild(TestReactorMixin, unittest.TestCase):
         lock.access = lambda mode: lock_access
 
         b.setLocks([lock_access])
+        yield b._setup_locks()
+
+        self._setup_lock_claim_log(b._locks_to_acquire[0][0], claim_log)
+
+        step = self.create_fake_build_step()
+        b.setStepFactories([FakeStepFactory(step)])
+
+        b.startBuild(self.workerforbuilder)
+
+        self.assertEqual(b.results, SUCCESS)
+        self.assertEqual(len(claim_log), 1)
+
+    @defer.inlineCallbacks
+    def test_build_locks_acquired_renderable(self):
+        b = self.build
+
+        lock = WorkerLock('lock')
+        claim_log = []
+        lock_access = lock.access('counting')
+        lock.access = lambda mode: lock_access
+
+        @renderer
+        def render_locks(props):
+            return [lock_access]
+
+        b.setLocks(render_locks)
         yield b._setup_locks()
 
         self._setup_lock_claim_log(b._locks_to_acquire[0][0], claim_log)