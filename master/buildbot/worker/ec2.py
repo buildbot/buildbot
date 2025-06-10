bb981c626e7fa99ce6f84e0a0b9211286d7c2aab
Jaqueline R Aguilar <jaguilarodrigues@gmail.com>
2025-06-09 23:58:09 -0300

    master: decoupling the sort candite images logic into helper functions

--- master/buildbot/worker/ec2.py
+++ master/buildbot/worker/ec2.py
@@ -301,6 +301,37 @@ class EC2LatentWorker(AbstractLatentWorker):
             return self.image
         images = self._get_all_images()
 
+        images_options_sorted = self._sort_images_options(images)
+
+        candidate_images = [f'{candidate[-1].id} ({candidate[-1].image_location})' for candidate in images_options_sorted]
+        log.msg(f"sorted images (last is chosen): {', '.join(candidate_images)}")
+        if not images_options_sorted:
+            raise ValueError('no available images match constraints')
+        return images_options_sorted[-1][-1]
+
+    def _get_all_images(self):
+        images = self.ec2.images.all()
+        if self.valid_ami_owners:
+            images = images.filter(Owners=self.valid_ami_owners)
+        return [img for img in images if img.state == 'available']
+
+    def _extract_sort(self, level, match):
+        # Gather sorting information
+        alpha_sort = int_sort = None
+        if level < 2:
+            try:
+                alpha_sort = match.group(1)
+            except IndexError:
+                return None, None, 2
+            else:
+                if level == 0:
+                    try:
+                        int_sort = int(alpha_sort)
+                    except ValueError:
+                        return None, None, 1
+        return alpha_sort, int_sort, level
+
+    def _sort_images_options(self, images):
         if self.valid_ami_location_regex:
             level = 0
             options = []
@@ -310,37 +341,16 @@ class EC2LatentWorker(AbstractLatentWorker):
                 match = get_match(image.image_location)
                 if not match:
                     continue
-                # Gather sorting information
-                alpha_sort = int_sort = None
-                if level < 2:
-                    try:
-                        alpha_sort = match.group(1)
-                    except IndexError:
-                        level = 2
-                    else:
-                        if level == 0:
-                            try:
-                                int_sort = int(alpha_sort)
-                            except ValueError:
-                                level = 1
+
+                alpha_sort, int_sort, level = self._extract_sort(level, match)
+
                 options.append([int_sort, alpha_sort, image.image_location, image.id, image])
             if level:
                 log.msg(f'sorting images at level {level}')
                 options = [candidate[level:] for candidate in options]
         else:
             options = [(image.image_location, image.id, image) for image in images]
-        options.sort()
-        images = [f'{candidate[-1].id} ({candidate[-1].image_location})' for candidate in options]
-        log.msg(f"sorted images (last is chosen): {', '.join(images)}")
-        if not options:
-            raise ValueError('no available images match constraints')
-        return options[-1][-1]
-
-    def _get_all_images(self):
-        images = self.ec2.images.all()
-        if self.valid_ami_owners:
-            images = images.filter(Owners=self.valid_ami_owners)
-        return [img for img in images if img.state == 'available']
+        return options.sort()
 
     def _dns(self):
         if self.instance is None:
