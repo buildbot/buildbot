b7ae9d51dacdeb8b2bb482111b2d917bd5d3af93
Thomas Desveaux <desveaux.thomas@gmail.com>
2026-02-27 18:18:26 +0100

    buildbot.util.bbcollections: add type hints

--- master/buildbot/util/bbcollections.py
+++ master/buildbot/util/bbcollections.py
@@ -13,33 +13,38 @@
 #
 # Copyright Buildbot Team Members
 
+from __future__ import annotations
 
 # this is here for compatibility
 from collections import defaultdict  # noqa: F401
+from typing import Generic
+from typing import TypeVar
+
+_T = TypeVar('_T')
 
 
-class KeyedSets:
-    def __init__(self):
-        self.d = {}
+class KeyedSets(Generic[_T]):
+    def __init__(self) -> None:
+        self.d: dict[str, set[_T]] = {}
 
-    def add(self, key, value):
+    def add(self, key: str, value: _T) -> None:
         if key not in self.d:
             self.d[key] = set()
         self.d[key].add(value)
 
-    def discard(self, key, value):
+    def discard(self, key: str, value: _T) -> None:
         if key in self.d:
             self.d[key].discard(value)
             if not self.d[key]:
                 del self.d[key]
 
-    def __contains__(self, key):
+    def __contains__(self, key: str) -> bool:
         return key in self.d
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str) -> set[_T]:
         return self.d.get(key, set())
 
-    def pop(self, key):
+    def pop(self, key: str) -> set[_T]:
         if key in self.d:
             return self.d.pop(key)
         return set()
