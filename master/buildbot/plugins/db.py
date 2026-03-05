d9cad1b3f720b4647312be49ca63227e552a6b02
Povilas Kanapickas <povilas@radix.lt>
2026-03-05 20:37:11 +0200

    plugins: Add type hints

--- master/buildbot/plugins/db.py
+++ master/buildbot/plugins/db.py
@@ -15,13 +15,22 @@
 #
 # pylint: disable=C0111
 
+from __future__ import annotations
+
 import traceback
 import warnings
+from importlib.metadata import EntryPoint
 from importlib.metadata import distributions
 from importlib.metadata import entry_points
+from typing import TYPE_CHECKING
+from typing import Any
+from typing import Callable
 
 from zope.interface import Invalid
 from zope.interface.verify import verifyClass
+
+if TYPE_CHECKING:
+    from zope.interface.interface import InterfaceClass
 
 from buildbot.errors import PluginDBError
 from buildbot.interfaces import IPlugin
@@ -31,7 +40,9 @@ from buildbot.util.importlib_compat import entry_points_get
 _NAMESPACE_BASE = 'buildbot'
 
 
-def find_distribution_info(entry_point_name, entry_point_group):
+def find_distribution_info(
+    entry_point_name: str, entry_point_group: str
+) -> tuple[str | None, str | None]:
     for distribution in distributions():
         # each distribution can have many entry points
         try:
@@ -44,15 +55,15 @@ def find_distribution_info(entry_point_name, entry_point_group):
 
 
 class _PluginEntry:
-    def __init__(self, group, entry, loader):
+    def __init__(self, group: str, entry: EntryPoint, loader: Callable[[EntryPoint], Any]) -> None:
         self._group = group
         self._entry = entry
-        self._value = None
+        self._value: Any = None
         self._loader = loader
-        self._load_warnings = []
-        self._info = None
+        self._load_warnings: list[warnings.WarningMessage] = []
+        self._info: tuple[str | None, str | None] | None = None
 
-    def load(self):
+    def load(self) -> None:
         if self._value is None:
             with warnings.catch_warnings(record=True) as all_warnings:
                 warnings.simplefilter("always")
@@ -60,20 +71,20 @@ class _PluginEntry:
                 self._load_warnings = list(all_warnings)
 
     @property
-    def group(self):
+    def group(self) -> str:
         return self._group
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._entry.name
 
     @property
-    def info(self):
+    def info(self) -> tuple[str | None, str | None]:
         if self._info is None:
             self._info = find_distribution_info(self._entry.name, self._group)
         return self._info
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         if not isinstance(other, _PluginEntry):
             return NotImplemented
         return self.info == other.info
@@ -81,11 +92,11 @@ class _PluginEntry:
     def __hash__(self) -> int:
         return hash(self.info)
 
-    def __ne__(self, other):
+    def __ne__(self, other: object) -> bool:
         return not self.__eq__(other)
 
     @property
-    def value(self):
+    def value(self) -> Any:
         self.load()
         for w in self._load_warnings:
             warnings.warn_explicit(w.message, w.category, w.filename, w.lineno)
@@ -98,46 +109,46 @@ class _PluginEntryProxy(_PluginEntry):
     Used to provided access to the same entry from different namespaces.
     """
 
-    def __init__(self, group, plugin_entry):
+    def __init__(self, group: str, plugin_entry: _PluginEntry) -> None:
         assert isinstance(plugin_entry, _PluginEntry)
         self._plugin_entry = plugin_entry
         self._group = group
 
-    def load(self):
+    def load(self) -> None:
         self._plugin_entry.load()
 
     @property
-    def group(self):
+    def group(self) -> str:
         return self._group
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._plugin_entry.name
 
     @property
-    def info(self):
+    def info(self) -> tuple[str | None, str | None]:
         return self._plugin_entry.info
 
     @property
-    def value(self):
+    def value(self) -> Any:
         return self._plugin_entry.value
 
 
 class _NSNode:
     # pylint: disable=W0212
 
-    def __init__(self):
-        self._children = {}
+    def __init__(self) -> None:
+        self._children: dict[str, _PluginEntry | _NSNode] = {}
 
-    def load(self):
+    def load(self) -> None:
         for child in self._children.values():
             child.load()
 
-    def add(self, name, entry):
+    def add(self, name: str, entry: _PluginEntry) -> None:
         assert isinstance(name, str) and isinstance(entry, _PluginEntry)
         self._add(name, entry)
 
-    def _add(self, name, entry):
+    def _add(self, name: str, entry: _PluginEntry) -> None:
         path = name.split('.', 1)
         key = path.pop(0)
         is_leaf = not path
@@ -161,7 +172,7 @@ class _NSNode:
             child._add(path[0], entry)
             self._children[key] = child
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Any:
         child = self._children.get(name)
         if child is None:
             raise PluginDBError(f'Unknown component name: {name}')
@@ -170,17 +181,17 @@ class _NSNode:
             return child.value
         return child
 
-    def info(self, name):
+    def info(self, name: str) -> tuple[str | None, str | None]:
         assert isinstance(name, str)
 
         return self._get(name).info
 
-    def get(self, name):
+    def get(self, name: str) -> Any:
         assert isinstance(name, str)
 
         return self._get(name).value
 
-    def _get(self, name):
+    def _get(self, name: str) -> _PluginEntry:
         path = name.split('.', 1)
         key = path.pop(0)
         is_leaf = not path
@@ -195,8 +206,8 @@ class _NSNode:
         else:
             return child._get(path[0])
 
-    def _info_all(self):
-        result = []
+    def _info_all(self) -> list[tuple[str, tuple[str | None, str | None]]]:
+        result: list[tuple[str, tuple[str | None, str | None]]] = []
         for key, child in self._children.items():
             if isinstance(child, _PluginEntry):
                 result.append((key, child.info))
@@ -206,7 +217,7 @@ class _NSNode:
                 ])
         return result
 
-    def info_all(self):
+    def info_all(self) -> dict[str, tuple[str | None, str | None]]:
         return dict(self._info_all())
 
 
@@ -215,16 +226,16 @@ class _Plugins:
     represent plugins within a namespace
     """
 
-    def __init__(self, namespace, interface=None):
+    def __init__(self, namespace: str, interface: InterfaceClass | None = None) -> None:
         if interface is not None:
             assert interface.isOrExtends(IPlugin)
 
         self._group = f'{_NAMESPACE_BASE}.{namespace}'
 
         self._interface = interface
-        self._real_tree = None
+        self._real_tree: _NSNode | None = None
 
-    def _load_entry(self, entry):
+    def _load_entry(self, entry: EntryPoint) -> Any:
         # pylint: disable=W0703
         try:
             result = entry.load()
@@ -243,7 +254,7 @@ class _Plugins:
         return result
 
     @property
-    def _tree(self):
+    def _tree(self) -> _NSNode:
         if self._real_tree is None:
             self._real_tree = _NSNode()
             entries = entry_points_get(entry_points(), self._group)
@@ -251,24 +262,24 @@ class _Plugins:
                 self._real_tree.add(entry.name, _PluginEntry(self._group, entry, self._load_entry))
         return self._real_tree
 
-    def load(self):
+    def load(self) -> None:
         self._tree.load()
 
-    def info_all(self):
+    def info_all(self) -> dict[str, tuple[str | None, str | None]]:
         return self._tree.info_all()
 
     @property
-    def names(self):
+    def names(self) -> list[str]:
         # Expensive operation
         return list(self.info_all())
 
-    def info(self, name):
+    def info(self, name: str) -> tuple[str | None, str | None]:
         """
         get information about a particular plugin if known in this namespace
         """
         return self._tree.info(name)
 
-    def __contains__(self, name):
+    def __contains__(self, name: str) -> bool:
         """
         check if the given name is available as a plugin
         """
@@ -277,16 +288,16 @@ class _Plugins:
         except PluginDBError:
             return False
 
-    def get(self, name):
+    def get(self, name: str) -> Any:
         """
         get an instance of the plugin with the given name
         """
         return self._tree.get(name)
 
-    def _get_entry(self, name):
+    def _get_entry(self, name: str) -> _PluginEntry:
         return self._tree._get(name)
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Any:
         try:
             return getattr(self._tree, name)
         except PluginDBError as e:
@@ -298,10 +309,15 @@ class _PluginDB:
     Plugin infrastructure support for Buildbot
     """
 
-    def __init__(self):
-        self._namespaces = {}
+    def __init__(self) -> None:
+        self._namespaces: dict[str, _Plugins] = {}
 
-    def add_namespace(self, namespace, interface=None, load_now=False):
+    def add_namespace(
+        self,
+        namespace: str,
+        interface: InterfaceClass | None = None,
+        load_now: bool = False,
+    ) -> _Plugins:
         """
         register given namespace in global database of plugins
 
@@ -319,17 +335,17 @@ class _PluginDB:
         return tempo
 
     @property
-    def namespaces(self):
+    def namespaces(self) -> list[str]:
         """
         get a list of registered namespaces
         """
         return list(self._namespaces)
 
-    def info(self):
+    def info(self) -> dict[str, dict[str, tuple[str | None, str | None]]]:
         """
         get information about all plugins in registered namespaces
         """
-        result = {}
+        result: dict[str, dict[str, tuple[str | None, str | None]]] = {}
         for name, namespace in self._namespaces.items():
             result[name] = namespace.info_all()
         return result
@@ -338,14 +354,14 @@ class _PluginDB:
 _DB = _PluginDB()
 
 
-def namespaces():
+def namespaces() -> list[str]:
     """
     provide information about known namespaces
     """
     return _DB.namespaces
 
 
-def info():
+def info() -> dict[str, dict[str, tuple[str | None, str | None]]]:
     """
     provide information about all known plugins
 
@@ -360,7 +376,9 @@ def info():
     return _DB.info()
 
 
-def get_plugins(namespace, interface=None, load_now=False):
+def get_plugins(
+    namespace: str, interface: InterfaceClass | None = None, load_now: bool = False
+) -> _Plugins:
     """
     helper to get a direct interface to _Plugins
     """
