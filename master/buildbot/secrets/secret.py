2e8527b1f68f214045a22d5fd967e3ce3cd19a86
Povilas Kanapickas <povilas@radix.lt>
2026-03-06 00:41:26 +0200

    secrets/secret: Add type hints

--- master/buildbot/secrets/secret.py
+++ master/buildbot/secrets/secret.py
@@ -13,6 +13,8 @@
 #
 # Copyright Buildbot Team Members
 
+from __future__ import annotations
+
 
 class SecretDetails:
     """
@@ -22,36 +24,38 @@ class SecretDetails:
     - value: secret value
     """
 
-    def __init__(self, source, key, value):
+    def __init__(self, source: str, key: str, value: str) -> None:
         self._source = source
         self._value = value
         self._key = key
 
     @property
-    def source(self):
+    def source(self) -> str:
         """
         source of the secret
         """
         return self._source
 
     @property
-    def value(self):
+    def value(self) -> str:
         """
         secret value
         """
         return self._value
 
     @property
-    def key(self):
+    def key(self) -> str:
         """
         secret value
         """
         return self._key
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self._source} {self._key}: {self.value!r}'
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, SecretDetails):
+            return NotImplemented
         return self._source == other._source and self.key == other.key and self.value == other.value
 
     def __hash__(self) -> int:
