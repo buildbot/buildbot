ab584040f4d912cac598ba01b4e54833a036548d
Povilas Kanapickas <povilas@radix.lt>
2026-03-08 21:35:54 +0200

    test/runprocess: Add type hints

--- master/buildbot/test/runprocess.py
+++ master/buildbot/test/runprocess.py
@@ -14,45 +14,56 @@
 # Copyright Buildbot Team Members
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from twisted.internet import defer
 
 from buildbot.test.steps import ExpectMasterShell
 from buildbot.test.steps import _check_env_is_expected
 from buildbot.util import runprocess
 
+if TYPE_CHECKING:
+    from twisted.internet.interfaces import IReactorProcess
+    from twisted.internet.interfaces import IReactorTime
+    from twisted.trial import unittest
 
-class MasterRunProcessMixin:
+    _MasterRunProcessMixinBase = unittest.TestCase
+else:
+    _MasterRunProcessMixinBase = object
+
+
+class MasterRunProcessMixin(_MasterRunProcessMixinBase):
     long_message = True
 
-    def setup_master_run_process(self):
+    def setup_master_run_process(self) -> None:
         self._master_run_process_patched = False
-        self._expected_master_commands = []
-        self._master_run_process_expect_env = {}
+        self._expected_master_commands: list[ExpectMasterShell] = []
+        self._master_run_process_expect_env: dict[str, str] = {}
 
-    def assert_all_commands_ran(self):
+    def assert_all_commands_ran(self) -> None:
         self.assertEqual(
             self._expected_master_commands, [], "assert all expected commands were run"
         )
 
     def patched_run_process(
         self,
-        reactor,
-        command,
-        workdir=None,
-        env=None,
-        collect_stdout=True,
-        collect_stderr=True,
-        stderr_is_error=False,
-        io_timeout=300,
-        runtime_timeout=3600,
-        sigterm_timeout=5,
-        initial_stdin=None,
-        use_pty=False,
-    ) -> defer.Deferred:
+        reactor: IReactorTime | IReactorProcess,
+        command: list[str],
+        workdir: str | None = None,
+        env: dict[str, str] | None = None,
+        collect_stdout: bool = True,
+        collect_stderr: bool = True,
+        stderr_is_error: bool = False,
+        io_timeout: int = 300,
+        runtime_timeout: int = 3600,
+        sigterm_timeout: int = 5,
+        initial_stdin: bytes | None = None,
+        use_pty: bool = False,
+    ) -> defer.Deferred[tuple[int | None, bytes, bytes] | tuple[int | None, bytes] | int | None]:
         _check_env_is_expected(self, self._master_run_process_expect_env, env)
 
         if not self._expected_master_commands:
-            self.fail(f"got command {command} when no further commands were expected")  # type: ignore[attr-defined]
+            self.fail(f"got command {command} when no further commands were expected")
 
         expect = self._expected_master_commands.pop(0)
 
@@ -69,15 +80,15 @@ class MasterRunProcessMixin:
             return defer.succeed((rc, stderr))
         return defer.succeed(rc)
 
-    def _patch_runprocess(self):
+    def _patch_runprocess(self) -> None:
         if not self._master_run_process_patched:
             self.patch(runprocess, "run_process", self.patched_run_process)
             self._master_run_process_patched = True
 
-    def add_run_process_expect_env(self, d):
+    def add_run_process_expect_env(self, d: dict[str, str]) -> None:
         self._master_run_process_expect_env.update(d)
 
-    def expect_commands(self, *exp):
+    def expect_commands(self, *exp: ExpectMasterShell) -> None:
         for e in exp:
             if not isinstance(e, ExpectMasterShell):
                 raise RuntimeError('All expectation must be an instance of ExpectMasterShell')
