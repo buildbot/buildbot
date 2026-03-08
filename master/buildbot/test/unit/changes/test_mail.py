8c344b877dc804a5e18d3fcb88408b6d877df948
Povilas Kanapickas <povilas@radix.lt>
2026-03-08 21:36:05 +0200

    test/unit/changes/test_mail: Add type hints

--- master/buildbot/test/unit/changes/test_mail.py
+++ master/buildbot/test/unit/changes/test_mail.py
@@ -13,7 +13,11 @@
 #
 # Copyright Buildbot Team Members
 
+from __future__ import annotations
+
 import os
+from typing import TYPE_CHECKING
+from typing import Any
 
 from twisted.internet import defer
 from twisted.trial import unittest
@@ -23,19 +27,24 @@ from buildbot.test.reactor import TestReactorMixin
 from buildbot.test.util import changesource
 from buildbot.test.util import dirs
 
+if TYPE_CHECKING:
+    from email.message import Message
+
+    from buildbot.util.twisted import InlineCallbacksType
+
 
 class TestMaildirSource(
     changesource.ChangeSourceMixin, dirs.DirsMixin, TestReactorMixin, unittest.TestCase
 ):
     @defer.inlineCallbacks
-    def setUp(self):
+    def setUp(self) -> InlineCallbacksType[None]:  # type: ignore[override]
         self.setup_test_reactor()
         self.maildir = os.path.abspath("maildir")
 
         yield self.setUpChangeSource()
         yield self.setUpDirs(self.maildir)
 
-    def populateMaildir(self):
+    def populateMaildir(self) -> None:
         "create a fake maildir with a fake new message ('newmsg') in it"
         newdir = os.path.join(self.maildir, "new")
         os.makedirs(newdir)
@@ -48,28 +57,28 @@ class TestMaildirSource(
         with open(mailfile, "w", encoding='utf-8') as f:
             f.write(fake_message)
 
-    def assertMailProcessed(self):
+    def assertMailProcessed(self) -> None:
         self.assertFalse(os.path.exists(os.path.join(self.maildir, "new", "newmsg")))
         self.assertTrue(os.path.exists(os.path.join(self.maildir, "cur", "newmsg")))
 
     # tests
 
-    def test_describe(self):
+    def test_describe(self) -> None:
         mds = mail.MaildirSource(self.maildir)
         self.assertSubstring(self.maildir, mds.describe())
 
     @defer.inlineCallbacks
-    def test_messageReceived_svn(self):
+    def test_messageReceived_svn(self) -> InlineCallbacksType[None]:
         self.populateMaildir()
         mds = mail.MaildirSource(self.maildir)
         yield self.attachChangeSource(mds)
 
         # monkey-patch in a parse method
-        def parse(message, prefix):
+        def parse(message: Message, prefix: str | None) -> tuple[str, dict[str, Any]] | None:
             assert 'this is a test' in message.get_payload()
             return ('svn', {"author": 'jimmy'})
 
-        mds.parse = parse
+        mds.parse = parse  # type: ignore[assignment, method-assign]
 
         yield mds.messageReceived('newmsg')
 
@@ -97,17 +106,17 @@ class TestMaildirSource(
         )
 
     @defer.inlineCallbacks
-    def test_messageReceived_bzr(self):
+    def test_messageReceived_bzr(self) -> InlineCallbacksType[None]:
         self.populateMaildir()
         mds = mail.MaildirSource(self.maildir)
         yield self.attachChangeSource(mds)
 
         # monkey-patch in a parse method
-        def parse(message, prefix):
+        def parse(message: Message, prefix: str | None) -> tuple[str, dict[str, Any]] | None:
             assert 'this is a test' in message.get_payload()
             return ('bzr', {"author": 'jimmy'})
 
-        mds.parse = parse
+        mds.parse = parse  # type: ignore[assignment, method-assign]
 
         yield mds.messageReceived('newmsg')
 
