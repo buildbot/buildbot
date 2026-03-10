58c76432a80109229056a3238928ee52f2ea287a
Thomas Desveaux <desveaux.thomas@gmail.com>
2026-03-10 14:28:55 +0100

    www/nestedexample: convert setup.cfg to pyproject.toml

--- common/maketarball.sh
+++ common/maketarball.sh
@@ -8,6 +8,7 @@ pkg=$1
         [ ${pkg} == "www/base" ] || \
         [ ${pkg} == "www/console_view" ] || \
         [ ${pkg} == "www/grid_view" ] || \
+        [ ${pkg} == "www/nestedexample" ] || \
         [ ${pkg} == "www/badges" ]; then
         python -m build
     elif [ ${pkg} == "worker" ] || [ ${pkg} == "pkg" ]; then
