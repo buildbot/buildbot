beb8dab9742e898684f768e2b18ce92bcd2b7909
Povilas Kanapickas <povilas@radix.lt>
2026-03-24 19:32:58 +0200

    test/unit/test_wamp_connector: Add type hints

--- master/buildbot/test/unit/test_wamp_connector.py
+++ master/buildbot/test/unit/test_wamp_connector.py
@@ -14,6 +14,9 @@
 # Copyright Buildbot Team Members
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+from typing import Any
+from typing import Callable
 from unittest import mock
 
 from parameterized import parameterized
@@ -25,9 +28,12 @@ from buildbot.test.reactor import TestReactorMixin
 from buildbot.util import service
 from buildbot.wamp import connector
 
+if TYPE_CHECKING:
+    from buildbot.util.twisted import InlineCallbacksType
+
 
 class FakeConfig:
-    def __init__(self, mq_dict):
+    def __init__(self, mq_dict: dict) -> None:
         self.mq = mq_dict
 
 
@@ -37,13 +43,20 @@ class FakeService(service.AsyncMultiService):
     # just call the maker on demand by the test
 
     def __init__(
-        self, url, realm, make, extra=None, debug=False, debug_wamp=False, debug_app=False
-    ):
+        self,
+        url: str,
+        realm: str,
+        make: Callable[..., Any],
+        extra: object = None,
+        debug: bool = False,
+        debug_wamp: bool = False,
+        debug_app: bool = False,
+    ) -> None:
         super().__init__()
         self.make = make
         self.extra = extra
 
-    def gotConnection(self):
+    def gotConnection(self) -> None:
         self.make(None)
         r = self.make(self)
         r.publish = mock.Mock(spec=r.publish)
@@ -58,7 +71,7 @@ class TestedWampConnector(connector.WampConnector):
 
 class WampConnector(TestReactorMixin, unittest.TestCase):
     @defer.inlineCallbacks
-    def setUp(self):
+    def setUp(self) -> InlineCallbacksType[None]:  # type: ignore[override]
         self.setup_test_reactor()
         master = yield fakemaster.make_master(self)
         self.connector = TestedWampConnector()
@@ -70,7 +83,7 @@ class WampConnector(TestReactorMixin, unittest.TestCase):
         yield self.connector.reconfigServiceWithBuildbotConfig(config)
 
     @defer.inlineCallbacks
-    def test_reconfig_same_config(self):
+    def test_reconfig_same_config(self) -> InlineCallbacksType[None]:
         config = FakeConfig({'type': 'wamp', 'router_url': "wss://foo", 'realm': "bb"})
         yield self.connector.reconfigServiceWithBuildbotConfig(config)
 
@@ -81,7 +94,9 @@ class WampConnector(TestReactorMixin, unittest.TestCase):
         ('wamp_debug_level', 'info'),
     ])
     @defer.inlineCallbacks
-    def test_reconfig_does_not_allow_config_change(self, attr_name, attr_value):
+    def test_reconfig_does_not_allow_config_change(
+        self, attr_name: str, attr_value: str
+    ) -> InlineCallbacksType[None]:
         mq_dict = {'type': 'wamp', 'router_url': "wss://foo", 'realm': "bb"}
         mq_dict[attr_name] = attr_value
         with self.assertRaises(
@@ -90,7 +105,7 @@ class WampConnector(TestReactorMixin, unittest.TestCase):
             yield self.connector.reconfigServiceWithBuildbotConfig(FakeConfig(mq_dict))
 
     @defer.inlineCallbacks
-    def test_startup(self):
+    def test_startup(self) -> InlineCallbacksType[None]:
         d = self.connector.getService()
         self.connector.app.gotConnection()
         yield d
@@ -98,21 +113,21 @@ class WampConnector(TestReactorMixin, unittest.TestCase):
         self.connector.service.publish.assert_called_with("org.buildbot.824.connected")
 
     @defer.inlineCallbacks
-    def test_subscribe(self):
+    def test_subscribe(self) -> InlineCallbacksType[None]:
         d = self.connector.subscribe('callback', 'topic', 'options')
         self.connector.app.gotConnection()
         yield d
         self.connector.service.subscribe.assert_called_with('callback', 'topic', 'options')
 
     @defer.inlineCallbacks
-    def test_publish(self):
+    def test_publish(self) -> InlineCallbacksType[None]:
         d = self.connector.publish('topic', 'data', 'options')
         self.connector.app.gotConnection()
         yield d
         self.connector.service.publish.assert_called_with('topic', 'data', options='options')
 
     @defer.inlineCallbacks
-    def test_OnLeave(self):
+    def test_OnLeave(self) -> InlineCallbacksType[None]:
         d = self.connector.getService()
         self.connector.app.gotConnection()
         yield d
