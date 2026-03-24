bd67f6fd56cbaafd3a3ecb64c262be2eb140904c
Povilas Kanapickas <povilas@radix.lt>
2026-03-24 16:30:04 +0200

    test/util/endpoint: Add type hints

--- master/buildbot/test/util/endpoint.py
+++ master/buildbot/test/util/endpoint.py
@@ -14,6 +14,9 @@
 # Copyright Buildbot Team Members
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+from typing import Any
+
 from twisted import trial
 from twisted.internet import defer
 
@@ -24,6 +27,9 @@ from buildbot.test.reactor import TestReactorMixin
 from buildbot.test.util import interfaces
 from buildbot.test.util import validation
 from buildbot.util import pathmatch
+
+if TYPE_CHECKING:
+    from buildbot.util.twisted import InlineCallbacksType
 
 
 class EndpointMixin(TestReactorMixin, interfaces.InterfaceTests):
@@ -37,15 +43,18 @@ class EndpointMixin(TestReactorMixin, interfaces.InterfaceTests):
     resourceTypeClass: type[base.ResourceType] | None = None
 
     @defer.inlineCallbacks
-    def setUpEndpoint(self):
+    def setUpEndpoint(self) -> InlineCallbacksType[None]:
         self.setup_test_reactor()
         self.master = yield fakemaster.make_master(self, wantMq=True, wantDb=True, wantData=True)
         self.data = self.master.data
-        self.matcher = pathmatch.Matcher()
+        self.matcher: pathmatch.Matcher[Any] = pathmatch.Matcher()
 
+        assert self.resourceTypeClass is not None
         rtype = self.rtype = self.resourceTypeClass(self.master)
+        assert rtype.name is not None
         setattr(self.data.rtypes, rtype.name, rtype)
 
+        assert self.endpointClass is not None
         self.ep = self.endpointClass(rtype, self.master)
 
         # this usually fails when a single-element pathPattern does not have a
@@ -56,27 +65,29 @@ class EndpointMixin(TestReactorMixin, interfaces.InterfaceTests):
                 continue
             if not pp.startswith('/') or pp.endswith('/'):
                 raise AssertionError(f"invalid pattern {pp!r}")
-        pathPatterns = [tuple(pp.split('/')[1:]) for pp in pathPatterns]
-        for pp in pathPatterns:
-            self.matcher[pp] = self.ep
+        parsed_patterns: list[tuple[str, ...]] = [tuple(pp.split('/')[1:]) for pp in pathPatterns]
+        for pat in parsed_patterns:
+            self.matcher[pat] = self.ep
 
         self.pathArgs = [
-            {arg.split(':', 1)[1] for arg in pp if ':' in arg}
-            for pp in pathPatterns
-            if pp is not None
+            {arg.split(':', 1)[1] for arg in pat if ':' in arg}
+            for pat in parsed_patterns
+            if pat is not None
         ]
 
-    def validateData(self, object):
+    def validateData(self, object: dict[str, Any]) -> None:
         validation.verifyData(self, self.rtype.entityType, {}, object)
 
     # call methods, with extra checks
 
     @defer.inlineCallbacks
-    def callGet(self, path, resultSpec=None):
+    def callGet(
+        self, path: tuple[str | int, ...], resultSpec: resultspec.ResultSpec | None = None
+    ) -> InlineCallbacksType[Any]:
         self.assertIsInstance(path, tuple)
         if resultSpec is None:
             resultSpec = resultspec.ResultSpec()
-        endpoint, kwargs = self.matcher[path]
+        endpoint, kwargs = self.matcher[path]  # type: ignore[index]
         self.assertIdentical(endpoint, self.ep)
         rv = yield endpoint.get(resultSpec, kwargs)
 
@@ -86,9 +97,11 @@ class EndpointMixin(TestReactorMixin, interfaces.InterfaceTests):
             self.assertIsInstance(rv, (dict, type(None)))
         return rv
 
-    def callControl(self, action, args, path):
+    def callControl(
+        self, action: str, args: dict[str, Any], path: tuple[str | int, ...]
+    ) -> defer.Deferred[Any]:
         self.assertIsInstance(path, tuple)
-        endpoint, kwargs = self.matcher[path]
+        endpoint, kwargs = self.matcher[path]  # type: ignore[index]
         self.assertIdentical(endpoint, self.ep)
         d = self.ep.control(action, args, kwargs)
         self.assertIsInstance(d, defer.Deferred)
@@ -96,25 +109,25 @@ class EndpointMixin(TestReactorMixin, interfaces.InterfaceTests):
 
     # interface tests
 
-    def test_get_spec(self):
+    def test_get_spec(self) -> None:
         try:
 
             @self.assertArgSpecMatches(self.ep.get)
-            def get(self, resultSpec, kwargs):
+            def get(self, resultSpec, kwargs):  # type: ignore[no-untyped-def,unused-ignore]
                 pass
 
         except trial.unittest.FailTest:
 
             @self.assertArgSpecMatches(self.ep.get)
-            def get(self, result_spec, kwargs):
+            def get(self, result_spec, kwargs):  # type: ignore[no-untyped-def,unused-ignore]
                 pass
 
-    def test_control_spec(self):
+    def test_control_spec(self) -> None:
         @self.assertArgSpecMatches(self.ep.control)
-        def control(self, action, args, kwargs):
+        def control(self, action, args, kwargs):  # type: ignore[no-untyped-def,unused-ignore]
             pass
 
-    def test_rootLinkName(self):
+    def test_rootLinkName(self) -> None:
         rootLinkName = self.ep.rootLinkName
         if not rootLinkName:
             return
